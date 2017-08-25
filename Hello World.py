6bac6ac83bd3aa53ffe172f8738d769456dd4815
jxie0755 <jxie0755@users.noreply.github.com>
2017-08-25 18:00:02 -0400

    Initial commit

---
+++ Hello World.py
@@ -0,0 +1,34 @@
+import sys
+print("python version:")
+print(sys.version)
+# this is nothing
+
+print("Hello, World.")
+print("  from Denis Xie")
+
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+
+a = 2
+a = a * 3
+print(a)
+
+a = 2
+a *= 4
+print(a)
+
+print('{0:.3f}'.format(1.0 / 3))
+print('{0:_^11}'.format('hello'))
+print('{0:_^12}'.format('hello'))
+print('{0:_^13}'.format('hello'))
+
+# has to be build in python 3
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+
+print('''I am 5 years older than you
+# sdfsdfsadf
+Therefore my age is 55
+''')
+print("a")
---
+++ testREPL.py
@@ -0,0 +1,8 @@
+age = input("What is your age?")
+myage = age + 5
+
+# The two print below is the same thing
+print("I am 5 years older than you")
+print("Therefore my age is {0}".format(myage))
+
+print("I am 5 years older than you\nTherefore my age is {0}".format(myage))
