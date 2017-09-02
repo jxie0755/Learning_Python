ea8350b1a385537a56892a29d79ef6d6615f0579
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-01 21:46:53 -0400

    DataStructure3

--- 11_DataStructure5.py
+++ 11_DataStructure5.py
@@ -12,3 +12,13 @@ bri.remove('russia')
 print(bri & bric)  # 交集
 print(bri.intersection(bric)) # 交集
 
+
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+shoplist2 = ['organge', 'mango', 'pear', 'banana']
+
+name = 'swaroop'
+
+print('apple' in shoplist)
+print('apple' in shoplist2)
+# print(shoplist & shoplist2) list是不能求子集交集的,只能用于set
+
