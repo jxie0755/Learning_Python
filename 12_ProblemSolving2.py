c228f6aa27600d1910728d602d356e4adaa5025f
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-04 13:44:48 -0400

    Problem solving 2

--- 12_ProblemSolving2.py
+++ 12_ProblemSolving2.py
@@ -1,24 +1,33 @@
+# 一款程序来备份我所有的重要文件
+# 改进目标:为备份创立一个日期的子文件夹,便于管理
+
 # 照常规引用os和time
 import os
 import time
 source = ['~/Documents/Account', '~/Documents/Books/Stocks', '~/Documents/Mind\ Maps']
+target_dir = '/Users/Jxie0755/Resources/Backup'
 
-# 给target创见一个子文件夹
-target_dir = '/Users/Jxie0755/Resources/Backup' + os.sep + str(time.strftime('%Y%m%d'))
-# 改进了一下zip名称的创见
-target = target_dir + os.sep + time.strftime('Time_%H_%M_%S') + '.zip'
-
-# 后面的输出维持原装
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
 
+# 增加一个创立子文件夹的路径
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+time = time.strftime('Time_%H_%M_%S')
+
+# 额外增加一个强制新建子文件夹的命令
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+target = today + os.sep + time + '.zip'
+
+# 5. 使用zip命令打包文件
 zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
 
+# 运行备份
 print('Zip command is:')
 print(zip_command)
 print('running:')
 if os.system(zip_command) == 0:
-    print('Backup successful')
+    print('Sucessful backup to', target)
 else:
     print('Backup FAILED')
-
---
+++ 12_ProblemSolving2b.py
@@ -0,0 +1,26 @@
+# 改进目标:为备份创立一个日期的子文件夹,便于管理
+
+# 照常规引用os和time
+import os
+import time
+source = ['~/Documents/Account', '~/Documents/Books/Stocks', '~/Documents/Mind\ Maps']
+
+# 给target创见一个子文件夹
+target_dir = '/Users/Jxie0755/Resources/Backup' + os.sep + str(time.strftime('%Y%m%d'))
+# 改进了一下zip名称的创见
+target = target_dir + os.sep + time.strftime('Time_%H_%M_%S') + '.zip'
+
+# 后面的输出维持原装
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+
+print('Zip command is:')
+print(zip_command)
+print('running:')
+if os.system(zip_command) == 0:
+    print('Backup successful')
+else:
+    print('Backup FAILED')
+
