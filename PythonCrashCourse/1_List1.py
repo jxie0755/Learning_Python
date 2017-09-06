77159f9e17ca1b91c64141780c645eb664ae8340
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-06 19:55:30 -0400

    1

---
+++ PythonCrashCourse/1_List1.py
@@ -0,0 +1,9 @@
+
+list_A = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+for letter in the list_A:
+    print(list_A.pop())
+    if 'c' in list_A = False:
+        break
+
+print("job done.")
