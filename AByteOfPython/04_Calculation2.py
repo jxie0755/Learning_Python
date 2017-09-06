71ebc26b4fc8c2f8f2a3bde38e0a3264e2e9364a
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-06 19:29:53 -0400

    1

--- AByteOfPython/04_Calculation2.py
+++ AByteOfPython/04_Calculation2.py
@@ -5,5 +5,5 @@ perimeter = 2 * (length + breadth)
 print('Area is', area)
 print('Perimeter is', 2 * (length + breadth))
 
-# 此处，必须先定义length和breadth的值，再定义area和perimeter的计算方式，反之不行。
+# 此处必须先定义length和breadth的值，再定义area和perimeter的计算方式，反之不行。
 
