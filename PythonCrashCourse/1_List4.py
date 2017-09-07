d1011565828e8b7bd695aa6d0ae8e92029bd85e4
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-06 22:46:30 -0400

    PCC list

---
+++ PythonCrashCourse/1_List4.py
@@ -0,0 +1,7 @@
+# 将一个list中所有item都变成大写/小写
+
+list_D = ['jack', 'Bob', 'frank', 'Benji', 'cathy', 'illy', 'ben', 'hank', 'billy', 'Adrienne']
+list_D = [element.upper() for element in list_D]
+print(list_D)
+
+# 注意使用 element.upper() 和 element.lower()命令来实现
