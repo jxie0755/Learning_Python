a1143216d1111355a5d7c251bcf3e1aae534864e
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-06 23:38:53 -0400

    PCC list

--- PythonCrashCourse/1_List4.py
+++ PythonCrashCourse/1_List4.py
@@ -6,3 +6,10 @@ list_D.reverse()
 print(list_D)
 
 # 此处使用for in loop在一个list中,使得list能够包括原所有的item
+
+list_E = ['jack', 'Bob', 'frank', 'Benji', 'cathy', 'illy', 'ben', 'hank', 'billy', 'Adrienne']
+for i in list_E:
+    i2 = i.upper()
+    print(i2, end=' ')
+
+# 此处将list拆解,成为每个item都变成大写,然后单独输出,使用end=' '不换行.
