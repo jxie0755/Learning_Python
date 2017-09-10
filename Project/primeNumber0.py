f4f4b5fd5ccbef31779354e4c273f95a7e9a078e
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-10 13:01:30 -0400

    Prime Number

--- Project/primeNumber0.py
+++ Project/primeNumber0.py
@@ -1,4 +1,4 @@
-pnumber = []
+pnumber1 = []
 for x in range(2, 101):
     for i in range(2, x):
         if x % i == 0:
@@ -7,23 +7,74 @@ for x in range(2, 101):
             # 这个break是针对第二重迭代,使得只要有一个i能满足余数=0,那么这个x就不符合质数条件
             # 然后回到第一重迭代,试下一个x,如此往复.
     else:
-        pnumber.append(x)
-print(pnumber)
+        pnumber1.append(x)
+print(pnumber1)
 
 
+# 这是跟上一版本相反的做法,先创造一个list把所有2-100的数字包括进去,然后去掉不是质数的数,剩下的保留
+# !!!!!这个不会work,因为: 当一个item被remove,下次迭代的时候,发现这个item对于下个i又需要被remove
+# 然后由于在上一个i的迭代中已经被remove,检查list的时候会发生冲突: 结果就是返回list.remove(x): x not in list
+pnumber1b = list(range(3, 101))
+for x in pnumber1b:
+    for i in range(3, x):
+        if x % i == 0:
+            pnumber1b.remove(x)
+        else:
+            break
+print(pnumber1b)
+
+# 由于上一个版本发生list冲突,所以解决办法就是设计两个一样的list,把条件放到list1,执行放到list2
+# 这样就实现了当1个x值被remove后,打断第二重迭代,继续第一重x的迭代,测试下个x.这样就不会出现相同的x需要被remove所形成的冲突.
+# 这样做虽然繁琐,但是能达到目的
+pnumber1b_fix = list(range(3, 101))
+pnumber1b_fixB = list(range(3, 101))
+for x in pnumber1b_fixB:
+    for i in range(3, x):
+        if x % i == 0:
+            if x
+            pnumber1b_fix.remove(x)
+            break
+print(pnumber1b_fix)
+
+
+
+# 这个版本就是利用all()来省去迭代i的for loop,使得过程更容易理解和控制: 必须是每一个i都能有余数(不为0,True)才可以满足条件.
 pnumber2 = []
 for x in range(2, 101):
-        if all(x % i for i in range(2, x)):
+    if all(x % i for i in range(2, x)):
         # 使用all(),等于简写break的第二重历遍.
         # all()要求只有x除以每一个i都能余数!=0,(=0就是False,!=0也就是必须为True),这个x才能符合质数条件.
-            pnumber2.append(x)
+        pnumber2.append(x)
 print(pnumber2)
+
+
+
+# 跟上一版本相比,这里用if not命令,如果不是质数,则跳过,否则就加入
+pnumber2b = []
+for x in range(2, 101):
+    if not all(x % i for i in range(2, x)):
+        continue         #使用contine因为此处是一重循环,如果break则会终止在第一个不是质数的x,也就是4.所以只会变成[2, 3]
+    else:
+        pnumber2b.append(x)
+print(pnumber2b)
+
 
 # 这个是反写, 首先创造一个list包含全部的2-100.
 # 然后条件反转,如果能整除(余数=0, not all), 则不符合质数条件,从而删掉一个x
 # 最终留下来的就是质数
-pnumber2 = list(range(2, 101))
+pnumber3 = list(range(2, 101))
 for x in range(2, 101):
-        if not all(x % i for i in range(2, x)):
-            pnumber2.remove(x)
-print(pnumber2)
+    if not all(x % i for i in range(2, x)):
+        pnumber3.remove(x)
+print(pnumber3)
+
+
+# 还是创造一个list包含全部的2-100.
+# 但是跟上一版不同的是：如果是质数就保留，不然就去除(非质数)
+pnumber3b = list(range(2, 101))
+for x in range(2, 101):
+    if all(x % i for i in range(2, x)):
+        continue
+    else:
+        pnumber3b.remove(x)
+print(pnumber3b)
--- Project/test.py
+++ Project/test.py
@@ -1,2 +1,9 @@
-for i in range(1, 3):
-    print(i)
+# 这样就实现了当1个x值被remove后,打断第二重迭代,继续第一重x的迭代,测试下个x.这样就不会出现相同的x需要被remove所形成的冲突.
+pnumber1b_fix = list(range(2, 101))
+pnumber1b_fixB = list(range(2, 101))
+for x in pnumber1b_fixB:
+    for i in range(2, x):
+        if x % i == 0:
+            pnumber1b_fix.remove(x)
+            break
+print(pnumber1b_fix)
