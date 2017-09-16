0939d594304499a777d4022640ad6998d02f3a0b
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-16 19:03:19 -0400

    Others

---
+++ AByteOfPython/16_Others1.py
@@ -0,0 +1,8 @@
+# Other 1:
+
+def get_error_details():
+    return (2, 'details')
+
+errnum, errstr = get_error_details()
+print(errnum)
+print(errstr)
---
+++ AByteOfPython/16_Others2.py
@@ -0,0 +1,8 @@
+# Other 2:
+
+a = 5; b = 8
+print(a, b)
+# >>> (5, 8)
+a, b = b, a
+print(a, b)
+# >>> (8, 5)
