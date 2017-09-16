73e9f6502862471280fa625c9183f56f19256fe1
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-16 15:18:16 -0400

    Input 3

---
+++ AByteOfPython/14_InputOutput3.py
@@ -0,0 +1,23 @@
+import pickle
+
+# The name of the file where we will store the object
+shoplistfile = 'shoplist.data'
+# The list of things to buy
+shoplist = ['apple', 'mango', 'carrot']
+
+# write to the file
+f = open(shoplistfile, 'wb')
+
+# Dump the object to a file
+pickle.dump(shoplist, f)
+
+# Destroy the shoplist variables
+del shoplist
+# print(shoplist)   ---------原数据已不存在(NameError: name 'shoplist' is not defined)
+
+# Read back from the storage.
+f = open(shoplistfile, 'rb')
+
+# Load the object from the file
+storedlist = pickle.load(f)
+print(storedlist)
