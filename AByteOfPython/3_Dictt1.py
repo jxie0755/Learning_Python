f782cb25e5e8afdc3287a897472913aa296b2dcd
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-16 22:40:10 -0400

    PCC dict

---
+++ AByteOfPython/3_Dictt1.py
@@ -0,0 +1,6 @@
+favorite_languages = { 'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python', }
+for name in favorite_languages.keys():
+    print(name.title())
+for language in favorite_languages.values():
+    print(language.title())
+
