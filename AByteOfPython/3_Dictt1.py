6668d331f498593a37378c25637489f99761c4ab
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-16 23:18:05 -0400

    d

--- AByteOfPython/3_Dictt1.py
+++ AByteOfPython/3_Dictt1.py
@@ -1,6 +1,16 @@
 favorite_languages = { 'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python', }
-for name in favorite_languages.keys():
+for name in favorite_languages.keys():     # 可以不写keys,默认也是寻找keys
     print(name.title())
-for language in favorite_languages.values():
+print()
+
+for language in favorite_languages.values():  # value必须要标明
+    print(language.title())   # 这样会出现重复的language, 可以使用set
+print()
+    # set跟list很像,但是要求每个item不能重复
+for language in set(favorite_languages.values()):
     print(language.title())
+print()
+
+for name in sorted(favorite_languages.keys()):    # sorted临时排序
+    print(name.title() + ", thank you for taking the poll.")
 
