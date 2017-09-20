8572c788fa68228898fd53832b83886a1fec6554
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-19 23:10:01 -0400

    PCC function

---
+++ PythonCrashCourse/4_function4.py
@@ -0,0 +1,17 @@
+# Use a function with a while loop
+# function is in an infinite loop
+
+def format_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print('\nPlease tell me your name:')
+    # 这里将input形成的f_name和l_name指定为function中的first_name和last_name
+    f_name = input('First name:')
+    l_name = input('last name:')
+    name = format_name(f_name, l_name)
+    print('\nHello,', name, '!')
+
+
+
