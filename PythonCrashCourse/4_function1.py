84f1561f554ab73a49075b0d293d6a49781e81fe
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-19 20:04:34 -0400

    Func

---
+++ PythonCrashCourse/4_function1.py
@@ -0,0 +1,14 @@
+# Simply set func to replace print
+
+def func(a):
+    print(a)
+
+func('dd')
+print('dd')
+
+
+def greet_user(username):
+    print('Hello', username.title(), '!')
+
+greet_user('Denis Xie')  # 记住这里输入string
+
