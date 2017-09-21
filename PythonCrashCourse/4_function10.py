4206ceec7928c1221963483dcbe59f01a54fc42d
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-20 22:53:50 -0400

    PCC module

---
+++ PythonCrashCourse/4_function10.py
@@ -0,0 +1,14 @@
+# Then import it in a different file,
+# Import a module (pizza)
+
+import pizza as p
+
+help(pizza)  # 查询帮助文档,如果忘记了怎么用
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushroom', 'green peppers', 'extra cheese')
+
+
+from pizza import make_pizza as mp  # use mp as an alias
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
