613b5d59307cb0dc4d8ba10a08121e70710565cb
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-24 15:29:06 -0400

    OOP Instance as attributes

--- PythonCrashCourse/5_OOP7_importClass2.py
+++ PythonCrashCourse/5_OOP7_importClass2.py
@@ -11,11 +11,11 @@ my_new_car.increment_odometer(2500)
 my_new_car.get_car_info()
 my_new_car.read_odometer()
 
-my_new_ECar = car0.ElectricCar('Tesla', 'Model S', 2017)
-my_new_ECar.get_car_info()
-my_new_ECar.read_odometer()
-my_new_ECar.gas_tank(20)
-my_new_ECar.BBBattery.battery_info()
-my_new_ECar.BBBattery.get_range()
+my_new_ecar = car0.ElectricCar('Tesla', 'Model S', 2017)
+my_new_ecar.get_car_info()
+my_new_ecar.read_odometer()
+my_new_ecar.gas_tank(20)
+my_new_ecar.BBBattery.battery_info()
+my_new_ecar.BBBattery.get_range()
 
 
---
+++ PythonCrashCourse/5_OOP7_importClass3.py
@@ -0,0 +1,19 @@
+# 如果把ecar这个模块导入其超类car, 那么这个ecar的模块就包含全部的类,直接可以从ecar中调用Car和Electric Car
+
+import ecar
+
+help(ecar.Car)
+help(ecar.ElectricCar)
+
+my_new_car = ecar.Car('Audi', 'S4', 2017)
+my_new_ecar = ecar.ElectricCar('Tesla', 'Model S', 2016)
+
+my_new_car.increment_odometer(2500)
+my_new_car.get_car_info()
+my_new_car.read_odometer()
+
+my_new_ecar.get_car_info()
+my_new_ecar.read_odometer()
+my_new_ecar.gas_tank(20)
+my_new_ecar.BBBattery.battery_info()
+my_new_ecar.BBBattery.get_range()
