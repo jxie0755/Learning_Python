66f79801208400a63cd22a531f3445e5655b7e63
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-26 18:07:18 -0400

    Files

---
+++ PythonCrashCourse/C10_files17_strings.py
@@ -0,0 +1,23 @@
+# string operation
+
+a = '***a b c d e f g**'
+b = a.strip('*')
+c = b.split()
+d = ' '.join(i for i in c)
+e = d.replace(' ', '')
+f = e.split('c')  # split命令不但通过c分割,还去掉c
+g = ''.join(f)
+
+
+h = []
+for i in g:
+    h.append(i)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
