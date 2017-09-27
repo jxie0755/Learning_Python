f3ef85df1b83edeff66dd519850d46aad08e4314
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-27 00:09:52 -0400

    Files

--- Project/test.py
+++ Project/test.py
@@ -1,8 +1,9 @@
-input = input('input:')
-with open('USN_list.txt') as f:
-    lines = f.readlines()
-print(lines)
-if input + '\n' in lines:
-    print('in')
-else:
-    print('not in')
+#input = input('input:')
+#with open('USN_list.txt') as f:
+#    lines = f.readlines()
+#if input + '\n' in lines:
+#    print('in')
+#else:
+#    print('not in')
+
+with open('USN_list.txt', 'a') as f:
