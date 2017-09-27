e669a4ae7d9f25849343976b1d0fd42287a15493
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-26 23:45:55 -0400

    Files

--- Project/test.py
+++ Project/test.py
@@ -1,10 +1,8 @@
-Dict = {'thin': 7.99, 'regular': 8.99, 'pan': 9.99, 'stuffed': 10.99}
-
-while True:
-    order = input('please input:')
-    if order in Dict.keys():
-        print('in')
-        break
-    if order not in Dict.keys():
-        print('not in')
-        continue
+input = input('input:')
+with open('USN_list.txt') as f:
+    lines = f.readlines()
+print(lines)
+if input + '\n' in lines:
+    print('in')
+else:
+    print('not in')
