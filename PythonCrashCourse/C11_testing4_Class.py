e5273ce9e01f1a0885a56466c42ce55395d52fc7
jxie0755 <jxie0755@users.noreply.github.com>
2017-09-27 22:56:15 -0400

    Files

---
+++ PythonCrashCourse/C11_testing4_Class.py
@@ -0,0 +1,35 @@
+class AnonymousSurvey():
+    """收集匿名调查问卷的答案"""
+
+    def __init__(self, question):
+        """存储一个问题，并为存储答案做准备"""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """显示调查问卷"""
+        print(self.question)
+
+    def store_response(self, new_response):        """存储单份调查答卷"""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """显示收集到的所有答卷"""
+        print("Survey results:")
+        for response in self.responses:
+            print('- ' + response)
+
+#定义一个问题，并创建一个表示调查的AnonymousSurvey对象
+my_survey = AnonymousSurvey("What language did you first learn to speak?" )
+
+#显示问题并存储答案 my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+
+# 显示调查结果
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
