133fde74297aa4d43d26d85bd59dc97a5e00df42
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-03 16:53:40 -0400

    Files

--- PyModuleWeekly/String.py
+++ PyModuleWeekly/String.py
@@ -9,31 +9,44 @@ print(string.capwords(s))
 print()
 
 # string.Template
-values = {'var': 'foo'}
-t = string.Template("""
-Variable:         $var
-Escape:           $$
-Variable in text: ${var}iable
-""")
-print('TEMPLATE', t.substitute(values))
+dict1 = {'gg': 'foo'}
 
-s = """
-Variable        : %(var)s
-Escape          : %%
-Variable in text: %(var)siable
+t = string.Template(""" 
+Variable        : $gg 
+Escape          : $$ 
+Variable in text: ${gg}iable 
+""")
+
+print('TEMPLATE:', t.substitute(dict1))
+
+s = """ 
+Variable        : %(gg)s 
+Escape          : %% 
+Variable in text: %(gg)siable 
 """
 
-print('INTERPOLATION', s % values)
+print('INTERPOLATION:', s % dict1)
 
-s = """
-Variable        : {var}
-Escape          : {{}}
-Variable in text: {var}iable"""
+s = """ 
+Variable        : {gg} 
+Escape          : {{}} 
+Variable in text: {gg}iable 
+"""
 
-print('FORMAT', s.format(**values))
+print('FORMAT:', s.format(**dict1))
 
 print()
 
+# 简单实例: 把一个字符串变成Template()的实例, 对于实例使用类的方法substitute,将$开头的变量置换为另一个字符
+s = 'hello, $world!'
+t = string.Template(s)
+print(t.substitute({'world':'nimei'}))
+
+print()
+
+# safe_substitute方法, 万一有些value
+# there is no value for missing in the values dictionary, a KeyError is raised by substitute().
+# Instead of raising the error, safe_substitute() catches it and leaves the variable expression alone in the text.
 values = {'var': 'foo'}
 
 t = string.Template("$var is here but $missing is not provided")
@@ -47,8 +60,14 @@ print('safe_substitute():', t.safe_substitute(values))
 
 print()
 
-try:
-    with open('1.txt') as f_obj:
-        t = f_obj.readlines()
-except FileNotFoundError as err:
-    print('error:', str(err))
+import inspect
+import string
+
+def is_str(value):
+    return isinstance(value, str)
+
+for name, value in inspect.getmembers(string, is_str):
+    if name.startswith('_'):
+        continue
+    print('%s=%r\n' % (name, value))
+
