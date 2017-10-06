3c9064ab22444fd1121111058a98eb9327832232
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-05 21:30:08 -0400

    Python module 3 of the week textwrap

--- PyModuleWeekly/textwrap_example.py
+++ PyModuleWeekly/textwrap_example.py
@@ -1,19 +1,12 @@
 import textwrap
 
 sample_text = '''
-     The textwrap module can be used to format text for output in
-    situations where pretty-printing is desired.  It offers
-      programmatic functionality similar to the paragraph wrapping
-    or filling features found in many text editors.
+        The textwrap module can be used to format text for output in
+        situations where pretty-printing is desired.  It offers
+        programmatic functionality similar to the paragraph wrapping
+        or filling features found in many text editors.
     '''
 
-# 注意or前方没有加indent是因为它输出时刚好是一行的开头,而且不是第一行
 
-sample_indent = textwrap.fill(sample_text, width=50)
-print(sample_indent)
 
-print()
-dedented_text = textwrap.dedent(sample_text)
-print('Dedented:')
-print(dedented_text)
 
