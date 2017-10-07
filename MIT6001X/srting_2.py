10bb2d5bb178a7f08d5c9d504e8665770f22e326
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-06 21:36:57 -0400

    MIT 6.00.1x strings

--- MIT6001X/srting_2.py
+++ MIT6001X/srting_2.py
@@ -1,7 +1,7 @@
 temp = 120
 if temp > 85:
     print("Hot")
-elif temp > 100:  # elif优先级低于if,被跳过
+if temp > 100:  # elif优先级低于if,被跳过
     print("REALLY HOT!")
 elif temp > 60:
     print("Comfortable")
