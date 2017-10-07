7a4f76538696b8809d091cc47289ed9d1d450993
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-06 21:17:08 -0400

    Python module 3 of the week textwrap

---
+++ MIT6001X/Quiztry.py
@@ -0,0 +1,15 @@
+str1 = 'hello'
+str2 = ','
+str3 = 'world'
+print(str1 == 'HELLO')
+print('e' in str1)
+
+str4 = str1 + str3
+print(str4)
+
+print(str4[::-1])
+
+listA = ['a', 'b', 'c', 'd']
+print(listA[0:2])
+print(listA[0::2])
+print(listA[2:1])
