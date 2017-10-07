8f155f05e896363f8039150582eb7258a3aa82e6
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-06 21:28:09 -0400

    MIT 6.00.1x strings

--- MIT6001X/Quiztry.py
+++ MIT6001X/Quiztry.py
@@ -1,15 +1,9 @@
-str1 = 'hello'
-str2 = ','
-str3 = 'world'
-print(str1 == 'HELLO')
-print('e' in str1)
-
-str4 = str1 + str3
-print(str4)
-
-print(str4[::-1])
-
-listA = ['a', 'b', 'c', 'd']
-print(listA[0:2])
-print(listA[0::2])
-print(listA[2:1])
+temp = 500
+if temp > 85:
+   print("Hot")
+elif temp > 100: # elif优先级低于if,被跳过
+   print("REALLY HOT!")
+elif temp > 60:
+   print("Comfortable") 
+else:
+   print("Cold")
