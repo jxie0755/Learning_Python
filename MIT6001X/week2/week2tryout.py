95b1b8ff6036eaa2677e52afc6b603595cdc249b
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-08 12:40:42 -0400

    MIT 6.00.1x strings, loop

--- MIT6001X/week2/week2tryout.py
+++ MIT6001X/week2/week2tryout.py
@@ -1,19 +1,24 @@
 # Calculation of cubic root by enumeration.
 
-target = 9
+target = 28
 
 guess = 0
-epsilon = 0.00001
-increment = 0.00001
+epsilon = 0.0001
+increment = 0.0001
+trycount = 0
 
 while guess**3 < target:
     guess += 1
+    trycount +=1
 if guess**3 == target:
     print("The cubic root of", target, "is", guess)
-if guess**3 > target:
-    guess = guess - 1
-
-while guess**3 - target > epsilon:
-    guess += increment
-print("The cubic root of", target, "is very close to", guess)
-
+    print("I tried enumeration", trycount, "times to get this result")
+else:
+    guess = guess -1
+    while target - guess**3 > epsilon:
+        guess += increment
+        trycount += 1
+    guess = guess - increment
+    print("The cubic root of", target, "is very close to", guess)
+    print("The closest cube I can for this guess is", guess**3)
+    print("I tried enumeration", trycount, "times to get this result")
