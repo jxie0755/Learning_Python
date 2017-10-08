9dc5a2d10da4ef425fad0d86e5dc1220b5e9eb41
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-08 14:47:07 -0400

    MIT 6.00.1x bisection search

--- MIT6001X/week2/Binary_convert.py
+++ MIT6001X/week2/Binary_convert.py
@@ -1,9 +1,17 @@
 # this is to convert a binary number to decimal number
 
-target = input("please input a binary number:")
+import re
+
+while True:
+    target = input("please input a binary number:")
+    if not re.match("^[0-1]*$", target):
+        print("Error! Only digits 0 and 1 allowed!")
+        continue
+    else:
+        break
 ans = 0
 numlevel = len(target)
 for index in range(0, numlevel):
     ans += int(target[numlevel - index - 1]) * 2**index
 print(ans)
-
+print(bin(ans))
--- MIT6001X/week2/week2tryout.py
+++ MIT6001X/week2/week2tryout.py
@@ -1,2 +1,10 @@
-for i in range(0, 10):
-    print(10 - i)
+import re
+
+while True:
+    input_str = input("Please provide some info: ")
+    if not re.match("^[0-1]*$", input_str):
+        print("Error! Only digits 0 and 1 allowed!")
+        continue
+    else:
+        print("ok, this is good")
+        break
