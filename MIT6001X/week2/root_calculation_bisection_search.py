03f84d6c6d9531c4dc9de118a31ccb3f09453aea
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-08 13:43:10 -0400

    MIT 6.00.1x strings, loop

--- MIT6001X/week2/root_calculation_bisection_search.py
+++ MIT6001X/week2/root_calculation_bisection_search.py
@@ -1,11 +1,12 @@
 # bisection search
 # narrow down to find sqrt much faster than numeration
+# square root of a target
 
-x = 26
+x = 0.5
 epsilon = 0.001
 numGuess = 0
 
-# set range
+# set range (if 0 < x < 1, then low = x, high = 1).
 low = 1.0
 high = x
 guess = (high + low) / 2
@@ -20,3 +21,27 @@ while abs(guess**2 - x) >= epsilon:
     guess = (high + low) / 2
 print("It took me", numGuess, "times to find the answer")
 print(guess, "is the sqrt of", x, "the square of", guess, "is", guess**2)
+
+print()
+
+# cube root of a target
+
+x = 27
+epsilon = 0.001
+numGuess = 0
+
+# set range
+low = 1.0
+high = x
+guess = (high + low) / 2
+
+while abs(guess**3 - x) >= epsilon:
+    print('low =', str(low), 'high=', high, 'guess=', guess)
+    numGuess += 1
+    if guess**3 < x:
+        low = guess
+    else:
+        high = guess
+    guess = (high + low) / 2
+print("It took me", numGuess, "times to find the answer")
+print(guess, "is the sqrt of", x, "the square of", guess, "is", guess**3)
