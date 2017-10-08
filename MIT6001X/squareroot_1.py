7af7dd119c3e6583a501d6d2d505c9ffcf9b2620
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-08 10:41:34 -0400

    MIT 6.00.1x strings, branch

---
+++ MIT6001X/squareroot_1.py
@@ -0,0 +1,11 @@
+# using algorithm to find a square root of a target.
+
+target = input('Please in put an integer, must >= 0:')
+x = 0
+while x**2 < int(target):
+    x += 1
+if x**2 == int(target):
+    print('The square root of', int(target), 'is', x)
+else:
+    print(int(target), 'is not a perfect square')
+
