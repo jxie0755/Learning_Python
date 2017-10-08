785b28adc365e3217c569eacc5910ea4c62de31f
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-08 15:53:29 -0400

    MIT 6.00.1x bisection search

--- MIT6001X/week2/Binary_convert.py
+++ MIT6001X/week2/Binary_convert.py
@@ -15,3 +15,39 @@ for index in range(0, numlevel):
     ans += int(target[numlevel - index - 1]) * 2**index
 print(ans)
 print(bin(ans))
+
+# this is to convert a decimal number to binary
+# 用2辗转相除至结果为1
+# 将余数和最后的1从下向上倒序写 就是结果 （逆序）
+# 例如302
+# 302/2 = 151 余0
+# 151/2 = 75 余1
+# 75/2 = 37 余1
+# 37/2 = 18 余1
+# 18/2 = 9 余0
+# 9/2 = 4 余1
+# 4/2 = 2 余0
+# 2/2 = 1 余0
+# 故二进制为100101110
+
+import re
+
+while True:
+    target = input("please input a decimal number:")
+    if not re.match("^[0-9]*$", target):
+        print("Error! Only digits 0 and 1 allowed!")
+        continue
+    else:
+        break
+int_target = int(target)
+biList = []
+while True:
+    biList.append(int_target % 2)
+    int_target = int(int_target / 2)
+    if int_target == 1:
+        biList.append(int_target % 2)
+        int_target = int(int_target / 2)
+        break
+biList.reverse()
+binumber = int(''.join(str(i) for i in biList))
+print('The binary number of', int(target), "is", binumber)
--- MIT6001X/week2/week2tryout.py
+++ MIT6001X/week2/week2tryout.py
@@ -1,10 +1,8 @@
-import re
+listA = ['A', 'B', 'C']
+print(listA)
+listA.reverse()
+print(listA)
 
-while True:
-    input_str = input("Please provide some info: ")
-    if not re.match("^[0-1]*$", input_str):
-        print("Error! Only digits 0 and 1 allowed!")
-        continue
-    else:
-        print("ok, this is good")
-        break
+print()
+s ='abc'
+print(reversed(s))
