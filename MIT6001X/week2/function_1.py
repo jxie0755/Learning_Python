3b2b7ef1f820928a72e218e9e6a55551a72709a0
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-10 21:16:48 -0400

    MIT 6.00.1x functions

---
+++ MIT6001X/week2/function_1.py
@@ -0,0 +1,26 @@
+def is_even(i):
+   """
+    Input: i, a positive int
+    Returns True if i is even, otherwise False
+    """
+   # """contains the specification docstring"""
+
+   print("hi")
+   return i % 2 == 0
+
+is_even(3)
+
+def f():
+    global x   # global 不能作用于argument
+    x = x + 1
+    print('x=', x)
+    return x
+    print('nnnn')  # code after return can not be executed
+
+x = 3
+f()
+f()
+
+
+
+
