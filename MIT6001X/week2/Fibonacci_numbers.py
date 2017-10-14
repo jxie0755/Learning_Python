1b962be7bb12a4dd750415ed06ee5fba127b8a0c
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-13 20:36:23 -0400

    MIT 6.00.1x Recursion, fibonaci

---
+++ MIT6001X/week2/Fibonacci_numbers.py
@@ -0,0 +1,33 @@
+# Fibonacci numbers
+# new rabbits born by month:
+# 0, 1, 2, 3 ,4 ,5, 6 (month)
+# 1, 1, 2, 3, 5, 8, 13....(rabbits)
+# how many female rabbits in total in any month?
+# females(n) = females(n-1)+females(n-2)
+
+# Use recursion to solve this problem
+def fibRecur(i):
+    """
+    This returns the number of female at month i
+    """
+    if i == 0:
+        return 1
+    elif i == 1:
+        return 1
+    else:
+        return fibRecur(i-1) + fibRecur(i-2)
+
+print(fibRecur(6))
+
+
+def sumFemale(i):
+    """
+    This is to calculate the total number of female rabbits after month i
+    """
+    sumFem = 0
+    while i >= 0:
+        sumFem += fibRecur(i)
+        i -= 1
+    return sumFem
+
+print(sumFemale(6))
