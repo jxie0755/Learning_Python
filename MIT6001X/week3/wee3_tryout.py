9098bf455e689b37eef9e433ab617e3615027c36
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-15 22:41:05 -0400

    MIT 6.00.1x Data structure, tuple

---
+++ MIT6001X/week3/wee3_tryout.py
@@ -0,0 +1,5 @@
+# MIT class said that printing t1[1] should out put ('two',)
+# with comma to indicate that it is a tuple
+# but not any more in python 3.6.2
+t1 = (1, 'two', 3, '4', 'five')
+print(t1[1])
