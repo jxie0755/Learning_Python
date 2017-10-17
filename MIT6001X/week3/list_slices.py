4fe14d57c6d45899607c765c0b8eead9edd70026
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-16 21:51:14 -0400

    MIT 6.00.1x Data structure, list

---
+++ MIT6001X/week3/list_slices.py
@@ -0,0 +1,26 @@
+x = [1, 2, [3, 'John', 4], 'Hi']
+
+print(x[0])
+# 1
+
+print(x[2])
+# [3, 'John', 4]
+
+print(x[-1])
+# Hi
+
+print(x[2][2])
+# 4
+
+print(x[0:1])  # 不再需要逗号,因为中括号识别
+# [1]
+
+print(2 in x)
+# True
+
+print(3 in x)
+# False
+
+x[0] =8
+print(x)
+# [8, 2, [3, 'John', 4], 'Hi']
