07ca87da149bb1f3ca977ecb17e4cce71beecf05
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-16 22:11:10 -0400

    MIT 6.00.1x Data structure, list

---
+++ MIT6001X/week3/list_concatenate.py
@@ -0,0 +1,20 @@
+# Concatenation and method extend
+
+L1 = [2, 1, 3]
+L2 = [4, 5, 6]
+L3 = L1 + L2
+print(L3)
+
+L3.extend((7, 8))
+L3.extend(['X', 'Y'])
+L3.extend({'a': 1, 'b': 2})  # does not have to be a list, a tuple is also ok.
+
+print(L3)
+
+# del and remove
+L4 = [1, 2, 3, 1, 2, 3, 4, 5]
+
+del(L4[1])    # del an item in the list
+L4.remove(1)  # remove a value in the list(only the first one occured
+print(L4)
+
