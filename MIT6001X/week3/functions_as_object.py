e2d19e333a2775b748e5494edc51f32e14c61f07
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-17 21:16:27 -0400

    MIT 6.00.1x Data structure, list

---
+++ MIT6001X/week3/functions_as_object.py
@@ -0,0 +1,21 @@
+# apply a function to each item in a list
+
+def applyToEach(L, f):
+    for i in range(len(L)):
+        L[i] = f(L[i])
+
+L = [1, -2., 3.4]
+applyToEach(L, abs)
+print(L)
+
+applyToEach(L, int)
+print(L)
+
+# apply a list of function to an argument
+def applyFuns(L, x):
+    for f in L:
+        print(f(x))
+
+L = [abs, int]
+applyFuns(L, -4.2)
+
