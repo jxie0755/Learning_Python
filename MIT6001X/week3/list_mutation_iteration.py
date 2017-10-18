3b6d3c5c35c4a54a1e9f3965407dbabd97ce5d97
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-17 20:24:10 -0400

    MIT 6.00.1x Data structure, list

--- MIT6001X/week3/list_mutation_iteration.py
+++ MIT6001X/week3/list_mutation_iteration.py
@@ -13,4 +13,7 @@ print(L1)
 # >>> [2, 3, 4]
 # not as expected to be [3, 4]
 
+# python uses an internal counter to keep track of index when in loop
+# mutating changes the list during iteration changed list length, but index counter was not updated
+
 
