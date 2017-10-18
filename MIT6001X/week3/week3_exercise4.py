f27afcfd9cf70e271ada352b59faa11ee6a790e8
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-17 20:33:02 -0400

    MIT 6.00.1x Data structure, list

--- MIT6001X/week3/week3_exercise4.py
+++ MIT6001X/week3/week3_exercise4.py
@@ -1,3 +1,5 @@
+# '==' vs 'is'
+
 aList = [0, 1, 2, 3, 4, 5]
 bList = aList
 aList[2] = 'hello'
@@ -5,3 +7,9 @@ aList[2] = 'hello'
 print(aList == bList)
 print(aList is bList)
 
+cList = [6, 5, 4, 3, 2]
+dList = []
+for num in cList:
+    dList.append(num)
+print(cList == dList)
+print(cList is dList)
