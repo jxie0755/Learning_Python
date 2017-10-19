8bbfe8b39556f94df2d69643dbe888e45af2212d
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-18 22:45:48 -0400

    MIT 6.00.1x Dict

---
+++ MIT6001X/week3/dict_fib_application.py
@@ -0,0 +1,31 @@
+# if write a fibonacci number search function by recursion
+# return the number at position n, if the number start from 1
+# this code is considered as inefficient because it has to call the function twice in each recursion
+
+def fib(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(35))
+# it repeatedly compute the fib(3), fib(2), and fib(1)
+# use dict to avoid
+
+def fib_e(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_e(n-1, d) + fib_e(n-2, d)
+        d[n] = ans         # add calculated result of n > 2, to the dict
+        return ans         # called memorization method
+
+# set a base dict
+d = {1: 1, 2: 2}
+print(fib_e(35, d))  # the dict will become longer and longer during the run, calculation become faster
+
+
+
+
