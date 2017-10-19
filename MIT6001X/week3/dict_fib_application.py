bba5fd2dfa34e926ae40c60f60e8f3e99cdac64b
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-18 23:20:16 -0400

    MIT 6.00.1x Dict

--- MIT6001X/week3/dict_fib_application.py
+++ MIT6001X/week3/dict_fib_application.py
@@ -3,6 +3,8 @@
 # this code is considered as inefficient because it has to call the function twice in each recursion
 
 def fib(n):
+    global numFibCalls
+    numFibCalls += 1
     if n == 1:
         return 1
     elif n == 2:
@@ -10,11 +12,16 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
+numFibCalls = 0
 print(fib(35))
+print('called', numFibCalls, 'times')
 # it repeatedly compute the fib(3), fib(2), and fib(1)
 # use dict to avoid
 
 def fib_e(n, d):
+    global numFibCalls
+    numFibCalls += 1
+
     if n in d:
         return d[n]
     else:
@@ -23,8 +30,10 @@ def fib_e(n, d):
         return ans         # called memorization method
 
 # set a base dict
+numFibCalls = 0
 d = {1: 1, 2: 2}
 print(fib_e(35, d))  # the dict will become longer and longer during the run, calculation become faster
+print('called', numFibCalls, 'times')
 
 
 
