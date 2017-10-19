d3b286849712aece8f309da4dd18ec6c63e9898c
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-18 20:58:09 -0400

    MIT 6.00.1x branch 2 foobar 2

---
+++ MIT6001X/week3/dict_operation.py
@@ -0,0 +1,19 @@
+# Introduce a number of operation to dict
+
+grades = {'Ana': 'B', 'John': 'A+', 'Denise': 'A', 'Katy': 'A', 'Lindsay': ('a', 'b', 'c')}
+
+grades['Cindy'] = 'A'
+
+del grades['John']
+
+print('Denise' in grades)
+
+print(list(grades.keys()))       # only return value, don't forget to list()
+
+print(list(grades.values()))     # only return value, don't forget to list()
+
+# True
+# dict_keys(['Ana', 'Denise', 'Katy', 'Lindsay', 'Cindy'])
+# dict_values(['B', 'A', 'A', ('a', 'b', 'c'), 'A'])
+
+
