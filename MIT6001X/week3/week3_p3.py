895f0647cb7a98e78c563f81e12a9d2c7ca1e14b
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-20 07:51:26 -0400

    MIT 6.00.1x Data structure problem set

---
+++ MIT6001X/week3/week3_p3.py
@@ -0,0 +1,7 @@
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    # FILL IN YOUR CODE HERE...
