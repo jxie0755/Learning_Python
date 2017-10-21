d5b13b19181f3d69383d9e9e66755886f91e47bb
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-20 21:08:43 -0400

    MIT 6.00.1x Data structure problem set (hangman)

--- MIT6001X/week3/hangman/ps3_hangman_to_submit.py
+++ MIT6001X/week3/hangman/ps3_hangman_to_submit.py
@@ -11,10 +11,11 @@ import random
 
 WORDLIST_FILENAME = "words.txt"
 
+
 def loadWords():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -26,6 +27,7 @@ def loadWords():
     wordlist = line.split()
     return wordlist
 
+
 def chooseWord(wordlist):
     """
     wordlist (list): list of words (strings)
@@ -34,12 +36,14 @@ def chooseWord(wordlist):
     """
     return random.choice(wordlist)
 
+
 # end of helper code
 # -----------------------------------
 
 # Load the list of words into the variable wordlist
 # so that it can be accessed from anywhere in the program
 wordlist = loadWords()
+
 
 def isWordGuessed(secretWord, lettersGuessed):
     '''
@@ -90,7 +94,7 @@ def getAvailableLetters(lettersGuessed):
             available_letters += i
 
     return available_letters
-    
+
 
 def hangman(secretWord):
     '''
@@ -98,37 +102,39 @@ def hangman(secretWord):
 
     Starts up an interactive game of Hangman.
 
-    * At the start of the game, let the user know how many 
+    * At the start of the game, let the user know how many
       letters the secretWord contains.
 
     * Ask the user to supply one guess (i.e. letter) per round.
 
-    * The user should receive feedback immediately after each guess 
+    * The user should receive feedback immediately after each guess
       about whether their guess appears in the computers word.
 
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
+    * After each round, you should also display to the user the
+      partially guessed word so far, as well as letters that the
       user has not yet guessed.
 
     Follows the other limitations detailed in the problem write-up.
     '''
     # show the secret word length
     print('Welcome to the game Hangman!')
-    print('\nI am thinking of a word that is', len(secretWord), 'letters long')
+    print('I am thinking of a word that is', len(secretWord), 'letters long')
     print('___________')
 
     show_target = '_ ' * len(secretWord)
-    print('Your need to guess this word:', show_target)
 
     guess_list = []
     guess_count = 1
     guess_left = 8
 
-    print('You have 8 guesses left!!')
     while guess_count <= 8:
         # To create an input for guessing the letter
         while True:
-            letter_guess = input('\nPlease guess a letter:')
+            # remind the available letters
+            print('You have', guess_left, 'guesses left')
+            print('Available Letters:', getAvailableLetters(guess_list))
+
+            letter_guess = input('Please guess a letter:')
             letter_guess = letter_guess.lower()
             if len(letter_guess) > 1:
                 print('\nPlease only input one letter')
@@ -137,31 +143,32 @@ def hangman(secretWord):
                 guess_list.append(letter_guess)
                 break
             else:
-                print('\nYou have already tried this letter before')
+                print("Oops! You've already guessed that letter:", getGuessedWord(secretWord, guess_list))
+                print('__________')
                 continue
-        guess_count += 1
+
         # show the inputted guess
         if letter_guess in secretWord:
-            print('\nGood guess!')
-        else:
-            print('\nSorry, it was wrong')
+            print('Good guess:', getGuessedWord(secretWord, guess_list))
+            print("___________")
 
-        print('This letter is:', end=' ')
-        print(getGuessedWord(secretWord, guess_list))
+        else:
+            print('Oops! That letter is not in my word:', getGuessedWord(secretWord, guess_list))
+            guess_left -= 1
+            guess_count += 1
+            print("___________")
+
 
         # End the game after winning
         if isWordGuessed(secretWord, guess_list):
             guess_count = 10
-            print("\nCongratulations! You've WON !!")
+            print("Congratulations! You WON !!")
             continue
-
-        # remind the available letters
-        guess_left -= 1
-        print('You have', guess_left, 'guesses left')
 
     # End the game if guess over 8 times
     if guess_count == 9:
-        print("\nSorry, you've lost the game!!")
+        print("Sorry, you ran out of guesses. The word was " + secretWord + ".")
+
 
 secretWord = 'apple'
 hangman(secretWord)
