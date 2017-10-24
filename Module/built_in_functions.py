b5eb521980cfc2bf1c2de450e4b983bf812bdbdf
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-23 20:11:40 -0400

    Python built-in functions

---
+++ Module/built_in_functions.py
@@ -0,0 +1,15 @@
+# A try out for all the built-in functions in python
+
+print(abs(-4))  # >>> 4
+# absolute value
+# argument: int and float
+# return the
+
+print(all([0, 4]))  # >>> False
+print(all([]))  # >>> True
+# argument: iterable objects
+# return True if all elements of the iterable are true
+# if the iterable is empty, return True
+
+
+
