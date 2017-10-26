6aab382b40e3f969d023dfb78599ea1ef3b4e9ce
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-25 23:19:52 -0400

    Checkio p1 most wanted letter

--- Checkio/tryout.py
+++ Checkio/tryout.py
@@ -1,4 +1,5 @@
-text = 'aBc deCEfcf!!!!'
+# text = 'aBc deCEfcf!!!!'
+text = 'aabbcceeddffgg'
 text = text.lower()
 text = text.replace(' ', '')
 text_list = sorted(text)
@@ -14,9 +15,19 @@ for i in range(0, index - 1):
         text_dict[text_list[i]] = v
         v = 1
 
-for k in text_dict.keys():
-    if ord(k) < 97 or ord(k) > 122:
-        del text_dict[k]
+text_dict_filtered = {}
+for k, v in text_dict.items():
+    if ord(k) >= 97 and ord(k) <= 122:
+        text_dict_filtered[k] = v
+
+if
+
+v_max = max(list(text_dict_filtered.values()))
+for k in text_dict_filtered.keys():
+    if text_dict_filtered[k] == v_max:
+        print(k)
+
 
 print(text_list)
-print(text_dict)
+print(text_dict_filtered)
+
