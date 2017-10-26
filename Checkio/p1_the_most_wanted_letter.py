3cdd6977e6b01907aaa98049650bc9074538a0a2
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-26 00:19:20 -0400

    Checkio p1 update

--- Checkio/p1_the_most_wanted_letter.py
+++ Checkio/p1_the_most_wanted_letter.py
@@ -2,24 +2,24 @@ def checkio(text):
     # raw string process, transfer to lower case and remove spaces
     text = text.lower()
     text = text.replace(' ', '')
+
     # raw string process 2, create a list, sort and add an empty string at the end.
     text_list = sorted(text)
+    text_list_clean = []
     for i in text_list:
-        if ord(i) < 97 or ord(i) > 122:
-            text_list.remove(i)
+        if ord(i) >= 97 and ord(i) <= 122:
+            text_list_clean.append(i)
+    text_list_clean.append('')
 
-    text_list.append('')
-    # raw string process 3, create a dict for each character and number of occurrence
+    # create an empty dict and a loop to add data to the dict
     text_dict = {}
-
-    # a loop to add data to the dict
-    index = len(text_list)
+    index = len(text_list_clean)
     v = 1
     for i in range(0, index - 1):
-        if text_list[i] == text_list[i + 1]:
+        if text_list_clean[i] == text_list_clean[i + 1]:
             v += 1
         else:
-            text_dict[text_list[i]] = v
+            text_dict[text_list_clean[i]] = v
             v = 1
 
     # generate a list that stores all the values
@@ -36,5 +36,7 @@ print(checkio("Oops!"))  # == "o", "Don't forget about lower case."
 print(checkio("AAaooo!!!!"))  # == "a", "Only letters."
 print(checkio("abe"))  # == "a", "The First."
 print(checkio("a" * 9000 + "b" * 1000))  # == "a", "Long."
+print(checkio('Lorem ipsum dolor sit amet 0000000000000000000!!!'))  # == 'm'
+
 
 # Passed
---
+++ Checkio/tryout.py
@@ -0,0 +1,9 @@
+text = 'Loremet 000!!!'
+text = text.lower()
+text = text.replace(' ', '')
+
+# raw string process 2, create a list, sort and add an empty string at the end.
+text_list = sorted(text)
+text_list_clean = []
+
+print(text_list)
