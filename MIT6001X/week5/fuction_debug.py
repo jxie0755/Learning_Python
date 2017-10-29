9306f7658ce1b3d21462b86506c48386ce91a8bf
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-28 21:48:38 -0400

    MIT 6.00.1x Testing and Debugging

---
+++ MIT6001X/week5/fuction_debug.py
@@ -0,0 +1,30 @@
+def isPal(x):
+    assert type(x) == list
+    temp = x  # should be x[:]
+    temp.reverse  # should be reverse()
+    print('x is', x)
+    print('temp is', temp)
+    if temp == x:
+        return True
+    else:
+        return False
+
+msg = [1, 2, 3]
+print(isPal(msg))
+
+
+def silly(n):
+    result = []
+    for i in range(n):
+        # result = [] should be outside of the loop
+        elem = input('Enter element: ')
+        result.append(elem)
+    if isPal(result):
+        print('Yes')
+    else:
+        print('No')
+
+silly(2)
+
+
+
