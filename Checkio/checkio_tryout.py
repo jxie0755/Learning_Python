b00d95503aab7995f31b4676e4303a9dcd68bf23
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-29 22:25:56 -0400

    Checkio p3

--- Checkio/checkio_tryout.py
+++ Checkio/checkio_tryout.py
@@ -1,9 +1,4 @@
-def checkio(data):
-    result = []
-    for i in data:
-        if data.count(i) > 1:
-            result.append(i)
-    print(result)
-
-data = [1, 2, 3, 1, 3]
-checkio(data)
+dd = 'abcdefg'
+print(dd.isupper())
+print(dd.islower())
+print(dd.isdigit())
