f1a890b4203c81f20d70189a94741c556d7293af
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-30 22:35:49 -0400

    Checkio p4

--- Checkio/p4_monkey_typing.py
+++ Checkio/p4_monkey_typing.py
@@ -1,16 +1,23 @@
 # Input: Two arguments. A text as a stringand words as a set of strings.
 # Output: The number of words in the text as an integer.
 
+# def count_words(text, words):
+#     text = text.lower().split()
+#     result = 0
+#     for i in words:
+#         for w in text:
+#             if i in w:
+#                 result += 1
+#                 break
+#     return result
+
 def count_words(text, words):
-    text = text.lower().split()
+    text = text.lower()
     result = 0
     for i in words:
-        for w in text:
-            if i in w:
-                result += 1
-                break
+        if i in text:
+            result += 1
     return result
-
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
