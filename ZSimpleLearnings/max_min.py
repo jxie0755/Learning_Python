55c2f26cfcb4c59cfbcd2ba6240986546c97f536
jxie0755 <jxie0755@users.noreply.github.com>
2017-10-31 20:35:28 -0400

    Simple Learnings on max and min

---
+++ ZSimpleLearnings/max_min.py
@@ -0,0 +1,19 @@
+a = [1, 2, 3, 2, 3, 4, 2, 3, 1]
+# max() will return max value in an iterable object
+print('max value is', max(a))
+# min() will return min value in an iterable object
+print('min value is', min(a))
+
+# also works for strings
+b = 'abcdegf'
+print('max char is', max(b))
+print('min char is', min(b))
+
+
+# or just give a few values as argument for max or min value
+print('max value is', max(1, 3, 4, 2))
+print('min value is', min(2, 1, 4, 3))
+
+# Pay attention to the key argument!
+
+
