e0c958bdc7014857752c60f9d88594a6cf0aff03
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-01 22:04:21 -0400

    MIT 6.00.1x raise exceptions

---
+++ MIT6001X/week5/raise_exception.py
@@ -0,0 +1,19 @@
+def get_ratios(L1, L2):
+    ratios = []
+    for index in range(len(L1)):
+        try:
+            ratios.append(L1[index]/float(L2[index]))
+        except ZeroDivisionError:
+            ratios.append(float('NaN'))  # NaN = Not a Number
+        except:
+            raise ValueError('get_ratios called with bad arg')
+    return ratios
+
+a = [1, 2, 3]
+b = [2, 3, 4]
+c = [2, 0, 5]
+d = [2, 3]
+print(get_ratios(a, b))
+print(get_ratios(a, c))
+print(get_ratios(a, d))
+
