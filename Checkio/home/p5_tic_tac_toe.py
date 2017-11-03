ab942307c89b219133bea74c342863d7518157b0
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2017-11-03 17:00:44 -0400

    Update p5_tic_tac_toe.py

--- Checkio/home/p5_tic_tac_toe.py
+++ Checkio/home/p5_tic_tac_toe.py
@@ -2,9 +2,9 @@ def checkio(game_result):
     def judge(R):
         for i in R:
             if i.count('X') == 3:
-                return 'X' 
+                return 'X'
             if i.count('O') == 3:
-                return 'O' 
+                return 'O'
     # Horizontal line, go straight with judge()
     
     # Vertical line
@@ -18,15 +18,12 @@ def checkio(game_result):
     c1 = []; c2 = []; cline = [c1, c2]
     for i in list(range(3)):
         c1.append(game_result[i][2-i])
-       
     for i in list(range(3)):
         c2.append(game_result[i][i])
     
     # Return result
-    return judge(game_result) \
-    or judge(list(map(f, (0, 1, 2)))) \
-    or judge(cline) \
-    or 'D'
+    return judge(game_result) or judge(list(map(f, (0, 1, 2)))) or judge(cline) or 'D'
+
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
