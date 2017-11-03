4c3fcf826de964d70f589952fd17b3187597d6f9
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-02 23:19:11 -0400

    Checkio home p7 max and min

--- Checkio/home/p7_max_and_min.py
+++ Checkio/home/p7_max_and_min.py
@@ -6,17 +6,17 @@ def Maximum(List, *args):
             Max = List
         else:
             Max = List[0]
-        
+
         for i in x:
             if i > Max:
                 Max = i
         return Max
-    
+
     def data_process():
-        result = [List,]
-        result = [List,] + [i for i in args]
+        result = [List, ]
+        result = [List, ] + [i for i in args]
         return result
-    
+
     if not args:
         # just List, and iterable
         return m(List)
@@ -24,19 +24,20 @@ def Maximum(List, *args):
         # multiple argument, each one single item
         if type(List) == int or type(List) == float:
             return m(data_process())
-        
+
         # multiple arugments, each one iterable
         else:
-            v = [m(List),] + [m(i) for  i in args]
+            v = [m(List), ] + [m(i) for i in args]
             for i in data_process():
                 if m(v) in i:
                     return i
-            
+
+
 a = [10.5, 2.3, 19.7]
 b = [8.1, 10.2, 27.2]
 c = [1.1, 2.0, 10.9]
-print(Maximum([1.5,-2,-3]))
-print(Maximum(1.5,2.3,-3,-4))
+print(Maximum([1.5, -2, -3]))
+print(Maximum(1.5, 2.3, -3, -4))
 print(Maximum(a, b, c))
 
 # TODO 还剩下Keyword参数
