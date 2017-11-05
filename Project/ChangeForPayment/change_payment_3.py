fb4be2160afcacd7b60d7eca622f6ad27136deee
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-05 14:23:15 -0500

    change payment add different bill types

--- Project/ChangeForPayment/change_payment_3.py
+++ Project/ChangeForPayment/change_payment_3.py
@@ -2,7 +2,7 @@ def pay_change(paid, price):
     # set up the change and an empty dictionary for result
     global change
     change = paid - price
-    bills = ['100', '$20', '$10', '$5', '$2', '$1', '$0.5', '$0.25', '$0.10', '$0.01']
+    bills = ['$100', '$20', '$10', '$5', '$2', '$1', '$0.5']
 
     # get a function to calculate the change, for the number of each bill
     def f(x):
@@ -10,13 +10,13 @@ def pay_change(paid, price):
         result = divmod(change, x)[0]
         change = divmod(change, x)[1]
         return result
-    temp = list(map(f, (100, 20, 10, 5, 2, 1, 0.5, 0.25, 0.10, 0.01)))
+    temp = list(map(f, list(map(lambda x: float(x[1:]), bills))))
     result = dict(zip(bills, temp))
+    print(result)
 
     # present the result, do not show if value is 0
     for k, v in result.items():
         if v != 0:
-            print('Need', v, 'bills of', k)
+            print('Need', int(v), 'bills of', k)
 
-pay_change(200.99, 8.37)
-
+pay_change(200, 17.5)
