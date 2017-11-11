f22afba53c69332bce78b174571212f6cc8aeed9
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-11 13:50:59 -0500

    Checkio oreilly p7

--- Checkio/oreilly/p7_boolean_algebra.py
+++ Checkio/oreilly/p7_boolean_algebra.py
@@ -54,3 +54,16 @@ if __name__ == '__main__':
     assert boolean(1, 1, "implication") == 1, "material"
     assert boolean(0, 1, "exclusive") == 1, "xor"
     assert boolean(0, 1, "equivalence") == 0, "same?"
+
+# 另解
+def boolean(x, y, operation):
+    if operation == 'conjunction':
+        return int(x and y)
+    elif operation == 'disjunction':
+        return int(x or y)
+    elif operation == 'exclusive':
+        return int(x != y )
+    elif operation == 'equivalence':
+        return int(x == y)
+    elif operation == 'implication':
+        return int(not(x) or y)
