2f9762fc54386c86f563d679d70839778a73a061
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-10 20:15:18 -0500

    Checkio oreilly update

--- Checkio/oreilly/p2_days_between.py
+++ Checkio/oreilly/p2_days_between.py
@@ -12,3 +12,4 @@ if __name__ == '__main__':
     assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
     assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
     assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    print('done')
