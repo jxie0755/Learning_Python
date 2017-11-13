27cbe9a7ee9a9be42897e2e5977095d400682157
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-12 21:44:47 -0500

    Checkio electronic station p1

--- PyModuleWeekly/re_example/re_simple_match.py
+++ PyModuleWeekly/re_example/re_simple_match.py
@@ -1,5 +1,6 @@
 import re
 
+# 建立在能够被search的条件
 pattern = 'this'
 text = 'Does this text match the pattern?'
 
@@ -10,6 +11,8 @@ e = match.end()
 
 print(match.re.pattern)  # >>> 'this'
 print(match.string)  # >>> 'Does this text match the pattern?'
+
+# 如果无法被search到,以下则报错
 print(match.start())  # >>> 5 --- means starting from text[5]
 print(match.end())    # >>> 9 --- means ends at text [9]
 print(text[s:e])
