508f493efd4d19586b8558f1e21305693d41ee36
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-12 22:02:27 -0500

    Learning re

--- PyModuleWeekly/re_example/re_simple_compiled.py
+++ PyModuleWeekly/re_example/re_simple_compiled.py
@@ -1,17 +1,18 @@
 import re
 
 # Precompile the patterns
-regexes = [
-    re.compile(p)
-    for p in ['this', 'that']
-]
+
+# 同时搜索多个pattern
+regexes = [re.compile(p) for p in ['this', 'that']]
 text = 'Does this text match the pattern?'
+
+print('regexes is', regexes)
+# >>> regexes is [re.compile('this'), re.compile('that')]
 
 print('Text: {!r}\n'.format(text))
 
 for regex in regexes:
-    print('Seeking "{}" ->'.format(regex.pattern),
-          end=' ')
+    print('Seeking "{}" ->'.format(regex.pattern), end=' ')
 
     if regex.search(text):
         print('match!')
