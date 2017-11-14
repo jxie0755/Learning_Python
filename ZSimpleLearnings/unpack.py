e824e835a05ca48e18eecb83a00783864189f8cb
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-13 20:13:36 -0500

    Simple Learning on unpack list

---
+++ ZSimpleLearnings/unpack.py
@@ -0,0 +1,9 @@
+dd = [('a', 1, 'x'), ('b', 2, 'y'), ('c', 3, 'z')]
+for k, v, c in dd:
+    print(k)
+# >>>
+# a
+# b
+#  c
+
+# must match k, v, c (3 items) to the same number of elements in sub-tuples (3 items).
