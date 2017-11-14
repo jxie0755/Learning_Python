3cb22b3798dc0c5f7ce0265cc621b2bcb8cb107c
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-13 20:10:12 -0500

    Learning re

---
+++ PyModuleWeekly/re_example/re_repetition.py
@@ -0,0 +1,23 @@
+# re_repetition
+
+from re_test_patterns import find_patterns
+
+# 特殊符号简化寻找目标
+# 接陈以上函数find_patterns()
+
+# find 'a' followed by zero or more 'b'
+find_patterns('abbaabbba', 'ab*')
+
+# find 'a followed by one or more b'
+find_patterns('abbaabbba', 'ab+')
+
+# find 'a' followed by zero or one 'b'
+find_patterns('abbaabbba', 'ab?')
+
+# find 'a' followed by three 'b'
+find_patterns('abbaabbba', 'ab{3}')
+
+# find 'a' followed by two to three 'b'
+find_patterns('abbaabbba', 'ab{2,3}')
+
+# output not shown
