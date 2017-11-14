2a4f2d13c247274ae61555a99f99b035724e5d31
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-13 23:03:56 -0500

    cipher map encrypt

--- Checkio/oreilly/p4b_cypher_map_encrypt.py
+++ Checkio/oreilly/p4b_cypher_map_encrypt.py
@@ -70,13 +70,17 @@ def write_password(cipher_grille, password):
         new_grille = list(map(new, list(range(4))))
         return new_grille
 
+    # write in the password with rotating grille 4 times
     for i in range(4):
         ciphered_password = encrypt(cipher_grille, password)
         password = password[4:]
         cipher_grille = rotate(cipher_grille)
+
+    # convert the list in ciphered_password into tuple with strings
     for i in range(4):
         str_element = ''.join(ciphered_password[i])
         ciphered_password[i] = str_element
+    ciphered_password = tuple(ciphered_password)
     return ciphered_password
 
 print(write_password(cipher_grille, password))
