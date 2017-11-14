e3f89f7c37121d66cd8ec4f689d2a87fee76fa74
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-13 23:01:55 -0500

    cipher map encrypt

--- Checkio/oreilly/p4b_cypher_map_encrypt.py
+++ Checkio/oreilly/p4b_cypher_map_encrypt.py
@@ -38,11 +38,45 @@ cipher_grille = ('X...',
                  'X..X',
                  '....')
 
-ciphered_password = ('itdf',
-                     'gdce',
-                     'aton',
-                     'qrdi')
+password = 'icantforgetiddqd'
 
-print(recall_password(cipher_grille, ciphered_password))
+
 def write_password(cipher_grille, password):
-    # todo
+    # define a function to express the password from the current grill
+    ciphered_password = [[0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
+    password = password
+    def encrypt(cipher_grille, password):
+        coor1 = ''.join((map(lambda x: str(x) * cipher_grille[x].count('X'), list(range(4)))))
+        coor2 = ''
+        for i in cipher_grille:
+            for n in range(4):
+                if i[n] == 'X':
+                    coor2 += str(n)
+        for c in range(0, 4):
+            ciphered_password[int(coor1[c])][int(coor2[c])] = password[c]
+        return ciphered_password
+
+    # define a function to rotate the cipher_grille 90 degree clockwise
+    def rotate(cipher_grille):
+        def new(n):
+            new_line = ''
+            for i in range(4):
+                new_line += cipher_grille[i][n]
+            return new_line[::-1]
+
+        new_grille = list(map(new, list(range(4))))
+        return new_grille
+
+    for i in range(4):
+        ciphered_password = encrypt(cipher_grille, password)
+        password = password[4:]
+        cipher_grille = rotate(cipher_grille)
+    for i in range(4):
+        str_element = ''.join(ciphered_password[i])
+        ciphered_password[i] = str_element
+    return ciphered_password
+
+print(write_password(cipher_grille, password))
