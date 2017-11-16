2409d7ccf1c4b08317080cceeca7a8dcbcfa45e3
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-15 21:42:57 -0500

    Checkio scientific expedition p3

--- Checkio/scientific_expedition/p3_friendly_number.py
+++ Checkio/scientific_expedition/p3_friendly_number.py
@@ -1,6 +1,3 @@
-p3_friendly_number
-
-
 # Input: A number as an integer. The keyword argument "base" as an integer, default 1000. The keyword argument "decimals" as an integer, default 0. The keyword argument "powers" as a list of string, default ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'].
 # Output: The converted number as a string.
 
