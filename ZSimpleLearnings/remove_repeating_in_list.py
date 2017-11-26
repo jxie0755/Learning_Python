4b75026182e424af527b325c3fa628d8f025b8e5
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-25 21:41:03 -0500

    Simple learning on remove repeated in a list

---
+++ ZSimpleLearnings/remove_repeating_in_list.py
@@ -0,0 +1,10 @@
+# remove repeating items in a list
+# 去除list中的重复元素
+a = ['b','c','d','b','c','a','a']
+print(set(a))  # set(a)根本就不会每次输出统一顺序
+print(list(set(a))) # 因此,list之后也不会每次输出统一顺序
+
+# 解决办法
+# sorted()函数,使用key,维持原来的顺序,a.index
+a = ['b','c','d','b','c','a','a']
+print(sorted(set(a), key=a.index))
