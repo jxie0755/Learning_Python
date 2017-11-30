1de19abb3f1bc79ff88784da5deceb20e6f45564
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-30 17:34:59 -0500

    Update prime number function

--- Project/PrimeNumber/primeNumber2.py
+++ Project/PrimeNumber/primeNumber2.py
@@ -9,3 +9,13 @@ def isPrime(n):
     return True
 
 print(isPrime(97))
+
+print(list(filter(isPrime, range(100))))
+
+
+def isPrime2(n):
+    """To tell if a number is prime number or not
+    n can be any integer"""
+    return n != 0 and n != 1 and all(map(lambda x: n % x != 0, range(2, int(n ** 0.5) + 1)))
+
+print(list(filter(isPrime2, range(100))))
