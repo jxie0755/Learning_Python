72f69acc0b855c6017169a8d317e395ed344c026
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-30 17:14:11 -0500

    Checkio Electronic station p8

--- Checkio/electronic_station/p8_numbers_factory.py
+++ Checkio/electronic_station/p8_numbers_factory.py
@@ -6,7 +6,15 @@
 # example:N = 20. We can factorize this number as 2*10, but 10 is not a digit. Also we can factorize it as 4*5 or 2*2*5. The smallest number for 2*2*5 is 225, for 4*5 -- 45. So we select 45.
 
 def checkio(number):
-    #replace this for solution
+    # Check if the number is a prime number:
+    def isPrime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
     return 0
 
 
