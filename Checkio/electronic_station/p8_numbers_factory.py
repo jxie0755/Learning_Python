aadc40c56f7d558bb8a0538ce8059f192875804f
jxie0755 <jxie0755@users.noreply.github.com>
2017-11-30 20:54:43 -0500

    Checkio electronic station p8

--- Checkio/electronic_station/p8_numbers_factory.py
+++ Checkio/electronic_station/p8_numbers_factory.py
@@ -5,15 +5,21 @@
 
 # example:N = 20. We can factorize this number as 2*10, but 10 is not a digit. Also we can factorize it as 4*5 or 2*2*5. The smallest number for 2*2*5 is 225, for 4*5 -- 45. So we select 45.
 
+# 本质上是求一个数能不能被分解成全是个位数的因数,如果不能就return 0, 如果可以,就return这些因数的组成的最小数字
+
 def checkio(number):
     # Form a list for all the divisor
-    divisors = list(filter(lambda x: number % x == 0, range(2, number)))
-    print(divisors)
-    # if len(divisors) == 0:
-    #     return 0
-    # else:
-
-
+    result = []
+    def divx(x):
+        for i in range(2, 10)[::-1]:
+            if number % i == 0:
+                a = i
+                b = number // i
+                result.append((a, b))
+                if b >= 10:
+                    divx(b)
+    divx(number)
+    print(result)
 
 # if __name__ == '__main__':
 #     assert checkio(20) == 45, "1st example"
