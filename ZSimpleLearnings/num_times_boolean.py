d3dd9d6c18eb5d6c515a2ec4e2ee739dde29aa5a
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-03 22:33:56 -0500

    Simple learning on calculation of boolean with numbers

--- ZSimpleLearnings/num_times_boolean.py
+++ ZSimpleLearnings/num_times_boolean.py
@@ -1,10 +1,24 @@
 # num乘以boolean, 如果是True就还是num,如果是False就直接等于0
 
+# 简单的说就是True = 1, False = 0, 然后计算
+
 a = 123
+
+# 乘法
 print(a * True)   # >>> 123
 print(a * False)  # >>> 0
-
-
 print([i * True for i in range(5)])   # >>> [0, 1, 2, 3, 4]
 print([i * False for i in range(5)])  # >>> [0, 0, 0, 0, 0]
+
+# 除法 (float result)
+print([i / True for i in range(5)])   # >>> [0.0, 1.0, 2.0, 3.0, 4.0]
+# print([i / False for i in range(5)])   # >>> ZeroDivisionError: division by zero
+
+# 加法
+print([i + True for i in range(5)])   # >>> [1, 2, 3, 4, 5]
+print([i + False for i in range(5)])  # >>> [0, 1, 2, 3, 4]
+
+# 减法
+print([i - True for i in range(5)])   # >>> [-1, 0, 1, 2, 3]
+print([i - False for i in range(5)])  # >>> [0, 1, 2, 3, 4]
 
