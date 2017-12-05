0d4ee37081a85e5639a892d401afcde505831c7c
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-04 22:53:14 -0500

    Checkio ice base p2

--- Checkio/ice_base/p2_can_you_pass.py
+++ Checkio/ice_base/p2_can_you_pass.py
@@ -6,34 +6,46 @@
 # Input: Three arguments. A matrix as a tuple of tuples with integers, first and second cell coordinates as tuples of two integers.
 # Output: The existence of a path between two given cells as a boolean or a value that can be converted to boolean.
 
+import itertools
 def can_pass(matrix, first, second):
+    # add 1 layer over the matrix with 'x'
+    matrix_new = list(map(lambda x: list(x), matrix))
+    addrow1, addrow2 = itertools.tee(list(map(lambda x: 'x', range(len(matrix[0])))))
+    matrix_new.insert(0, list(addrow1))
+    matrix_new.append(list(addrow2))
+
+    for i in matrix_new:
+        i.insert(0, 'x')
+        i.append('x')
+        print(i)
+
     # get value of first and second
     v1, v2 = matrix[first[0]][first[1]], matrix[second[0]][second[1]]
 
     # write a function to check neigher value
-    def neigher(x, y):
-        up = first[0]
+    # def neigher(x, y):
+    #     up = first[0]
 
     return None
 
 
-if __name__ == '__main__':
-    assert can_pass(((0, 0, 0, 0, 0, 0),
-                     (0, 2, 2, 2, 3, 2),
-                     (0, 2, 0, 0, 0, 2),
-                     (0, 2, 0, 2, 0, 2),
-                     (0, 2, 2, 2, 0, 2),
-                     (0, 0, 0, 0, 0, 2),
-                     (2, 2, 2, 2, 2, 2),),
-                    (3, 2), (0, 5)) == True, 'First example'
-    assert can_pass(((0, 0, 0, 0, 0, 0),
-                     (0, 2, 2, 2, 3, 2),
-                     (0, 2, 0, 0, 0, 2),
-                     (0, 2, 0, 2, 0, 2),
-                     (0, 2, 2, 2, 0, 2),
-                     (0, 0, 0, 0, 0, 2),
-                     (2, 2, 2, 2, 2, 2),),
-                    (3, 3), (6, 0)) == False, 'First example'
+# if __name__ == '__main__':
+#     assert can_pass(((0, 0, 0, 0, 0, 0),
+#                      (0, 2, 2, 2, 3, 2),
+#                      (0, 2, 0, 0, 0, 2),
+#                      (0, 2, 0, 2, 0, 2),
+#                      (0, 2, 2, 2, 0, 2),
+#                      (0, 0, 0, 0, 0, 2),
+#                      (2, 2, 2, 2, 2, 2),),
+#                     (3, 2), (0, 5)) == True, 'First example'
+#     assert can_pass(((0, 0, 0, 0, 0, 0),
+#                      (0, 2, 2, 2, 3, 2),
+#                      (0, 2, 0, 0, 0, 2),
+#                      (0, 2, 0, 2, 0, 2),
+#                      (0, 2, 2, 2, 0, 2),
+#                      (0, 0, 0, 0, 0, 2),
+#                      (2, 2, 2, 2, 2, 2),),
+#                     (3, 3), (6, 0)) == False, 'First example'
 
 matrix = ((0, 0, 0, 0, 0, 0),
           (0, 2, 2, 2, 3, 2),
@@ -44,3 +56,6 @@ matrix = ((0, 0, 0, 0, 0, 0),
           (2, 2, 2, 2, 2, 2),)
 first = (3, 2)
 second = (0, 5)
+
+print(can_pass(matrix,first,second))
+
