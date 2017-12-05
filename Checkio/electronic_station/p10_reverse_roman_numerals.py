949e334b8e5069eda8ed5621e0b348f2d25f992d
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-04 21:49:21 -0500

    Checkio electronic station p10

---
+++ Checkio/electronic_station/p10_reverse_roman_numerals.py
@@ -0,0 +1,28 @@
+# You are given a roman number as a string and your job is to convert this number into its decimal representation.
+# Input: A roman number as a string.
+# Output: The decimal representation of the roman number as an int.
+
+def reverse_roman(roman_string):
+    result = 0
+    # remove the possibility of 4 and 9
+    checklst = {'CM': 900, 'CD': 400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV': 4}
+    for i in checklst.keys():
+        if i in roman_string:
+            result += checklst[i]
+            roman_string = roman_string.replace(i, '')
+
+    # Calculate the rest of it
+    Roman_Nu = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for i in roman_string:
+        result += Roman_Nu[i]
+
+    return result
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert reverse_roman('VI') == 6, '6'
+    assert reverse_roman('LXXVI') == 76, '76'
+    assert reverse_roman('CDXCIX') == 499, '499'
+    assert reverse_roman('MMMDCCCLXXXVIII') == 3888, '3888'
+    print('Great! It is time to Check your code!');
