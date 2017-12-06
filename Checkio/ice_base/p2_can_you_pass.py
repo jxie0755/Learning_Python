fd01c09f6f0772314f2c0789e9bbbab133e6de8e
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-05 20:46:42 -0500

    Checkio ice base p2

--- Checkio/ice_base/p2_can_you_pass.py
+++ Checkio/ice_base/p2_can_you_pass.py
@@ -7,55 +7,62 @@
 # Output: The existence of a path between two given cells as a boolean or a value that can be converted to boolean.
 
 import itertools
+
+
 def can_pass(matrix, first, second):
     # add 1 layer over the matrix with 'x'
     matrix_new = list(map(lambda x: list(x), matrix))
     addrow1, addrow2 = itertools.tee(list(map(lambda x: 'x', range(len(matrix[0])))))
-    matrix_new.insert(0, list(addrow1))
+    matrix_new.insert(0, list(addrow1));
     matrix_new.append(list(addrow2))
 
     for i in matrix_new:
         i.insert(0, 'x')
         i.append('x')
-        print(i)
 
-    # get value of first and second
-    v1, v2 = matrix[first[0]][first[1]], matrix[second[0]][second[1]]
+    # get new coordinates for first and second
+    first, second = tuple(map(lambda x: x + 1, first)), tuple(map(lambda x: x + 1, second))
 
-    # write a function to check neigher value
-    # def neigher(x, y):
-    #     up = first[0]
+    # write a function to get the value of a coordinate
+    def coorvalue(coor):
+        return matrix_new[coor[0]][coor[1]]
 
-    return None
+    # write a function to check neighbor value
+    temp, result = [], []
+
+    def neighbor(coor):
+        if coor not in temp:  # only examine each coor once
+            temp.append(coor)  # by using a temp list
+            x, y = coor[0], coor[1]
+            neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]  # up, down, left, right
+
+            # examine and filter the one that has the same value as first and include in a list
+            connected = list(filter(lambda x: coorvalue(x) == coorvalue(coor), neighbors))
+            for i in connected:
+                result.append(i)  # if same value, then add to result list
+                neighbor(i)  # and also check the neighbor of it (recursion)
+
+    neighbor(first)  # start the recursion
+
+    return second in result
 
 
-# if __name__ == '__main__':
-#     assert can_pass(((0, 0, 0, 0, 0, 0),
-#                      (0, 2, 2, 2, 3, 2),
-#                      (0, 2, 0, 0, 0, 2),
-#                      (0, 2, 0, 2, 0, 2),
-#                      (0, 2, 2, 2, 0, 2),
-#                      (0, 0, 0, 0, 0, 2),
-#                      (2, 2, 2, 2, 2, 2),),
-#                     (3, 2), (0, 5)) == True, 'First example'
-#     assert can_pass(((0, 0, 0, 0, 0, 0),
-#                      (0, 2, 2, 2, 3, 2),
-#                      (0, 2, 0, 0, 0, 2),
-#                      (0, 2, 0, 2, 0, 2),
-#                      (0, 2, 2, 2, 0, 2),
-#                      (0, 0, 0, 0, 0, 2),
-#                      (2, 2, 2, 2, 2, 2),),
-#                     (3, 3), (6, 0)) == False, 'First example'
-
-matrix = ((0, 0, 0, 0, 0, 0),
-          (0, 2, 2, 2, 3, 2),
-          (0, 2, 0, 0, 0, 2),
-          (0, 2, 0, 2, 0, 2),
-          (0, 2, 2, 2, 0, 2),
-          (0, 0, 0, 0, 0, 2),
-          (2, 2, 2, 2, 2, 2),)
-first = (3, 2)
-second = (0, 5)
-
-print(can_pass(matrix,first,second))
+if __name__ == '__main__':
+    assert can_pass(((0, 0, 0, 0, 0, 0),
+                     (0, 2, 2, 2, 3, 2),
+                     (0, 2, 0, 0, 0, 2),
+                     (0, 2, 0, 2, 0, 2),
+                     (0, 2, 2, 2, 0, 2),
+                     (0, 0, 0, 0, 0, 2),
+                     (2, 2, 2, 2, 2, 2),),
+                    (3, 2), (0, 5)) == True, 'First example'
+    assert can_pass(((0, 0, 0, 0, 0, 0),
+                     (0, 2, 2, 2, 3, 2),
+                     (0, 2, 0, 0, 0, 2),
+                     (0, 2, 0, 2, 0, 2),
+                     (0, 2, 2, 2, 0, 2),
+                     (0, 0, 0, 0, 0, 2),
+                     (2, 2, 2, 2, 2, 2),),
+                    (3, 3), (6, 0)) == False, 'First example'
+    print('done')
 
