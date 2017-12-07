92235228598ad9e8e04fc2bd57615a72ec82b5c0
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2017-12-07 16:39:29 -0500

    Update all_identical_list_items.py

--- ZSimpleLearnings/all_identical_list_items.py
+++ ZSimpleLearnings/all_identical_list_items.py
@@ -9,18 +9,25 @@ xlist.count(xlist[0]) == len(xlist)
 # solution 2: 检查list中的每一个元素是否与第一个元素相等
 all(i==xlist[0] for i in xlist)
 
+# solution 3: 检查set(list)的长度是否等于1, won't work in empty list.
+lent(set(xlist)) == 1
+
 
 # 利用以上方法
 # 假设一个matrix中含有很多list,写一个函数: 检查是否其中一个list中存在所有元素相等的情况
 
-def verify(matrix):
+def verify1(matrix):
     return any(xlist.count(xlist[0]) == len(xlist) for xlist in matrix)
 
 def verify2(matrix):
     return any(all(i==xlist[0] for i in xlist) for xlist in matrix)
 
+def verify3(matrix):
+    return any(len(set(i))==1 for i in matrix)
+
 target = [[1, 2, 1, 1], [1, 1, 1, 1], [1, 3, 1, 6], [1, 7, 2, 5]]
-print(verify(target))
+print(verify1(target))
 print(verify2(target))
+print(verify3(target))
 
 # 注意使用all()和any(),配合list comprehension
