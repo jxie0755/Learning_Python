1c87f1ac3d48fb8a09721cec35d60169e7184992
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2017-12-12 11:01:48 -0500

    Update reversed.py

--- ZSimpleLearnings/reversed.py
+++ ZSimpleLearnings/reversed.py
@@ -1,22 +1,25 @@
+# reversed()
+# list()
+
+# reversed () generate an iterator
+
 # for string
 seqString = 'Python'
-print(list(seqString))
 print(list(reversed(seqString)))
-
-print()
+>>>
+['n', 'o', 'h', 't', 'y', 'P']
 
 # for tuple
 seqTuple = ('P', 'y', 't', 'h', 'o', 'n')
 print(list(reversed(seqTuple)))
-
-print()
+['n', 'o', 'h', 't', 'y', 'P']
 
 # for range
 seqRange = range(5, 9)
 print(list(reversed(seqRange)))
-
-print()
+[8, 7, 6, 5]
 
 # for list
 seqList = [1, 2, 4, 3, 5]
 print(list(reversed(seqList)))
+[5, 3, 4, 2, 1]
