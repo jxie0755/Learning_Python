726660d2ac4764948ef223350e741a3946e07501
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-11 22:01:51 -0500

    Checkio dropbox p3

---
+++ Checkio/dropbox/p3_the_end_of_other.py
@@ -0,0 +1,25 @@
+# In this task, you are given a set of words in lower case.
+# Check whether there is a pair of words, such that one word is the end of another (a suffix of another).
+# For example: {"hi", "hello", "lo"} -- "lo" is the end of "hello", so the result is True.
+
+# Input: Words as a set of strings.
+# Output: True or False, as a boolean.
+
+import itertools
+def checkio(words_set):
+    # get sub group of two words in combination
+    subgroup = [sorted([x, y], key=len) for x, y in itertools.combinations(words_set, 2)]
+
+    # check each sub group, if first item is a suffix of the second
+    for i in subgroup:
+        if i[1].endswith(i[0]):  # use string methods!!
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
