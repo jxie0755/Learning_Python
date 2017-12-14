10637bd2bc89c5ca434ff00f1708df5a9baac1b3
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-13 22:14:23 -0500

    Checkio incinerator p2

---
+++ Checkio/incinerator/p2_binary_count.py
@@ -0,0 +1,17 @@
+# You are given a number (a positive integer).
+# You should convert it to the binary format and count how many unities (1) are in the number spelling.
+# For example: 5 = 0b101 contains two unities, so the answer is 2.
+
+# Input: A number as a positive integer.
+# Output: The quantity of unities in the binary form as an integer.
+
+def checkio(number):
+    return str(bin(number)).count('1')
+
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
+    print('done')
+
