486f1b137777176c887739a3d49e026731e6a178
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-15 21:15:21 -0500

    Checkio mines p3

---
+++ Checkio/mine/p3_flatten_a_list.py
@@ -0,0 +1,19 @@
+# your code should be shorter than 140 characters (with whitespaces).
+# Input data: A nested list with integers.
+# Output data: The one-dimensional list with integers.
+
+def flat_list(a):
+    t = []
+    for i in a:
+        if type(i) == list:
+            t = t + flat_list(i)
+        else:
+            t.append(i)
+    return t
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
