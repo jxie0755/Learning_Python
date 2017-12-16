07b5adb8977cb1b276c883e48abe8898686f3f15
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-15 21:14:49 -0500

    Checkio mines p2

---
+++ Checkio/mine/p2_skew_symmetric_matrix.py
@@ -0,0 +1,26 @@
+# A = (aij) then the symmetric condition becomes aij = −aji.
+# Input: A square matrix as a list of lists with integers.
+# Output: If the matrix is skew-symmetric or not as a boolean.
+
+def checkio(matrix):
+    index = len(matrix)
+    for i in range(index):
+        for j in range(index):
+            if matrix[i][j] != -matrix[j][i]:
+                return False
+    return True
+
+if __name__ == '__main__':
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]) == True, "1st example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 1, 1],
+        [-2, -1, 0]]) == False, "2nd example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-3, -1, 0]]) == False, "3rd example"
+    print('done')
