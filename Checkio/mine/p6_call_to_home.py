ce3faf117a7a891e8504d82a8390eeb9959a1f56
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-17 22:48:36 -0500

    Checkio mine p6

--- Checkio/mine/p6_call_to_home.py
+++ Checkio/mine/p6_call_to_home.py
@@ -25,39 +25,48 @@ def total_cost(calls):
     # grouby 'date'
     lstg = itertools.groupby(dataset, key=lambda x: x['date'])
 
-    for k, g in lstg:
-        print(list(g))
-
     def dayprice(x):
-        "calculate the price by 1 day"
+        """"calculate the price by 1 day"""
+        minutes = list(map(lambda y:int(y['minutes']), x))
+        billable = []
+        for i in minutes:
+            if i % 60 != 0:
+                minutecount = i // 60 + 1
+            else:
+                minutecount = i // 60
+            billable.append(minutecount)
+        if sum(billable) > 100:
+            return 100 + (sum(billable) - 100) * 2
+        else:
+            return sum(billable)
+
+    final = 0
+    for k, g in lstg:
+        final += dayprice(list(g))
+
+    return final
 
 
-
-    return 0
-
-
-
-
-
-# if __name__ == '__main__':
-#     assert total_cost(("2014-01-01 01:12:13 181",
-#                        "2014-01-02 20:11:10 600",
-#                        "2014-01-03 01:12:13 6009",
-#                        "2014-01-03 12:13:55 200")) == 124, "Base example"
-#     assert total_cost(("2014-02-05 01:00:00 1",
-#                        "2014-02-05 02:00:00 1",
-#                        "2014-02-05 03:00:00 1",
-#                        "2014-02-05 04:00:00 1")) == 4, "Short calls but money..."
-#     assert total_cost(("2014-02-05 01:00:00 60",
-#                        "2014-02-05 02:00:00 60",
-#                        "2014-02-05 03:00:00 60",
-#                        "2014-02-05 04:00:00 6000")) == 106, "Precise calls"
+if __name__ == '__main__':
+    assert total_cost(("2014-01-01 01:12:13 181",
+                       "2014-01-02 20:11:10 600",
+                       "2014-01-03 01:12:13 6009",
+                       "2014-01-03 12:13:55 200")) == 124, "Base example"
+    assert total_cost(("2014-02-05 01:00:00 1",
+                       "2014-02-05 02:00:00 1",
+                       "2014-02-05 03:00:00 1",
+                       "2014-02-05 04:00:00 1")) == 4, "Short calls but money..."
+    assert total_cost(("2014-02-05 01:00:00 60",
+                       "2014-02-05 02:00:00 60",
+                       "2014-02-05 03:00:00 60",
+                       "2014-02-05 04:00:00 6000")) == 106, "Precise calls"
+    print('done')
 
 
-print(total_cost(("2014-01-01 01:12:13 181",
-                  "2014-01-02 20:11:10 600",
-                  "2014-01-03 01:12:13 6009",
-                  "2014-01-03 12:13:55 200")))
+# print(total_cost(("2014-01-01 01:12:13 181",
+#                   "2014-01-02 20:11:10 600",
+#                   "2014-01-03 01:12:13 6009",
+#                   "2014-01-03 12:13:55 200")))
 
 
 
