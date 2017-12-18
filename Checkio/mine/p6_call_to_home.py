825918feeb28db670ba3a99f5246dcd95aefa798
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-17 22:20:34 -0500

    Checkio mine p6

--- Checkio/mine/p6_call_to_home.py
+++ Checkio/mine/p6_call_to_home.py
@@ -13,3 +13,62 @@
 # Input: Information about calls as a tuple of strings.
 # Output: The total cost as an integer.
 
+import itertools
+def total_cost(calls):
+    # create dataset as a list of dictionary for groupby
+    dataset = []
+    for i in calls:
+        temp = {'date': i[0:10], 'minutes': i[20:]}
+        dataset.append(temp)
+    # sort first to avoid accidents
+    dataset.sort(key=lambda x:x['date'])
+    # grouby 'date'
+    lstg = itertools.groupby(dataset, key=lambda x: x['date'])
+
+    for k, g in lstg:
+        print(list(g))
+
+    def dayprice(x):
+        "calculate the price by 1 day"
+
+
+
+    return 0
+
+
+
+
+
+# if __name__ == '__main__':
+#     assert total_cost(("2014-01-01 01:12:13 181",
+#                        "2014-01-02 20:11:10 600",
+#                        "2014-01-03 01:12:13 6009",
+#                        "2014-01-03 12:13:55 200")) == 124, "Base example"
+#     assert total_cost(("2014-02-05 01:00:00 1",
+#                        "2014-02-05 02:00:00 1",
+#                        "2014-02-05 03:00:00 1",
+#                        "2014-02-05 04:00:00 1")) == 4, "Short calls but money..."
+#     assert total_cost(("2014-02-05 01:00:00 60",
+#                        "2014-02-05 02:00:00 60",
+#                        "2014-02-05 03:00:00 60",
+#                        "2014-02-05 04:00:00 6000")) == 106, "Precise calls"
+
+
+print(total_cost(("2014-01-01 01:12:13 181",
+                  "2014-01-02 20:11:10 600",
+                  "2014-01-03 01:12:13 6009",
+                  "2014-01-03 12:13:55 200")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
