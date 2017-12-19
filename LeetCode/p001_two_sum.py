14421efb95d07059ec1bb4d52e047a27d27cbbb4
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-18 23:11:03 -0500

    LeetCode p001

---
+++ LeetCode/p001_two_sum.py
@@ -0,0 +1,27 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in sorted(nums):
+            if target - i in nums:
+                result.append(i)
+                result.append(target - i)
+            break
+        output = []
+        for j in result:
+            output.append(nums.index(j))
+            nums[nums.index(j)] = 'x'
+        return output
+
+
+a = [2, 7, 11, 15]
+test = Solution()
+print(test.twoSum(a, 9))
