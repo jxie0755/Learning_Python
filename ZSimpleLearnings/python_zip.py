cdf6c1d6b4abfcd5500b4c8f62324cb5557cbab5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2017-12-21 15:57:30 -0500

    Create python_zip.py

---
+++ ZSimpleLearnings/python_zip.py
@@ -0,0 +1,24 @@
+# zip function, connects the items, but do not return anything until use a data structure to it.
+# only pair the least item numbers in all list.
+
+L1 = [1, 28, 36, 99]
+L2 = [2, 57, 9]
+L3 = [3, 39, 77]
+
+li = list(zip(L1, L2))
+print(li)
+
+di = dict(zip(L1, L2)) # must be two arguments
+print(di)
+
+tu = tuple(zip(L1, L2))
+print(tu)
+
+tu2 = tuple(zip(L1, L2, L3))
+print(tu2)
+
+>>>
+[(1, 2), (28, 57), (36, 9)]
+{1: 2, 28: 57, 36: 9}
+((1, 2), (28, 57), (36, 9))
+((1, 2, 3), (28, 57, 39), (36, 9, 77))
