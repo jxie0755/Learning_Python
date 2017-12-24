6d7a02a871635610e10db67078dbfa08e5dce155
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-24 13:46:00 -0500

    Learn python module collections

---
+++ Module/learn_collections.py
@@ -0,0 +1,33 @@
+import collections
+
+print()
+print('collections.ChainMap(*maps)')
+# 一个 ChainMap组合多个字典或其它映射集创建一个单一且可更新的视图。如果没有指定 maps ，那么将会用一个空字典作为新chain里面的一个mapping
+# 基础的mappings被存储在一个列表当中。这个列表是公开的，可以用 maps 属性访问和更新。没有其它的情况。
+
+# 用于将多个map组合到一起 (把map串联到一起)
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}  # 注意这里'b'为重复的key
+chain = collections.ChainMap(dict1, dict2)
+print(chain)  # >>> ChainMap({'a': 1, 'b': 2}, {'b': 3, 'c': 4})
+print(chain.maps)  # >>> [{'a': 1, 'b': 2}, {'b': 3, 'c': 4}]
+
+# 如果只是单纯组合, 可以等同于
+chainmap = [dict1, dict2]
+print(chainmap == chain.maps)  # >>> True
+
+# 但是这个chain的优点是可以输出各种key和value,因为chain object不是单纯的一个list
+print(list(chain.keys()))  # >>> ['b', 'a', 'c'] 过滤掉重复的key
+print(list(chain.values()))  # >>> [2, 1, 4]  注意这里3没有被记入,要遵循先入为主的原则
+
+# 访问这个chain object可以取出各key的value
+print(chain['b'])  # >>> 2
+print(chain.get('b'))  # >>> 2
+
+# 更改mapping
+chain['b'] = 99
+print(chain.maps)  # >>> [{'a': 1, 'b': 99}, {'b': 3, 'c': 4}] 只更改第一个值
+del chain['b']
+print(chain.maps)  # >>> [{'a': 1}, {'b': 3, 'c': 4}] 删除了第一个值以后,后面的key开始起作用
+print(chain['b'])  # >>> 3
+print(chain.maps[0], ',', chain.maps[1]) # >>> {'a': 1} , {'b': 3, 'c': 4}
