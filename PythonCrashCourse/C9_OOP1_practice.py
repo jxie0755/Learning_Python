cdc2e818e0efdcefdf2cae2240c7e7d90d4f591b
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-28 15:37:22 -0500

    OOP practice 9.1

---
+++ PythonCrashCourse/C9_OOP1_practice.py
@@ -0,0 +1,27 @@
+# 9-1 餐馆 ：创建一个名为Restaurant 的类，其方法__init__() 设置两个属性：restaurant_name 和cuisine_type 。
+# 创建一个名 为describe_restaurant() 的方法和一个名为open_restaurant() 的方法，其中前者打印前述两项信息，而后者打印一条消息，指出餐馆正在营业。
+# 根据这个类创建一个名为restaurant 的实例，分别打印其两个属性，再调用前述两个方法。
+
+class Restaurant():
+    def __init__(self, name, type):
+        self.restaurant_name = name
+        self.cuisine_type = type
+
+    def describe_restaurant(self):
+        print(f'{self.restaurant_name} serves {self.cuisine_type} food')
+
+    def open_restaurant(self):
+        print(f'{self.restaurant_name} is now opened for business!')
+
+elgalloblanco = Restaurant('El Gallo Blanco', 'Mexican')
+teressa = Restaurant("Teressa's Cafe", 'Italian')
+
+print(elgalloblanco.restaurant_name)  # >>> El Gallo Blanco
+print(teressa.cuisine_type)  # >>> Italian
+
+elgalloblanco.describe_restaurant()  # >>> El Gallo Blanco serves Mexican food
+teressa.open_restaurant()  # >>> Teressa's Cafe is now opened for business!
+
+
+
+
