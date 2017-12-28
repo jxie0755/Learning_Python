a2191c6e051eeb57f3bc5fb30c94dca77e39591a
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-28 11:09:34 -0500

    Dictionary order need to be considered

--- LeetCode/p012_integer_to_roman.py
+++ LeetCode/p012_integer_to_roman.py
@@ -49,7 +49,8 @@ class Solution(object):
         result = ''
         roman_list = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
                       (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
-                      (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+                      (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
+                      (1, 'I')
                       ]
         for arabic, roman in roman_list:
             repeat, num = divmod(num, arabic)
