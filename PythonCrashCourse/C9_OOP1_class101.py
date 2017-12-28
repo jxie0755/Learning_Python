99fa57b5e0e76f45da8b6dc8c17d3a70b0a4491f
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-28 15:27:36 -0500

    update

--- PythonCrashCourse/C9_OOP1_class101.py
+++ PythonCrashCourse/C9_OOP1_class101.py
@@ -3,9 +3,10 @@
 class Dog():
     '''a simple attemp to model a dog'''
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, doggy):
         self.dog_name = name
         self.dog_age = age
+        self.__doggy = doggy  # attribute前加双__使得属性变为私有,外部无法访问
 
     def sit(self):
         print(self.dog_name.title() + ' is now sitting')
@@ -13,7 +14,10 @@ class Dog():
     def roll_over(self):
         print(self.dog_name.title() + ' rolled over!')
 
-myDog = Dog('didi', 3)
+    def doggystyle(self):
+        print('doggy is', self.__doggy)
+
+myDog = Dog('didi', 3, '123')
 print('Dog name is ' + myDog.dog_name.title() +
       '. Dog age is ' + str(myDog.dog_age))
 
@@ -23,4 +27,10 @@ myDog.roll_over() # 等同于 Dog.roll_over(myDog)
 # 两者逻辑不同,前者在于面向对象,后者在面向过程, 还是推荐前者
 # 请教STOF网友,得到结论,第二种方式不符合OOP精神.在面对无数对象时,需要花太多时间去确定object的类型.
 
-print(myDog.name())
+print(myDog.dog_name)  # >>> didi
+print(myDog.dog_age)   # >>> 3
+# print(myDog.__doggy)  # 此处会报错,因为__doggy无法直接被调用
+print(myDog.doggystyle())  # >>> doggy is 123  # 而类方法调用__doggy则可输出结果
+
+print(dir(myDog))
+
