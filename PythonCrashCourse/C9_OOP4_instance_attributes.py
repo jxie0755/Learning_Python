fd2f2263501b69d5d9ebb23d10390fa34c70ff89
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-30 23:36:35 -0500

    OOP update

--- PythonCrashCourse/C9_OOP4_instance_attributes.py
+++ PythonCrashCourse/C9_OOP4_instance_attributes.py
@@ -16,45 +16,16 @@ class Car():
         description = str(self.year) + ' ' + self.make + ' ' + self.model + ' ' + self.type
         return description
 
-    def read_odometer(self):
-        print('The car has been driven: ' + str(self.odometer) + ' miles')
-        return self.odometer
-
-    def update_odometer(self, mileage):
-        if mileage > self.odometer:
-            self.odometer = mileage
-        else:
-            print('you can\'t roll an odometer back!')
-        return Car.read_odometer(self)
-
-    def increment_odometer(self, add_mileage):
-        if add_mileage > 0:
-            self.odometer += add_mileage
-            print(add_mileage, 'miles has been added to the odometer')
-        else:
-            print('what are you trying to do? \nYou think I am stupid?')
-
-    def fill_tank(self, volume):
-        if volume <= self.tank_size:
-            print('Fill up the gas', volume, 'gallons')
-        else:
-            print("You can't fill up the tank more than it can take!")
-
 class ElectricCar(Car):
     def __init__(self, make, model, year, type='sports car'):
         super().__init__(make, model, year, type)
         self.odometer = 0
-        self.battery = Battery()
+        self.battery = Battery()  # 将Battery类的一个实例Battery()做为电动车的一个属性
 
     # special attributes
     def battery(self):
         print('battery size is ' + str(self.battery_size))
         return self.battery_size
-
-    # 如果父类的方法对于子类不再合适,那么可以进行重写来覆盖
-    def fill_tank(self, volume):
-        print('Electric car does not have a gas tank!')
-        # 以上方法如果被删除,则会转而执行父类的相同名称的方法
 
 class Battery():
     # 独立battery成为一个类
@@ -72,17 +43,16 @@ class Battery():
 
 my_car = Car('Audi', 'S4', 2016)
 print(my_car.get_car_info())
-my_car.read_odometer()
-my_car.fill_tank(40)
 
 print()
 
 my_tesla = ElectricCar('Tesla', 'Model S', 2017)
-my_tesla.get_car_info()
+print(my_tesla.get_car_info())
+
+print()
+
 my_tesla.battery.battery_info()
-# 注意这里,BBBattery是来自my_tesla的self.BBBattery属性,而不是Battery这个class的名字!!
-my_tesla.read_odometer()
-my_tesla.fill_tank(40)
+# 注意这里,battery是来自my_tesla的self.battery属性,而不是Battery这个class的名字!!
 my_tesla.battery.get_range()
 
 
