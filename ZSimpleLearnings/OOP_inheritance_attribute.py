f51def8cdc3f33290bf57abb0179690516aab64b
jxie0755 <jxie0755@users.noreply.github.com>
2017-12-30 20:14:47 -0500

    Learning OOP inheritance

--- ZSimpleLearnings/OOP_inheritance_attribute.py
+++ ZSimpleLearnings/OOP_inheritance_attribute.py
@@ -34,34 +34,33 @@ print(sub1.E)  # >>> 隐性E what?   # 隐性默认值也是同上行为
 print(sub1.F)  # >>> Y1      # 新属性 f 不受干扰
 
 print()
-# 缺点就是默认值无法被更改
-# 即使class中强制新默认值,而且super()不包含,然后实例中强行设定,三者齐下也不行
+# 一个办法就是子类__init__和super()中都加入显性默认值的变量
 class SubC2(SuperC):
-    def __init__(self, a, b, c, f, d='DDDD', e='EEEE'):
-        super().__init__(a, b, c)
+    def __init__(self, a, b, c, f, d='new d2'):  # 此处无法强制
+        super().__init__(a, b, c, d)  # 此处不能放置e,因为e不属于父类__init()方法创建时必须输入的参数
         self.F = f
 
-sub2 = SubC2('A2', 'B2', 'C2', d='new d', e='new e', f='Y2')
+sub2 = SubC2('A2', 'B2', 'C2', 'Y2', d='new d22')
 print(sub2.A)  # >>> A2  # Ａ, B, C被继承而且数值被刷新
 print(sub2.B)  # >>> B2
 print(sub2.C)  # >>> C2
-print(sub2.D)  # >>> 显性D what?   # 虽然设定新D,但是却仍然继承父类数值
-print(sub2.E)  # >>> 隐性E what?   # 虽然设定新E,但是却仍然继承父类数值
+print(sub2.D)  # >>> new d22     # 显性默认值不再强制父类数值,而是跟随新默认值,或者新实例设定值
+print(sub2.E)  # >>> 隐性E what?  # 隐性默认值继续继承,而且无法被更改
 print(sub2.F)  # >>> Y2
 
 print()
-# 唯一的方法就是在__init__()创建新属性self.变量
+# 另一个的方法就是在__init__()创建新属性self.变量
 class SubC3(SuperC):
-    def __init__(self, a, b, c, f, d='DDDD', e='EEEE'):
-        super().__init__(a, b, c)
-        self.D = d  # 新属性D设定
-        self.E = e  # 新属性E设定
+    def __init__(self, a, b, c, f, d='new D', e='new E'):
+        super().__init__(a, b, c)  # 即使继续选择不继承d和e,但是下面写入新设定
+        self.D = d  # 新属性D设定, 覆盖父类数值
+        self.E = e  # 新属性E设定, 覆盖父类数值
         self.F = f
 
-sub3 = SubC3('A3', 'B3', 'C3', d='D3', e='E3', f='Y3')
+sub3 = SubC3('A3', 'B3', 'C3', 'Y3', 'new D again', 'new E again')  # 此处完美继承
 print(sub3.A)  # >>> A3  # Ａ, B, C被继承而且数值被刷新
 print(sub3.B)  # >>> B3
 print(sub3.C)  # >>> C3
-print(sub3.D)  # >>> D3  # 虽然设定新D和E,但是却仍然继承
+print(sub3.D)  # >>> D3  # D和E都可以跟随新默认值,或者新实例设定值
 print(sub3.E)  # >>> E3
 print(sub3.F)  # >>> Y3
