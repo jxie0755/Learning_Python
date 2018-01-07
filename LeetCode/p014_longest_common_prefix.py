08484d509804d1ec08c273251a82ba30d7f1df13
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-07 15:34:40 -0500

    LeetCode p014 added deque method, but not fast

--- LeetCode/p014_longest_common_prefix.py
+++ LeetCode/p014_longest_common_prefix.py
@@ -9,21 +9,35 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0:
-            return ''
         result = ''
+        if len(strs) == 0:
+            return result
+
         for i in range(len(min(strs, key=len))):
             temp = []
             for j in range(len(strs)):
                 temp.append(strs[j][i])
-            print(temp)
             if len(set(temp)) == 1:
                 result += temp[0]
             else:
                 break
         return result
 
+    def longestCommonPrefix2(self, strs):  # beats 8.17%
+        result = ''
+        if len(strs) == 0:
+            return result
+        from collections import deque  # 利用deque的maxlen特性
+        for i in range(len(min(strs, key=len))):
+            temp = list(map(lambda x: deque(x[i], 1), strs))
+            if temp.count(temp[0]) == len(temp):
+                result += temp[0][0]
+            else:
+                break
+        return result
 
-lst = ['Denis Xie', 'Dennis X', 'Dendi Den', 'Deln']
-lst2 = ["aca", "cba"]
-print(Solution().longestCommonPrefix(lst2))  # >>> Den
+
+lst = ['Denis Xie', 'Dennis X', 'Dendi Den', 'Denn']
+lst2 = []
+print(Solution().longestCommonPrefix(lst))  # >>> Den
+print(Solution().longestCommonPrefix2(lst2))  # >>> Den
