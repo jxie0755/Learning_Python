f34506041d659b62e0394feda4395efde41c0ac4
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-08 22:42:41 -0500

    LeetCode p026

--- LeetCode/p026_remove_duplicates_from_sorted_array.py
+++ LeetCode/p026_remove_duplicates_from_sorted_array.py
@@ -5,16 +5,25 @@
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 # 意思是不能再造一个新list,而是在原list上改动
 
-
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeDuplicates(self, nums):  # beats 83.31%
         """
         :type nums: List[int]
         :rtype: int
         """
-        return len(nums)
+        if not nums:
+            return 0
 
-nums = [1,1,2]
+        index = 1  # The index where the character needs to be placed
+        start = 0  # The index of repeating characters
+        for i in range(1, len(nums)):
+            if nums[start] != nums[i]:
+                nums[index] = nums[i]
+                index += 1
+                start = i
+        return index
+
+nums = [1,1,2,2,3]
 print(Solution().removeDuplicates(nums))
 
 
