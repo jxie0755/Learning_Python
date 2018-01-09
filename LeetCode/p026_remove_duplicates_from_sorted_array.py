62fcfbe83b3068c4fe98105f59ec302e5a464274
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-08 22:11:53 -0500

    LeetCode p026

--- LeetCode/p026_remove_duplicates_from_sorted_array.py
+++ LeetCode/p026_remove_duplicates_from_sorted_array.py
@@ -5,11 +5,16 @@
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 # 意思是不能再造一个新list,而是在原list上改动
 
-class Solution:
+
+class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        return len(nums)
+
+nums = [1,1,2]
+print(Solution().removeDuplicates(nums))
 
 
