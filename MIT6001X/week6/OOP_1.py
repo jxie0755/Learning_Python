1798ed9779dc80c213853692a960d7f5dea04983
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-13 11:21:17 -0500

    MIT6001X Learning OOP

--- MIT6001X/week6/OOP_1.py
+++ MIT6001X/week6/OOP_1.py
@@ -2,14 +2,30 @@ class Coordinate(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def distance(self, other):
         x_diff_sq = (self.x - other.x)**2
         y_diff_sq = (self.y - other.y)**2
         return (x_diff_sq + y_diff_sq)**0.5
 
+    def __str__(self):
+        return f'<{self.x},{self.y}>'
+
+    def __sub__(self, other):      # this is actually from operator module!!
+        return Coordinate(self.x - other.x, self.y - other.y)
+
 c = Coordinate(3,4)
 print(c.x, c.y)  # >>> 3 4
 origin = Coordinate(0,0)
+
+# if using instance to call a method, then self is already the instance, so only need one paramenter
 print(c.distance(origin))  # >>> 5.0
 
+# if use a class method, then you must provide self as an parament
+print(Coordinate.distance(c, origin))  # >>> 5.0
 
+print(c)  # >>> <3,4>
+print(isinstance(c, Coordinate))  # >>> True
+
+c2 = Coordinate(1,1)
+print(c-c2) # >>> <2,3>
