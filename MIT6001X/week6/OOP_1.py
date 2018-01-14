e54c0a4fe993915eb1df9bf81e5aa46e36476359
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-13 23:21:50 -0500

    MIT 6.00.1x Learning OOP

--- MIT6001X/week6/OOP_1.py
+++ MIT6001X/week6/OOP_1.py
@@ -83,7 +83,7 @@ class Intset(object):
                 setlst.append(i)
         self.data = setlst
     def __str__(self):
-        return "{" + str(self.data)[1:-1] + "}"
+        return "{" + str(sorted(self.data))[1:-1] + "}"
 
     def insert(self, elm):
         if elm not in self.data:
