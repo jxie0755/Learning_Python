e5e46543823c187c88f1e2aae75ebf98a6d6c210
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-13 22:48:41 -0500

    MIT 6.00.1x Learning OOP

--- MIT6001X/week6/OOP_1.py
+++ MIT6001X/week6/OOP_1.py
@@ -31,3 +31,33 @@ print(isinstance(c, Coordinate))  # >>> True
 c2 = Coordinate(1,1)
 print(c-c2) # >>> <2,3>
 
+
+# create a new type as a fraction type of number
+# 分子Numerator and 分母denominator
+class Fraction(object):
+    def __init__(self, numer, denom):
+        self.numer = numer
+        self.denom = denom
+    def __str__(self):
+        return f'{self.numer} / {self.denom}'
+
+    def getNumer(self):
+        return self.numer
+    def getDenom(self):
+        return self.denom
+
+oneHalf = Fraction(1,2)
+twoThird = Fraction(2,3)
+print(f'{oneHalf}, {twoThird}')  # >>> 1 / 2, 2 / 3
+
+
+
+
+
+
+
+
+
+
+
+
