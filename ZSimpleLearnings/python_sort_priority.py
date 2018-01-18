f61dea3f8598b32d4d22ae1bc4f505eb490b586b
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-17 22:44:50 -0500

    Simple Learning on sort() by __lt__ and __gt__

--- ZSimpleLearnings/python_sort_priority.py
+++ ZSimpleLearnings/python_sort_priority.py
@@ -1,3 +1,5 @@
+# why python __lt__ has higher priority than __gt__?
+
 class Person(object):
     id = 0
     def __init__(self, name):
@@ -8,38 +10,32 @@ class Person(object):
     def __str__(self):
         return self.name
 
-    def __gt__(self, other):
-        return self.id > other.id
-
-
-p1 = Person('AAA')
-p2 = Person('BBB')
-p3 = Person('CCC')
-p4 = Person('DDD')
-
-lst = [p2, p3, p1, p4]
-lst.sort()
-print([i.__str__() for i in lst])  # >>> ['AAA', 'BBB', 'CCC', 'DDD']
-
+    def __lt__(self, other):
+        return self.id < other.id
 
 class SubPerson(Person):
     def __init__(self, name):
         Person.__init__(self, name)
     def __gt__(self, other):
-        return self.id > other.id
+        return self.name > other.name
 
-s1 = SubPerson('WWW'); s1.id = 14
-s2 = SubPerson('XXX'); s2.id = 12
-s3 = SubPerson('YYY'); s3.id = 11
-s4 = SubPerson('ZZZ'); s4.id = 13
-lst2 = [s2, s1, s4, s3]
-lst2.sort()
-print([i.__str__() for i in lst2])  # >>> ['YYY', 'XXX', 'ZZZ', 'WWW']
+s1 = SubPerson('AAA'); s1.id = 14
+s2 = SubPerson('BBB'); s2.id = 12
+s3 = SubPerson('CCC'); s3.id = 11
+s4 = SubPerson('DDD'); s4.id = 13
+
+lst = [s2, s1, s4, s3]
+lst.sort()
+print([i.__str__() for i in lst])  # >>> ['CCC', 'BBB', 'DDD', 'AAA']  # still sort by id in superclass
+
+# Observation:
+# __gt__ will work solo without __lt__
+# __lt__ has higher priority, even from superclass
 
 
 # https://stackoverflow.com/questions/48313301/python-sort-has-higher-priority-for-lt-than-gt?noredirect=1#48313338
-# conclusion: sort based on __lt__ only.
-# __gt__ solo is a side effect from a reflective test application:
+# conclusion: always sort based on __lt__ only, __gt__ will work solo by side-effect
+#  __gt__ solo is a side effect from a reflective test application:
 # checking the __gt__ when the __lt__ is missing in comparison
 
 
