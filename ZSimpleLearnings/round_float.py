a53664579b43250b0623073de36e7bc53902abc9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-01-18 15:13:40 -0500

    Update round_float.py

--- ZSimpleLearnings/round_float.py
+++ ZSimpleLearnings/round_float.py
@@ -3,24 +3,51 @@
 # 当指定取舍的小数点位数的时候，一般情况也是使用四舍五入的规则，
 # 但是碰到.5的这样情况，如果要取舍的位数前的小树是奇数，则直接舍弃，如果偶数这向上取舍。看下面的示例：
 
-print(round(2.635, 2))
-# >>> 2.63
-print(round(2.645, 2))
-# >>> 2.65
-print(round(2.655, 2))
-# >>> 2.65
-print(round(2.665, 2))
-# >>> 2.67
-print(round(2.675, 2))
-# >>> 2.67
+# python 四舍五入的问题
+
+# 当一个值刚好在两个边界的中间的时候， round 函数返回离它最近的偶数。
+print(round(1.5, 0))  # >>> 2.0
+print(round(2.5, 0))  # >>> 2.0
+# 也就是说，对1.5或者2.5的舍入运算都会得到2
+
+print(round(1.45, 1))  # >>> 1.4
+print(round(2.55, 1))  # >>> 2.5
+
+# 即使调用decimal也不行?
+from decimal import *
+
+decimal.getcontext().prec = 1
+print(Decimal('1.5') / Decimal('1'))  # >>> 1
+print(Decimal('2.5') / Decimal('1'))  # >>> 3
+
+decimal.getcontext().prec = 2
+print(Decimal('1.45') / Decimal('1'))  # >>> 1.4
+print(Decimal('2.55') / Decimal('1'))  # >>> 2.6
+
+print(round(Decimal('1.45'), 1))  # >>> 1.4
+print(round(Decimal('2.55'), 1))  # >>> 2.6
+
+print('{:.1f}'.format(Decimal('1.45')))  # >>> 1.4
+print('{:.1f}'.format(Decimal('2.55')))  # >>> 2.6
+
+# 不要将舍入和格式化输出搞混淆了。 如果你的目的只是简单的输出一定宽度的数，你不需要使用 round() 函数。
+#  而仅仅只需要在格式化的时候指定精度即可。
+
+print(format(1.5, '0.0f'))  # >>> 2
+print(format(2.5, '0.0f'))  # >>> 2
+
+# 由于小数转float过程中的误差造成
+print(format(1.55, '0.1f'))  # >>> 1.6
+print(format(2.55, '0.1f'))  # >>> 2.5
 
 
-# 使用格式化
-# 效果和round（）是一样的。
+# 但是format仍然不解决四舍五入精度问题
 
-print("%.2f" % 2.635)
-# >>> '2.63'
-print("%.2f" % 2.645)
-# >>> '2.65'
-print(int(2.5))
-# >>> 2
+
+# 消除四舍五入误差请用decimal模块!!
+from decimal import *
+print(Decimal(Decimal('1.45').quantize(Decimal('.1'), rounding=ROUND_HALF_UP)))  # >>> 1.5
+print(Decimal(Decimal('2.55').quantize(Decimal('.1'), rounding=ROUND_HALF_UP)))  # >>> 2.6
+
+print(Decimal(Decimal('1.5').quantize(Decimal('1'), rounding=ROUND_HALF_UP)))  # >>> 2
+print(Decimal(Decimal('2.5').quantize(Decimal('1'), rounding=ROUND_HALF_UP)))  # >>> 3
