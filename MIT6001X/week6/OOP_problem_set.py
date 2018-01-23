3695d5614a6e907011c03f5980e3cf52aac133ea
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-22 23:04:30 -0500

    MIT 6.00.1X problem set

---
+++ MIT6001X/week6/OOP_problem_set.py
@@ -0,0 +1,13 @@
+# This assignment will deal with a well-known (though not very secure) encryption method called the Caesar cipher.
+# Some vocabulary to get you started on this problem
+
+# Encryption - the process of obscuring or encoding messages to make them unreadable until they are decrypted
+# Decryption - making encrypted messages readable again by decoding them
+# Cipher - algorithm for performing encryption and decryption
+# Plaintext - the original message
+# Ciphertext - the encrypted message.
+
+# ps6.py - a file containing three classes that you will have to implement.
+# words.txt - a file containing valid English words (should be in the same folder as your ps6.py file).
+# story.txt - a file containing an encrypted message that you will have to decode (should be in the same folder as your ps6.py file).
+
