f1f94faea5838eeee1daf5d502985664d9e18e46
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-01-24 08:32:08 -0500

    Update p001_two_sum.py

--- LeetCode/p001_two_sum.py
+++ LeetCode/p001_two_sum.py
@@ -9,7 +9,16 @@
 # rtype: List[int]
  
 class Solution(object):
-    def twoSum(self, nums, target):  # beats 1.25%
+ 			def twoSum(self, nums, target):  # brutal force, slowest
+        for i in nums:
+            index_i = nums.index(i)
+            for j in nums[index_i+1:]:
+                if i + j == target:
+                    if i != j:
+                        return [index_i, nums.index(j)]
+                    else:
+                        return [index_i, nums.index(i, index_i+1)]
+    def twoSum2(self, nums, target):  # brutal force, check half first
         half = target / 2
         if nums.count(half) == 2:
             indexHalf =nums.index(half)
@@ -20,17 +29,9 @@ class Solution(object):
                     return [nums.index(i), nums.index(target - i)]
 
 if __name__ == '__main__':
-    assert Solution().twoSum([11, 2, 7, 15], 9) == [1,2],  'regular'
-    assert Solution().twoSum([11, 7, 2, 15], 9) == [1,2],  'regular revert'
-    assert Solution().twoSum([3, 3], 6) == [0,1],  'two identical'
-    assert Solution().twoSum([-1, -2, -3, -4, -5], -8) == [2, 4],  'negative int'
-    assert Solution().twoSum([3, 2, 4], 6) == [1, 2],  'struggle with 1/2 target'
+    assert Solution().twoSum2([11, 2, 7, 15], 9) == [1,2],  'regular'
+    assert Solution().twoSum2([11, 7, 2, 15], 9) == [1,2],  'regular revert'
+    assert Solution().twoSum2([3, 3], 6) == [0,1],  'two identical'
+    assert Solution().twoSum2([-1, -2, -3, -4, -5], -8) == [2, 4],  'negative int'
+    assert Solution().twoSum2([3, 2, 4], 6) == [1, 2],  'struggle with 1/2 target'
     print('test done, all passed')
-
-if __name__ == '__main__':
-    from timeit import repeat
-    result = repeat('Solution().twoSum([11, 2, 7, 15], 9)',
-                    setup='from __main__ import Solution',
-                    repeat=3, number=1000000)
-    print(f'{sum(result)/ len(result):.4f}')
-
