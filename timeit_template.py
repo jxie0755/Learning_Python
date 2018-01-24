8ceb66d04cb61a0921fb4287905403b5e64a9372
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-23 22:59:19 -0500

    LeetCode p001

--- timeit_template.py
+++ timeit_template.py
@@ -1,10 +1,11 @@
 # this is to create a template using the timeit module
 # the codes can be used for algorithm test by calculating the time
 
-def f(x):  # power function
-    return x**2
+class Solution(object):
+    def f(x):  # power function
+        return x**2
 
 if __name__ == '__main__':
     from timeit import repeat
-    result = repeat('f(3)', setup='from __main__ import f', repeat=3, number=1000000)
+    result = repeat('Solution.f(3)', setup='from __main__ import Solution', repeat=3, number=1000000)
     print(round(sum(result)/len(result), 4))
