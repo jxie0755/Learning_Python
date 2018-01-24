9754f0baa165bd484574fb0758d7052a7232745d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-01-24 15:18:46 -0500

    Update p020_valid_parentheses.py

--- LeetCode/p020_valid_parentheses.py
+++ LeetCode/p020_valid_parentheses.py
@@ -4,22 +4,23 @@
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
+# """
+# :type s: str
+# :rtype: bool
+# """
+
 class Solution():
+    OPEN_BRACKETS = {'(': 1, '{': 2, '[': 3}
+    CLOSE_BRACKETS = {')': 1, '}': 2, ']': 3}
     def isValid(self, s):  # beats 60.51%
-        """
-        :type s: str
-        :rtype: bool
-        """
-        OPEN_BRACKETS = {'(': 1, '{': 2, '[': 3}
-        CLOSE_BRACKETS = {')': 1, '}': 2, ']': 3}
         result = []
         for i in s:
-            if i in OPEN_BRACKETS:
-                result.append(OPEN_BRACKETS[i])
-            if i in CLOSE_BRACKETS:
+            if i in Solution.OPEN_BRACKETS:
+                result.append(Solution.OPEN_BRACKETS[i])
+            if i in Solution.CLOSE_BRACKETS:
                 if len(result) == 0:
                     return False
-                if result.pop() != CLOSE_BRACKETS[i]:
+                if result.pop() != Solution.CLOSE_BRACKETS[i]:
                     return False
         return len(result) == 0
 
@@ -34,6 +35,5 @@ if __name__ == '__main__':
     assert Solution().isValid("[1+1]+(2*2)-{3/3}") == True, "Different operators"
     assert Solution().isValid("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
     assert Solution().isValid("2+3") == True, "No brackets, no problem"
-    print('all pased')
-
+    print('all passed')
     
