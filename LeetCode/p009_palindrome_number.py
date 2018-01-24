3e6e4a3d9998dd09f1947c9903c212e666c37aec
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-01-24 13:46:41 -0500

    Update p009_palindrome_number.py

--- LeetCode/p009_palindrome_number.py
+++ LeetCode/p009_palindrome_number.py
@@ -4,16 +4,29 @@
 # Determine whether an integer is a palindrome. Do this without extra space.
 # Negative integer will not be a palindrome
 
+# """
+# :type x: int
+# :rtype: bool
+# """
+
 class Solution(object):
-    def isPalindrome(self, x):  # beats 24.10%
-        """
-        :type x: int
-        :rtype: bool
-        """
+    def isPalindrome(self, x):  # string method, takes extra space
         if x < 0:
             return False
         else:
             return str(x) == str(x)[::-1]
 
-print(Solution().isPalindrome(2147447412))
-print(Solution().isPalindrome(-2147447412))
+    def isPalindrome2(self, x):  # use divmod method, no need for extra space
+        if x < 0:
+            return False
+        half_palindrome = 0
+        while x > half_palindrome:
+            half_palindrome = half_palindrome * 10 + x % 10
+            x //= 10
+            print(x, half_palindrome)
+        return x == half_palindrome
+
+if __name__ == '__main__':
+    assert Solution().isPalindrome2(2147447412) == True, 'Is palindrome'
+    assert Solution().isPalindrome2(-2147447412) == False, 'Not palindrome'
+    print('all passed')
