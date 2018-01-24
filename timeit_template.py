3412939700e17e132724dde7de13c70caa073337
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-23 22:38:09 -0500

    timeit template

---
+++ timeit_template.py
@@ -0,0 +1,10 @@
+# this is to create a template using the timeit module
+# the codes can be used for algorithm test by calculating the time
+
+def f(x):  # power function
+    return x**2
+
+if __name__ == '__main__':
+    from timeit import repeat
+    result = repeat('f(3)', setup='from __main__ import f', repeat=3, number=10000)
+    print(round(sum(result)/len(result), 4))
