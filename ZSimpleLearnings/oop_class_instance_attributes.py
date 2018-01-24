3b52da2d427623eafa53ca512b09b8fd77a52eaa
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-01-24 16:12:40 -0500

    Update oop_class_instance_attributes.py

--- ZSimpleLearnings/oop_class_instance_attributes.py
+++ ZSimpleLearnings/oop_class_instance_attributes.py
@@ -11,12 +11,12 @@ o2 = foo('o2')
 print(o1.i, o2.i)  # >>> o1 o2
 print(o1.c, o2.c)  # >>> class attribute class attribute
 
-o1.c = 'WTF'       # by doing so, we create o1.c override foo.c (become instance attributes)
+o1.c = 'WTF'       # BE CAREFUL!!! by doing so, we create o1.c override foo.c (become instance attributes)
 print(o1.c, o2.c)  # >>> WTF class attribute  # revise o1.c does not change o2.c
 print(foo.c)       # >>> class attribute      # revise o1.c does not change foo.c
 
 foo.c = 'Really?'
-print(o1.c, o2.c)  # >>> WTF really?          # only change o2.c, because o1.c got override by previous setting.
+print(o1.c, o2.c)  # >>> WTF really?          # only change o2.c, because o1.c got override by previous operation.
 
 # In generall, we do not override class attribute with instance attribute.
 # And class attributes should be used separately for systematic purpose.
