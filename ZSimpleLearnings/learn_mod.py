94c3de3bd2d0b20ceb329ee8d987fc9ac9c32c0b
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-24 21:40:09 -0500

    Simple Learning on python import command

---
+++ ZSimpleLearnings/learn_mod.py
@@ -0,0 +1,9 @@
+# this is to learn how import command will work differently
+
+var1 = 5
+def fun1():
+    return var1
+
+var2 = 5
+def fun2():
+    return var2
---
+++ ZSimpleLearnings/learn_mod_import.py
@@ -0,0 +1,26 @@
+# In learn_mod.py we define a variable and a functin to call this variable
+
+# var1 = 5
+# def fun1():
+    # return var1
+
+# var2 = 5
+# def fun2():
+    # return var2
+
+import learn_mod
+
+print(learn_mod.var1)    # >>> 5
+learn_mod.var = 10
+print(learn_mod.var1)    # >>> 10   # changed var value
+print(learn_mod.fun1())  # >>> 10   # also change the call of var by fun()
+
+from learn_mod import *
+
+print(var2)    # >>> 5
+var2 = 10
+print(var2)    # >>> 10   # changed var value
+print(fun2())  # >>> 5    # BUT does NOT change the call of var by fun()
+
+# For more details:
+# https://stackoverflow.com/questions/3536620/how-to-change-a-module-variable-from-another-module
