d959d2943708435bea29fe1f6c667881cdceec93
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-28 22:04:03 -0500

    Code snippets for flatten nested lists

---
+++ ZCodeSnippets/flatten_list.py
@@ -0,0 +1,19 @@
+# This is a useful function from Checkio/mine/p5_flatten_a_list.py
+# Function is to flatten a list that contains list, and nested to unlimited levels.
+
+def flat_list(nested_list):
+    flattened_list = []
+    for i in nested_list:
+        if type(i) == list:
+            flattened_list = flattened_list + flat_list(i)
+        else:
+            flattened_list.append(i)
+    return flattened_list
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
+
