019900cf1f3e29bf9c152ac16f74065c63e112c4
jxie0755 <jxie0755@users.noreply.github.com>
2018-01-29 23:06:43 -0500

    Code snippets for generate a list of sub-string

--- ZCodeSnippets/substring.py
+++ ZCodeSnippets/substring.py
@@ -2,63 +2,65 @@
 # different ways to get substrings from a long string
 
 # from short to long
-def sub_short_long(x):
+def substring_SL(s):
+    result = []
+    for lenth in range(1, len(s) + 1):
+        for start in range(len(s) - lenth + 1):
+            result.append(s[start:start + lenth])
+    return result
+
+if __name__ == '__main__':
+    print(substring_SL('abcd'))
+# >>> ['a', 'b', 'c', 'd', 'ab', 'bc', 'cd', 'abc', 'bcd', 'abcd']
+
+# version 2
+def substring_SL2(s):
     result = []
     n, m = 0, 0
-    index = len(x)
+    index = len(s)
     while n < index:
         for m in range(0, index):
             if 1 + m + n <= index:
-                result.append(x[m:1 + m + n])
+                result.append(s[m:1 + m + n])
         n += 1
     return result
 
 if __name__ == '__main__':
-    print(sub_short_long('abcd'))
-# >>> ['a', 'b', 'c', 'd', 'ab', 'bc', 'cd', 'abc', 'bcd', 'abcd']
-
-# version 2
-def sub_short_long2(x):
-    result = []
-    for lenth in range(1, len(x) + 1):
-        for start in range(len(x) - lenth + 1):
-            result.append(x[start:start + lenth])
-    return result
-
-if __name__ == '__main__':
-    print(sub_short_long2('abcd'))
+    print(substring_SL2('abcd'))
 # >>> ['a', 'b', 'c', 'd', 'ab', 'bc', 'cd', 'abc', 'bcd', 'abcd']
 
 
 # from long to short
-def sub_long_short(x):
+def substring_LS(s):
+    result = []
+    for lenth in range(len(s), 0, -1):
+        for start in range(len(s) - lenth + 1):
+            result.append(s[start:start + lenth])
+    return result
+
+if __name__ == '__main__':
+    print(substring_LS('abcd'))
+# >>> ['abcd', 'abc', 'bcd', 'ab', 'bc', 'cd', 'a', 'b', 'c', 'd']
+
+# version 2
+def substring_LS2(s):
     result = []
     n, m = 0, 0
-    index = len(x)
+    index = len(s)
     while n < index:
         for m in range(0, n + 1):
-            result.append(x[m:index - n + m])
+            result.append(s[m:index - n + m])
         n += 1
     return result
 
 if __name__ == '__main__':
-    print(sub_long_short('abcd'))
+    print(substring_LS2('abcd'))
 # >>> ['abcd', 'abc', 'bcd', 'ab', 'bc', 'cd', 'a', 'b', 'c', 'd']
 
-# version 2
-def sub_long_short2(x):
-    result = []
-    for lenth in range(len(x), 0, -1):
-        for start in range(len(x) - lenth + 1):
-            result.append(x[start:start + lenth])
-    return result
 
-if __name__ == '__main__':
-    print(sub_long_short2('abcd'))
-# >>> ['abcd', 'abc', 'bcd', 'ab', 'bc', 'cd', 'a', 'b', 'c', 'd']
 
 # 第三种方式找substring, 新的寻找顺序.
-def sub_seq(x):
+def substring_seq(x):
     result = []
     for start in range(len(x)):
         for end in range(start+1,len(x)+1):
@@ -66,5 +68,5 @@ def sub_seq(x):
     return result
 
 if __name__ == '__main__':
-    print(sub_seq('abcd'))
+    print(substring_seq('abcd'))
 # >>> ['a', 'ab', 'abc', 'abcd', 'b', 'bc', 'bcd', 'c', 'cd', 'd']
