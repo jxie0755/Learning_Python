a61ee44fc6234fcfa8b9013db86db14978ee5820
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-02 14:24:45 -0500

    Update remove_repeating_in_list.py

--- ZCodeSnippets/remove_repeating_in_list.py
+++ ZCodeSnippets/remove_repeating_in_list.py
@@ -1,11 +1,14 @@
 # remove repeating items in a list
 # 去除list中的重复元素
-a = ['b','c','d','b','c','a','a']
-print(set(a))  # set(a)根本就不会每次输出统一顺序
-print(list(set(a))) # 因此,list之后也不会每次输出统一顺序
 
-# 解决办法
 # sorted()函数,使用key,维持原来的顺序,a.index
-a = ['b','c','d','b','c','a','a']
-print(sorted(set(a), key=a.index))
+def remove_repeating(L):
+    return sorted(set(L), key=L.index)
 
+if __name__ == '__main__':
+  a = ['a','a','b','b','c','c','d']
+  print(remove_repeating(a))
+
+  a = ['a','a','b','b','c','c','d']
+  print(set(a))  # set(a)根本就不会每次输出统一顺序
+  print(list(set(a))) # 因此,list之后也不会每次输出统一顺序
