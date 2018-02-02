a7f1aaee19cd69cd8a28e03dd038136ab7c58487
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-02 13:16:55 -0500

    Update binary_convert.py

--- ZCodeSnippets/binary_convert.py
+++ ZCodeSnippets/binary_convert.py
@@ -1,10 +1,8 @@
-# this is to convert a binary number to decimal number
-
+# A function to convert a binary number to decimal number
 # 1011 = 1*2^3 + 0*2^2 + 1*2^1 + 1*2^0
-
-# 引入re来限制输入,只能输入0和1
-def bi_to_deci(target):
-    """target should be a string of binary numbers"""
+def bi_to_deci_0(target):
+    """target should be a binary number made with '1' and '0'"""
+    target = str(target)
     ans = 0
     numlevel = len(target)
     for index in range(0, numlevel):
@@ -13,66 +11,39 @@ def bi_to_deci(target):
     return ans
 
 if __name__ == '__main__':
-    print(bi_to_deci('1010'))  # >>> 10
+    print(bi_to_deci_0(1010))  # >>> 10
 
 # 先把target反过来,再计算就更简短
-target = target[::-1]
-ans = 0
-for i in range(len(target)):
-    ans += int(target[i]) * 2**i
-print(ans)
+def bi_to_deci(target):
+    """target should be a binary number made with '1' and '0'"""
+    target = str(target)
+    target = target[::-1]
+    ans = 0
+    for i in range(len(target)):
+        ans += int(target[i]) * 2**i
+    return ans
 
+if __name__ == '__main__':
+    print(bi_to_deci(1010))  # >>> 10
+    
 
-# this is to convert a decimal number to binary
+# A function to convert a decimal number to binary
 # 用2辗转相除至结果为1
 # 将余数和最后的1从下向上倒序写 就是结果 （逆序）
-# 例如302
-# 302/2 = 151 余0
-# 151/2 = 75 余1
-# 75/2 = 37 余1
-# 37/2 = 18 余1
-# 18/2 = 9 余0
-# 9/2 = 4 余1
-# 4/2 = 2 余0
+# 例如10
+# 10/2 = 5 余0
+# 5/2 = 2 余1
 # 2/2 = 1 余0
-# 1/2 = 0 余1, 一定要除以2结果最终到0
-# 故二进制为100101110
+# 1/2 = 0 余1  # 一定要除以2结果最终到0
+# 故二进制为1010
 
-import re
+def deci_to_bin(target):
+    """target should be an positive integer"""
+    ans = ''
+    while target != 0:
+        target, digit = divmod(target, 2)
+        ans = str(digit) + ans
+    return int(ans)
 
-while True:
-    target = input("please input a decimal number:")
-    if not re.match("^[0-9]*$", target):
-        print("Error! Only digits 0 and 1 allowed!")
-        continue
-    else:
-        break
-int_target = int(target)
-biList = []
-while int_target != 0:
-    biList.append(int_target % 2)
-    int_target = int_target // 2
-binumber = int(''.join(str(i) for i in biList[::-1]))  # 要注意join必须是string,不能是int
-print('The binary number of', target, "is", binumber)
-
-
-# MIT example, a different way of writing
-
-num = 11
-if num < 0:
-    isNeg = True
-    num = abs(num)
-else:
-    isNeg = False
-
-result = ''  # 定义一个空字符也很关键!
-if num == 0:
-    result = '0'
-while num > 0:
-    result = str(num % 2) + result  # 核心写法: 将result定义为string, 拆解字符为'1011' = '1'+'0'+'1'+'1'
-                                    # 1和0来自于余数,但是每次生成余数都被放置在之前的余数的前位
-                                    # 无形中完成了逆序排列
-    num = num // 2                  
-if isNeg:
-    result = '-' + result
-print('\nConvert to binary number:', result)
+if __name__ == '__main__':
+    print(deci_to_bin(10))  # >>> 1010
