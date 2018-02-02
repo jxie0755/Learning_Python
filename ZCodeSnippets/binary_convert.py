97ba21db869fd9ace23980a44c787a7887b2d830
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-02 13:54:56 -0500

    Update binary_convert.py

--- ZCodeSnippets/binary_convert.py
+++ ZCodeSnippets/binary_convert.py
@@ -47,3 +47,39 @@ def deci_to_bin(target):
 
 if __name__ == '__main__':
     print(deci_to_bin(10))  # >>> 1010
+    
+    
+# what if the decimal number is a float?
+def deci_float_to_bi(target):
+    """converts a decimal float number to binary float numebr
+    target should be a positive decimal float number"""
+    p = 0
+    
+    # 首先要把浮点数乘以2的p次方,变成一个整数
+    while ((2 ** p) * target) % 1 != 0:  # 很关键,这里设定必须被1整除,才是整数
+        p += 1
+    print('Magnify target 2^', p, 'times, to be an integer:')
+    num = int(target * (2 ** p))  # 这里得到一个整数num
+    print('target is now', num)
+    
+    result = ''
+    if num == 0:
+        result = '0'
+    while num > 0:
+        result = str(num % 2) + result
+        num = num // 2
+    
+    # 预防措施,万一p大于result的长度,需要补0在前方
+    for i in range(p - len(result)):
+        result = '0' + result
+    
+    
+    # 由于之前放大了target, 2^p倍,所以现在要缩小回去,除以10^p得到最终结果
+    # 这里用字符串处理,避免前置0被省略
+    result = result[0:-p] + '.' + result[-p:]
+    print('Convert target to binary number:', result)
+    
+    return float(result)
+
+if __name__ == '__main__':
+    print(deci_float_to_bi(1.123123))
