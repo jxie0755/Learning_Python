50801b5e4938c738f140a3dd4d89aa77765dce85
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-02 11:25:44 -0500

    Create restrict_input.py

---
+++ ZCodeSnippets/restrict_input.py
@@ -0,0 +1,13 @@
+# this provides a sample of how to restrict input by using re module and a while loop
+# in this case it only allows to input '0' and '1'
+
+import re
+
+while True:
+    target = input("please input a binary number:")
+    if not re.match("^[0-1]*$", target):
+        print("Error! Only digits 0 and 1 allowed!")
+        continue
+    else:
+        break
+        
