4a27f8e3d45900c5ed9e96c4f231864d37c87d46
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-02 13:30:59 -0500

    Update all_identical_list_items.py

--- ZCodeSnippets/all_identical_list_items.py
+++ ZCodeSnippets/all_identical_list_items.py
@@ -1,23 +1,28 @@
 # 检查一个list中是否所有元素都相同
 # check if all items are identical in a list
 
-xlist = [1, 1, 1, 1]
+# All following function does the same thing, check if the set contains all identical elements
+def all_identical_1(iterable):
+    return iterable.count(matrix[0]) == len(iterable)
 
-# solution 1: 检查list中的第一个元素的数量是否等于list的长度
-xlist.count(xlist[0]) == len(xlist)
+def all_identical_2(iterable):
+    return all(i==iterable[0] for i in iterable)
 
-# solution 2: 检查list中的每一个元素是否与第一个元素相等
-all(i==xlist[0] for i in xlist)
+def all_identical_3(iterable):
+    return len(set(iterable)) <= 1   
 
-# solution 3: 检查set(list)的长度是否等于1, won't work in empty list.
-lent(set(xlist)) <= 1
+def all_identical_4(iterable):  # 错位对比法
+   return iterable[1:] == iterable[:-1]  # can not be used for unslicable matrix (tuple)
 
-# solution 4: 错位对比法
-xlist[1:] == xlist[:-1]
+if __name__ == '__main__':
+    print(all_identical_3((1,1,1,1)))  # >>> True
+    print(all_identical_4([1,2,3,4,5]))  # >>> False
+    
 
 # 利用以上方法
 # 假设一个matrix中含有很多list,写一个函数: 检查是否其中一个list中存在所有元素相等的情况
 
+# """matrix is a list containing sub-lists"""
 def verify1(matrix):
     return any(xlist.count(xlist[0]) == len(xlist) for xlist in matrix)
 
@@ -30,11 +35,11 @@ def verify3(matrix):
 def verify4(matrix):  # 错位对比法
    return any(i[1:] == i[:-1] for i in matrix)
 
-
-target = [[1, 2, 1, 1], [1, 1, 1, 1], [1, 3, 1, 6], [1, 7, 2, 5]]
-print(verify1(target))
-print(verify2(target))
-print(verify3(target))
-print(verify4(target))
+if __name__ == '__main__':
+    target = [[1, 2, 1, 1], [1, 1, 1, 1], [1, 3, 1, 6], [1, 7, 2, 5]]
+    print(verify1(target))
+    print(verify2(target))
+    print(verify3(target))
+    print(verify4(target))
 
 # 注意使用all()和any(),配合list comprehension
