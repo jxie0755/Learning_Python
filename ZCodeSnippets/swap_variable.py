78c082c26d353fd4deeebdb8a5a2c2a3bbed9102
jxie0755 <jxie0755@users.noreply.github.com>
2018-02-01 22:11:25 -0500

    Code snippet for swapping two variables without third one

--- ZCodeSnippets/swap_variable.py
+++ ZCodeSnippets/swap_variable.py
@@ -7,6 +7,7 @@ b = a - b
 a = a - b
 print('a is now', a, '\nb is now', b)
 
+print()
 a = 5
 b = 7
 
@@ -15,3 +16,9 @@ b = a + b
 a = b - a
 print('a is now', a, '\nb is now', b)
 
+print()
+a = 5
+b = 7
+
+a, b = b, a   # the trick here is when at the same line, the setting in parallel
+print('a is now', a, '\nb is now', b)
