e643fe231049f668a30f4dfb44ba761eda63df48
jxie0755 <jxie0755@users.noreply.github.com>
2018-02-07 19:49:03 -0500

    LeetCode p344

---
+++ LeetCode/p344_reverse_string.py
@@ -0,0 +1,21 @@
+# p344 Reverse String
+# Easy
+
+# Write a function that takes a string as input and returns the string reversed.
+
+# """
+# :type s: str
+# :rtype: str
+# """
+
+class Solution:
+    def reverseString(self, s):
+        return s[::-1]
+
+    def reverseString(self, s):
+        return ''.join(reversed(s))
+
+
+if __name__ == '__main__':
+    assert Solution().reverseString('hello') == 'olleh'
+    print('all passed')
