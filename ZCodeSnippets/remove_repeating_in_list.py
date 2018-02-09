80d4f2a4069ecccd1e2b88ee667f332acf70bb62
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-09 16:15:18 -0500

    Update remove_repeating_in_list.py

--- ZCodeSnippets/remove_repeating_in_list.py
+++ ZCodeSnippets/remove_repeating_in_list.py
@@ -12,3 +12,7 @@ if __name__ == '__main__':
   a = ['a','a','b','b','c','c','d']
   print(set(a))  # set(a)根本就不会每次输出统一顺序
   print(list(set(a))) # 因此,list之后也不会每次输出统一顺序
+
+# 注意 sorted(iterable, *, key=None, reverse=False), will return a list!
+# 不需要list()命令, sorted(list(set(a)), key=a.index), 因为参数只要iterable即可
+# 直接 sorted(set(a), key=a.index)
