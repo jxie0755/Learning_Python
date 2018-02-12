a0644b09d2d0591cd34f1089e73b0e9d53599022
jxie0755 <jxie0755@users.noreply.github.com>
2018-02-11 23:29:58 -0500

    MIT 6.00.1X algorithmetic complexity

--- MIT6001X/week6/searching_sorting_algorithm.py
+++ MIT6001X/week6/searching_sorting_algorithm.py
@@ -216,3 +216,41 @@ if __name__ == '__main__':
     bub_Sort([1, 5, 3, 8, 4, 9, 6, 2, 7])  # >>> 13 times
     sel_Sort([1, 5, 3, 8, 4, 9, 6, 2, 7])  # >>> 13 times
     # They both swap the value same amount of times
+
+
+# Merge Sort
+def merge(left, right):  # O(n), n as the length of the longer list
+    """both left and right are sorted list """
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+    # Divide part: Break down problem into half, O(log(n))
+    # Each divide, merge O(n)
+    # Overall O(n*log(n)), better than O(n^2)
+
+if __name__ == '__main__':
+    L = [1, 5, 3, 8, 4, 9, 6, 2, 7]
+    print(merge_sort([1, 5, 3, 8, 4, 9, 6, 2, 7]))
+    print(L)  # this method takes extra space
