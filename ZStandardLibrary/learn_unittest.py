ce2967961398064e92f48a4e39a1b862ac55e988
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-02-12 13:36:11 -0500

    Update learn_unittest.py

--- ZStandardLibrary/learn_unittest.py
+++ ZStandardLibrary/learn_unittest.py
@@ -113,6 +113,64 @@ class PNumTest(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
     
+
     
+# Testing Class
+
+# Example 4, create a simple class for dogs
+class dog(object):
+    def __init__(self, name):
+        self.name = name
+        self.age = 0
+    def set_age(self,age):
+        if self.age < age:
+            self.age = age
+        else:
+            print("you can't roll back on ages")
+        
+
+dog1 = dog('dog one')
+print(dog1.age)  # >> 0
+dog1.set_age(3)
+print(dog1.age)  # >>> 3
+dog1.set_age(1)  # >>> you can't roll back on age
+print(dog1.age)  # >>> 3
+
+class TestDog(unittest.TestCase):
+    def test_dog_1(self): # single case
+        dog_t1 = dog('T1')
+        dog_t1.set_age(5)
+        self.assertEqual(dog_t1.age, 5)
+    def test_dog_2(self): # multiple test on names
+        dog_t2 = dog('T2')
+        dog_ages = [4, 5, 3]
+        for i in dog_ages:
+            dog_t2.set_age(i)
+            self.assertEqual(dog_t2.age, i)  # this will report error as age can't be set back
+
+# if __name__ == '__main__':
+#     unittest.main()
     
+# Use setUp() for convenience
+# setUp中的这一个实例,既能够充当测试中的属性,又成为源代码的一个实例, 这样可以多次引用源代码中的方法和属性
+# 可以有效简化代码,避免重复创造实例和结果.
+class TestDog(unittest.TestCase):
+    def setUp(self):
+        """创造一个实例以供后面测试使用"""
+        self.dog_test = dog('dog_test')
+
+    # 所有的test case都是根据setUp中创作的实例,但是每个test的实例之间互相又不会干扰
+    def test_1(self):
+        self.dog_test.set_age(1)
+        self.assertEqual(self.dog_test.age, 1)
+    def test_2(self):
+        self.dog_test.set_age(3)
+        self.assertEqual(self.dog_test.age, 3)
+    def test_3(self):
+        self.dog_test.set_age(2)                # This will work
+        self.assertEqual(self.dog_test.age, 2)  # The instance is shared by all tests, but have no connection with each other
+
+if __name__ == '__main__':
+    unittest.main()
+
 
