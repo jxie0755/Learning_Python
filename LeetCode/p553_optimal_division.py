11c5d264ea5cd9ad973cd2206f8988b67f5d546f
jxie0755 <jxie0755@users.noreply.github.com>
2018-02-17 11:50:42 -0500

    LeetCode p553

---
+++ LeetCode/p553_optimal_division.py
@@ -0,0 +1,28 @@
+# p553 Optimal Division
+# Easy
+
+# Given a list of positive integers, the adjacent integers will perform the float division.
+# For example, [2,3,4] -> 2 / 3 / 4.
+# However, you can add any number of parenthesis at any position to change the priority of operations.
+# You should find out how to add parenthesis to get the maximum result
+# and return the corresponding expression in string format.
+# Your expression should NOT contain redundant parenthesis.
+# Note:
+# The length of the input array is [1, 10].
+# Elements in the given array will be in range [2, 1000].
+# There is only one optimal division for each test case.
+
+# """
+# :type nums: List[int]
+# :rtype: str
+# """
+
+class Solution:
+    def optimalDivision(self, nums):
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
+
