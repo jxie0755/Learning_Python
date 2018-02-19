5605b18862087190f6377e10d3ec24b523e909c8
jxie0755 <jxie0755@users.noreply.github.com>
2018-02-18 22:07:21 -0500

    LeetCode p22

---
+++ LeetCode/p22_generate_parenthesis.py
@@ -0,0 +1,19 @@
+# p22 Generate Parentheses
+# Medium
+
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# """
+# :type n: int
+# :rtype: List[str]
+# """
+
+class Solution:
+    def generateParenthesis(self, n):
+        pass
+
+
+if __name__ == '__main__':
+    assert Solution().generateParenthesis(3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
+    print('all passed')
+
