c5987f1d8cdc67137f835d3899e3a6a854520262
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-03 10:47:00 -0500

    CS61A lab_01

---
+++ CS_61A/week_2/lab_01.py
@@ -0,0 +1,119 @@
+# Lab 01: Expressions and Control Structures
+
+# Expression and Stamentment
+print('\nExpression and Staments')
+print(3)
+print(5)
+print(True)
+
+# Assignment Statements
+print('\nAssignment statements')
+a = (100 + 50) // 2
+print(a)
+
+# Boolean operator
+print('\nBoolean operator')
+print(5>3 and 3>5)
+print(5>3 or 3<5)
+print(not 5>3)
+
+# Testing short circuiting theory
+print('\nShort circuiting theory')
+print(True or 1/0)  # >>> True, bypassing the evaluation of 1/0
+# print(1/0 or 5/3)  # ZeroDivisionError
+print(False and 1/0)  # >>> False
+
+# Division
+print('\nDivision')
+print(1 / 2)
+print(5 // 2)
+print(5 % 2)
+
+# Function
+print('\nFunction')
+def f(x):
+    return x**2
+print(f(5))
+
+# Control
+# if statement
+print('\nif statement')
+def largerthanthree(x):
+    if x > 3:
+        return True
+    else:
+        return False
+print(largerthanthree(2))
+
+# while loop
+print('\nwhile loop')
+def power_ten(x):
+    result = 2
+    while x < 10:
+        result *= 2
+        x += 1
+    return result
+print(power_ten(2))
+
+# Quiz
+print('\nQuiz')
+print('\nQ1 what would python display')
+print(True and 13)  # >>> 13
+print(False or 0)   # >>> 0
+print(not 10)       # >>> False
+print(not None)     # >>> True
+
+print()
+# print(True and 1 / 0 and False)     # >>> ZeroDivisionError
+print(True or 1 / 0 or False)       # >>> True
+print(True and 0)                   # >>> 0
+print(False or 1)                   # >>> 1
+print(1 and 3 and 6 and 10 and 15)  # >>> 15
+print(0 or False or 2 or 1 / 0)     # >>> 2
+
+print()
+print(not 0)         # >>> True
+print((1+1) and 1)   # >>> 1
+# print((1/0) or True)  # ZeroDivisionError  # Still have to evaluate <left> even if returning <right>
+print((True or False) and False)  # >>> False
+
+# Q2 Loops
+print('\nQ2 Loops')
+n = 3
+while n >= 0:
+    n -= 1
+    print(n)
+# >>>
+# 2
+# 1
+# 0
+# -1
+
+# n = 4
+# while n > 0:
+#     n += 1
+#     print(n)
+# >>>
+# 5
+# 6
+# 7
+# ...forever continue
+
+def go(n):
+    if n % 2 != 0:
+        print(n / 2)
+        return
+    while n > 0:
+        print(n)
+        n = n // 2
+
+go(4)
+# >>>
+# 4
+# 2
+# 1
+
+go(5)
+# >>>
+# 2.5
+
