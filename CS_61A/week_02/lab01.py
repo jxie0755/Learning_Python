e985e234e53297ebef3051fd4977b135a58332f4
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-03 22:44:49 -0500

    CS61A Lab 02

--- CS_61A/week_02/lab01.py
+++ CS_61A/week_02/lab01.py
@@ -157,7 +157,9 @@ while negative:
 
 # Coding practice
 print('\nCoding Practice')
+
 print('\nQ3: Repeated')
+
 # Implement the repeated function, which takes a one-argument function f, a positive integer n, and a parameter x. It returns the result of composing, or applying, f n times on x, i.e., f(f(...f(x)...)).
 def repeated(f, n, x):
     """Returns the result of composing f n times on x."""
@@ -181,6 +183,7 @@ if __name__ == '__main__':
     print('all passed')
 
 print('\nQ4: Sum Digits')
+
 # Write a function that takes in a nonnegative integer and sums its digits. (Using floor division and modulo might be helpful here!)
 def sum_digits(n):
     """Sum all the digits of n."""
@@ -198,6 +201,7 @@ if __name__ == '__main__':
     print('all passed')
 
 print('\nQ5: Double Eights')
+
 # Write a function that takes in a number and determines if the digits contain two adjacent 8s.
 def double_eights(n):
     """Return true if n has two eights in a row."""
@@ -237,6 +241,8 @@ print(False or 1)           # >>> True but actually show the value 1
 print(not 0 and 12 or 0)    # >>> True but actually show the value 12
 
 print('\nQ7: WWPD: What If?')
+
+
 def xk(c, d):
     if c == 4:
         return 6
@@ -244,8 +250,102 @@ def xk(c, d):
         return 6 + 7 + c
     else:
         return 25
+print('\nxk')
+print(xk(10, 10))  # >>> 23
+print(xk(10, 6))   # >>> 23
+print(xk(4, 6))    # >>> 6
+print(xk(0, 0))    # >>> 25
 
-print(xk(10, 10))  # >>>
-print(xk(10, 6))   # >>>
-print(xk(4, 6))    # >>>
-print(xk(0, 0))    # >>>
+
+def how_big(x):
+    if x > 10:
+        print('huge')
+    elif x > 5:
+        return 'big'
+    elif x > 0:
+        print('small')
+    else:
+        print("nothin'")
+
+print('\nhow_big')
+how_big(7)   # >>> return 'big'  # terminal will still show the return as the actuall string
+how_big(12)  # >>> huge   # when print, it will ingore the "'"
+how_big(1)   # >>> small
+how_big(-1)  # >>> nothin'
+
+
+def so_big(x):
+    if x > 10:
+        print('huge')
+    if x > 5:
+        return 'big'
+    if x > 0:
+        print('small')
+    print("nothin'")
+
+print('\nso_big')
+so_big(7)   # >>> return 'big'
+so_big(12)  # >>> huge and return 'big'
+so_big(1)   # >>> small and nothin'
+
+
+def ab(c, d):
+    if c > 5:
+        print(c)
+    elif c > 7:
+        print(d)
+    print('foo')
+
+print('\nab')
+ab(10, 20)  # >>> 10 and foo
+
+
+def bake(cake, make):
+    if cake == 0:
+        cake = cake + 1
+        print(cake)
+    if cake == 1:
+        print(make)
+    else:
+        return cake
+    return make
+
+print('\nbake')
+bake(0, 29)                 # >>>  1 and 29 and return 29
+bake(1, 'mashed potatoes')  # >>>  mashed potatoes and return 'mashed potatoes
+
+
+# More Coding Practice
+
+print('\nQ8: Fix the Bug')
+
+# The following snippet of code doesn't work! Figure out what is wrong and fix the bugs.
+def both_positive(x, y):
+    """Returns True if both x and y are positive."""
+    # return x and y > 0  # You can replace this line!
+    return x > 0 and y > 0
+
+if __name__ == '__main__':
+    print('\nboth_positve')
+    assert both_positive(-1, 1) == False
+    assert both_positive(1, 1) == True
+    print('all passed')
+
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k."""
+    result = 1
+    while k != 0:
+        result *= n
+        n -= 1
+        k -= 1
+    return result
+
+if __name__ == '__main__':
+    print('\nfalling')
+    assert falling(6, 3) == 120, '6 * 5 * 4'
+    assert falling(4, 0) == 1
+    assert falling(4, 3) == 24, '4 * 3 * 2'
+    assert falling(4, 1) == 4
+    print('all passed')
+
