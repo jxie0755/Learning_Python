dbaeede1b11e290d6d82ef78865d33e355779575
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-07 22:30:03 -0500

    Simple Learning on if/else condtions

---
+++ ZSimpleLearnings/if_else_lazy_evaluation.py
@@ -0,0 +1,17 @@
+# This is to learn the lazy evaluation effect of if/else condition in python
+
+def lucky(n):
+    if n < 7:      # if the condition is False, won't evaluate the rest prt
+        return 1 / 0     # bypassed
+    elif n == 13:
+        return 'A' + 5
+    else:
+        print('omg')
+lucky(37)  # >>> omg
+
+
+def uh_oh():
+    if 1 / 0 == 5:      # Will always evaluate the condition
+        print('really?')
+
+# uh_oh()  # >>> Error
