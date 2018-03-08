92c4644e3deaf4a3caad523fcec94195dba29a3a
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-08 14:12:59 -0500

    Update codes
    bug fix

--- ZStandardLibrary/learn_timeit.py
+++ ZStandardLibrary/learn_timeit.py
@@ -14,22 +14,23 @@ print("timeit.timeit(stmt='pass', setup='pass', timer=<default timer>, number=10
 print(timeit.timeit('list(range(0, 999))', number=10000))
 # >>> 0.1661170139996102 # will vary depend on computer resource
 print(type(timeit.timeit('list(range(0, 999))', number=10000)))
-# >>> <class 'float'>
+# >>> <class 'float'>  输出的是一个浮点数的时间,单位为秒
 
 print(timeit.timeit('char in text', setup="text ='sample string'; char = 'g'", number=10000000))
 # >>> 0.2998773199997231
 print(timeit.timeit('text.find(char)', setup='text = "sample string"; char = "g"'))
 # >>> 0.14675580299990543
 
+# Setup是用于设定环境,若运行python builtin函数则不必import其他都需要
 
-# print(timeit.timeit('fib_gen_r(30)', number=10000))  # >>> 不能运行一个函数?
-print(timeit.timeit('fib_gen_r(30)', setup='from __main__ import fib_gen_r', number=5))
+
+
+# 测试一个函数?
+print(timeit.timeit('fib_gen_r(30)', setup='from ZCodeSnippets.fibonacci import fib_gen_r', number=5))
 # >>> 1.9347527580011956
 # 此命令既可以对import的函数或者对本文件中的函数运行
+# 若fib_gen_r()为本地函数则'from __main__ import fib_gen_r'
 
-print(timeit.timeit('fib_gen_r(30)', setup='from ZSimpleLearnings.fibonacci import fib_gen_r', number=5))
-# >>> 2.0236477670005115
-# 这样可以省略import,直接在timeit.timeit中引用
 
 
 print()
@@ -40,9 +41,8 @@ print(timeit.repeat('list(range(0, 999))', repeat=3, number=10000))
 # >>> [0.17210359499949845, 0.15265621899925463, 0.15970960199956608] # 输出一个list可以算平均值
 
 # 函数同样可以在repeat中相似方法运行
-print(timeit.repeat('fib_gen_r(30)', setup='from __main__ import fib_gen_r', repeat=3, number=3))
+print(timeit.repeat('fib_gen_r(30)', setup='from ZCodeSnippets.fibonacci import fib_gen_r', repeat=3, number=3))
 # >>> [1.2522393469989765, 1.2651939509996737, 1.2109857380000904]
-
 
 
 print()
@@ -54,7 +54,7 @@ print(timeit.Timer('list(range(0, 999))').timeit(number=10000))  # >>> 0.1609123
 # repeat(repeat=3, number=1000000) 方法
 print(timeit.Timer('list(range(0, 999))').repeat(repeat=3, number=10000))
 # >>> [0.1699522140006593, 0.15189915299924905, 0.16639813700021477]
-print(timeit.Timer('fib_gen_r(30)', setup='from __main__ import fib_gen_r').repeat(repeat=3, number=5))
+print(timeit.Timer('fib_gen_r(30)', setup='from ZCodeSnippets.fibonacci import fib_gen_r').repeat(repeat=3, number=5))
 # >>> [1.9957611610007007, 1.9907497040003364, 1.9418537080000533]
 
 
