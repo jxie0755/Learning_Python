4f3e616b0a7961a06ccf54699ea477a455e8d11a
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-12 23:14:27 -0400

    CS61A HW 02

--- CS_61A/hw/hw02/hw02.py
+++ CS_61A/hw/hw02/hw02.py
@@ -32,7 +32,11 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    "*** YOUR CODE HERE ***"
+    count, result = 1, 1
+    while count <= n:
+        result *= term(count)
+        count += 1
+    return result
 
 # The identity function, defined using a lambda expression!
 identity = lambda k: k
@@ -48,7 +52,7 @@ def factorial(n):
     >>> check(HW_SOURCE_FILE, 'factorial', ['Recursion', 'For', 'While'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    return product(n, identity)
 
 def make_adder(n):
     """Return a function that takes an argument K and returns N + K.
@@ -59,5 +63,5 @@ def make_adder(n):
     >>> make_adder(1)(2)
     3
     """
-    "*** YOUR CODE HERE ***"
-    return 'REPLACE ME'
+    adder = lambda k: k + n
+    return adder
