f0e3b4876ea078a45493eb268992cec62ccd29d1
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-16 22:38:49 -0400

    Project Euler P006 simple question solved

---
+++ ProjectEuler/p006_sum_square_difference.py
@@ -0,0 +1,21 @@
+# P006 Sum square difference
+
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
+
+# The square of the sum of the first ten natural numbers is:
+# (1 + 2 + ... + 10)^2 = 552 = 3025
+
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+def sum_square_difference(n1, n2):
+    square_sum = sum(map(lambda x:x**2, range(n1, n2+1)))
+    sum_square = sum(range(n1, n2+1)) ** 2
+    return sum_square - square_sum
+
+if __name__ == '__main__':
+    print(sum_square_difference(1, 100))
+
+
