3eea673e1610339ba6d9e7d31fd07f7ecd10bb02
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-16 22:17:45 -0400

    Project Euler P005 Solved by Lowest Common Multiple

--- ProjectEuler/p005_smallest_multiple.py
+++ ProjectEuler/p005_smallest_multiple.py
@@ -4,24 +4,21 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-from functools import reduce
-
-if __name__ == '__main__':
-    print(reduce(lambda x, y: x*y, range(1,21)))
-    # >>>> 2432902008176640000
-    # maximum is a very big number
 
 # Brutal force will not work as the number is too big
 # use greatest common divisor, to calculate
+
+from functools import reduce
+
 def smallest_multiple(n1, n2):
     """"find the positive number that can be evenly divided by numbers from range(n1, n2+1)"""
 
-    def common_multiple(x, y):
+    def lowest_common_multiple(x, y):
         for i in range(min(x, y), 0, -1):
             if x % i == 0 and y % i == 0:
                 return x * y // i
 
-    return reduce(common_multiple, range(n1, n2+1))
+    return reduce(lowest_common_multiple, range(n1, n2+1))
 
 if __name__ == '__main__':
      print(smallest_multiple(1, 20))
