b5a5292368a2dd88e1a9ccc0b108c107c22042af
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-16 22:15:12 -0400

    Project Euler P005 Solved by Lowest Common Multiple

--- ProjectEuler/p005_smallest_multiple.py
+++ ProjectEuler/p005_smallest_multiple.py
@@ -4,18 +4,26 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-
-# Brutal force
 from functools import reduce
 
-def even_distribute(n1, n2):
-    """"find the positive number that can be evenly divided by numbers from range(n1, n2+1)"""
-    sample, maximum = 1, reduce(lambda x, y: x*y, range(n1,n2+1))
-    while sample <= maximum:
-        print(sample)
-        if all(sample % i == 0 for i in range(n1, n2+1)):
-            return sample
-        sample += 1
+if __name__ == '__main__':
+    print(reduce(lambda x, y: x*y, range(1,21)))
+    # >>>> 2432902008176640000
+    # maximum is a very big number
 
-# print(even_distribute(1, 20))
-# >>> this will take too much time
+# Brutal force will not work as the number is too big
+# use greatest common divisor, to calculate
+def smallest_multiple(n1, n2):
+    """"find the positive number that can be evenly divided by numbers from range(n1, n2+1)"""
+
+    def common_multiple(x, y):
+        for i in range(min(x, y), 0, -1):
+            if x % i == 0 and y % i == 0:
+                return x * y // i
+
+    return reduce(common_multiple, range(n1, n2+1))
+
+if __name__ == '__main__':
+     print(smallest_multiple(1, 20))
+     # >>> 232792560
+     # passed
