9288b5a258a9238be15455cd247191b05ec7a082
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-16 22:18:19 -0400

    Project Euler P005 Solved by Lowest Common Multiple

--- ProjectEuler/p005_smallest_multiple.py
+++ ProjectEuler/p005_smallest_multiple.py
@@ -4,7 +4,6 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-
 # Brutal force will not work as the number is too big
 # use greatest common divisor, to calculate
 
