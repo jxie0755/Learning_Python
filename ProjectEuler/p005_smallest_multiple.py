aae2fbc81b94468716330f9fad3b911d7844ebce
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-17 14:24:27 -0400

    Project Euler P005 Add back the brutal force method

--- ProjectEuler/p005_smallest_multiple.py
+++ ProjectEuler/p005_smallest_multiple.py
@@ -4,8 +4,23 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-# Brutal force will not work as the number is too big
-# use greatest common divisor, to calculate
+# Version 1: Brutal force
+from functools import reduce
+
+def even_distribute(n1, n2):
+    """"find the positive number that can be evenly divided by numbers from range(n1, n2+1)"""
+    sample, maximum = 1, reduce(lambda x, y: x*y, range(n1,n2+1))
+    while sample <= maximum:
+        if all(sample % i == 0 for i in range(n1, n2+1)):
+            return sample
+        sample += 1
+
+print(even_distribute(1, 20))
+# >>> this will take too much time
+
+
+
+# Version 2: Use greatest common divisor, to calculate
 
 from functools import reduce
 
@@ -23,21 +38,3 @@ if __name__ == '__main__':
      print(smallest_multiple(1, 20))
      # >>> 232792560
      # passed
-
-
-# Created by Ye Luo
-def LCM(num):
-    def GCD(a,b):
-        while b:
-            a,b = b,a%b
-        return a
-
-    a, y = 1,1
-    for a in range(1,num+1):
-        y = y*a//GCD(y,a)
-
-    return y
-
-if __name__ == '__main__':
-    result = LCM(20)
-    print(result)
