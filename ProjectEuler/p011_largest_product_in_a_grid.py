e7e6a99f9f90c37681be257380ef1a6dc080e5df
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-20 22:45:26 -0400

    ProjectEuler P011 finished

--- ProjectEuler/p011_largest_product_in_a_grid.py
+++ ProjectEuler/p011_largest_product_in_a_grid.py
@@ -44,12 +44,38 @@ def product_in_grid(data, n):
     grid = [line.strip().split() for line in raw_grid]
 
     # define the logic to find 4 adjacent numbers as required
-    length_x, length_y = len(grid[0]), len(grid)
+    length = len(grid)
     max_so_far = 0
 
-    for y in range(0, length_y - n):
-        for x in range(0, length_x - n):
+
+    # diagonal from top left to down bottom
+    for y in range(0, length - n):
+        for x in range(0, length - n):
             sample = [int(grid[y + i][x + i]) for i in range(n)]
+            product = reduce(lambda x, y: x * y, sample)
+            if product > max_so_far:
+                max_so_far = product
+
+    # diagonal top right to down left
+    for y in range(n-1, length):
+        for x in range(0, length - n):
+            sample = [int(grid[y - i][x + i]) for i in range(n)]
+            product = reduce(lambda x, y: x * y, sample)
+            if product > max_so_far:
+                max_so_far = product
+
+    # horizontally
+    for y in range(0, n):
+        for x in range(0, length - n):
+            sample = [int(grid[y][x + i]) for i in range(n)]
+            product = reduce(lambda x, y: x * y, sample)
+            if product > max_so_far:
+                max_so_far = product
+
+    # vertically
+    for y in range(0, length - n):
+        for x in range(0, n):
+            sample = [int(grid[y + i][x]) for i in range(n)]
             product = reduce(lambda x, y: x * y, sample)
             if product > max_so_far:
                 max_so_far = product
@@ -60,4 +86,4 @@ def product_in_grid(data, n):
 if __name__ == '__main__':
     print(product_in_grid('p011_data.txt', 4))
     # >>> 32719995
-    # TODO
+    # passed
