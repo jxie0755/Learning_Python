89a52f565c9746bbf5694e7b86220b23f3c34746
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-24 17:17:00 -0400

    Simple Learning on iterator vs generator

--- ZSimpleLearnings/py_iterator_generator.py
+++ ZSimpleLearnings/py_iterator_generator.py
@@ -13,26 +13,31 @@ def squares(start, stop):
         yield i * i
 
 if __name__ == '__main__':
+    print('Test generator:')
     generator_1 = squares(5, 9)
     for i in generator_1:
         print(i)
 
 
-# Iterator version
+# Iterator version (not a real iterator)
+# would take more code to build as a custom iterator:
 class Squares(object):
     def __init__(self, start, stop):
        self.start = start
        self.stop = stop + 1
-    def __iter__(self): return self
-    def next(self):
-       if self.start >= self.stop:
-           raise StopIteration
-       current = self.start * self.start
-       self.start += 1
-       return current
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start >= self.stop:
+            raise StopIteration
+        current = self.start * self.start
+        self.start += 1
+        return current
 
 if __name__ == '__main__':
+    print('Test iterator:')
     iterator_1 = Squares(5, 9)
-    for i in range(5):
-        print(iterator_1.next())
-
+    for i in iterator_1:
+        print(i)
