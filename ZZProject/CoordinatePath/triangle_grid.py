d6128a495a684ac4ec781e429d5bb9226c1501b6
jxie0755 <jxie0755@users.noreply.github.com>
2018-03-29 22:35:53 -0400

    Project triangle grid

---
+++ ZZProject/CoordinatePath/triangle_grid.py
@@ -0,0 +1,82 @@
+# This is code snippet developed from ProjectEuler P015
+# https://projecteuler.net/problem=18
+# See details in ProjectEuler/p015_lattice_paths.py
+
+# A typical triangluler grid
+#    1
+#   1 2
+#  1 2 3
+# 1 2 3 4
+
+# in the data structure:
+# grid = [
+#     [1],
+#     [1,2]
+#     [1,2,3]
+#     [1,2,3,4]
+# ]
+
+
+# Brutal force
+def coor_value(coor, grid):
+    """coor is coordinate of the grid in the form of (x, y)
+
+    grid: a nested list with list of numbers
+    """
+    return grid[coor[0]][coor[1]]
+
+def find_triangle_paths(T):
+    """calculate the max sum of a route in triangle
+
+    T as triangle: a nested list as triangle grid
+    return: the max sum of the numbers in one route of the triangle grid
+    """
+    row, depth = 1, len(T)
+    current = [(0,0)]
+    possible_path = [current]
+
+    while row < depth:
+        # 对上一行进行分裂
+        possible_routes_temp = []
+        for i in possible_path:
+            possible_routes_temp += [i[:], i[:]]
+        possible_path = possible_routes_temp[:]
+
+        # 准备下一行的新数据,以便添加
+        temp = []
+        for coor in current:
+            temp += [(coor[0] + 1, coor[1]), (coor[0] + 1, coor[1] + 1)]
+        current = temp[:]
+
+        # 按位置对应添加
+        for idx in range(len(temp)):
+            possible_path[idx].append(temp[idx])
+
+        row += 1
+
+    # for print the result, first translate coor to value
+    path_in_value = []
+    for i in possible_path:
+        values = [coor_value(j, T) for j in i]
+        path_in_value.append(values)
+
+    for i in path_in_value:
+        print(i)
+
+    print('total path number', len(path_in_value))
+    return sum(max(path_in_value, key=sum))
+
+
+
+
+if __name__ == '__main__':
+    grid_1 = [
+    [1],
+    [1,2],
+    [1,2,3],
+    [1,2,3,4]
+    ]
+
+    print(find_triangle_paths(grid_1))
+    # >>> 20 (from 1,3,6,10)
+
