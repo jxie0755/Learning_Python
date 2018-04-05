03d301fd91f5e95f60a8df50e38f568f3c609dea
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-04-05 08:16:36 -0400

    Update whiteboard.py

--- whiteboard.py
+++ whiteboard.py
@@ -1,2 +1,19 @@
+# Test speed
 
+# Version 1: Brutal force
+from functools import reduce
+import time
 
+def even_distribute(n1, n2):
+    """"find the positive number that can be evenly divided by numbers from range(n1, n2+1)"""
+    sample, maximum = 1, reduce(lambda x, y: x*y, range(n1,n2+1))
+    while sample <= maximum:
+        if all(sample % i == 0 for i in range(n1, n2+1)):  # list comprehension is used
+            return sample
+        sample += 1
+
+t1 = time.time()
+print(even_distribute(1, 20))
+t2 = time.time()
+print(t2 - t1)
+# >>> 232792560   # this will take too much time (~200 sec)
