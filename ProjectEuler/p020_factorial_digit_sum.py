db22adb97adfecd97f81726795a168a7c1ff73a7
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-04-11 15:41:35 -0400

    Update p020_factorial_digit_sum.py

--- ProjectEuler/p020_factorial_digit_sum.py
+++ ProjectEuler/p020_factorial_digit_sum.py
@@ -13,7 +13,7 @@ def fact_digit_sum(n):
     result = 1
     for i in range(1, n+1):
         result *= i
-    return sum([int(i) for i in str(result)])
+    return sum(int(i) for i in str(result))
 
 
 if __name__ == '__main__':
