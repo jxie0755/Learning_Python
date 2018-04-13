8a09632171024582aa59c2b6976fef7a32bedc27
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-04-13 11:40:42 -0400

    Update learn_time.py

--- ZStandardLibrary/learn_time.py
+++ ZStandardLibrary/learn_time.py
@@ -1,11 +1,9 @@
+# time module
 # https://docs.python.org/3/library/time.html#module-time
 
 # Check the time length the python execution
 
 import time
 start_time = time.time()
-
 # run main() codes
-
 print("--- %s seconds ---" % (time.time() - start_time))
-
