8227e7c6ed7d0e6da4808a3f7d2b870534469240
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-04-26 21:39:26 -0400

    CS61A Lecture 08 Tree Recursion

--- CS_61A/week03/lecture_08_tree_recursion.py
+++ CS_61A/week03/lecture_08_tree_recursion.py
@@ -1,2 +1,24 @@
 # CS61A Lecture 08 Tree Recursion
 
+def cascade(n):
+    if n < 10:
+        print(n)
+    else:
+        print(n)
+        cascade(n//10)
+        print(n)
+
+cascade(5)
+# >>> 5
+
+cascade(12345)
+# 12345
+# 1234
+# 123
+# 12
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
