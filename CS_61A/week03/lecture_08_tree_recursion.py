cf2b744e9c31b83f0f843f6e0463d5f65a0f6b7d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-04-28 23:08:40 -0400

    CS61A Lecture 08 Tree Recursion

--- CS_61A/week03/lecture_08_tree_recursion.py
+++ CS_61A/week03/lecture_08_tree_recursion.py
@@ -1,5 +1,8 @@
 # CS61A Lecture 08 Tree Recursion
 
+
+# Ordering
+
 def cascade(n):
     if n < 10:
         print(n)
@@ -22,3 +25,60 @@ cascade(12345)
 # 1234
 # 12345
 
+
+def inverse_cascade(n):
+    grow(n)
+    print(n)
+    shrink(n)
+
+def f_then_g(f, g, n):
+    if n:
+        f(n)
+        g(n)
+
+grow = lambda n: f_then_g(grow, print, n//10 )
+shrink = lambda n: f_then_g(print, shrink, n//10)
+
+inverse_cascade(1234)
+# >>>
+# 1
+# 12
+# 123
+# 1234
+# 123
+# 12
+
+
+# Tree recursion
+
+def fib(n):
+    """Compute the nth Fibonacci number.
+
+    >>> fib(8)
+    21
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+def count_partitions(n, m):
+    """Count the partitions of n using parts up to size m.
+
+    >>> count_partitions(6, 4)
+    9
+    >>> count_partitions(10, 10)
+    42
+    """
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        with_m = count_partitions(n - m, m)
+        without_m = count_partitions(n, m - 1)
+        return with_m + without_m
