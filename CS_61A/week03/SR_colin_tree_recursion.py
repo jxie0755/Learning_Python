36fbb3c6cbf2a33f6a056c0e3b723d9a22bd7a84
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-04 22:20:25 -0400

    CS61A Week 03 Colin's tree recursion notes

--- CS_61A/week03/SR_colin_tree_recursion.py
+++ CS_61A/week03/SR_colin_tree_recursion.py
@@ -1,2 +1,20 @@
 # Colin's notes on recursion
 
+def stairs(n):
+    """Give the number of ways to take n steps, given that at each step, you can choose to take 1, 2
+    >>> stairs(2)
+    2
+    >>> stairs(4)
+    5
+    >>> stairs(1)
+    1
+    >>> stairs(3)
+    3
+    """
+    if n <= 0:
+        return 0
+    elif n == 1 or n == 2:
+        return n
+    else:
+        return stairs(n-1) + stairs(n-2)
+
