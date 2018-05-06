88aec9c67a8be06db69e7eb319c9bf4bb8a8fbbb
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-05 23:12:06 -0400

    CS61A Week 03 Colin's tree recursion notes

--- CS_61A/week03/SR_colin_tree_recursion.py
+++ CS_61A/week03/SR_colin_tree_recursion.py
@@ -19,3 +19,18 @@ def stairs(n):
         return 2
     else:
         return stairs(n-1) + stairs(n-2)
+
+def kstairs(n, k):
+    """Give the number of ways to take n steps, given that at each step, you can choose to take 1,2,3,k-2,k-1 or k steps,
+    >>> kstairs(5, 2)
+    8
+    >>> kstairs(5, 5)
+    16
+    >>> kstairs(10, 5)
+    464
+    """
+    if n == 0:
+        return 0
+    if n <= k:
+        return 2**(n-1)
+    return sum([kstairs(n - i, k) for i in range(1, k + 1)])
