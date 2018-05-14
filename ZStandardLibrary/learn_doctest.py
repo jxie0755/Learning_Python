dd12d5da7d14190a11329e290f6f96c20ceb9b13
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-14 00:31:05 -0400

    Learn doctest

--- ZStandardLibrary/learn_doctest.py
+++ ZStandardLibrary/learn_doctest.py
@@ -27,6 +27,11 @@ if __name__ == '__main__':
     doctest.testmod(verbose=True)  # 此处verbose默认为True,会输出结果,若为false则只有出错才出结果
     # 这里会测试整个文件的所有函数
 
+# 若只单独测试一个函数, 可以利用独立测试文件,只测试模块中的一个函数
+if __name__ == '__main__':
+    import doctest
+    doctest.run_docstring_examples(f, globals(),verbose=True)
+
 
 
 # 在terminal中使用
@@ -40,4 +45,5 @@ if __name__ == '__main__':
 # 果不想将doctest测试用例嵌入到python的源码中，则可以建立一个独立的文本文件来保存测试用例
 # 建立leanr_doctest_indi.txt
 # 注意txt文件中需要import原文件
-# 若只单独测试一个函数, 可以利用独立测试文件,只测试模块中的一个函数
+
+
