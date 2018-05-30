b7f9b3a531927478c17820a1c3634d01e9b6f1bd
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-30 00:36:01 -0400

    cs61A week04 lab03

--- CS_61A/lab/lab03/lab03.py
+++ CS_61A/lab/lab03/lab03.py
@@ -106,7 +106,8 @@ print(welcome(), cs61a())
 
 
 # Higher order functions
-# Q4 I heard you liked functions....
+print('-------------\n')
+# Q4 I heard you liked functions....(orginal in lab02, extra)
 def cycle(f1, f2, f3):
     """Returns a function that is itself a higher-order function.
 
@@ -133,35 +134,17 @@ def cycle(f1, f2, f3):
     >>> do_two_cycles(1)
     19
     """
-    def hf(n):
-        if n % 4 == 0:
-            return lambda n: n
-        elif n % 4 == 1:
-            return lambda n: f1(n)
-        elif n % 4 == 2:
-            return lambda n: f2(f1(n))
-        elif n % 4 == 3:
-            return lambda n: f3(f2(f1(n)))
-    return hf
-
-def add1(x):
-    return x + 1
-def times2(x):
-    return x * 2
-def add3(x):
-    return x + 3
-my_cycle = cycle(add1, times2, add3)
-identity = my_cycle(0)
-print(identity(5))
-
-add_one_then_double = my_cycle(2)
-print(add_one_then_double(1))
-
-do_all_functions = my_cycle(3)
-print(do_all_functions(2))
-
-do_more_than_a_cycle = my_cycle(4)
-print(do_more_than_a_cycle(2))
-
-do_two_cycles = my_cycle(6)
-print(do_two_cycles(1))
+    def f(n):
+        def g(x):
+            i = 0
+            while i < n:
+                if i % 3 == 0:
+                    x = f1(x)
+                elif i % 3 == 1:
+                    x = f2(x)
+                elif i % 3 == 2:
+                    x = f3(x)
+                i += 1
+            return x
+        return g
+    return f
