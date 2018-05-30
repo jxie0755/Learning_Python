502224ecaa5523f446045f0877d43c545f28e39c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-29 23:18:16 -0400

    CS61A week04 lab03

--- CS_61A/lab/lab03/lab03.py
+++ CS_61A/lab/lab03/lab03.py
@@ -5,6 +5,7 @@ def factorial(n):
     return n * factorial(n-1)
 
 
+# Q1 GCD
 def gcd(a, b):
     """Returns the greatest common divisor of a and b.
     Should be implemented using recursion.
@@ -25,6 +26,7 @@ def gcd(a, b):
     return gcd(a % b, b)
 
 
+# Q2 Hailstone recuision
 def hailstone(n, r=0):
     """Print out the hailstone sequence starting at n, and return the
     number of elements in the sequence.
@@ -52,8 +54,8 @@ print(hailstone(10))
 
 
 # Midterm Review
-# Q3 Call expressions
 
+# Q3 Call expressions
 from operator import add
 def double(x):
     return x + x
@@ -104,8 +106,7 @@ print(welcome(), cs61a())
 
 
 # Higher order functions
-
-
+# Q4 I heard you liked functions....
 def cycle(f1, f2, f3):
     """Returns a function that is itself a higher-order function.
 
@@ -132,4 +133,35 @@ def cycle(f1, f2, f3):
     >>> do_two_cycles(1)
     19
     """
-    "*** YOUR CODE HERE ***"
+    def hf(n):
+        if n % 4 == 0:
+            return lambda n: n
+        elif n % 4 == 1:
+            return lambda n: f1(n)
+        elif n % 4 == 2:
+            return lambda n: f2(f1(n))
+        elif n % 4 == 3:
+            return lambda n: f3(f2(f1(n)))
+    return hf
+
+def add1(x):
+    return x + 1
+def times2(x):
+    return x * 2
+def add3(x):
+    return x + 3
+my_cycle = cycle(add1, times2, add3)
+identity = my_cycle(0)
+print(identity(5))
+
+add_one_then_double = my_cycle(2)
+print(add_one_then_double(1))
+
+do_all_functions = my_cycle(3)
+print(do_all_functions(2))
+
+do_more_than_a_cycle = my_cycle(4)
+print(do_more_than_a_cycle(2))
+
+do_two_cycles = my_cycle(6)
+print(do_two_cycles(1))
