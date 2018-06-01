4c559296860a84cac033421b045a69974a1d00f3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-05-31 23:05:15 -0400

    cs61a week 04 lab03

--- CS_61A/lab/lab03/lab03_extra.py
+++ CS_61A/lab/lab03/lab03_extra.py
@@ -79,7 +79,9 @@ def skip_mul(n):
     >>> skip_mul(8) # 8 * 6 * 4 * 2
     384
     """
-    if n == 2:
+    if n == 1:
+        return 1
+    elif n == 2:
         return 2
     else:
         return n * skip_mul(n - 2)
@@ -94,7 +96,7 @@ def is_prime(n):
     >>> is_prime(521)
     True
     """
-    "*** YOUR CODE HERE ***"
+    return all(n % i !=0 for i in range(2, round(n**0.5) + 1))
 
 def interleaved_sum(n, odd_term, even_term):
     """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
