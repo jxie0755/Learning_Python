1dfde43a000db0b31fc1421f495c4d38234e609d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-05 14:26:49 -0400

    cs61a week4 csm00

--- CS_61A/week04/csm00_midterm_review.py
+++ CS_61A/week04/csm00_midterm_review.py
@@ -1,2 +1,30 @@
 # CSM00 Mideterm 1 Review
 
+# Q1
+apple = 4
+def orange(apple):
+    apple = 5
+    def plum(x):
+        return lambda plum: plum * 2
+    return plum
+
+print(orange(apple)("hiii")(4))
+# >>> 8
+
+
+# Q2
+def bar(f):
+    def g(x):
+        return f(x-1)
+    return g
+f = 4
+print(bar(lambda x: x + f)(2))
+# >>> 5
+
+
+# Q3
+inception = lambda secret: lambda: secret
+print(inception(5)())
+# >>> 5
+
+
