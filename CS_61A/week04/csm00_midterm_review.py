9c0a8cbd20bc63e1387053e19a4d11a07f1d2bcd
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-05 14:41:49 -0400

    cs61a week04 csm00

--- CS_61A/week04/csm00_midterm_review.py
+++ CS_61A/week04/csm00_midterm_review.py
@@ -28,3 +28,50 @@ print(inception(5)())
 # >>> 5
 
 
+# High order function
+def mystery(f, x):
+    """
+    >>> from operator import add, mul
+    >>> a = mystery(add, 3)
+    >>> a(4) # add(3, 4)
+    7
+    >>> a(12)
+    15
+    >>> b = mystery(mul, 5)
+    >>> b(7) # mul(5, 7)
+    35
+    >>> b(1)
+    5
+    >>> c = mystery(lambda x, y: x * x + y, 4)
+    >>> c(5)
+    21
+    >>> c(7)
+    23
+    """
+
+    def g(y):
+        return f(x, y)
+    return g
+
+    # on-liner
+    return lambda y: f(x, y)
+
+
+# What would python display:
+square = lambda x: x**2
+foo = mystery(lambda a, b: a(b), lambda c: 5 + square(c))
+print(foo(-2))
+# >>> 9
+
+
+# cannot use for or while loop.
+# use recursion in repeat
+# cannot use string operation except '+' operation
+def fox_says(start, middle, end, num):
+    """
+    >>> fox_says('wa', 'pa', 'pow', 3)
+    'wa-pa-pa-pa-pow'
+    >>> fox_says('fraka', 'kaka', 'kow', 4)
+    'fraka-kaka-kaka-kaka-kaka-kow'
+    """
+    def repeat(k):
