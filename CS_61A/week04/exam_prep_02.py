f9fa893c93a7e3d2865469934e5c550c162c6253
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-06 15:33:25 -0400

    cs61a week04 examp prep 02

---
+++ CS_61A/week04/exam_prep_02.py
@@ -0,0 +1,24 @@
+# Express yourself
+# a
+def kbonacci(n, k):
+    """Return element N of a K-bonacci sequence.
+    >>> kbonacci(3, 4)
+    1
+    >>> kbonacci(9, 4)
+    29
+    >>> kbonacci(4, 2)
+    3
+    >>> kbonacci(8, 2)
+    21
+    """
+    if n < k - 1:
+        return 0
+    elif n == k - 1:
+        return 1
+    else:
+        total = 0
+        i = ____________________________________________
+        while i < n:
+            total = total + ______________________________
+            i = i + 1
+    return total
