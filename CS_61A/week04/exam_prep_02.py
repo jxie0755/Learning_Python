cf6c67520e64cdd2aa180707b0f3626aabbb70a3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-08 08:41:09 -0400

    cs61a week04 exam prep 02

--- CS_61A/week04/exam_prep_02.py
+++ CS_61A/week04/exam_prep_02.py
@@ -124,3 +124,29 @@ def frog(croak):
 
 print(mouse(357))
 # >>> 47
+
+
+# If (s)he can wield the Hammer...
+# # what would python show:
+
+from operator import add
+avengers = 6
+
+def vision(avengers):
+    print(avengers)
+    return avengers + 1
+
+def hawkeye(thor, hulk):
+    love = lambda black_widow: add(black_widow, hulk)
+    return thor(love)
+
+def hammer(worthy, stone):
+    if worthy(stone) < stone:
+        return stone
+    elif worthy(stone) > stone:
+        return -stone
+    return 0
+
+capt = lambda iron_man: iron_man(avengers)
+
+print(capt(vision))
