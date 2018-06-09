4c7085a23179c5bbafae27c567f95db1a369c31e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-08 23:51:57 -0400

    cs61a week04 midterm 1

---
+++ CS_61A/week04/midterm_1.py
@@ -0,0 +1,38 @@
+# CS61A spring 2018 midterm exam 1
+
+# Q1 Frame of Thrones
+from operator import add, sub
+def winterfell(a, b):
+    a
+    b
+    return b(a+1, b(a))
+
+da, ny = 20, 18
+
+while da > ny:
+    da = ny
+    da, ny = ny + 1, da + 3
+
+def tar(gar, yen):
+    if print(yen):
+        print(yen + 1)
+    return gar(yen)
+
+def st(ar, k=None):
+    return lambda a, y: ar(y, a)
+
+night = st(sub)
+king = st(st(pow))
+
+def jon(sn, ow):
+    print(ow)
+    jon = sn(ow)
+    print(ow)
+    return jon
+
+def snow(ow):
+    def tarly(snow):
+        return ow + snow
+    ow += 2
+    return tarly
+    
