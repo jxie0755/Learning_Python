317d4b46264c1279da094976089c25048975d13b
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-14 14:42:36 -0400

    code snippet count partition by tree recursion

--- ZCodeSnippets/count_partitions.py
+++ ZCodeSnippets/count_partitions.py
@@ -19,3 +19,20 @@ if __name__ == '__main__':
     assert count_partitions(15, 15) == 176
     assert count_partitions(20, 20) == 627
     print('all passed')
+
+
+# the m-1 is a step-down process to include all integers from 0 to m.
+# in some other condition, m steps down not by 1, but by other mechanisms, for example: by half.
+# In that case the function should be:
+def count_partitions_modified(n, m):
+    """Count the ways to partition n using parts up to m.
+    modified m-1 (steps of 1) to m // 2 (steps of half)"""
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        return count_partitions_modified(n-m, m) + count_partitions_modified(n, m//2)
+        
