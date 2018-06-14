7cc8333b1d3f89ba77c06b69dc378fc2f503ef25
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-14 14:13:42 -0400

    cs61a week04 hw04 q5

--- CS_61A/hw/hw04/hw04.py
+++ CS_61A/hw/hw04/hw04.py
@@ -202,8 +202,18 @@ def count_change(amount):
     9828
     """
     "*** YOUR CODE HERE ***"
-    
 
+
+def count_partitions(n, m):
+    """Count the ways to partition n using parts up to m."""
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        return count_partitions(n-m, m) + count_partitions(n, m-1)
 
 ###################
 # Extra Questions #
