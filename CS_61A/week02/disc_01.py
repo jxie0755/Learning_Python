a49df783f4ccbaff894bc3595d5f03990b49596e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-19 19:49:05 -0400

    cs61a week04 disc01

---
+++ CS_61A/week02/disc_01.py
@@ -0,0 +1,57 @@
+def wears_jacket(temp, raining):
+    """
+    >>> wears_jacket(90, False)
+    False
+    >>> wears_jacket(40, False)
+    True
+    >>> wears_jacket(100, True)
+    True
+    """
+    return temp < 60 or raining
+
+def handle_overflow(s1, s2):
+    """
+    >>> handle_overflow(27, 15)
+    No overflow
+    >>> handle_overflow(35, 29)
+    Move to Section 2: 1
+    >>> handle_overflow(20, 32)
+    Move to Section 1: 10
+    >>> handle_overflow(35, 30)
+    No space left in either section
+    """
+    if s1 <= 30 and s2 <= 30:
+        print("No overflow")
+    elif s2 > 30 and s1 < 30:
+        print("Move to Section 1:" + str(30 - s1))
+    elif s1 > 30 and s2 < 30:
+        print("Move to Section 2:" + str(30 - s2))
+    else:
+        print("No space left in either section")
+
+def square(x):
+    return x * x
+def so_slow(num):
+    x = num
+    while x > 0:
+        x = x + 1
+    return x / 0
+
+# square(so_slow(5))
+# Infinite loop
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    """
+    if n == 1:
+        return False
+    k = 2
+    while k < n:
+        if n % k == 0:
+            return False
+        k += 1
+    return True
