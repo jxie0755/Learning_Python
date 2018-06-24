4533892b34e981040487a178c18d2ffd92482a7d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-24 14:07:56 -0400

    update flatten

--- ZCodeSnippets/flatten_list.py
+++ ZCodeSnippets/flatten_list.py
@@ -28,4 +28,3 @@ if __name__ == '__main__':
     assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
     assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
     print('Done! Check it')
-
