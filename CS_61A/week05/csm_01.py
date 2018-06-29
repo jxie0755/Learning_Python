bca10147c951501eff0d6ccaa0e7883be8149715
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-29 09:43:19 -0400

    cs61a week05 csm01 Finished

--- CS_61A/week05/csm_01.py
+++ CS_61A/week05/csm_01.py
@@ -1,5 +1,6 @@
 # CSM01 Tree Recursion and Data Abstraction
 
+# Recursion
 # Q1
 # Write a function is_sorted that takes in an integer n and returns true if the digits of that number are increasing from right to left.
 
@@ -37,7 +38,8 @@ def is_sorted(n):
         return is_sorted(rest)
 
 
-# Q2
+# Tree Recursion
+# Q1
 # Mario needs to jump over a series of Piranha plants, represented as a string of 0’s and 1’s.
 # Mario only moves forward and can either step (move forward one space) or jump (move forward two spaces) from each position.
 # How many different ways can Mario traverse a level without stepping or jumping into a Piranha plant?
@@ -132,4 +134,88 @@ if __name__ == '__main__':
         if make_change(i) != make_change_2(i):
             print('Whoops!')
     print('done')
-    
+
+
+# Data Abstraction
+# Q1
+# The following is an Abstract Data Type (ADT) for elephants.
+# Each elephant keeps track of its name, age, and whether or not it can fly.
+# Given our provided constructor, fill out the selectors
+
+def elephant(name, age, can_fly):
+    """
+    Takes in a string name, an int age, and a boolean can_fly.
+    Constructs an elephant with these attributes.
+    >>> dumbo = elephant("Dumbo", 10, True)
+    >>> elephant_name(dumbo)
+    "Dumbo"
+    >>> elephant_age(dumbo)
+    10
+    >>> elephant_can_fly(dumbo)
+    True
+    """
+    return [name, age, can_fly]
+
+
+def elephant_name(e):
+    return elephant[0]
+
+def elephant_age(e):
+    return elephant[1]
+
+def elephant_can_fly(e):
+    return elephant[2]
+
+# Q2
+# What's wrong?
+def elephant_roster(elephants):
+    """
+    Takes in a list of elephants and returns a list of their
+    names.
+    """
+    # return [elephant[0] for elephant in elephants]  # break abstraction barrier
+    return [elephant_name(elephant) for elephant in elephants]
+
+# Q3
+def elephant(name, age, can_fly):
+    return[[name, age], can_fly]
+
+def elephant_name(e):
+    return e[0][0]
+def elephant_age(e):
+    return e[0][1]
+def elephant_can_fly(e):
+    return e[1]
+
+# Q4
+# How can we write the fixed elephant_roster function for the constructors and
+# selectors in the previous question?
+def elephant_roster(elephants):
+    return [elephant_name(elephant) for elephant in elephants]
+
+# Q5 (optional)
+def elephant(name, age, can_fly):
+    """
+    >>> chris = elephant("Chris Martin", 38, False)
+    >>> elephant_name(chris)
+    'Chris Martin'
+    >>> elephant_age(chris)
+    38
+    >>> elephant_can_fly(chris)
+    False
+    """
+    def select(command):
+        command_dict = {'name':name, 'age':age, 'can_fly':can_fly}
+        return command_dict[command]
+
+    return select
+
+
+def elephant_name(e):
+    return e("name")
+
+def elephant_age(e):
+    return e("age")
+
+def elephant_can_fly(e):
+    return e("can_fly")
