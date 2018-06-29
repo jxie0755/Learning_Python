0fcead978b1d45c45d71cf105b5aeb7a93ff5a63
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-06-29 15:07:59 -0400

    update

--- CS_61A/week05/lecture12_trees.py
+++ CS_61A/week05/lecture12_trees.py
@@ -16,5 +16,5 @@ print(sum({3:9, 5:25}))
 print(max(range(10)))
 print(max(range(10), key=lambda x: 7 - (x-2)*(x-4)))
 print(all([x < 5 for x in range(5)]))
-print(perfect_square = lambda x: x == round(x ** 0.5) ** 2)
-print(any([perfect_square(x) for x in range(50, 60)]) # Try ,65))
+perfect_square = lambda x: x == round(x ** 0.5) ** 2
+print(any([perfect_square(x) for x in range(50, 60)])) # Try ,65
