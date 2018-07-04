96c7b91e469c125d66937766b22e27cfbe3116c1
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-04 19:20:41 -0400

    cs61a week05 lecture 12 extra binarize tree

--- CS_61A/week05/lecture12_trees.py
+++ CS_61A/week05/lecture12_trees.py
@@ -238,3 +238,17 @@ print_parts(partition_tree(6, 4))
 # 2 + 2 + 1 + 1
 # 2 + 1 + 1 + 1 + 1
 # 1 + 1 + 1 + 1 + 1 + 1
+
+
+# Right binarized tree
+def right_binarize(tree):
+    """Construct a right-branching binary tree."""
+    if is_leaf(tree):
+        return tree
+    if len(tree) > 2:
+        tree = [tree[0], tree[1:]]
+    return [right_binarize(b) for b in tree]
+
+# print(right_binarize([1, 2, 3, 4, 5, 6, 7])) some how does not work
+# >>>
+# [1, [2, [3, [4, [5, [6, 7]]]]]]
