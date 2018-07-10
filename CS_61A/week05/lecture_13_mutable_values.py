291a41be0f543c83d783c5f37f6ac210b287164f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-10 16:40:30 -0400

    cs61a week05 lecture 13 mutable values

--- CS_61A/week05/lecture_13_mutable_values.py
+++ CS_61A/week05/lecture_13_mutable_values.py
@@ -1,5 +1,13 @@
 # CS61A Lecture 13 Mutable Values
 
+a = [1,2,3]
+b = list(a)
+print(b)  # >>> [1,2,3]
+b[0] = a
+print(b)  # >>> [[1, 2, 3], 2, 3]
+
+
+
 def make_withdraw(balance):
     """Return a withdraw function with a starting balance."""
     def withdraw(amount):
@@ -9,6 +17,25 @@ def make_withdraw(balance):
         balance = balance - amount
         return balance
     return withdraw
+
+def make_withdraw2(balance):
+    """Return a withdraw function with a starting balance."""
+    def withdraw(amount):
+        # nonlocal balance
+        if amount > balance:
+            return 'Insufficient funds'
+        return balance - amount
+    return withdraw
+
+if __name__ == '__main__':
+
+    hundred = make_withdraw(100)
+    print(hundred(25)) # >>> 75
+    print(hundred(15)) # >>> 60  balance changed from previous operation
+
+    hundred = make_withdraw2(100)
+    print(hundred(25)) # >>> 75
+    print(hundred(15)) # >>> 85 balance not changed, still calculate from 100 everytime
 
 def make_withdraw_list(balance):
     b = [balance]
