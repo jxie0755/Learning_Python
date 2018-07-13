29f3476adf98d591e7828b4204f68a9d9fa55c5a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-13 11:26:56 -0400

    cs61a week05 lecture 13 constraint system

--- CS_61A/week05/lecture_13_mutable_values.py
+++ CS_61A/week05/lecture_13_mutable_values.py
@@ -123,3 +123,121 @@ d('setitem', 4, 16)
 print(d) # >>> <function dictionary.<locals>.dispatch at 0x00000129BB1956A8>
 print(d('getitem', 3))  # >>> 9
 print(d('getitem', 4))  # >>> 16
+
+
+# Constraint system
+# Fahrenheit and Celsius convert:
+# 9 * c = 5 * (f - 32)
+
+# The adder function constructs an adder constraint over three connectors, where the first two must add to the third: a + b = c
+
+from operator import add, sub
+
+def adder(a, b, c):
+    """The constraint that a + b = c."""
+    return make_ternary_constraint(a, b, c, add, sub, sub)
+
+# A multiplier is very similar to an adder
+
+from operator import mul, truediv
+
+def multiplier(a, b, c):
+    """The constraint that a * b = c."""
+    return make_ternary_constraint(a, b, c, mul, truediv, truediv)
+
+# define constant a value
+def constant(connector, value):
+        """The constraint that connector = value."""
+        constraint = {}
+        connector['set_val'](constraint, value)
+        return constraint
+
+def make_ternary_constraint(a, b, c, ab, ca, cb):
+        """The constraint that ab(a,b)=c and ca(c,a)=b and cb(c,b) = a."""
+        def new_value():
+            av, bv, cv = [connector['has_val']() for connector in (a, b, c)]
+            if av and bv:
+                c['set_val'](constraint, ab(a['val'], b['val']))
+            elif av and cv:
+                b['set_val'](constraint, ca(c['val'], a['val']))
+            elif bv and cv:
+                a['set_val'](constraint, cb(c['val'], b['val']))
+        def forget_value():
+            for connector in (a, b, c):
+                connector['forget'](constraint)
+        constraint = {'new_val': new_value, 'forget': forget_value}
+        for connector in (a, b, c):
+            connector['connect'](constraint)
+        return constraint
+
+# Design the connector
+
+def connector(name=None):
+    """A connector between constraints."""
+    informant = None
+    constraints = []
+    def set_value(source, value):
+        nonlocal informant
+        val = connector['val']
+        if val is None:
+            informant, connector['val'] = source, value
+            if name is not None:
+                print(name, '=', value)
+            inform_all_except(source, 'new_val', constraints)
+        else:
+            if val != value:
+                print('Contradiction detected:', val, 'vs', value)
+    def forget_value(source):
+        nonlocal informant
+        if informant == source:
+            informant, connector['val'] = None, None
+            if name is not None:
+                print(name, 'is forgotten')
+            inform_all_except(source, 'forget', constraints)
+    connector = {'val': None,
+                    'set_val': set_value,
+                    'forget': forget_value,
+                    'has_val': lambda: connector['val'] is not None,
+                    'connect': lambda source: constraints.append(source)}
+    return connector
+
+
+def inform_all_except(source, message, constraints):
+        """Inform all constraints of the message, except source."""
+        for c in constraints:
+            if c != source:
+                c[message]()
+
+
+def converter(c, f):
+    """Connect c to f with constraints to convert from Celsius to Fahrenheit."""
+    u, v, w, x, y = [connector() for i in range(5)]
+    multiplier(c, w, u)
+    multiplier(v, x, u)
+    adder(v, y, f)
+    constant(w, 9)
+    constant(x, 5)
+    constant(y, 32)
+
+
+celsius = connector('Celsius')
+fahrenheit = connector('Fahrenheit')
+converter(celsius, fahrenheit)
+
+if __name__ == '__main__':
+    fahrenheit['set_val']('user', 212)
+    fahrenheit['forget']('user')
+    fahrenheit['set_val']('user', 450)
+    fahrenheit['forget']('user')
+    celsius['set_val']('user', 37)
+    # >>>
+    # Fahrenheit = 212
+    # Celsius = 100.0
+    # Fahrenheit is forgotten
+    # Celsius is forgotten
+    # Fahrenheit = 450
+    # Celsius = 232.22222222222223
+    # Fahrenheit is forgotten
+    # Celsius is forgotten
+    # Celsius = 37
+    # Fahrenheit = 98.6
