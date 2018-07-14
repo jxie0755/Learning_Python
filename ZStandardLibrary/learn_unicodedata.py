c2c22f364496d81486f73d3cf418eb132a73ff06
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-14 15:55:29 -0400

    update

--- ZStandardLibrary/learn_unicodedata.py
+++ ZStandardLibrary/learn_unicodedata.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from unicodedata import name, lookup
 
 print(name('A'))
 # >>> LATIN CAPITAL LETTER A
 
-# print(lookup('WHITE SMILING FACE'))
+print(lookup('WHITE SMILING FACE'))
 # >>>
 
 print(lookup('BABY').encode())
