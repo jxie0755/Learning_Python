b2b537638667bc20b9cc07dd9aa8ce15d235da68
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-14 15:40:10 -0400

    learn python unicode data in standard library

---
+++ ZStandardLibrary/learn_unicodedata.py
@@ -0,0 +1,10 @@
+from unicodedata import name, lookup
+
+print(name('A'))
+# >>> LATIN CAPITAL LETTER A
+
+# print(lookup('WHITE SMILING FACE'))
+# >>>
+
+print(lookup('BABY').encode())
+# >>> b'\xf0\x9f\x91\xb6'
