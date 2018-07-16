c260912fc065b4118d5052e84f38418e34a8750d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-16 16:17:44 -0400

    cs61a  week05 project maps

--- CS_61A/project/maps/utils.py
+++ CS_61A/project/maps/utils.py
@@ -16,7 +16,7 @@ def map_and_filter(s, map_fn, filter_fn):
     [1, 9, 25]
     """
     # BEGIN Question 0
-    return ['REPLACE THIS WITH YOUR LIST COMPREHENSION']
+    return [map_fn(i) for i in s if filter_fn(i)]
     # END Question 0
 
 def key_of_min_value(d):
@@ -29,7 +29,7 @@ def key_of_min_value(d):
     'c'
     """
     # BEGIN Question 0
-    return min('REPLACE THIS WITH YOUR SOLUTION')
+    return min(d, key=d.get)
     # END Question 0
 
 def zip(*sequences):
@@ -61,7 +61,7 @@ def enumerate(s, start=0):
     [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
     """
     # BEGIN Question 0
-    "*** YOUR CODE HERE ***"
+    return zip(range(start, start+len(s)), s)
     # END Question 0
 
 def distance(pos1, pos2):
@@ -81,5 +81,5 @@ def mean(s):
     -0.5
     """
     # BEGIN Question 1
-    "*** YOUR CODE HERE ***"
+    return sum(s) / len(s)
     # END Question 1
