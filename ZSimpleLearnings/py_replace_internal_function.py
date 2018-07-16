d4695ecb0facdb4b95b67a197b233ef7c2bf5e1c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-07-16 16:20:01 -0400

    simple learning on how to create a new function with the same name of internal function

---
+++ ZSimpleLearnings/py_replace_internal_function.py
@@ -0,0 +1,15 @@
+# To repalce the internal functions in python, we can create another name for the function first before create a new version
+
+_list = list  # change the original list() to _list()
+
+# define the new list funciton
+def list(x):
+    """return the last item of a list"""
+    a = _list(x)
+    # you can still use the original method, with the new name _list
+
+    return a[-1]
+
+print(list((1,2,3)))
+# >>>
+# 3
