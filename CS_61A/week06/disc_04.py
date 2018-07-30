36c249558d625005726d23850cb4d1fa136a7be8
jxie0755 <jxie0755@users.noreply.github.com>
2018-07-30 16:49:43 -0400

    cs61a week06 disc 04

--- CS_61A/week06/disc_04.py
+++ CS_61A/week06/disc_04.py
@@ -1,2 +1,14 @@
 # CS61A Discussion 04 Nonlocals & Mutation
 
+
+# Nonlocal
+def stepper(num):
+    def step():
+        nonlocal num
+        num = num + 1
+        return num
+    return step
+
+s = stepper(3)
+print(s()) # >>> 4
+print(s()) # >>> 5
