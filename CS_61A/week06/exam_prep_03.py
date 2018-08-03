ea1dec66c3da4b4cc79d03a5c878b07b367e1c5a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-03 14:33:16 -0400

    cs61a week06 exam prep 03

--- CS_61A/week06/exam_prep_03.py
+++ CS_61A/week06/exam_prep_03.py
@@ -1,1 +1,41 @@
 # CS61A Exam Prep 03: List, Trees, & Tree Recursion
+
+
+def tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch)
+    return [label] + list(branches)
+
+
+def label(tree):
+    return tree[0]
+
+
+def branches(tree):
+    return tree[1:]
+
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree): # every branch must be a tree as well
+        if not is_tree(branch):
+            return False
+    return True
+
+# Q1
+
+def sum_range(t):
+    """Returns the range of the sums of t, that is,
+    the difference between the largest and the smallest sums of t."""
+    def helper(t):
+        if _________________________________________:
+            return [_____________, _____________]
+        else:
+            a = min([___________________________])
+            b = max([___________________________])
+            x = __________________________________
+            return [b + x, a + x]
+    x, y = helper(t)
+    return x - y
+
