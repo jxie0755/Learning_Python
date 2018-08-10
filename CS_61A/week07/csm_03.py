2332fb28c5e8b82da9ace1bd558ce1eec3014e1d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-10 18:43:24 -0400

    cs61a week07 csm03 finish

--- CS_61A/week07/csm_03.py
+++ CS_61A/week07/csm_03.py
@@ -1,6 +1,6 @@
 # CS61A CSM 03: Mutation and Nonlocal
 
-
+# Mutation
 # Q1 What would python display?
 
 a = [1, 2]
@@ -80,3 +80,71 @@ def accumulate(lst):
             sum_so_far += elem
             lst[i] = sum_so_far
     return sum_so_far
+
+
+# Non Local
+# Q1 Nonlocal Kale
+
+eggplant = 8
+def vegetable(kale):
+    def eggplant(spinach):
+        nonlocal eggplant
+        nonlocal kale
+        kale = 9
+        eggplant = spinach
+        print(eggplant, kale)
+    eggplant(kale)
+    return eggplant
+
+spinach = vegetable('kale')
+
+print(spinach)
+# >>>
+# kale 9
+# when execute eggplant(kale), eggplant = 'kale', kale = 9 (changed locally)
+
+# eggplant changed to spinach, which was 'kale'
+# >>> kale
+
+
+# Q2 Pingpong again
+# The ping-pong sequence counts up starting from 1 and is always either counting up or counting down.
+# At element k, the direction switches if k is a multiple of 7 or contains the digit 7.
+
+# The first 30 elements of the ping-pong sequence are listed below,
+# with direction swaps marked using brackets at the 7th, 14th, 17th, 21st, 27th, and 28th elements:
+# 1 2 3 4 5 6 [7] 6 5 4 3 2 1 [0] 1 2 [3] 2 1 0 [-1] 0 1 2 3 4 [5] [4] 5 6
+
+# Implement a function make_pingpong_tracker that returns the next value in the pingpong sequence each time it is called.
+#  You may use assignment statements
+
+def has_seven(k): # Use this function for your answer below
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+def make_pingpong_tracker():
+    """ Returns a function that returns the next value in the
+    pingpong sequence each time it is called.
+    >>> output = []
+    >>> x = make_pingpong_tracker()
+    >>> for _ in range(9):
+    ...    output += [x()]
+    >>> output
+    [1, 2, 3, 4, 5, 6, 7, 6, 5]
+    """
+    index, current, add = 1, 0, True
+    def pingpong_tracker():
+        nonlocal index, current, add
+        if add:
+            current = current + 1
+        else:
+            current = current - 1
+        if has_seven(current) or index % 7 == 0:
+            add = not add
+        index += 1
+        return current
+    return pingpong_tracker
