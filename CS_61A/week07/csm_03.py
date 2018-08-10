a7d6daf7bc05dc73fae9078af6393325d690f3f2
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-10 16:43:42 -0400

    cs61a week07 csm03 Q3

---
+++ CS_61A/week07/csm_03.py
@@ -0,0 +1,82 @@
+# CS61A CSM 03: Mutation and Nonlocal
+
+
+# Q1 What would python display?
+
+a = [1, 2]
+a.append([3, 4])
+print(a)
+# >>> [1, 2, [3, 4]]
+
+b = list(a)
+a[0] = 5  # [5, 2, [3, 4]]
+a[2][0] = 6 # [5, 2, [6, 4]]
+print(b)
+# >>> [1, 2, [6, 4]]
+# Pay attention: modification of item inside the list will change b because it is a pointer.
+
+a.extend([7])
+a += [8] # [5, 2, [6, 4], 8]
+# a += 9  # error
+print(a)
+# >>> [5, 2, [6, 4], 8]
+print(b)
+# >>> [1, 2, [6, 4]]
+
+# Challenge:
+b[2][1] = a[2:]
+# [1, 2, [6, [[[6, 4], 8]]]]
+
+print(a[2][1][0][0])
+# >>> 6
+
+# Q2
+
+a = [1, 2, [3]]
+def mystery(s, t):
+    s.pop(1)
+    t.append(s)
+    print(t)
+
+b = a
+a += [b[0]]
+# a = [1, 2, [3], 1], b same
+print(a == b)
+
+mystery(b, a[1:]) # a slice is a new list
+# b = [1, 2, [3], 1]
+# a[1:] = [2, [3], 1]
+
+# s.pop(1) -- b = [1, [3], 1], same as a
+
+# >>>[2, [3], 1, [1, [3], 1]]
+
+
+# Q3
+# Given some list lst, possibly a deep list, mutate lst to have the accumulated sum of all elements so far in the list. If there is a nested list
+#  mutate it to similarly reflect the accumulated sum of all elements so far in the nested list.
+# Return the total sum of lst
+
+def accumulate(lst):
+    """
+    >>> l = [1, 5, 13, 4]
+    >>> accumulate(l)
+    23
+    >>> l
+    [1, 6, 19, 23]
+    >>> deep_l = [3, 7, [2, 5, 6], 9]
+    >>> accumulate(deep_l)
+    32
+    >>> deep_l
+    [3, 10, [2, 7, 13], 32]
+    """
+    sum_so_far = 0
+    for i in range(len(lst)):
+        elem = lst[i]
+        if isinstance(elem, list):
+            inside = accumulate(elem)
+            sum_so_far = sum_so_far + inside
+        else:
+            sum_so_far += elem
+            lst[i] = sum_so_far
+    return sum_so_far
