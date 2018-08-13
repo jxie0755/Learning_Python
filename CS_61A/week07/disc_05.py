6ad2a186754348dabf4c525126e1d94fa80f3889
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-13 13:58:56 -0400

    cs61a week07 disc05 Q1 Q2

--- CS_61A/week07/disc_05.py
+++ CS_61A/week07/disc_05.py
@@ -1,6 +1,8 @@
 # CS61A Discussion 05 Object Oriented Programming
 
 
+# OOP
+# Q1
 class Instructor:
     degree = "PhD (Magic)" # this is a class attribute
 
@@ -46,18 +48,76 @@ if __name__ == '__main__':
     snape = TeachingAssistant("Snape")
     harry = Student("Harry", snape)
     harry.attend_lecture("potions")
+    # >>>
+    # Today we're learning about potions
+    # Dumbledore is awesome!
 
     hermione = Student("Hermione", snape)
     hermione.attend_lecture("herbology")
-
+    # >>>
+    # Today we're learning about herbology
+    # Dumbledore is awesome
 
     hermione.visit_office_hours(TeachingAssistant("Hagrid"))
+    # >>>
+    # Thanks, Hagrid
 
     print(harry.understanding)
+    # >>> 1
 
     print(snape.students["Hermione"].understanding)
+    # >>> 2  # even though not get this from snape (for visit office hour)
 
     Student.instructor = Instructor("Umbridge")
-
     Student.attend_lecture(harry, "transfiguration")
+    # >>>
+    # Today we're learning about transfiguration
+    # I miss Dumbledore
+
     # Equivalent to harry.attend_lecture("transfiguration")
+
+
+# Q2 Email
+# We now want to write three different classes, Mailman, Client, and Email to simulate email.
+# Fill in the definitions below to finish the implementation!
+
+class Email:
+    """Every email object has 3 instance attributes: the message, the sender name, and the recipient name.
+    """
+    def __init__(self, msg, sender_name, recipient_name):
+        pass
+
+
+class Mailman:
+    """Each Mailman has an instance attribute clients, which is a dictionary that associates client names with client objects.
+    """
+
+    def __init__(self):
+        self.clients = {}
+
+    def send(self, email):
+        """Take an email and put it in the inbox of the client it is addressed to."""
+        pass
+
+
+    def register_client(self, client, client_name):
+        """Takes a client object and client_name and adds it to the clients instance attribute."""
+        pass
+
+
+class Client:
+    """Every Client has instance attributes name (which is used for addressing emails to the client), mailman (which is used to send emails out to other clients), and inbox (a list of all emails the client has received).
+    """
+
+    def __init__(self, mailman, name):
+        self.inbox = []
+
+    def compose(self, msg, recipient_name):
+        """Send an email with the given message msg to the given recipient client."""
+        pass
+
+    def receive(self, email):
+        """Take an email and add it to the inbox of this client."""
+        pass
+
+
