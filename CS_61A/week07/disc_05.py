b379f55da7a59d58608c638577c8a3263d77387f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-13 15:48:03 -0400

    cs61a week07 disc 05 finished

--- CS_61A/week07/disc_05.py
+++ CS_61A/week07/disc_05.py
@@ -127,3 +127,151 @@ class Client:
         """Take an email and add it to the inbox of this client."""
         self.inbox.append(email)
 
+
+# Inheritance
+
+class Dog(object):
+
+    def __init__(self, name, owner):
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name + " says woof!")
+
+
+class Cat(object):
+
+    def __init__(self, name, owner, lives=9):
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name + " says meow!")
+
+# Notice that there’s a lot of repeated code! This is where inheritance comes in.
+# In Python, a class can inherit the instance variables and methods of another class
+
+class Pet(object):
+
+    def __init__(self, name, owner):
+        self.is_alive = True # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+class Dog(Pet):
+
+    def __init__(self, name, owner):
+        Pet.__init__(self, name, owner)
+    def talk(self):
+        print(self.name + ' says woof!')
+
+# Implement the Cat class by inheriting from the Pet class.
+# Make sure to use superclass methods wherever possible.
+# In addition, add a lose_life method to the Cat class.
+
+class Cat(Pet):
+    def __init__(self, name, owner, lives=9):
+        Pet.__init__(self, name, owner)
+        self.lives = lives
+        self.is_alive = True
+
+    def talk(self):
+        """A cat says meow! when asked to talk."""
+        print(self.name + " says meow!")
+
+
+    def lose_life(self):
+        """A cat can only lose a life if they have at least one life.
+        When lives reaches zero, 'is_alive' becomes False.
+        """
+        if self.is_alive == True:
+            if self.lives > 1:
+                self.lives -= 1
+            if self.lives == 1:
+                self.lives = 0
+                self.is_alive = False
+        else:
+            print('This cat has no more lives to lose:(')
+
+# More cats! Fill in the methods for NoisyCat, which is just like a normal Cat.
+# However, NoisyCat talks a lot, printing twice whatever a Cat says.
+
+class NoisyCat(Cat): # Fill me in!
+    """A Cat that repeats things twice."""
+    def __init__(self, name, owner, lives=9):
+        Cat.__init__(self, name, owner, lives)
+    # Is this method necessary? Why or why not?
+    # No, because everything is the same, no need to create __init__ again.
+
+    def talk(self):
+        """Repeat what a Cat says twice."""
+        Cat.talk(self)
+        Cat.talk(self)
+
+
+
+# What would Python display
+class A:
+
+    def f(self):
+        return 2
+
+    def g(self, obj, x):
+        if x == 0:
+            return A.f(obj)
+        return obj.f() + self.g(self, x - 1)
+
+
+class B(A):
+    def f(self):
+        return 4
+
+x, y = A(), B()
+print(x.f())
+# >>> 2
+
+
+# print(B.f()) # should be B.f(y)
+# >>> error
+
+
+print(x.g(x, 1))
+# >>> 2 + 2 = 4
+
+
+print(y.g(x, 2))
+# >>> 2 + 4 + 2 = 8
+
+
+class Yolo(object):
+    """# Implement the Yolo class so that the following interpreter session works as expected.
+
+    >>> x = Yolo(1)
+    >>> x.g(3)
+    4
+    >>> x.g(5)
+    6
+    >>> x.motto = 5
+    >>> x.g(5)
+    10
+    """
+
+    def __init__(self, num):
+        self.motto = num
+
+    def g(self, x):
+        return self.motto + x
