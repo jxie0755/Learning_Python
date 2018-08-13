65f58bae32bfe0c4e9a66798d7f33d4fec140a17
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-13 16:52:38 -0400

    update with a time-check function

--- ZStandardLibrary/learn_time.py
+++ ZStandardLibrary/learn_time.py
@@ -3,7 +3,38 @@
 
 # Check the time length the python execution
 
+
 import time
-start_time = time.time()
-# run main() codes
-print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    start_time = time.time()
+    # run main() codes
+    print(f"--- {time.time() - start_time}s seconds ---\n")
+
+
+# write into a fucntion
+def time_spend(code_to_check, *args, **kwds):
+    import time
+    start_time = time.time()
+    result = code_to_check(*args)
+    print(result)
+    print(f"--- {time.time() - start_time}s seconds ---\n")
+
+
+
+if __name__ == '__main__':
+
+    # testing function
+    def fib_gen_r(i):
+        """
+        Fibonacci function generator
+        generate the fibonacci number at 'i'th posistion
+        """
+        if i == 0:
+            return 0
+        elif i == 1:
+            return 1
+        else:
+            return fib_gen_r(i - 1) + fib_gen_r(i - 2)
+
+    time_spend(fib_gen_r, 35)
