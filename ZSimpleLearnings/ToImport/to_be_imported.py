eb0e3e0619ee7a4c754a96528983d2cfa2f492db
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-13 22:51:01 -0400

    learn python import method

---
+++ ZSimpleLearnings/ToImport/to_be_imported.py
@@ -0,0 +1,2 @@
+def foo_to_be_imported(x):
+    return x*10
--- ZSimpleLearnings/py_import.py
+++ ZSimpleLearnings/py_import.py
@@ -3,3 +3,17 @@ import itertools
 import functools
 
 # import a sub-directory
+from ToImport.to_be_imported import foo_to_be_imported
+
+# import a different directory in another folder
+
+# Wrong way
+# from ZStandardLibrary.learn_time import time_spent
+# >>> No module named 'ZStandardLibrary'
+
+# Correct way
+import sys
+
+sys.path.insert(0, 'c:\\Users\\jxie0\\Documents\\GitHub\\Learning_Python\\ZStandardLibrary')
+
+from learn_time import time_spent
