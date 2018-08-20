fa0afe70f39460b88773f5d9811bbf6c534a6e20
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-20 16:09:38 -0400

    cs61a week07 exam prep 04

--- CS_61A/week07/exam_prep_04.py
+++ CS_61A/week07/exam_prep_04.py
@@ -84,8 +84,7 @@ def decrypt(s, d):
     ... 'ring': 'ovaltine'
     ... }
     >>> decrypt('alanturing', codes)
-    ['drink your ovaltine', 'spooky ghosts', 'spooky scary
-    skeletons']
+    ['drink your ovaltine', 'spooky ghosts', 'spooky scary skeletons']
     """
     if s == '':
         return []
@@ -98,3 +97,52 @@ def decrypt(s, d):
             for rest in decrypt(suffix, d):
                 ms.append(d[first] + ' ' + rest)
     return ms
+
+
+# Q2
+# Consistency is Key
+
+def ensure_consistency(fn):
+    """Returns a function that calls fn on its argument,
+    returns fn's return value,
+    and returns None if fn's return value is different from any of its previous return values for those same argument.
+    Also returns None if more than 20 calls are made.
+    >>> def consistent(x):
+    ...    return x
+    >>>
+    >>> lst = [1, 2, 3]
+    >>> def inconsistent(x):
+    ...     return x + lst.pop()
+    >>>
+    >>> a = ensure_consistency(consistent)
+    >>> a(5)
+    5
+    >>> a(5)
+    5
+    >>> a(6)
+    6
+    >>> a(6)
+    6
+    >>> b = ensure_consistency(inconsistent)
+    >>> b(5)
+    8
+    >>> b(5)
+    >>> b(6)
+    7
+    """
+    n = 20
+    z = {}
+    def helper(x):
+        nonlocal n
+        n = n - 1
+        if n < 0:
+            return None
+        val = fn(x)
+        if x not in z:
+            z[x] = [val]
+        if x in z and z[x] == val:
+            return val
+        else:
+            z[x] = None
+            return None
+    return helper
