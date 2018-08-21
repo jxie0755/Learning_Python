85d7b8ba56905993abd1d4540ff321a66eb79a08
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-21 10:20:13 -0400

    update

--- CS_61A/week07/exam_prep_04.py
+++ CS_61A/week07/exam_prep_04.py
@@ -45,13 +45,6 @@ def about_equal(t1, t2):
     """Returns whether two trees are 'about equal.'
     Two trees are about equal if and only if they contain
     the same labels the same number of times.
-    >> x = tree(1, [tree(2), tree(2), tree(3)])
-    >> y = tree(3, [tree(2), tree(1), tree(2)])
-    >> about_equal(x, y)
-    True
-    >> z = tree(3, [tree(2), tree(1), tree(2), tree(3)])
-    >> about_equal(x, z)
-    False
     """
     def label_counts(t):
         if is_leaf(t):
