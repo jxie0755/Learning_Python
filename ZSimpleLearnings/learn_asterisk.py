8bb23fcf627197ce927748c9f2cfeeb2fc8c094a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-08-24 16:04:31 -0400

    Update learn_asterisk.py

--- ZSimpleLearnings/learn_asterisk.py
+++ ZSimpleLearnings/learn_asterisk.py
@@ -1,3 +1,7 @@
+# Asterisk is used different in def statement and actual code application.
+# in def statement, it allows function to carry multiple arguments
+# in code application, it force to unpack the iterable variables
+
 def asterisk(*args): # this allows carry multiple arguments
     print(*args)  # this does not pack args into a single items
     # because print let you carry multiple args as well
@@ -37,3 +41,24 @@ print(asterisk2(*[1,2,'a','b']))
 # Again, equal to asterisk2(1,2,'a','b'), as it forced to break down the list into individual args.
 # But then force to pack back into a tuple
 # >>> (1, 2, 'a', 'b')
+
+
+
+# double asterisk for keyword arguments
+def keyword(**kwargs):
+    print(kwargs)
+
+keyword(first='A', second='B', fourth='C', third='D', fifth='E')
+# >>>
+# {'first': 'A', 'second': 'B', 'fourth': 'C', 'third': 'D', 'fifth': 'E'}
+# kwargs output a dictionary
+
+
+def keyword(**kwargs):
+    return kwargs
+
+a = keyword(first='A', second='B', fourth='C', third='D', fifth='E')
+print(a)
+# >>> {'first': 'A', 'second': 'B', 'fourth': 'C', 'third': 'D', 'fifth': 'E'}
+print(type(a))
+# >>> <class 'dict'>
