0ad0d7509e579a78b9ef9eea2c4c8ca56287805a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-09-01 22:04:26 -0400

    Learn linked list class from CS61A

---
+++ ZCodeSnippets/class_linked_list.py
@@ -0,0 +1,39 @@
+# Linked list
+# a linked list is either empty or a first value and the rest of the linked list
+# one link contains a pair of objects: 1. The value 2. another linked list
+# At the end, use link.empty as a special instance.
+
+class Link:
+    """A linked list class"""
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        # This is different from type(rest) == Link
+        # Check detail from ZSimpleLearnings.py_instance_vs_type.py
+
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(self.first, rest_str)
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ', '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+    @property
+    def second(self):
+        return self.rest.first
+
+    @second.setter
+    def second(self, value):
+        self.rest.first = value
+
