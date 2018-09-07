2317f1cb904a03fbbe15bf12d935f197ff4a620d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-09-07 12:00:56 -0400

    cs61a week08 csm04 Q2 and Q3

--- CS_61A/week08/csm_04.py
+++ CS_61A/week08/csm_04.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
 
     # print(surya.pass_ball(surya, ajay)  # Error
 
+
 # Q2
 # Write TeamBaller, a subclass of Baller.
 # An instance of TeamBaller cheers on the team every time it passes a ball
@@ -56,12 +57,43 @@ class TeamBaller(Baller):
     I don't have the ball
     False
     """
+    # No need to rewrite the whole function.
     def pass_ball(self, other_player):
-        if self.has_ball:
-            self.has_ball = False
-            other_player.has_ball = True
-            print('Yay!')
-            return True
-        else:
-            print("I don't have the ball")
-            return False
+        did_pass = Baller.pass_ball(self, other_player)
+        print('Yay!' if did_pass else "I don't have the ball")
+        return did_pass
+
+# Q3 Pingpong OOP
+# As a reminder, the ping-pong sequence counts up starting from 1 and is always either
+# counting up or counting down.
+# At element k, the direction switches if k is a multiple of 7 or contains the digit 7.
+# The first 30 elements of the ping-pong sequence are listed below, with direction swaps
+# marked using brackets at the 7th, 14th, 17th, 21st, 27th, and 28th elements:
+# 1 2 3 4 5 6 [7] 6 5 4 3 2 1 [0] 1 2 [3] 2 1 0 [-1] 0 1 2 3 4
+# [5] [4] 5 6
+# Assume you have a function has seven(k) that returns True if k contains the digit
+# 7.
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+    """
+    return '7' in str(k)
+
+class PingPongTracker:
+    def __init__(self):
+        self.current = 0
+        self.index = 1
+        self.add = True
+
+    def next(self):
+        self.current = self.current + 1 if self.add else self.current - 1
+
+        if has_seven(self.index) or self.index % 7 == 0:
+            if self.add == True:
+                self.add = False
+            elif self.add == False:
+                self.add = True
+
+        self.index += 1
+
+        return self.current
