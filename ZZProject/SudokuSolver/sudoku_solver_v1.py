965ede21fa731e69bed9eea3efba300f7709a09d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-09-12 16:18:57 -0400

    setup a original puzzle for roll back

--- ZZProject/SudokuSolver/sudoku_solver_v1.py
+++ ZZProject/SudokuSolver/sudoku_solver_v1.py
@@ -35,6 +35,9 @@ class Sudoku(object):
         print(self)
         print('\n')
 
+        # Also create a permanent puzzle copy for future use
+        self.puzzle = [self.board[i][:] for i in range(9)]
+
     # Build up class attributes to for future to check up the rows, columns and grids.
     def row(self, n):
         """output a row of numbers
@@ -47,10 +50,34 @@ class Sudoku(object):
     def col(self, n):
         """output a column of numbers
         n: int 1-9
-        return: a list of numbers extracted from the row
+        return: a list of numbers extracted from the column
         """
         self.columns = [[self.board[i][j] for i in range(9)] for j in range(9)]
         return self.columns[n-1]
+
+    def grid(self, n):
+        """output a grid of 3*3 in the checkboard
+        n: int 1-9
+
+        The grid index on the checkerboad will be:
+        1 2 3
+        4 5 6
+        7 8 9
+
+        return: a list of numbers extracted from the grid
+        """
+        g1 = sum([[self.board[i][j] for j in range(0, 3)] for i in range(0, 3)], [])
+        g2 = sum([[self.board[i][j] for j in range(3, 6)] for i in range(0, 3)], [])
+        g3 = sum([[self.board[i][j] for j in range(6, 9)] for i in range(0, 3)], [])
+        g4 = sum([[self.board[i][j] for j in range(0, 3)] for i in range(3, 6)], [])
+        g5 = sum([[self.board[i][j] for j in range(3, 6)] for i in range(3, 6)], [])
+        g6 = sum([[self.board[i][j] for j in range(6, 9)] for i in range(3, 6)], [])
+        g7 = sum([[self.board[i][j] for j in range(0, 3)] for i in range(6, 9)], [])
+        g8 = sum([[self.board[i][j] for j in range(3, 6)] for i in range(6, 9)], [])
+        g9 = sum([[self.board[i][j] for j in range(6, 9)] for i in range(6, 9)], [])
+        self.grids = [g1, g2, g3, g4, g5, g6, g7, g8, g9]
+        return self.grids[n-1]
+
 
     def __str__(self):
         """to just print the current checker board
@@ -68,6 +95,18 @@ class Sudoku(object):
         to_print += x_num
         return to_print
 
+    def print_puzzle(self):
+        to_print = ''
+        y_num = 9
+        x_num = '   1, 2, 3, 4, 5, 6, 7, 8, 9'
+
+        for i in self.puzzle:
+            to_print += str(y_num) + ' ' + str(i) + '\n'
+            y_num -= 1
+
+        to_print += x_num
+        print(to_print)
+
     def insert(self, x, y, value):
         """to insert a value into the checkerboard
         for convenience, indext start from 1, and act like coordinates
@@ -79,64 +118,59 @@ if __name__ == '__main__':
     # list out 4 problems for test case
 
     # websudoku easy puzzle 10
-    easy_data = [
-                 (2,1,9), (5,1,3), (8,1,8),
-                 (6,2,4), (7,2,7), (9,2,5),
-                 (3,3,4), (4,3,8), (6,3,5), (7,3,9), (8,3,2),
-                 (2,4,5), (3,4,3), (5,4,4), (7,4,2), (8,4,6),
-                 (1,5,8), (9,5,7),
-                 (2,6,7), (3,6,6), (5,6,8), (7,6,1), (8,6,5),
-                 (2,7,4), (3,7,8), (4,7,7), (6,7,9), (7,7,6),
-                 (1,8,9), (3,8,5), (4,8,1),
-                 (2,9,1), (5,9,6), (8,9,9),
-                 ]
+    easy_data =[
+        (2,1,9), (5,1,3), (8,1,8),
+        (6,2,4), (7,2,7), (9,2,5),
+        (3,3,4), (4,3,8), (6,3,5), (7,3,9), (8,3,2),
+        (2,4,5), (3,4,3), (5,4,4), (7,4,2), (8,4,6),
+        (1,5,8), (9,5,7),
+        (2,6,7), (3,6,6), (5,6,8), (7,6,1), (8,6,5),
+        (2,7,4), (3,7,8), (4,7,7), (6,7,9), (7,7,6),
+        (1,8,9), (3,8,5), (4,8,1),
+        (2,9,1), (5,9,6), (8,9,9),
+    ]
 
     # websudoku medium puzzle 10
     medium_data = [
-                   (5,1,2), (6,1,8), (7,1,9), (9,1,6),
-                   (3,2,9), (5,2,1), (8,2,7), (9,2,4),
-                   (1,3,1), (4,3,7), (7,3,8), (8,3,5),
-                   (1,4,4), (4,4,3), (6,4,9),
-                   (3,5,1), (7,5,4),
-                   (4,6,2), (6,6,1), (9,6,8),
-                   (2,7,4), (3,7,6), (6,7,2), (9,7,1),
-                   (1,8,9), (2,8,1), (5,8,5), (7,8,2),
-                   (1,9,5), (3,9,2), (4,9,1), (5,9,8),
-                   ]
+        (5,1,2), (6,1,8), (7,1,9), (9,1,6),
+        (3,2,9), (5,2,1), (8,2,7), (9,2,4),
+        (1,3,1), (4,3,7), (7,3,8), (8,3,5),
+        (1,4,4), (4,4,3), (6,4,9),
+        (3,5,1), (7,5,4),
+        (4,6,2), (6,6,1), (9,6,8),
+        (2,7,4), (3,7,6), (6,7,2), (9,7,1),
+        (1,8,9), (2,8,1), (5,8,5), (7,8,2),
+        (1,9,5), (3,9,2), (4,9,1), (5,9,8)
+    ]
 
      # websudoku hard puzzle 10
     hard_data = [
-                 (1,1,5), (5,1,6), (6,1,4),
-                 (3,2,3), (4,2,1), (5,2,2), (9,2,7),
-                 (1,3,1), (2,3,6), (8,3,4),
-                 (8,4,5), (9,4,4),
-                 (4,5,7), (6,5,8),
-                 (1,6,9), (2,6,4),
-                 (2,7,5), (8,7,6), (9,7,2),
-                 (1,8,8), (5,8,5), (6,8,1), (7,8,3),
-                 (4,9,3), (5,9,7), (9,9,5),
-                 ]
+        (1,1,5), (5,1,6), (6,1,4),
+        (3,2,3), (4,2,1), (5,2,2), (9,2,7),
+        (1,3,1), (2,3,6), (8,3,4),
+        (8,4,5), (9,4,4),
+        (4,5,7), (6,5,8),
+        (1,6,9), (2,6,4),
+        (2,7,5), (8,7,6), (9,7,2),
+        (1,8,8), (5,8,5), (6,8,1), (7,8,3),
+        (4,9,3), (5,9,7), (9,9,5),
+    ]
 
      # websudoku evil puzzle 10
     evil_data = [
-                 (2,1,1), (5,1,9),
-                 (5,2,4), (6,2,3), (8,2,5),
-                 (7,3,2), (8,3,3), (9,3,6),
-                 (3,4,6), (9,4,4),
-                 (2,5,9), (4,5,7), (6,5,2), (8,5,8),
-                 (1,6,2), (7,6,5),
-                 (1,7,9), (2,7,3), (3,7,7),
-                 (2,8,5), (4,8,6), (5,8,8),
-                 (5,9,5), (8,9,9),
-                 ]
+        (2,1,1), (5,1,9),
+        (5,2,4), (6,2,3), (8,2,5),
+        (7,3,2), (8,3,3), (9,3,6),
+        (3,4,6), (9,4,4),
+        (2,5,9), (4,5,7), (6,5,2), (8,5,8),
+        (1,6,2), (7,6,5),
+        (1,7,9), (2,7,3), (3,7,7),
+        (2,8,5), (4,8,6), (5,8,8),
+        (5,9,5), (8,9,9),
+    ]
 
 
     easy = Sudoku(easy_data)
-    medium = Sudoku(medium_data)
-    hard = Sudoku(hard_data)
-    evil = Sudoku(evil_data)
-
-    print(evil.col(8))
-    evil.insert(8,1,3)
-    print(evil.col(8))
-
+    # medium = Sudoku(medium_data)
+    # hard = Sudoku(hard_data)
+    # evil = Sudoku(evil_data)
