1e5dbdbebb7397964fdf443ff8dd5cbb7b21f064
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-10-03 22:51:37 -0400

    update with a Binary Tree subclass

--- ZCodeSnippets/class_tree.py
+++ ZCodeSnippets/class_tree.py
@@ -427,3 +427,65 @@ if __name__ == '__main__':
     #     2
     #     3
     #       2
+
+
+
+# Binary trees
+
+# A sub class of regular Tree, that defines the binary tree:
+# A tree has exactly two branch, left and right
+
+class BTree(Tree):
+    """A tree with exactly two branches, which may be empty."""
+    empty = Tree(None)
+
+    def __init__(self, label, left=empty, right=empty):
+        for b in (left, right):
+            assert isinstance(b, BTree) or b is BTree.empty
+        Tree.__init__(self, label, (left, right))
+
+    @property
+    def left(self):
+        return self.branches[0]
+
+    @property
+    def right(self):
+        return self.branches[1]
+
+    def is_leaf(self):
+        return [self.left, self.right] == [BTree.empty] * 2
+
+    def __repr__(self):
+        if self.is_leaf():
+            return 'BTree({0})'.format(self.label)
+        elif self.right is BTree.empty:
+            left = repr(self.left)
+            return 'BTree({0}, {1})'.format(self.label, left)
+        else:
+            left, right = repr(self.left), repr(self.right)
+            if self.left is BTree.empty:
+                left = 'BTree.empty'
+            template = 'BTree({0}, {1}, {2})'
+            return template.format(self.label, left, right)
+
+if __name__ == '__main__':
+    t = BTree(3, BTree(1),
+                 BTree(7, BTree(5),
+                          BTree(9, BTree.empty,
+                                   BTree(11))))
+
+    print(t)
+    # >>>
+    # 3
+    #   1
+    #     None
+    #     None
+    #   7
+    #     5
+    #       None
+    #       None
+    #     9
+    #       None
+    #       11
+    #         None
+    #         None
