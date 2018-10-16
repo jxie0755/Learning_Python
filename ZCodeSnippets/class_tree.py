c5ea648fd1d57178221cf126d613b622b01a56b5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-10-15 23:34:06 -0400

    add a pretty tree function

--- ZCodeSnippets/class_tree.py
+++ ZCodeSnippets/class_tree.py
@@ -180,7 +180,6 @@ class Tree:
         for b in self.branches:
             b.prune_repeats(seen)
 
-
     # Set basic calculation and comparison
     def __add__(self, other):
         """
@@ -210,6 +209,84 @@ class Tree:
             tail = [b.convert_to_list() for b in self.branches]
             return [[self.label] + i for i in tail]
 
+
+# Beautiful vertical tree
+from io import StringIO
+# A StringIO is a file-like object that builds a string instead of printing
+# anything out.
+
+def height(tree):
+    """The height of a tree."""
+    if tree.is_leaf():
+        return 0
+    else:
+        return 1 + max([height(b) for b in tree.branches])
+
+def width(tree):
+    """Returns the printed width of this tree."""
+    label_width = len(str(tree.label))
+    w = max(label_width,
+            sum([width(t) for t in tree.branches]) + len(tree.branches) - 1)
+    extra = (w - label_width) % 2
+    return w + extra
+
+def pretty(tree):
+    """Print a tree laid out horizontally rather than vertically."""
+
+    def gen_levels(tr):
+        w = width(tr)
+        label = str(tr.label)
+        label_pad = " " * ((w - len(label)) // 2)
+        yield w
+        print(label_pad, file=out, end="")
+        print(label, file=out, end="")
+        print(label_pad, file=out, end="")
+        yield
+
+        if tr.is_leaf():
+            pad = " " * w
+            while True:
+                print(pad, file=out, end="")
+                yield
+        below = [ gen_levels(b) for b in tr.branches ]
+        L = 0
+        for g in below:
+            if L > 0:
+                print(" ", end="", file=out)
+                L += 1
+            w1 = next(g)
+            left = (w1-1) // 2
+            right = w1 - left - 1
+            mid = L + left
+            print(" " * left, end="", file=out)
+            if mid*2 + 1 == w:
+                print("|", end="", file=out)
+            elif mid*2 > w:
+                print("\\", end="", file=out)
+            else:
+                print("/", end="", file=out)
+            print(" " * right, end="", file=out)
+            L += w1
+        print(" " * (w - L), end="", file=out)
+        yield
+        while True:
+            started = False
+            for g in below:
+                if started:
+                    print(" ", end="", file=out)
+                next(g);
+                started = True
+            print(" " * (w - L), end="", file=out)
+            yield
+
+    out = StringIO()
+    h = height(tree)
+    g = gen_levels(tree)
+    next(g)
+    for i in range(2*h + 1):
+        next(g)
+        print(file=out)
+    print(out.getvalue(), end="")
 
 
 # Fibonacci tree setup by Tree class
@@ -256,6 +333,14 @@ if __name__ == '__main__':
     #   3
     #     6
     #     7
+
+    pretty(T)
+    # >>>
+    #    1
+    #  /   \
+    #  2   3
+    # / \ / \
+    # 4 5 6 7
 
     print(repr(T))
     # >>>
