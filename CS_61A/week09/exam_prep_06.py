c13918e9efa9fb439bfbae783844e9db5a831f6a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-11-05 22:26:35 -0500

    CS61a week09 exam prep 06 Q1 Q2

--- CS_61A/week09/exam_prep_06.py
+++ CS_61A/week09/exam_prep_06.py
@@ -2,7 +2,10 @@
 
 from CS_61A.week09.csm_05 import Link, Tree
 
+
 # Order of Growth
+
+# Q1
 # The Weakest Link (Su15 Midterm 2 Q5d)
 def append(link, value):
     """Mutates link by adding value to the end of link."""
@@ -10,6 +13,8 @@ def append(link, value):
         link.rest = Link(value)
     else:
         append(link.rest, value)
+
+# O(n)
 
 def extend(link1, link2):
     """Mutates link1 so that all elements of link2 are added
@@ -19,8 +24,30 @@ def extend(link1, link2):
         append(link1, link2.first)
         link2 = link2.rest
 
+# O(n^2)
 
-# Consider the following linked list functions: Circle the order of growth that best describes the runtime of calling append, where n is the number of elements in the input link.
+
+# Q2 Interpretation (Fa14 Mock Final Q5e)
+def g(n):
+    if n % 2 == 0 and g(n + 1) == 0:
+        return 0
+    return 5
+
+# O(1)
 
 
-# Assuming the two input linked lists to extend both contain n elements, circle the order of growth that best describes the runtime of calling extend.
+# Q3 Not with a fizzle, but with a bang (Su13 Midterm 2 Q2b)
+def boom(n):
+    if n == 0:
+        return "BOOM!"
+    return boom(n - 1)
+
+def explode(n):
+    if n == 0:
+        return boom(n)
+    i = 0
+    while i < n:
+        boom(n)
+        i += 1
+    return boom(n)
+
