b83bdd73706e6f1245dd1aa59c9514601612ce40
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-11-11 14:06:44 -0500

    CS61A week09 midterm 02 Q1

--- CS_61A/week09/midterm_2.py
+++ CS_61A/week09/midterm_2.py
@@ -1,2 +1,57 @@
 # CS61A spring 2018 midterm exam 1
 
+# Q1 Buggy Quidditch
+
+# If an error occurs, write Error, but include all output displayed before the error.
+# To display a function value, write Function.
+# If an expression would take forever to evaluate, write Forever.
+
+class Ball:
+    points = 0
+    time = lambda: 'Draco'
+
+    def score(self, who):
+        print(who, self.points)
+
+    def __str__(self):
+        return 'Magic'
+
+class Snitch(Ball):
+    points = 100
+    time = lambda: 'Harry'
+
+    def __init__(self):
+        self.points = self.points + 50
+
+    def score(self, p):
+        if not time():
+            print(Ball().score(p))
+        else:
+            Ball.score(self, p)
+
+def chase(r):
+    r.time = Snitch.time
+    r.points += 1
+    quaffle.points += 10
+    print(r().points)
+
+quaffle = Ball()
+quaffle.points = 10
+chasing = quaffle.score
+time = lambda: Ball.points
+malfoy = lambda: Ball.time()
+
+print(Snitch().points)
+
+
+print(chase(quaffle))
+
+
+print(Snitch().score('Seeker'))
+
+
+print(chase(Ball))
+
+
+print(Snitch().score(malfoy()))
+
