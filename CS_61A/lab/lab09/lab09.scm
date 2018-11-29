32c6f73d02cde48ebf154364a53e0c5b951840a9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-11-28 22:14:22 -0500

    CS61A Week10 Lab 09 Q2

--- CS_61A/lab/lab09/lab09.scm
+++ CS_61A/lab/lab09/lab09.scm
@@ -8,7 +8,7 @@
 ; scm> '(1 . (2 . 3))
 ; >>> (1 2 . 3)
 ; scm> (list 1 (cons 2 3))
-; >>> (1 (2 . 3)) ; list会强行完美造list,加一个nil在最后
+; >>> (1 (2 . 3)) ; list will create a perfect list, by adding a nil at the end
 
 ; scm> (cons 1 '(list 2 3))
 ; >>> (1 list 2 3)
@@ -19,20 +19,19 @@
 ; scm> '(cons 4 (cons (cons 6 8) ()))
 ; >>> (cons 4 (cons (cons 6 8) ()))
 
-; Q2
+
+; Q2 Over or Under
+
 (define (over-or-under x y)
-  'YOUR-CODE-HERE
+  (cond
+    ((< x y)-1)
+    ((> x y)1)
+    ((= x y)0))
 )
 
-;;; Tests
-(over-or-under 1 2)
-; expect -1
-(over-or-under 2 1)
-; expect 1
-(over-or-under 1 1)
-; expect 0
 
-; Q3
+; Q3 Filter
+
 (define (filter f lst)
   'YOUR-CODE-HERE
 )
