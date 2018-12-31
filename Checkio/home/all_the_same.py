7a3b724678fc65b24d5e4168ddcc669f2b5425d0
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2018-12-31 15:36:28 -0500

    Checkio Home all_the_same

---
+++ Checkio/home/all_the_same.py
@@ -0,0 +1,21 @@
+# In this mission you should check if all elements in the given list are equal.
+
+from typing import List, Any
+
+
+def all_the_same(elements: List[Any]) -> bool:
+    return elements[1:] == elements[:-1]
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
--- ZCodeSnippets/all_identical_list_items.py
+++ ZCodeSnippets/all_identical_list_items.py
@@ -3,13 +3,13 @@
 
 # All following function does the same thing, check if the set contains all identical elements
 def all_identical_1(iterable):
-    return iterable.count(matrix[0]) == len(iterable)
+    return iterable.count(iterable[0]) == len(iterable)
 
 def all_identical_2(iterable):
     return all(i==iterable[0] for i in iterable)
 
 def all_identical_3(iterable):
-    return len(set(iterable)) <= 1   
+    return len(set(iterable)) <= 1
 
 def all_identical_4(iterable):  # 错位对比法
    return iterable[1:] == iterable[:-1]  # can not be used for unslicable matrix (tuple)
@@ -17,7 +17,7 @@ def all_identical_4(iterable):  # 错位对比法
 if __name__ == '__main__':
     print(all_identical_3((1,1,1,1)))  # >>> True
     print(all_identical_4([1,2,3,4,5]))  # >>> False
-    
+
 
 # 利用以上方法
 # 假设一个matrix中含有很多list,写一个函数: 检查是否其中一个list中存在所有元素相等的情况
