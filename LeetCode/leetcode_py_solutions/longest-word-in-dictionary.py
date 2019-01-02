436e7e0baa1f7838a4f72ce0aa0f4376a301196c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-02 14:33:45 -0500

    Update with Leetcode solutions

---
+++ LeetCode/leetcode_py_solutions/longest-word-in-dictionary.py
@@ -0,0 +1,29 @@
+# Time:  O(n), n is the total sum of the lengths of words
+# Space: O(t), t is the number of nodes in trie
+
+import collections
+
+
+class Solution(object):
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie)["_end"] = i
+
+        # DFS
+        stack = trie.values()
+        result = ""
+        while stack:
+            curr = stack.pop()
+            if "_end" in curr:
+                word = words[curr["_end"]]
+                if len(word) > len(result) or (len(word) == len(result) and word < result):
+                    result = word
+                stack += [curr[letter] for letter in curr if letter != "_end"]
+        return result
+
---
+++ LeetCode/leetcode_py_solutions/merge-intervals.py
@@ -0,0 +1,31 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __repr__(self):
+        return "[{}, {}]".format(self.start, self.end)
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if not intervals:
+            return intervals
+        intervals.sort(key=lambda x: x.start)
+        result = [intervals[0]]
+        for i in xrange(1, len(intervals)):
+            prev, current = result[-1], intervals[i]
+            if current.start <= prev.end:
+                prev.end = max(prev.end, current.end)
+            else:
+                result.append(current)
+        return result
+
+
