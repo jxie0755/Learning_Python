8b2900ff4d37ea461c7a27e574e1d344450cddc9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-02 00:00:01 -0500

    CS61A Project Scheme Problem 1

---
+++ CS_61A/project/scheme/wb.py
@@ -0,0 +1,116 @@
+# Logistics
+# First unlock some tests:
+# python3 ok -u
+
+
+# Problem 1 > Suite 1 > Case 1
+# -- Already unlocked --
+
+
+# Problem 1 > Suite 1 > Case 2
+from scheme_reader import *
+tokens = tokenize_lines(["(+ 1 ", "(23 4)) ("])
+src = Buffer(tokens)
+
+src.current()
+# >>> '('
+
+src.remove_front()
+# >>> '('
+
+src.current()
+# >>> '+'
+
+src.remove_front()
+# >>> '+'
+
+src.remove_front()
+# >>> 1
+
+scheme_read(src)  # Returns and removes the next complete expression in src
+# >>> Pair(23, Pair(4, nil))
+
+src.current()
+# >>> ')'
+
+
+# Problem 1 > Suite 1 > Case 3
+from scheme_reader import *
+
+scheme_read(Buffer(tokenize_lines(['(23 4)'])))
+# >>> Pair(23, Pair(4, nil))
+
+read_line('(23 4)')  # Shorter version of above!
+# >>> Pair(23, Pair(4, nil))
+
+
+# Problem 1 > Suite 1 > Case 4
+from scheme_reader import *
+
+read_tail(Buffer(tokenize_lines([')'])))
+# >>> nil
+
+read_tail(Buffer(tokenize_lines(['1 2 3)'])))
+# >>> Pair(1, Pair(2, Pair(3, nil)))
+
+read_tail(Buffer(tokenize_lines(['2 (3 4))'])))
+# >>> Pair(2, Pair(Pair(3, Pair(4, nil)), nil))
+
+
+# Problem 1 > Suite 1 > Case 5
+# -- Already unlocked --
+
+
+# Problem 1 > Suite 1 > Case 6
+# -- Already unlocked --
+
+
+# Problem 1 > Suite 1 > Case 7
+read_line("(+ (- 2 3) 1)")
+# >>> Pair('+', Pair(Pair('-', Pair(2, Pair(3, nil))), Pair(1, nil)))
+# 相当于一个分叉链表
+
+
+# Problem 1 > Suite 1 > Case 8
+# -- Already unlocked --
+
+# OK! All cases for Problem 1 unlocked.
+
+
+
+# Problem 2 > Suite 1 > Case 1 - Case 4, Case 6-8 omitted
+from scheme_reader import *
+
+read_line("(a . b)")
+# >>> Pair('a', 'b')
+
+read_line("(a b . c)")
+# >>> Pair('a', Pair('b', 'c'))
+
+read_line("(a b . c d)")
+# >>> Syntax Error   # 若'.'不在末尾, 则不行, 必须是 (a (b . c) d)
+
+read_line("(a . (b . (c . ())))")
+# >>> Pair('a', Pair('b', Pair('c', nil)))  # 就是标准链表
+
+read_line("(a . ((b . (c))))")
+# >>> Pair('a', Pair(Pair('b', Pair('c', nil)), nil))  # 双括号则又相当于分叉链表了
+
+# OK! All cases for Problem 2 unlocked.
+
+
+
+# Understanding scheme.py > Suite 1 > Case 1 - Case
+# Q: A Scheme expression can be either...
+# A: A primitive expression or a list expression
+
+# Q: What expression in the body of scheme_eval finds the value of a name?
+
+
+
+
+
+
+
+
+
