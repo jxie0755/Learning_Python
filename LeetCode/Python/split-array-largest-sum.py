2765384c78774b370a376695aadf6643d3cef1b3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-02 19:14:14 -0500

    Update with Leetcode solutions

---
+++ LeetCode/Python/split-array-largest-sum.py
@@ -0,0 +1,32 @@
+# Time:  O(nlogs), s is the sum of nums
+# Space: O(1)
+
+class Solution(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        def canSplit(nums, m, s):
+            cnt, curr_sum = 1, 0
+            for num in nums:
+                curr_sum += num
+                if curr_sum > s:
+                    curr_sum = num
+                    cnt += 1
+            return cnt <= m
+
+        left, right = 0, 0
+        for num in nums:
+            left = max(left, num)
+            right += num
+
+        while left <= right:
+            mid = left + (right - left) / 2
+            if canSplit(nums, m, mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
---
+++ LeetCode/Python/word-ladder-ii.py
@@ -0,0 +1,42 @@
+# Time:  O(n * d), n is length of string, d is size of dictionary
+# Space: O(d)
+
+class Solution(object):
+    # @param start, a string
+    # @param end, a string
+    # @param dict, a set of string
+    # @return an integer
+    def findLadders(self, start, end, dict):
+        dict.add(start)
+        dict.add(end)
+
+        result, cur, visited, found, trace = [], [start], set([start]), False, {word: [] for word in dict}
+
+        while cur and not found:
+            for word in cur:
+                visited.add(word)
+
+            next = set()
+            for word in cur:
+                for i in xrange(len(word)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        candidate = word[:i] + j + word[i + 1:]
+                        if candidate not in visited and candidate in dict:
+                            if candidate == end:
+                                found = True
+                            next.add(candidate)
+                            trace[candidate].append(word)
+            cur = next
+
+        if found:
+            self.backtrack(result, trace, [], end)
+
+        return result
+
+    def backtrack(self, result, trace, path, word):
+        if not trace[word]:
+            result.append([word] + path)
+        else:
+            for prev in trace[word]:
+                self.backtrack(result, trace, [word] + path, prev)
+
