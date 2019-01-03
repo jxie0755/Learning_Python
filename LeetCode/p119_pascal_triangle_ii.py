a425f0fddb4a6a9596f6a7bd3ad330a6c0cb7f4d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-03 15:38:38 -0500

    LeetCode p119 Updated with a new method where go half-list, then mirror-copy

--- LeetCode/p119_pascal_triangle_ii.py
+++ LeetCode/p119_pascal_triangle_ii.py
@@ -22,12 +22,43 @@ class Solution:
         result = [1, 1]
 
         while current < rowIndex:
-            result = [1] + [result[i] + result[i-1] for i in range(1, len(result))] + [1]
+            result = [1] + [result[i] + result[i-1] for i in range(1, len(result))] + [1]  # 用list comprehension不好吗?
             current += 1
 
         return result
 
+    # Try to use O(k) space.
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+
+        if rowIndex == 0:
+            return [1]
+        elif rowIndex == 1:
+            return [1, 1]
+
+        current = 2
+        result = [1, 2]
+
+        while current < rowIndex:
+            x = result[-1]
+
+            result = [1] + [result[i] + result[i+1] for i in range(current // 2)]  # 不如只求一半,另一半用复制法节省时间
+            if current % 2 != 0:
+                result += [x*2]
+
+            current += 1
+
+        if rowIndex % 2 == 0:
+            return result + result[-2::-1]
+        else:
+            return result + result[::-1]
+
 
 if __name__ == '__main__':
-    assert Solution().getRow(3) == [1,3,3,1]
+    assert Solution().getRow(2) == [1, 2, 1]
+    assert Solution().getRow(3) == [1, 3, 3, 1]
+    assert Solution().getRow(4) == [1, 4, 6, 4, 1]
     print('all passed')
