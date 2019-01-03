ed8c0ab8b460464c0381964365ef00ebce07b505
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-03 18:19:14 -0500

    Final update on all 4 method None of them can go below 50ms

--- LeetCode/p119_pascal_triangle_ii.py
+++ LeetCode/p119_pascal_triangle_ii.py
@@ -9,7 +9,8 @@
 from math import factorial
 
 class Solution:
-    def getRow_old(self, rowIndex):
+
+    def getRow_old(self, rowIndex):  # 方法1, 普通
         """
         :type rowIndex: int
         :rtype: List[int]
@@ -24,13 +25,13 @@ class Solution:
         result = [1, 1]
 
         while current < rowIndex:
-            result = [1] + [result[i] + result[i-1] for i in range(1, len(result))] + [1]  # 用list comprehension不好吗?
+            result = [1] + [result[i] + result[i-1] for i in range(1, len(result))] + [1]
             current += 1
 
         return result
 
-    # Try to use O(k) space.
-    def getRow(self, rowIndex):
+
+    def getRow(self, rowIndex):  # 方法二, 利用对称性只做一半的工作
         """
         :type rowIndex: int
         :rtype: List[int]
@@ -52,19 +53,22 @@ class Solution:
 
             current += 1
 
+        # 根据奇数或者偶数长度判断怎么对折复制
         if rowIndex % 2 == 0:
             return result + result[-2::-1]
         else:
             return result + result[::-1]
 
-    # 利用杨辉三角数学性质, 第 n 行的第  k 个数字为组合数 C(k-1/n-1)
+
+    # 利用杨辉三角数学性质, 第 n 行的第  k 个数字为组合数 C(k/n)
     def getRow_math(self, rowIndex):
 
         result = []
         for k in range(0, rowIndex+1):
-            result.append(factorial(rowIndex) // factorial(k) // factorial(rowIndex-k))
+            result.append(factorial(rowIndex) // factorial(k) // factorial(rowIndex-k))  # 用阶乘做组合运算
 
         return result
+
 
     # 就这样还是慢,阶乘算太多次了, 必须结合折半法...
     def getRow_math2(self, rowIndex):
@@ -86,4 +90,4 @@ if __name__ == '__main__':
     assert Solution().getRow(4) == [1, 4, 6, 4, 1]
     print('all passed')
 
-    Solution().getRow_math2(1000)
+    print(Solution().getRow_math(4))
