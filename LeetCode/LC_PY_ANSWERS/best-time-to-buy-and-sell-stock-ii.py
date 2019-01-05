1a0ce54e26b2e88d007e1bd9df1a9ee4b4c0068c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-05 14:42:55 -0500

    update xrange

--- LeetCode/LC_PY_ANSWERS/best-time-to-buy-and-sell-stock-ii.py
+++ LeetCode/LC_PY_ANSWERS/best-time-to-buy-and-sell-stock-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
     # @return an integer
     def maxProfit(self, prices):
         profit = 0
-        for i in xrange(len(prices) - 1):
+        for i in range(len(prices) - 1):
             profit += max(0, prices[i + 1] - prices[i])
         return profit
 
