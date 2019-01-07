19309c6bd07365c20d73c834c728371b6d42bcf9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-07 13:27:58 -0500

    LeetCode Finish

--- LeetCode/p122_best_time_to_buy_and_sell_stock_ii.py
+++ LeetCode/p122_best_time_to_buy_and_sell_stock_ii.py
@@ -1,3 +1,7 @@
+# P122 Best time to buy and sell stock II
+# Easy
+
+
 # Say you have an array for which the ith element is the price of a given stock on day i.
 
 # Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
--- LeetCode/p167_two_sum_ii.py
+++ LeetCode/p167_two_sum_ii.py
@@ -1,3 +1,7 @@
+# P167 Two Sum II
+# Easy
+
+
 # Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 
 # The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
