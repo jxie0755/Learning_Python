69e7135968ef9d325651d78adc8a33637eb7860a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-07 13:58:41 -0500

    LeetCode P189 finished

---
+++ LeetCode/p189_rotate_array.py
@@ -0,0 +1,39 @@
+# P189 Rotate Array
+# Easy
+
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution:
+    def rotate(self, nums, k):
+        ### O(N) space, with O(N) time.
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        real_k = k % len(nums)
+        target = nums[-real_k:] + nums[:-real_k]
+        i = 0
+        while i != len(nums):
+            nums[i] = target[i]
+            i += 1
+
+
+if __name__ == '__main__':
+    a = [1,2,3,4,5,6,7]
+    Solution().rotate(a, 0)
+    assert a == [1,2,3,4,5,6,7], 'Edge'
+
+    a = [1,2,3,4,5,6,7]
+    Solution().rotate(a, 3)
+    assert a == [5,6,7,1,2,3,4], 'Example 1'
+
+    a = [-1,-100,3,99]
+    Solution().rotate(a, 2)
+    assert a == [3,99,-1,-100], 'Example 2'
+
+    a = [1,2,3,4]
+    Solution().rotate(a, 5)
+    assert a == [4,1,2,3], 'Large k'
+
+    print('all passed')
