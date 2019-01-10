d6f4cb1cd8f85bd196ddce8115a40ea11b5689de
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-10 11:32:49 -0500

    Leetcode p01 Finished

--- LeetCode/p017_letter_combination_of_a_phone_number.py
+++ LeetCode/p017_letter_combination_of_a_phone_number.py
@@ -10,20 +10,54 @@
 
 class Solution:
     def letterCombinations(self, digits):
+        ### Time:  O(n * 4^n)
         """
         :type digits: str
         :rtype: List[str]
         """
-        pass
+        hashtable = {'0': [' '],
+                     '1': [''],
+                     '2': ['a', 'b', 'c'],
+                     '3': ['d', 'e', 'f'],
+                     '4': ['g', 'h', 'i'],
+                     '5': ['j', 'k', 'l'],
+                     '6': ['m', 'n', 'o'],
+                     '7': ['p', 'q', 'r', 's'],
+                     '8': ['t', 'u', 'v'],
+                     '9': ['w', 'x', 'y', 'z']
+                     }
+
+        result = []
+        i = 0
+        while i != len(digits):
+            current = digits[i]
+
+            if i == 0:
+                result = hashtable[current]
+            else:
+                new = hashtable[current]
+                result_len, new_len = len(result), len(new)
+                result = sum([[i] * new_len for i in result], [])
+                j, cycle = 0, 0
+                while j != len(result):
+                    result[j] = result[j] + new[cycle % new_len]
+                    j += 1
+                    cycle += 1
+            i += 1
+
+        return result
 
 
 
 if __name__ == '__main__':
     assert Solution().letterCombinations('') == [], 'Edge 1'
-    assert Solution().letterCombinations('1') == [], 'Edge 2'
+    assert Solution().letterCombinations('1') == [''], 'Edge 2'
+    assert Solution().letterCombinations('0') == [' '], 'Edge 3'
+
 
     assert Solution().letterCombinations('2') == ['a', 'b', 'c'], 'Example 1'
-    assert Solution().letterCombinations('23') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], 'Example 1'
-    assert Solution().letterCombinations('213') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], 'Example 2'
+    assert Solution().letterCombinations('20') == ['a ', 'b ', 'c '], 'Example 2'
+    assert Solution().letterCombinations('23') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], 'Example 3'
+    assert Solution().letterCombinations('213') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], 'Example 4'
 
     print('all passed')
