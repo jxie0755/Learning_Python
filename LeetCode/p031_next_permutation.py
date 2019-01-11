aa8c3b50492935549fef14b60e916e2c799ee922
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-10 22:04:39 -0500

    Leetcode p031 Finished

--- LeetCode/p031_next_permutation.py
+++ LeetCode/p031_next_permutation.py
@@ -64,6 +64,30 @@ class Solution:
         if first_sample:
             nums[:] = first
 
+class Solution:
+    def nextPermutation(self, nums):
+        ###
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        cur_i = None
+
+        for i in range(-2, -length-1, -1):
+            if nums[i] < nums[i+1]:
+                cur_i = i
+                break
+
+        if not cur_i:
+            nums[:] = nums[::-1]  # 直接结束,因为本身是倒着排序的,返回正排序即可
+        else:
+            for rev_i in range(-1, cur_i, -1):
+                if nums[rev_i] > nums[cur_i]:
+                    nums[cur_i], nums[rev_i] = nums[rev_i], nums[cur_i]  # switch
+                    nums[cur_i+1:] = nums[cur_i+1:][::-1]
+                    break
+
 
 if __name__ == '__main__':
     a = []
@@ -102,13 +126,8 @@ if __name__ == '__main__':
     Solution().nextPermutation(a)
     assert a == [2,1,2,2], 'Extra 3'
 
+    a = [2,3,1]
+    Solution().nextPermutation(a)
+    assert a == [3,1,2]
+
     print('all passed')
-
-
-
-
-
-
-
-
-
