8d25fbf78bbb2a10a71e4599cb4191af81357e62
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-14 16:54:55 -0500

    Leetcode P005 update with Manacher's algorithm

--- LeetCode/LC_PY_ANSWERS/longest-palindromic-substring.py
+++ LeetCode/LC_PY_ANSWERS/longest-palindromic-substring.py
@@ -1,6 +1,16 @@
 # Time:  O(n)
 # Space: O(n)
 
+# Detail explanation on Manacher Algorithm:
+# https://www.youtube.com/watch?v=V-sEwsca1ak
+# They key is to use the symmetry of palindrome to predict next center and (expand from a new length, if partially covered)
+
+#                       1  1 1 1 1 1 1 1
+#  0 1 2 3 4 5 6 7 8 9  0  1 2 3 4 5 6 7
+#  a b a x a b a x a b  y  b a x a b y b
+#  1 3 1 7 1 0 1 5 1 1 11  1 1 5 9
+
+
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -8,6 +18,7 @@ class Solution(object):
         :rtype: str
         """
         t = '#' + '#'.join(s) + '#'
+
         n = len(t)
         RL = [0] * n
         ans = ""
@@ -16,7 +27,9 @@ class Solution(object):
         pos = 0
         for i in range(n):
             RL[i] = min(maxRight - i, RL[pos*2-i]) if i < maxRight else 1
+            print(RL[i])
             while i >= RL[i] and i+RL[i] < len(t) and t[i+RL[i]] == t[i-RL[i]]:
+                print(RL)
                 RL[i] += 1
             if i + RL[i]-1 > maxRight:
                 maxRight = i + RL[i] - 1
@@ -24,7 +37,5 @@ class Solution(object):
             if RL[i] > maxLen:
                 maxLen = RL[i]
                 ans = s[(i + 1 - maxLen)//2: (i - 1 + maxLen)//2]
+        print(t)
         return ans
-
-
-print(Solution().longestPalindrome('cbbd'))
