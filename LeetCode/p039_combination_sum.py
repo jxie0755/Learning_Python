d924731a2542475afb083a56e8979b223245acda
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-14 17:27:49 -0500

    Leetcode P039 Initiation

---
+++ LeetCode/p039_combination_sum.py
@@ -0,0 +1,47 @@
+# P039 Cobination Sum
+# Medium
+
+
+# Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
+# find all unique combinations in candidates where the candidate numbers sums to target.
+
+# The same repeated number may be chosen from candidates unlimited number of times.
+
+# Note:
+# All numbers (including target) will be positive integers.
+# The solution set must not contain duplicate combinations.
+
+
+
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+
+
+
+
+
+
+
+assert Solution().combinationSum([2,3,6,7], 7) == [[7], [2,2,3]], 'Example 1'
+assert Solution().combinationSum([2,3,5], 8) == [[2,2,2,2], [2,3,3], [3,5]], 'Example 2'
+
+
+
+
+
+
+
+
+
+
+
+
+
