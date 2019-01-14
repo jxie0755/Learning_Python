04bdd3204a866dfeb4eb8b47d17dcd77e988b4d6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-14 13:23:49 -0500

    update with O(N) method

--- LeetCode/LC_PY_ANSWERS/longest-palindromic-substring.py
+++ LeetCode/LC_PY_ANSWERS/longest-palindromic-substring.py
@@ -7,36 +7,24 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        def preProcess(s):
-            if not s:
-                return ['^', '$']
-            T = ['^']
-            for c in s:
-                T +=  ['#', c]
-            T += ['#', '$']
-            return T
-
-        T = preProcess(s)
-        P = [0] * len(T)
-        center, right = 0, 0
-        for i in xrange(1, len(T) - 1):
-            i_mirror = 2 * center - i
-            if right > i:
-                P[i] = min(right - i, P[i_mirror])
-            else:
-                P[i] = 0
-
-            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-                P[i] += 1
-
-            if i + P[i] > right:
-                center, right = i, i + P[i]
-
-        max_i = 0
-        for i in xrange(1, len(T) - 1):
-            if P[i] > P[max_i]:
-                max_i = i
-        start = (max_i - 1 - P[max_i]) / 2
-        return s[start : start + P[max_i]]
+        t = '#' + '#'.join(s) + '#'
+        n = len(t)
+        RL = [0] * n
+        ans = ""
+        maxLen = 0
+        maxRight = 0
+        pos = 0
+        for i in range(n):
+            RL[i] = min(maxRight - i, RL[pos*2-i]) if i < maxRight else 1
+            while i >= RL[i] and i+RL[i] < len(t) and t[i+RL[i]] == t[i-RL[i]]:
+                RL[i] += 1
+            if i + RL[i]-1 > maxRight:
+                maxRight = i + RL[i] - 1
+                pos = i
+            if RL[i] > maxLen:
+                maxLen = RL[i]
+                ans = s[(i + 1 - maxLen)//2: (i - 1 + maxLen)//2]
+        return ans
 
 
+print(Solution().longestPalindrome('cbbd'))
