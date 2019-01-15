eecf0d85f432a9d39b0db79585e186ff945af6a7
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-15 15:24:01 -0500

    Leetcode P039 Version 1 brutal force

--- LeetCode/p039_combination_sum.py
+++ LeetCode/p039_combination_sum.py
@@ -15,15 +15,39 @@
 
 class Solution:
     def combinationSum(self, candidates, target):
+        ### Brutal Force O(N^2), very slow but passed
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        pass
+        if not candidates:
+            return []
 
+        def process(temp):
+            new_temp = []
+            i = 0
+            while i < len(temp):
+                if sum(temp[i]) == target:
+                    result.append(temp[i])
+                for j in candidates:
+                    if sum(temp[i]) + j < target:
+                        new_temp.append(temp[i] + [j])
+                    elif sum(temp[i]) + j == target:
+                        ans = sorted(temp[i] + [j])
+                        if ans not in result:
+                            result.append(ans)
+                i += 1
+            return new_temp
 
+        candidates = [i for i in sorted(candidates) if i <= target]
+        temp = [[i] for i in candidates]
+        result = []
 
+        while temp:
+            temp = process(temp)
+
+        return result
 
 
 
@@ -35,18 +59,8 @@ assert Solution().combinationSum([2], 1) == [], 'Edge 4'
 assert Solution().combinationSum([2], 5) == [], 'Edge 5'
 
 assert Solution().combinationSum([2,3,6,7], 7) == [[7], [2,2,3]], 'Example 1'
-assert Solution().combinationSum([2,3,5], 8) == [[2,2,2,2], [2,3,3], [3,5]], 'Example 2'
+assert Solution().combinationSum([2,3,5], 8) == [[3, 5], [2, 3, 3], [2, 2, 2, 2]], 'Example 2'
+
+assert Solution().combinationSum([2,4], 10) == [[2, 4, 4], [2, 2, 2, 4], [2, 2, 2, 2, 2]], 'Extra 1'
 
 print('all passed')
-
-
-
-
-
-
-
-
-
-
-
-
