73b0c455bd431f8e9ce58c1b3e7746cf64bcf320
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-16 11:04:05 -0500

    Leetcode P141 New iead by break down node-links

--- LeetCode/p141_linked_list_cycle.py
+++ LeetCode/p141_linked_list_cycle.py
@@ -19,7 +19,7 @@ class ListNode(object):
 
 class Solution(object):
     def hasCycle(self, head):
-        ### Time O(N), Space O(N), Passed, but very slow.
+        ### Time O(N^2), Space O(N), Passed, but very slow.
         """
         :type head: ListNode
         :rtype: bool
@@ -34,8 +34,70 @@ class Solution(object):
             else:
                 node_list.append(tail)
                 head = tail
+
         return False
 
+class Solution(object):
+    def hasCycle(self, head):
+        ### Time O(N), Space O(N), use hashtable to search faster
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        hashtable = {head:1}
+        tail = head.next
+        while tail is not None:
+            if tail not in hashtable:
+                hashtable[tail] = 1
+            elif hashtable[tail] == 1:
+                return True
+            else:
+                hashtable[tail] += 1
+            tail = tail.next
+
+        return False
+
+class Solution_X(object):
+    def hasCycle(self, head):
+        ### Time O(N), Space O(1)
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        checkednode = ListNode(99)
+        if not head:
+            return False
+        while head.next:
+            if head.next == checkednode:
+                return True
+            checked = head    # mark previous head
+            head = head.next  # move to next head
+            checked.next = checkednode  # break down the previous head link to next head, force previous head all to link to the same checknode
+            # if any node cycle back to a previous node, then it will be caught as the next of that previous node is a checknode
+
+        return False
+
+class Solution_Y(object):
+    def hasCycle(self, head):
+        ### Time O(N), Space O(N)
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        while head.next:
+            if head.next.val == 'X': # assume 'X' will not be in any of the original linked list node value
+                return True
+            checked = head
+            head = head.next
+            checked.next = ListNode('X')  # break down the previous head link to next head, force previous head all to link to a ListNode
+        return False
+
+# The Solution X is better as it only create one additional checknode for every previous node to point to
+# The Solution Y forces every node pointed to a different node but has the same value. You can not guarantee that the value is not in orignal linked list. And it also uses much more space.
 
 if __name__ == '__main__':
     assert Solution().hasCycle(None) is False, 'Edge'
@@ -53,19 +115,18 @@ if __name__ == '__main__':
 
     assert Solution().hasCycle(a) is True, 'Example 1'
 
-
     # Example 2
     a = ListNode(1)
     b = ListNode(2)
     a.next = b
     b.next = a
 
-    assert Solution().hasCycle(a) is True, 'Example 2'
+    # assert Solution().hasCycle(a) is True, 'Example 2'
 
 
     # Example 3
     a = ListNode(1)
 
-    assert Solution().hasCycle(a) is False, 'Example 3'
+    # assert Solution().hasCycle(a) is False, 'Example 3'
 
     print('all passed')
