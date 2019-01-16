a21f3c52f368b46273156ff4f5463046b6f59e80
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-15 21:14:56 -0500

    Leetcode P039 Learning backtracking method

--- LeetCode/p039_combination_sum.py
+++ LeetCode/p039_combination_sum.py
@@ -40,7 +40,7 @@ class Solution:
                 i += 1
             return new_temp
 
-        candidates = [i for i in sorted(candidates) if i <= target]
+        candidates = sorted(candidates)
         temp = [[i] for i in candidates]
         result = []
 
@@ -50,17 +50,51 @@ class Solution:
         return result
 
 
+class Solution(object):
+    # @param candidates, a list of integers
+    # @param target, integer
+    # @return a list of lists of integers
+    def combinationSum(self, candidates, target):
 
+        # Add a recursive process method to update result
+        def process(candidates, start, intermediate, target):
+            """
+            Args:
+                candidates:    ingredient numbers (sorted)
+                start:         from 0, full candidates, to a shorter candidate list
+                intermediate:  temp list to record current status
+                target:        tartget to compose
 
-assert Solution().combinationSum([], 1) == [], 'Edge 1'
-assert Solution().combinationSum([1], 1) == [[1]], 'Edge 2'
-assert Solution().combinationSum([1], 2) == [[1,1]], 'Edge 3'
-assert Solution().combinationSum([2], 1) == [], 'Edge 4'
-assert Solution().combinationSum([2], 5) == [], 'Edge 5'
+            Returns: None, Imperfect function, just update the result
+            """
+            if target == 0:
+                result.append(list(intermediate))  # 终止case, target降到0就完成
 
-assert Solution().combinationSum([2,3,6,7], 7) == [[7], [2,2,3]], 'Example 1'
-assert Solution().combinationSum([2,3,5], 8) == [[3, 5], [2, 3, 3], [2, 2, 2, 2]], 'Example 2'
+            while start < len(candidates) and candidates[start] <= target: # while loop 走完全部candidates
+                intermediate.append(candidates[start])
+                process(candidates, start, intermediate, target - candidates[start])
+                print(intermediate)
+                intermediate.pop()
+                start += 1
 
-assert Solution().combinationSum([2,4], 10) == [[2, 4, 4], [2, 2, 2, 4], [2, 2, 2, 2, 2]], 'Extra 1'
+        result = []
+        candidates = sorted(candidates)
+        process(candidates, 0, [], target)
 
-print('all passed')
+        return result
+
+Solution().combinationSum([2,3,6,7], 7)
+
+# if __name__ == '__main__':
+#     assert Solution().combinationSum([], 1) == [], 'Edge 1'
+#     assert Solution().combinationSum([1], 1) == [[1]], 'Edge 2'
+#     assert Solution().combinationSum([1], 2) == [[1,1]], 'Edge 3'
+#     assert Solution().combinationSum([2], 1) == [], 'Edge 4'
+#     assert Solution().combinationSum([2], 5) == [], 'Edge 5'
+#
+#     assert Solution().combinationSum([2,3,6,7], 7) == [[2, 2, 3], [7]], 'Example 1'
+#     assert Solution().combinationSum([2,3,5], 8) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]], 'Example 2'
+#
+#     assert Solution().combinationSum([2,4], 10) == [[2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4]], 'Extra 1'
+#
+#     print('all passed')
