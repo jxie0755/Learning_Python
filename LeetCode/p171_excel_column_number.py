3c2c43336496ff7f6a17e35a1f65f720ebdcf11a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-17 16:50:44 -0500

    Leetcode p171 Passed

---
+++ LeetCode/p171_excel_column_number.py
@@ -0,0 +1,61 @@
+# P171 Excel Column Number
+# Easy
+
+# Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        translate = {
+            'A': 1,
+            'B': 2,
+            'C': 3,
+            'D': 4,
+            'E': 5,
+            'F': 6,
+            'G': 7,
+            'H': 8,
+            'I': 9,
+            'J': 10,
+            'K': 11,
+            'L': 12,
+            'M': 13,
+            'N': 14,
+            'O': 15,
+            'P': 16,
+            'Q': 17,
+            'R': 18,
+            'S': 19,
+            'T': 20,
+            'U': 21,
+            'V': 22,
+            'W': 23,
+            'X': 24,
+            'Y': 25,
+            'Z': 26,
+        }
+
+        s_end, s_head = s[-1], s[-2::-1]
+        end_val = translate[s_end]
+        head_val = 0
+
+        step = 26
+        for i in s_head:
+            head_val += translate[i] * step
+            step *= 26
+
+        return head_val + end_val
+
+
+if __name__ == '__main__':
+    assert Solution().titleToNumber('A') == 1, 'Example 1'
+    assert Solution().titleToNumber('AA') == 27, 'Example 2'
+    assert Solution().titleToNumber('AB') == 28, 'Example 3'
+    assert Solution().titleToNumber('AZ') == 52, 'Example 4'
+    assert Solution().titleToNumber('BA') == 53, 'Example 5'
+    assert Solution().titleToNumber('ZY') == 701, 'Example 6'
+    assert Solution().titleToNumber('AAB') == 704, 'Example 7'
+    print('all passed')
