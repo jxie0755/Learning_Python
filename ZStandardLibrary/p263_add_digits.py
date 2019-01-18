d35047d15987d65a8553d9da9642dcba2a584aa9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-18 14:23:09 -0500

    Leetcode 263 Finished

---
+++ ZStandardLibrary/p263_add_digits.py
@@ -0,0 +1,62 @@
+# P263 Add Digits
+# Easy
+
+
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+class Solution:
+    def addDigits(self, num):
+        ### O(len(n)) ?
+        """
+        :type num: int
+        :rtype: int
+        """
+        def process(num):
+            result = 0
+            while num != 0:
+                num, digit = divmod(num, 10)
+                result += digit
+            return result
+
+        while num >= 10:
+            num = process(num)
+
+        return num
+
+class Solution(object):
+    def addDigits(self, num):
+        ### Time O(1)
+        """
+        :type num: int
+        :rtype: int
+        """
+        # if num==0:
+        #     return 0
+        # t = num % 9
+        # return t if t else 9
+
+        return 0 if not num else (num-1)%9+1
+
+    # Explain:
+    # 123 = 100 + 20 + 3
+    # 123 = 100 + 10*2 + 1*3
+    # 注意数学规律: 1 % 9 = 1, 10 % 9 = 1, 100 % 9 = 1 --> 10^n % 9 = 1
+
+    # 123 % 9 = 6
+    # 100 % 9 = (100 % 9) * 1 = 1  (也就是数位1)
+    # 20 % 9  = (10 % 9) * 2  = 2  (也就是数位2)
+    # 3 % 9   = (1 % 9) * 3   = 3  (也就是数位3)
+    # 余数就是 1+2+3 = 6
+
+    # 为什么一直累积数字之和永远维持%9的余数呢?
+    # 因为 各项数字之和加起来如果大于9, 也就是各部分除以9的余数相加后大于9
+    # 那样就继续除以9,直到余数小于10为止.
+    # 若各项加起来刚好等于9 是什么意思? 也就是数字可以被9整除,但是当余到9时我们人为终止,因为9已经是个位数了.
+
+
+if __name__ == '__main__':
+    assert Solution().addDigits(1) == 1, 'Edge 1'
+    assert Solution().addDigits(10) == 1, 'Edge 2'
+
+    assert Solution().addDigits(38) == 2, 'Example 1'
+    print('all passed')
