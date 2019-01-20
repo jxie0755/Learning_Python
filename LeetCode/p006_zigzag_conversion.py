988225a45731f5ba71808b7f613c18b04f4a9f51
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-20 13:31:36 -0500

    leetcode p006 Finished

--- LeetCode/p006_zigzag_conversion.py
+++ LeetCode/p006_zigzag_conversion.py
@@ -72,8 +72,9 @@ class Solution(object):
         if numRows == 1:
             return s
         step, zigzag = 2 * numRows - 2, ""
+
         for i in range(numRows):
-            for j in range(i, len(s), step):
+            for j in range(i, len(s), step):  # 使用step法直接原地找到index值
                 zigzag += s[j]
                 if 0 < i < numRows - 1 and j + step - 2 * i < len(s):
                     zigzag += s[j + step - 2 * i]
