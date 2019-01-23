c7802a124503f9163be595fe21d6b3dd1767d0bc
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-23 13:43:07 -0500

    Learn python loop else

---
+++ ZSimpleLearnings/py_loop_else.py
@@ -0,0 +1,62 @@
+# This is to learn python looping + condition
+
+# while...else...
+i = 0
+while i != 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+else:
+    print('finished') # not printed because loop break
+
+# >>>
+# 0
+# 1
+# 2
+# 3
+
+
+i = 0
+while i != 5:
+    print(i)
+    i += 1
+else:
+    print('finished') # printed as loop end normally
+
+# >>>
+# 0
+# 1
+# 2
+# 3
+# 4
+# finished
+
+
+# for...else...
+
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+else:
+    print('finished')  # not printed because loop break
+
+# >>>
+# 0
+# 1
+# 2
+# 3
+
+for i in range(5):
+    print(i)
+else:
+    print('finished')  # printed as loop end normally
+
+# >>>
+# 0
+# 1
+# 2
+# 3
+# 4
+# finished
