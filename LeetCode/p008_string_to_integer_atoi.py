d43d316fc1b039afbf4a5a27ad80abc964617443
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-01-31 19:10:42 -0500

    Leetcode P008 Initiation

---
+++ LeetCode/p008_string_to_integer_atoi.py
@@ -0,0 +1,81 @@
+# P008 String to Integer (atoi)
+# Medium
+
+
+# Implement atoi which converts a string to an integer.
+# The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
+# Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible,
+# and interprets them as a numerical value.
+
+# If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed. (Example 4)
+# If no valid conversion could be performed, a zero value is returned.
+
+# Note:
+# Only the space character ' ' is considered as whitespace character.
+# Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (2^31 − 1) or INT_MIN (−2^31) is returned.
+
+class Solution:
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        hashtable = {
+            '0': 0,
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+        }
+
+        num_result = 0
+        base = 1
+        found = False
+        trigger = False
+        for i in str[::-1]:
+            if i not in hashtable and not found:
+                pass
+            elif i in hashtable:
+                found = True
+                num_result += hashtable[i] * base
+                base *= 10
+            elif i == '.':
+                num_result = 0
+                base = 1
+            elif i == '-' and not trigger:
+                num_result *= -1
+                trigger = True
+            elif i == '+' and not trigger:
+                trigger = True
+            elif i not in hashtable and i != ' ' and found:
+                return 0
+
+        if num_result > 2**31 -1:
+                return 2**32 -1
+        elif num_result < -(2**31):
+                return -(2**31)
+        else:
+            return num_result
+
+
+if __name__ == '__main__':
+    assert Solution().myAtoi("ABC") == 0, 'Edge 1'
+
+    assert Solution().myAtoi("42") == 42, 'Example 1'
+    assert Solution().myAtoi("   -42") == -42, 'Example 2'
+    assert Solution().myAtoi("4193 with words") == 4193, 'Example 3'
+    assert Solution().myAtoi("words and 987") == 0, 'Example 4'
+    assert Solution().myAtoi("-91283472332") == -2147483648, 'Example 5, return -2^31'
+
+    assert Solution().myAtoi("3.14159") == 3, 'Extra 1'
+    assert Solution().myAtoi("+1") == 1, 'Extra 2'
+    assert Solution().myAtoi("+-2") == 0, 'Extra 3'
+    assert Solution().myAtoi("  -0012a42") == -12, 'Extra 4'
+    print('all passed')
+
+
