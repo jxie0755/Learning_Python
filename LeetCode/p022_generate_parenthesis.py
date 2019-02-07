43aa1ca4808c2de2a5b8289f6824ded8765ee70f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-07 10:17:58 -0500

    Leetcode P022 sucessful self version

--- LeetCode/p022_generate_parenthesis.py
+++ LeetCode/p022_generate_parenthesis.py
@@ -8,6 +8,35 @@
 # :rtype: List[str]
 # """
 
+# my own solusion
+class Solution:
+
+    ### recursive
+    ### Time  O(N^2)
+    ### Space O(N^2)
+    def gen_par(self, par):
+        index_list = []    # 找到所有右括号')'的位置
+        for i in range(0, len(par)):
+            if par[i] == ')':
+                index_list.append(i)
+
+        result = []        # 根据右括号的位置,分别插入一个'()'在左侧,和右侧
+        for i in index_list:
+            result.append(par[:i] + '()' + par[i:])
+            result.append(par[:i+1] + '()' + par[i+1:])
+        return result
+
+    def generateParenthesis(self, n):
+        if n == 1:
+            return ['()']
+        else:
+            result = []
+            prev = self.generateParenthesis(n-1)   # 根据n-1的答案, 其中每一个括号组做一个新的生成
+            for par in prev:
+                result += self.gen_par(par)
+            return list(set(result))              # 需要用set来去重复
+
+
 class Solution:
     def generateParenthesis(self, n):
         def generate(p, left, right, parens=[]):
@@ -22,45 +51,12 @@ class Solution:
         return generate('', n, n)
 
 
-class Solution:
-    def generateParenthesis(self, n: 'int') -> 'List[str]':
-        if n == 0:
-            return []
-        elif n == 1:
-            return ['()']
-        else:
-            prev = self.generateParenthesis(n-1)
-            result = []
-            result += ['(' + i + ')' for i in prev]
-            result += ['()' + i for i in prev]
-            result += [i + '()' for i in prev]
-            return list(set(result))
 
+if __name__ == '__main__':
+    assert Solution().generateParenthesis(1) == ['()'], "Edge 1"
 
-
-# if __name__ == '__main__':
-#     assert Solution().generateParenthesis(0) == [], "Edge 1"
-#     assert Solution().generateParenthesis(1) == ['()'], "Edge 2"
-
-#     assert set(Solution().generateParenthesis(2)) == set(["(())", "()()"])
-#     assert set(Solution().generateParenthesis(3)) == set(["((()))", "(()())", "(())()", "()(())", "()()()"])
-#     assert set(Solution().generateParenthesis(4)) == set(['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())(())', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()'])
-#     print('all passed')
-
-
-print(Solution().generateParenthesis(0))
-print(Solution().generateParenthesis(1))
-print(Solution().generateParenthesis(2))
-print(Solution().generateParenthesis(3))
-a = Solution().generateParenthesis(4)
-print(len(a))
-
-print(sorted(a))
-
-# ['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()']
-# ['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()']
-#       x          x            x           x           x           x           x          x           x            x           x           x           x
-# '(())(())'
-
-# TODO Need to learn why it works
+    assert set(Solution().generateParenthesis(2)) == set(["(())", "()()"])
+    assert set(Solution().generateParenthesis(3)) == set(["((()))", "(()())", "(())()", "()(())", "()()()"])
+    assert set(Solution().generateParenthesis(4)) == set(['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())(())', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()'])
+    print('all passed')
 
