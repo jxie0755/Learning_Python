29d08f8e7eb7f99d2113c84cde115315f3946861
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-02-13 11:06:18 -0500

    Learn bit calculation

--- LeetCode/p029_divide_two_integers.py
+++ LeetCode/p029_divide_two_integers.py
@@ -47,6 +47,49 @@ class Solution:
             return result
 
 
+class Solution(object):
+    ### Same idea but use bit calculation << and >>
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        result, dvd, dvs = 0, abs(dividend), abs(divisor)
+        while dvd >= dvs:
+            inc = dvs
+            i = 0
+            while dvd >= inc:
+                dvd -= inc
+                result += 1 << i
+                inc <<= 1
+                i += 1
+        if dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0:
+            return -result
+        else:
+            return result
+
+    def divide2(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        positive = (dividend < 0) is (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        res = 0
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                res += i
+                i <<= 1
+                temp <<= 1
+        if not positive:
+            res = -res
+        return min(max(-2147483648, res), 2147483647)
+
+
 if __name__ == '__main__':
     assert Solution().divide(10, 0) == None, "Edge1"
     assert Solution().divide(1, 3) == 0, "Edge 2"
