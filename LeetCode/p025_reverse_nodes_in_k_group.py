21a8e467841dea16b39e09be3fa30367ffc99aa0
jxie0755 <jxie0755@users.noreply.github.com>
2019-03-03 22:49:55 -0500

    Leetcode P025 Passed

--- LeetCode/p025_reverse_nodes_in_k_group.py
+++ LeetCode/p025_reverse_nodes_in_k_group.py
@@ -18,7 +18,32 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        pass
+        if k == 1 or not head:
+            return head
+
+        cur = head
+
+        # 先找到k-group的终点
+        for i in range(k-1):
+            if head.next:
+                head = head.next
+            else:  # 若是不满k个,就直接不要动
+                return cur
+
+        # 断开,但是不能忘记下一个节点,以备用
+        head.next, next_head = None, head.next
+        # reverse前k个节点
+        new_head = self.reverseNodes(cur)
+        # 反转后,cur也就是tail了
+        cur.next = self.reverseKGroup(next_head, k)
+        return new_head
+
+
+    def reverseNodes(self, head):
+        dummy = ListNode(float("-inf"))
+        while head:
+            dummy.next, head.next, head = head, dummy.next, head.next
+        return dummy.next
 
 
 
@@ -35,7 +60,6 @@ if __name__ == '__main__':
     c.next = d
     d.next = e
 
-    # Given 1->2->3->4, you should return the list as 2->1->4->3.
     f = Solution().reverseKGroup(a, 2)
     assert f.val == 2
     assert f.next.val == 1
@@ -43,11 +67,21 @@ if __name__ == '__main__':
     assert f.next.next.next.val == 3
     assert f.next.next.next.next.val == 5
 
+    a = ListNode(1)
+    b = ListNode(2)
+    c = ListNode(3)
+    d = ListNode(4)
+    e = ListNode(5)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+
     g = Solution().reverseKGroup(a, 3)
-    assert f.val == 3
-    assert f.next.val == 2
-    assert f.next.next.val == 1
-    assert f.next.next.next.val == 4
-    assert f.next.next.next.next.val == 5
+    assert g.val == 3
+    assert g.next.val == 2
+    assert g.next.next.val == 1
+    assert g.next.next.next.val == 4
+    assert g.next.next.next.next.val == 5
 
     print('all passed')
