da6baed410378e17f6d9318106e1c9d5470e2e4c
jxie0755 <jxie0755@users.noreply.github.com>
2019-03-03 23:18:11 -0500

    Leetcode P206 Finished

--- LeetCode/p206_reverse_linked_list.py
+++ LeetCode/p206_reverse_linked_list.py
@@ -12,8 +12,49 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        pass
+    # iteration
+    # @return {ListNode}
+    def reverseList(self, head: ListNode) -> ListNode:# @param {ListNode} head
+        dummy = ListNode(float("-inf"))
+        while head:
+            dummy.next, head.next, head = head, dummy.next, head.next
+        return dummy.next
+
+    def reverseList(self, head: ListNode) -> ListNode:# @param {ListNode} head
+        if not head or not head.next:
+            return head
+        pre = None
+        while head:
+            temp = head.next
+            head.next = pre
+            pre = head
+            head = temp
+        return pre
+
+
+# Time:  O(n)
+# Space: O(n)
+# Recursive solution.
+class Solution2(object):
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        [begin, end] = self.reverseListRecu(head)
+        return begin
+
+    def reverseListRecu(self, head):
+        if not head:
+            return [None, None]
+
+        [begin, end] = self.reverseListRecu(head.next)
+
+        if end:
+            end.next = head
+            head.next = None
+            return [begin, head]
+        else:
+            return [head, head]
+
 
 
 
@@ -28,7 +69,7 @@ if __name__ == '__main__':
     c.next = d
     d.next = e
 
-    f = Solution().reverse_nodes(a)
+    f = Solution().reverseList(a)
     assert f.val == 5
     assert f.next.val == 4
     assert f.next.next.val == 3
