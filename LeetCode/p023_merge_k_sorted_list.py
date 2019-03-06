30abff3bc3261fd44c1fa5b702c9fb01ac25962f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-06 14:49:41 -0500

    Leetcode p023 finished merge two method

--- LeetCode/p023_merge_k_sorted_list.py
+++ LeetCode/p023_merge_k_sorted_list.py
@@ -26,7 +26,6 @@ def genNode(*nodes, end=None):
 
 
 class Solution:
-
     ### Exceeded time limit
     def mergeKLists(self, lst) -> ListNode:
         cur = dummy = ListNode('X')
@@ -46,7 +45,6 @@ class Solution:
 
         return dummy.next
 
-
 class Solution:
     ### take all val out, then return the sorted linked list
     ### not recommended
@@ -60,27 +58,58 @@ class Solution:
 
 
 
-class Solution:
+
+class SolutionFinal:
     ### use merge two list, then use it multiple times
-    def mergeKLists(self, lst) -> ListNode:
-        dummy = ListNode(float('-inf'))
-        for i in lst:
-            dummy = self.merge_two(dummy, i)
-        return dummy.next
-
-
     def merge_two(self, l1, l2):
         """merge two sorted linked list"""
+        global count
         curr = dummy = ListNode('X')
         while l1 and l2:
             if l1.val < l2.val:
                 curr.next, l1 = l1, l1.next
+                count += 1
             else:
                 curr.next, l2 = l2, l2.next
+                count += 1
             curr = curr.next
         curr.next = l1 or l2
         return dummy.next
 
+    ### 但是这个合并的方法偏慢, 相当于要流经的重复节点太多次了
+    def mergeKLists_X(self, lst) -> ListNode:
+        dummy = ListNode(float('-inf'))
+        for i in lst:
+            dummy = self.merge_two(dummy, i)
+
+        print("count: ", count)
+        return dummy.next
+
+    ### 这个方法采用收尾合并, 这样能显著减少重复流经的节点
+    def mergeKLists_O(self, lst) -> ListNode:
+        if not lst:
+            return None
+        left, right = 0, len(lst) - 1
+        while right > 0:
+            if left >= right:
+                left = 0
+            else:
+                lst[left] = self.merge_two(lst[left], lst[right])
+                left += 1
+                right -= 1
+        print("count: ", count)
+        return lst[0]
+
+
+count = 0
+ls = [ListNode(i) for i in range(1000)]
+SolutionFinal().mergeKLists_X(ls)
+# >>>  count:  500500
+
+count = 0
+ls = [ListNode(i) for i in range(1000)]
+SolutionFinal().mergeKLists_O(ls)
+# >>> count:  8977
 
 
 if __name__ == '__main__':
