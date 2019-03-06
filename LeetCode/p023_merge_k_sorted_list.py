3f5eec8a988cb3763c3435661aac66936ff03cac
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-06 13:58:38 -0500

    Leetcode p023 finished merge two method

--- LeetCode/p023_merge_k_sorted_list.py
+++ LeetCode/p023_merge_k_sorted_list.py
@@ -24,12 +24,12 @@ def genNode(*nodes, end=None):
     return n if nodes else None
 
 
+
 class Solution:
 
     ### Exceeded time limit
     def mergeKLists(self, lst) -> ListNode:
-        dummy = ListNode('X')
-        cur = dummy
+        cur = dummy = ListNode('X')
         while any([i for i in lst]):
             i, min_val, min_idx = 0, float('inf'), float('inf')
 
@@ -48,30 +48,8 @@ class Solution:
 
 
 class Solution:
-
-    ### Exceeded time limit
-    def mergeKLists(self, lst) -> ListNode:
-        dummy = ListNode('X')
-        cur = dummy
-        while any([i for i in lst]):
-            i, min_val, min_idx = 0, float('inf'), float('inf')
-
-            while i < len(lst):
-                node = lst[i]
-                if node:
-                    if node.val < min_val:
-                        min_val, min_idx = node.val, i
-                i += 1
-
-            cur.next = ListNode(min_val)
-            lst[min_idx] = lst[min_idx].next
-            cur = cur.next
-
-        return dummy.next
-
-
-class Solution:
-
+    ### take all val out, then return the sorted linked list
+    ### not recommended
     def mergeKLists(self, lst) -> ListNode:
         result = []
         for node in lst:
@@ -79,6 +57,30 @@ class Solution:
                 result.append(node.val)
                 node = node.next
         return genNode(sorted(result))
+
+
+
+class Solution:
+    ### use merge two list, then use it multiple times
+    def mergeKLists(self, lst) -> ListNode:
+        dummy = ListNode(float('-inf'))
+        for i in lst:
+            dummy = self.merge_two(dummy, i)
+        return dummy.next
+
+
+    def merge_two(self, l1, l2):
+        """merge two sorted linked list"""
+        curr = dummy = ListNode('X')
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next, l1 = l1, l1.next
+            else:
+                curr.next, l2 = l2, l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
+
 
 
 if __name__ == '__main__':
