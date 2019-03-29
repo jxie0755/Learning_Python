c407aaf685dba262db196d35eb2d80fb6807f6ff
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-03-28 22:25:30 -0400

    Learn datetime

--- ZStandardLibrary/learn_datetime.py
+++ ZStandardLibrary/learn_datetime.py
@@ -21,6 +21,24 @@ print(datetime.MAXYEAR)  # >>> 9999
 # class datetime.tzinfo
 # class datetime.timezone
 
+
+
+# datetime转换为timestamp
+# 在计算机中，时间实际上是用数字表示的。我们把1970年1月1日 00:00:00 UTC+00:00时区的时刻称为epoch time，记为0（1970年以前的时间timestamp为负数），当前时间就是相对于epoch time的秒数，称为timestamp。
+    # 你可以认为：
+        # timestamp = 0 = 1970-1-1 00:00:00 UTC+0:00
+    # 对应的北京时间是：
+        # timestamp = 0 = 1970-1-1 08:00:00 UTC+8:00
+
+# 可见timestamp的值与时区毫无关系，因为timestamp一旦确定，其UTC时间就确定了，转换到任意时区的时间也是完全确定的，这就是为什么计算机存储的当前时间是以timestamp表示的，因为全球各地的计算机在任意时刻的timestamp都是完全相同的（假定时间已校准）。
+# 把一个datetime类型转换为timestamp只需要简单调用timestamp()方法：
+dt = datetime.datetime(2015, 4, 19, 12, 20, microsecond=2999) # 用指定日期时间创建datetime
+print(dt.timestamp()) # 把datetime转换为timestamp
+# >>> 1429460400.002999   # 1000微秒=1豪秒
+# 注意Python的timestamp是一个浮点数。如果有小数位，小数位表示毫秒数。
+# 某些编程语言（如Java和JavaScript）的timestamp使用整数表示毫秒数，这种情况下只需要把timestamp除以1000就得到Python的浮点表示方
+
+
 print()
 print('class datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)')
 
