418c8c7d7a0f65d95cd8c0268bc7015a99853951
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-01 16:52:42 -0400

    p041 passed

---
+++ LeetCode/p041_first_missing_positive.py
@@ -0,0 +1,61 @@
+# P041 First Missing Positive
+# Hard
+
+
+# Given an unsorted integer array, find the smallest missing positive integer.
+
+# Note:
+# Your algorithm should run in O(n) time and uses constant extra space.
+
+from typing import *
+
+class Solution:
+    # O(N), space O(N)
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        if not nums:
+            return 1
+        all_int = list(range(1, len(nums)+1))
+        max_i = all_int[-1]
+        for i in nums:
+            try:
+                all_int.remove(i)
+            except:
+                pass
+        if not all_int:
+            return max_i + 1
+        else:
+            return all_int[0]
+
+
+class Solution(object):
+    # @param A, a list of integers
+    # @return an integer
+    def firstMissingPositive(self, nums):
+        nums.append(0)
+        n = len(nums)
+        for i in range(len(nums)):  # delete those useless elements
+            if nums[i] < 0 or nums[i] >= n:
+                nums[i] = 0
+
+        for i in range(len(nums)):  # use the index as the hash to record the frequency of each number
+            nums[nums[i] % n] += n  # 在每个数上加n, 则不会改变这个数%n的余数
+        for i in range(1, len(nums)):
+            if nums[i] / n == 0:
+                return i
+        return n
+
+
+
+if __name__ == '__main__':
+    assert Solution().firstMissingPositive([]) == 1, "Edge 1"
+    assert Solution().firstMissingPositive([1]) == 2, "Edge 2"
+    assert Solution().firstMissingPositive([2]) == 1, "Edge 3"
+    assert Solution().firstMissingPositive([-1]) == 1, "Edge 4"
+
+    assert Solution().firstMissingPositive([1,2,0]) == 3, "Example 1"
+    assert Solution().firstMissingPositive([3,4,-1,1]) == 2, "Example 2"
+    assert Solution().firstMissingPositive([7,8,9,11,12]) == 1, "Example 3"
+
+    print('all passed')
+
+
