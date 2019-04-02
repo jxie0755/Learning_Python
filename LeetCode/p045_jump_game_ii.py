5f5b69484a23c2d4acca93204513db5c5cbe4b0e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-02 16:30:39 -0400

    Leetcode p045 Finished simple recursion method, but failed by time limit create a memorization step by using hash map

--- LeetCode/p045_jump_game_ii.py
+++ LeetCode/p045_jump_game_ii.py
@@ -11,12 +11,52 @@
 from typing import *
 
 class Solution:
+    ### Simple Recursion method that worked but exceed max time limit
     def jump(self, nums: List[int]) -> int:
-        pass
+
+        def helper(cur_idx, cur_step=0):
+
+            if cur_idx >= last_idx:
+                all_ways.append(cur_step)
+            else:
+                cur_value = nums[cur_idx]
+                if cur_value != 0:
+                    for i in range(1, cur_value+1):
+                        helper(cur_idx + i, cur_step + 1)
+
+        last_idx = len(nums) - 1
+        all_ways = []
+        helper(0)
+        return min(all_ways)
+
+
+class Solution:
+    ### A modified method with hashmap search to reduce repeating calculation
+    def jump(self, nums: List[int]) -> int:
+
+        hmp = {}
+
+        def helper(cur_idx, cur_step=0):
+
+            if cur_idx >= last_idx:
+                all_ways.append(cur_step)
+            else:
+                cur_value = nums[cur_idx]
+                if cur_value != 0:
+                    for i in range(1, cur_value+1):
+                        helper(cur_idx + i, cur_step + 1)
+
+        last_idx = len(nums) - 1
+        all_ways = []
+        helper(0)
+        return min(all_ways)
 
 
 
 if __name__ == '__main__':
+    assert Solution().jump([2,1]) == 1, 'Edge 1'
     assert Solution().jump([2,3,1,1,4]) == 2, "Example 1"
-
+    assert Solution().jump([2,9,6,5,7,0,7,2,7,9,3,2,2,5,7,8,1,6,6,6,3,5,2,2,6,3]) == 5, "Long 1"
+    assert Solution().jump([5,6,5,3,9,8,3,1,2,8,2,4,8,3,9,1,0,9,4,6,5,9,8,7,4,2,1,0,2]) == 5, "Long 2"
+    print(Solution().jump([5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5]))
     print('all passed')
