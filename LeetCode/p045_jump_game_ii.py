dbf0d472b92830af1fec9ea322bf6122a136d3fd
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-02 16:02:31 -0400

    Leetcode p045 initiation

---
+++ LeetCode/p045_jump_game_ii.py
@@ -0,0 +1,22 @@
+# P045 Jump Game II
+# Hard
+
+
+# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+# Each element in the array represents your maximum jump length at that position.
+# Your goal is to reach the last index in the minimum number of jumps.
+
+# Note:
+# You can assume that you can always reach the last index.
+from typing import *
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        pass
+
+
+
+if __name__ == '__main__':
+    assert Solution().jump([2,3,1,1,4]) == 2, "Example 1"
+
+    print('all passed')
