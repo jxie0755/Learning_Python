fa274328c63bca37dead057c1a34878261a4eef4
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-04 16:27:32 -0400

    Leetcode p046 finished

--- LeetCode/p046_permutations.py
+++ LeetCode/p046_permutations.py
@@ -7,62 +7,61 @@
 from typing import *
 
 import math
+
 class Solution:
+    def next_permute(self, idx_list):
+        ### Use next permutation method from leetcode p031
+        """calculate the next permuatation, with integers 0 to N-1 (for N elements)
+        this will both modify idx_list and return the updated idx_list"""
+        length = len(idx_list)
+        cur_i = None
+
+        for i in range(-2, -length - 1, -1):
+            if idx_list[i] < idx_list[i + 1]:
+                cur_i = i
+                break
+
+        if not cur_i:
+            idx_list.reverse()
+            return idx_list
+
+        else:
+            for rev_i in range(-1, cur_i, -1):
+                if idx_list[rev_i] > idx_list[cur_i]:  # tail must already be sorted!
+                    idx_list[cur_i], idx_list[rev_i] = idx_list[rev_i], idx_list[cur_i]  # switch
+                    idx_list[cur_i + 1:] = idx_list[cur_i + 1:][::-1]
+                    break
+            return idx_list
+
     def permute(self, nums: List[int]):
-
-        length = len(nums)
-
-
-
-
-
-
-
+        total_n = math.factorial(len(nums))
+        result = []
+        idxs = list(range(len(nums)))[::-1]
+        for i in range(total_n):
+            idxs = self.next_permute(idxs)
+            result.append([nums[i] for i in idxs])
+        return result
 
 
-# Solution().permute([1,2,3])
-Solution().permute([1,2,3,4])
 
 
-# if __name__ == '__main__':
-#     assert Solution().permute([1]) == [
-#         [1]
-#     ], "Edge 1"
-#
-#
-#     assert Solution().permute([1,2,3]) == [
-#         [1, 2, 3],
-#         [1, 3, 2],
-#         [2, 1, 3],
-#         [2, 3, 1],
-#         [3, 1, 2],
-#         [3, 2, 1]
-#     ], "Example 1"
-#
-#     print('all passed')
+
+if __name__ == '__main__':
+    assert Solution().permute([1]) == [
+        [1]
+    ], "Edge 1"
 
 
-# (1, 2, 3, 4)
-# (1, 2, 4, 3)
-# (1, 3, 2, 4)
-# (1, 3, 4, 2)
-# (1, 4, 2, 3)
-# (1, 4, 3, 2)
-# (2, 1, 3, 4)
-# (2, 1, 4, 3)
-# (2, 3, 1, 4)
-# (2, 3, 4, 1)
-# (2, 4, 1, 3)
-# (2, 4, 3, 1)
-# (3, 1, 2, 4)
-# (3, 1, 4, 2)
-# (3, 2, 1, 4)
-# (3, 2, 4, 1)
-# (3, 4, 1, 2)
-# (3, 4, 2, 1)
-# (4, 1, 2, 3)
-# (4, 1, 3, 2)
-# (4, 2, 1, 3)
-# (4, 2, 3, 1)
-# (4, 3, 1, 2)
-# (4, 3, 2, 1)
+    assert Solution().permute([1,2,3]) == [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1]
+    ], "Example 1"
+
+    print('all passed')
+
+
+
