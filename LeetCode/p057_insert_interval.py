6a5adc7097280dda3c59a8023f571bcacd289340
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-11 11:17:00 -0400

    Leetcode P057 initiation

---
+++ LeetCode/p057_insert_interval.py
@@ -0,0 +1,57 @@
+# P056 Insert Interval
+# Hard
+
+
+
+# Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+# You may assume that the intervals were initially sorted according to their start times.
+
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+    def __repr__(self):
+        return f'[{self.start} -> {self.end}]'
+    def __eq__(self, other):
+        if self.start ==other.start and self.end == other.end:
+            return True
+        return False
+
+
+class Solution:
+    def insert(self, intervals, newInterval):
+        pass
+
+
+
+if __name__ == '__main__':
+    assert Solution().insert([], Interval(1,2)) == [Interval(1,2)], 'Edge 1'
+
+
+    A1 = Interval(1,3)
+    A2 = Interval(6,9)
+    lst = [A1, A2]
+    assert Solution().insert(lst, Interval(2,5)) == [
+        Interval(1,5),
+        Interval(6,9)
+    ], 'Example 1'
+
+    B1 = Interval(1,2)
+    B2 = Interval(3,5)
+    B3 = Interval(6,7)
+    B4 = Interval(8,10)
+    B5 = Interval(12,16)
+    lst = [B1, B2, B3, B4, B5]
+    assert Solution().insert(lst, Interval(4, 8)) == [
+        Interval(1, 2),
+        Interval(3, 10),
+        Interval(12,16)
+    ], 'Example 2'
+
+    print('all passed')
+
+
+
+
