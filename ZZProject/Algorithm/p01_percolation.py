34e470eff7bd93e3fa39d86575e69027b723c870
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-13 09:02:16 -0400

    percolation initiation

---
+++ ZZProject/Algorithm/p01_percolation.py
@@ -0,0 +1,28 @@
+# Give a matrix of values @ m * n, in two status, pass or block (1 and 0) or ("O" and "X")
+# Determine if there is a open path that could go from the top to bottom
+
+
+def percolation():
+    pass
+
+
+if __name__ == '__main__':
+    sample_Y1 = [
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1, 1],
+    ]
+
+    sample_N1 = [
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 1, 0, 1, 0],
+    ]
