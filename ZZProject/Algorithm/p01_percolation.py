eae36d0fefcaff732f6e900a9a4c5d58c0cf30c6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-14 18:16:25 -0400

    percolation algorithm incorrect re-work determination complete

--- ZZProject/Algorithm/p01_percolation.py
+++ ZZProject/Algorithm/p01_percolation.py
@@ -39,19 +39,9 @@ class Percolation:
 
 
 
-    # def narrowpoint(self):
-    #     hmp = dict()
-    #     hmp[0] = self.matrix[0].count(1)
-    #     for i in range(self.m - 1):
-    #         r1, r2 = self.matrix[i], self.matrix[i + 1]
-    #         hmp[i+1] = self.isPercolate(r1, r2)
-    #     narrow_point = min(hmp, key=lambda x:hmp[x])
-    #     print('Narrow Point at', narrow_point)
-    #     print('Narrow Width is', hmp[narrow_point])
-    #
-    #     return hmp
 
     def determination(self, matrix):
+        """reverse check the"""
         tunnel = [i for i in range(len(matrix[0]))]
         for i in range(len(matrix)):
             tunnel = self.isPercolate(tunnel, matrix[i])
@@ -82,6 +72,16 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 1, 1, 0],
     ]
 
+    sample_Y3 = [
+        [0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 0],
+    ]
+
     sample_N1 = [
         [1, 0, 0, 0, 1, 1, 0, 1],
         [0, 0, 1, 1, 1, 1, 1, 0],
@@ -103,11 +103,12 @@ if __name__ == '__main__':
     ]
 
 
-    assert Percolation().determination(sample_Y1), "Example 1"
-    assert Percolation().determination(sample_Y2), "Example 2"
+    assert Percolation().determination(sample_Y1), "Example Y1"
+    assert Percolation().determination(sample_Y2), "Example Y2"
+    assert Percolation().determination(sample_Y2), "Example Y3"
 
-    assert not Percolation().determination(sample_N1), "Example 3"
-    assert not Percolation().determination(sample_N2), "Example 4"
+    assert not Percolation().determination(sample_N1), "Example N1"
+    assert not Percolation().determination(sample_N2), "Example N2"
 
 
     print('All passed')
