ed002c205c9a26c3a0c31f4aa4a3a25b63828662
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-17 11:34:05 -0400

    leetcode 078 initiation

---
+++ LeetCode/p078_subsets.py
@@ -0,0 +1,49 @@
+# P078 Subsets
+# Medium
+
+
+# Given a set of distinct integers, nums, return all possible subsets (the power set).
+# Note: The solution set must not contain duplicate subsets.
+
+
+
+class Solution:
+    def combinationSolo(self, nums, k):
+        if k == 0:
+            return []
+        if k == len(nums):
+            return [nums]
+        elif k == 1:
+            return [[i] for i in nums]
+        else:
+            result = []
+            next_list = nums[:]
+            head = next_list.pop(0)
+            result += [[head] + com for com in self.combinationSolo(next_list, k - 1)] + self.combinationSolo(nums[1:], k)
+            return result
+
+
+    # ### Get all first then remove repeats
+    # def subsets(self, nums):
+    #     result = []
+    #     for i in range(0, )
+
+
+print(sorted(Solution().combinationSolo([1,2,3], 0)))
+# print(sorted(Solution().subsets([1,2,3])))
+
+# if __name__ == '__main__':
+#     nums = [1, 2, 3]
+#     assert sorted(Solution().subsets(nums)) == sorted([
+#         [3],
+#         [1],
+#         [2],
+#         [1, 2, 3],
+#         [1, 3],
+#         [2, 3],
+#         [1, 2],
+#         []
+#     ])
+#
+#
+#     print('all passed')
