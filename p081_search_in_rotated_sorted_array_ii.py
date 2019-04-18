433a5425192a14646e979e1172019a8bbfc619c5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-18 14:08:03 -0400

    Leetcode p081 finished

--- p081_search_in_rotated_sorted_array_ii.py
+++ p081_search_in_rotated_sorted_array_ii.py
@@ -19,11 +19,67 @@ from typing import *
 
 
 class Solution:
+    def binarySearch(self, nums, target):
+        """regular binary search in sorted list"""
+        lo, high = 0, len(nums)-1
+        while lo <= high:
+            mid = (lo + high) // 2
+            if nums[mid] == target:
+                return True
+            elif nums[mid] < target:
+                lo = mid + 1
+            elif nums[mid] > target:
+                high = mid -1
+        return False
+
+    def isSortedQuick(self, nums):
+
+        if len(set(nums)) <= 1:
+            return True
+        else:
+            if nums[0] < nums[-1]:
+                return True
+            else:
+                return False
+
+
     def search(self, nums: List[int], target: int) -> bool:
-        pass
+        lo, high = 0, len(nums) -1
+        mid = (lo + high) // 2 + 1
+        first, second = nums[:mid], nums[mid:]
+
+        A, B = self.isSortedQuick(first), self.isSortedQuick(second)
+
+        if A and B:
+            return self.binarySearch(first, target) or self.binarySearch(second, target)
+        elif A:
+            if self.binarySearch(first, target):
+                return True
+            else:
+                return self.search(second, target)
+        elif B:
+            if self.binarySearch(second, target):
+                return True
+            else:
+                return self.search(first, target)
+
+
+
 
 
 if __name__ == '__main__':
+    nums = [1]
+    target = 1
+    assert Solution().search(nums, target), 'Edge 1'
+
+    nums = [1, 1]
+    target = 1
+    assert Solution().search(nums, target), 'Edge 2'
+
+    nums = [3, 1]
+    target = 0
+    assert not Solution().search(nums, target), 'Edge 3'
+
     nums = [2, 5, 6, 0, 0, 1, 2]
     target = 0
     assert Solution().search(nums, target), 'Example 1'
@@ -31,3 +87,5 @@ if __name__ == '__main__':
     nums = [2, 5, 6, 0, 0, 1, 2]
     target = 3
     assert not Solution().search(nums, target), 'Example 2'
+
+    print('all passed')
