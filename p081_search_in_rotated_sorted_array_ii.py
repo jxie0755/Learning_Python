60f67573369982f6b1506c8db8924f682fb05102
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-18 11:47:22 -0400

    Leetcode p081 Initiation

--- p081_search_in_rotated_sorted_array_ii.py
+++ p081_search_in_rotated_sorted_array_ii.py
@@ -11,7 +11,12 @@
 # This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
 # Would this affect the run-time complexity? How and why?
 
+# before: [0,1,2,3,4,5,6] might become [3,4,5,6,0,1,2]).
+# now:    [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]
+
+
 from typing import *
+
 
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
@@ -19,14 +24,10 @@ class Solution:
 
 
 if __name__ == '__main__':
-    nums = [2,5,6,0,0,1,2]
+    nums = [2, 5, 6, 0, 0, 1, 2]
     target = 0
     assert Solution().search(nums, target), 'Example 1'
 
     nums = [2, 5, 6, 0, 0, 1, 2]
     target = 3
     assert not Solution().search(nums, target), 'Example 2'
-
-
-
-
