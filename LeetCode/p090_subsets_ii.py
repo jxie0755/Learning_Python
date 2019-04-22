79748429f5e0936d81a46c9887ab8e54a532633f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-22 17:37:25 -0400

    Leetcode P090 Initiation

---
+++ LeetCode/p090_subsets_ii.py
@@ -0,0 +1,19 @@
+# P090 Subsets II
+# Medium
+
+
+# Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+# Note: The solution set must not contain duplicate subsets.
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        pass
+
+
+if __name__ == '__main__':
+    assert sorted(Solution().subsetsWithDup([])) == [[]], 'Edge 0'
+    assert sorted(Solution().subsetsWithDup([1])) == [[], [1]], 'Edge 1'
+
+    assert sorted(Solution().subsetsWithDup([1, 2])) == [[], [1], [1, 2], [2]], 'Example 0'
+    assert sorted(Solution().subsetsWithDup([1, 2, 2])) == [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]], 'Example 1'
+    print('all passed')
