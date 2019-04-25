42b478aa6e087e42d1935b6ce497e0f20a7009a1
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-24 22:43:29 -0400

    Leetcode p092 first trial failed

--- LeetCode/p092_reverse_linked_list_ii.py
+++ LeetCode/p092_reverse_linked_list_ii.py
@@ -28,17 +28,44 @@ def genNode(*nodes, end=None):
 
 class Solution:
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-        pass
+        if m == n:
+            return head
+
+        ans = prehead = ListNode('X')
+        prehead.next = head
+        cur = head
+        idx = 0
+
+        while cur:
+            idx += 1
+            nex = cur.next
+
+            if idx < m:
+                prehead = prehead.next
+            elif idx == m:
+                revhead = cur
+            elif n >= idx > m:
+                temp = cur
+                cur.next = revhead
+                revhead.next = temp
+                revhead = cur
+
+            cur = nex
+        return ans.next
 
 
-if __name__ == '__main__':
-    s1 = genNode(1)
-    assert repr(Solution().reverseBetween(s1, 1, 1)) == '1', 'Edge 1'
 
-    s1 = genNode(1,2)
-    assert repr(Solution().reverseBetween(s1, 1, 2)) == '2->1', 'Edge 2'
+s1 = genNode(1,2)
+print(repr(Solution().reverseBetween(s1, 1, 2)))
 
-    s1 = genNode(1,2,3,4,5)
-    assert repr(Solution().reverseBetween(s1, 2, 4)) == '1->4->3->2->5', "Example 1"
-
-    print('all passed')
+# if __name__ == '__main__':
+#     s1 = genNode(1)
+#     assert repr(Solution().reverseBetween(s1, 1, 1)) == '1', 'Edge 1'
+#
+#     s1 = genNode(1,2)
+#     assert repr(Solution().reverseBetween(s1, 1, 2)) == '2->1', 'Edge 2'
+#
+#     s1 = genNode(1,2,3,4,5)
+#     assert repr(Solution().reverseBetween(s1, 2, 4)) == '1->4->3->2->5', "Example 1"
+#
+#     print('all passed')
