445296e37f65718e91862d8236aef9923852952f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-26 13:13:00 -0400

    Leetcode P200 Union Find optimization

--- LeetCode/p200_number_of_islands.py
+++ LeetCode/p200_number_of_islands.py
@@ -37,6 +37,7 @@ class LC200UnionFind(object):
                 self.id[rt_p] = rt_q
                 self.sz[rt_q] += self.sz[rt_p]
 
+
 class Solution:
 
     def numIslands(self, grid) -> int:
@@ -83,79 +84,52 @@ class Solution:
                     UF.id[p] = -1
         return UF.groupCount()
 
-# ### Improved by using dict for Union-Find
-# class LC200UnionFind_hmp(object):
-#     ### Designed for Leetcode P200, with hash-map as the id
-#     ### It is still slow
-#     def __init__(self, m, n):
-#         self.id = {(x, y):(x, y) for x in range(m) for y in range(n)}
-#         self.sz = {(x, y):1 for x in range(m) for y in range(n)}  # track the size of each tree (only on the root)
-#
-#     def root(self, p):
-#         while self.id[p] != p:
-#             p = self.id[p]
-#         return p
-#
-#     def groupCount(self):
-#         count = 0
-#         for coor in self.id:
-#             if self.id[coor] == coor:
-#                 count += 1
-#         return count
-#
-#     def union(self, p, q):
-#         rt_p = self.root(p)
-#         rt_q = self.root(q)
-#
-#         if rt_p != rt_q:
-#             if self.sz[q] <= self.sz[p]:
-#                 self.id[rt_q] = rt_p  # assign the new root to the bigger tree
-#                 self.sz[rt_p] += self.sz[rt_q]  # update the size of the new root
-#             else:
-#                 self.id[rt_p] = rt_q
-#                 self.sz[rt_q] += self.sz[rt_p]
-#
-#
-# class Solution:
-#
-#     def numIslands(self, grid) -> int:
-#         if not grid:
-#             return 0
-#
-#         def neighbor(coor):
-#             """return the UF id's index at up, down, left, right"""
-#             x, y = coor[0], coor[1]
-#             up = (x-1, y)
-#             down = (x+1,y)
-#             left = (x, y-1)
-#             right = (x, y+1)
-#             candidates = list(filter(isValid, [up, down, left, right]))
-#             candidates = [i for i in candidates if grid[i[0]][i[1]] == '1']
-#             return candidates
-#
-#         def isValid(coor):
-#             x, y = coor[0], coor[1]
-#             if x < 0 or x > row - 1 or y < 0 or y > col - 1:
-#                 return False
-#             else:
-#                 return True
-#
-#         row = len(grid)
-#         col = len(grid[0])
-#         UF = LC200UnionFind_hmp(row, col)
-#
-#         for r in range(row):
-#             for c in range(col):
-#                 coor = (r, c)
-#                 coorval = grid[r][c]
-#                 if coorval == '1':
-#                     for q in neighbor(coor):
-#                         UF.union(coor, q)
-#                 else:
-#                     UF.id[coor] = None
-#
-#         return UF.groupCount()
 
+
+class Solution:
+
+    def numIslands(self, grid) -> int:
+        if not grid:
+            return 0
+
+        def neighbor(coor):
+            """return the UF id's index at up, down, left, right"""
+            x, y = coor[0], coor[1]
+            right = (x, y + 1)
+            down = (x+1,y)
+
+            candidates = [right, down]# 由于按顺序遍历coor, 只需要和右边和下面的link就行, 同样能覆盖全局
+            if x == row-1:
+                candidates.pop()
+            if y == col-1:
+                candidates.pop(0)
+            return [i[0] * col + i[1] for i in candidates if grid[i[0]][i[1]] == '1']
+                   # translate coor on the run
+
+        row = len(grid)
+        col = len(grid[0])
+        N = len(grid) * len(grid[0])
+        UF = LC200UnionFind(N)
+
+        for r in range(row):
+            for c in range(col):
+                coor = (r, c)
+                coorval = grid[r][c]
+                p = r * col + c      # translate the coor on the run
+                if coorval == '1':
+                    for q in neighbor(coor):
+                        UF.union(p, q)
+                else:
+                    UF.id[p] = -1
+        return UF.groupCount()
+
+
+M3 = [
+        ['0', '1', '0'],
+        ['1', '0', '1'],
+        ['0', '1', '0'],
+    ]
+print(Solution().numIslands(M3))
 
 
 if __name__ == '__main__':
@@ -185,5 +159,12 @@ if __name__ == '__main__':
         ['0', '0', '0', '1', '1'],
     ]
     assert Solution().numIslands(M2) == 3, 'Example 2'
+
+    M3 = [
+        ['0', '1', '0'],
+        ['1', '0', '1'],
+        ['0', '1', '0'],
+    ]
+    assert Solution().numIslands(M3) == 4, 'Additional 1'
 
     print('all passed')
