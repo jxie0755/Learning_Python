b76f5a6e70f7a24415a70cb2043e56adc43fd727
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-04-26 12:49:09 -0400

    Leetcode P200 Union Find updated Solved

--- LeetCode/p200_number_of_islands.py
+++ LeetCode/p200_number_of_islands.py
@@ -60,12 +60,9 @@ class Solution:
         def neighbor(coor):
             """return the UF id's index at up, down, left, right"""
             x, y = coor[0], coor[1]
-            up = (x-1, y)
             down = (x+1,y)
-            left = (x, y-1)
             right = (x, y+1)
-
-            candidates = list(filter(isValid, [up, down, left, right]))
+            candidates = list(filter(isValid, [down, right]))  # 由于按顺序遍历coor, 只需要和右边和下面的link就行, 同样能覆盖全局
             candidates = [translate(i) for i in candidates if grid[i[0]][i[1]] == '1']
             return candidates
 
@@ -74,7 +71,6 @@ class Solution:
         col = len(grid[0])
         N = len(grid) * len(grid[0])
         UF = LC200UnionFind(N)
-
         for r in range(row):
             for c in range(col):
                 coor = (r, c)
@@ -87,6 +83,78 @@ class Solution:
                     UF.id[p] = -1
         return UF.groupCount()
 
+# ### Improved by using dict for Union-Find
+# class LC200UnionFind_hmp(object):
+#     ### Designed for Leetcode P200, with hash-map as the id
+#     ### It is still slow
+#     def __init__(self, m, n):
+#         self.id = {(x, y):(x, y) for x in range(m) for y in range(n)}
+#         self.sz = {(x, y):1 for x in range(m) for y in range(n)}  # track the size of each tree (only on the root)
+#
+#     def root(self, p):
+#         while self.id[p] != p:
+#             p = self.id[p]
+#         return p
+#
+#     def groupCount(self):
+#         count = 0
+#         for coor in self.id:
+#             if self.id[coor] == coor:
+#                 count += 1
+#         return count
+#
+#     def union(self, p, q):
+#         rt_p = self.root(p)
+#         rt_q = self.root(q)
+#
+#         if rt_p != rt_q:
+#             if self.sz[q] <= self.sz[p]:
+#                 self.id[rt_q] = rt_p  # assign the new root to the bigger tree
+#                 self.sz[rt_p] += self.sz[rt_q]  # update the size of the new root
+#             else:
+#                 self.id[rt_p] = rt_q
+#                 self.sz[rt_q] += self.sz[rt_p]
+#
+#
+# class Solution:
+#
+#     def numIslands(self, grid) -> int:
+#         if not grid:
+#             return 0
+#
+#         def neighbor(coor):
+#             """return the UF id's index at up, down, left, right"""
+#             x, y = coor[0], coor[1]
+#             up = (x-1, y)
+#             down = (x+1,y)
+#             left = (x, y-1)
+#             right = (x, y+1)
+#             candidates = list(filter(isValid, [up, down, left, right]))
+#             candidates = [i for i in candidates if grid[i[0]][i[1]] == '1']
+#             return candidates
+#
+#         def isValid(coor):
+#             x, y = coor[0], coor[1]
+#             if x < 0 or x > row - 1 or y < 0 or y > col - 1:
+#                 return False
+#             else:
+#                 return True
+#
+#         row = len(grid)
+#         col = len(grid[0])
+#         UF = LC200UnionFind_hmp(row, col)
+#
+#         for r in range(row):
+#             for c in range(col):
+#                 coor = (r, c)
+#                 coorval = grid[r][c]
+#                 if coorval == '1':
+#                     for q in neighbor(coor):
+#                         UF.union(coor, q)
+#                 else:
+#                     UF.id[coor] = None
+#
+#         return UF.groupCount()
 
 
 
