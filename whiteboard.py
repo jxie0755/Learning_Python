b38e5b3174741b471361455473ab8c454016ce29
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-03 13:59:32 -0400

    Quick solve for Shengfeng

--- whiteboard.py
+++ whiteboard.py
@@ -2,36 +2,38 @@
 # No two adjeacent numbers are the same
 # Sequences may use any intergers between 1 and a given number k where 1 <= j <= k
 # Count the number of valid sequences.
+# Return the answer of remainder of 10^10 + 7
 
 
 
 def foo(n, k, j):
+    R = pow(10, 10) + 7
     base = k-1
     expo = n-2-2
 
     # 直接算,导致数字太大溢出
     # A = pow(base, expo) * (base - 1) * (base - 1)
     # B = pow(base, expo) * 1 * (base)
-    # print((A + B) % 10)
+    # print((A + B) % R)
 
     # 迭代计算余数
     ans = 1
     for i in range(expo):
         ans *= base
-        if ans >= 10:
-            ans = ans % 10
-    ans_A = ans * (base-1) * (base-1) % 10
-    ans_B = ans * base % 10
+        if ans >= R:
+            ans = ans % R
+    ans_A = ans * (base-1) * (base-1) % R
+    ans_B = ans * 1 * base % R
 
-    return (ans_A + ans_B) % 10
+    return (ans_A + ans_B) % R
 
 # Q1:
 # n = 100, k = 100, j = 1
-print(foo(100, 100, 1)) # >>> 3
+print(foo(100, 100, 1)) # >>> 8691140351
 
 # Q2:
 # n = 347, k = 2281, j = 829
-print(foo(347, 2281, 829)) # >>>  0
+print(foo(347, 2281, 829)) # >>>  9256858202
 
 
 
