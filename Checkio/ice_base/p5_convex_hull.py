1736436351bf59eb77abf04f284f6f6a4481b813
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-02 23:07:01 -0400

    Trying a new algorithm with Princeton's Algorithm P1 Lecture 04 update to sorting method

--- Checkio/ice_base/p5_convex_hull.py
+++ Checkio/ice_base/p5_convex_hull.py
@@ -6,110 +6,6 @@
 # The sequence starts from the bottom leftmost point.
 # points lie on the convex hull in clockwise order (see the picture)
 
-def checkio(data):
-    # find the points at most up/down/left/right in a list called edge, edge is safe
-    xlst, ylst = [i[0] for i in data], [i[1] for i in data]
-    edge = list(
-        filter(lambda i: i[0] == min(xlst) or i[0] == max(xlst) or i[1] == min(ylst) or i[1] == max(ylst), data))
-
-    # the rest of the coor is suspecious
-    suspecious = [i for i in data if i not in edge]
-
-    print(edge)
-    print(suspecious)
-
-    def inside_polygon(point, polygon):
-        """
-        Return True if a coordinate (x, y) is inside a polygon defined by
-        a list of verticies [(x1, y1), (x2, x2), ... , (xN, yN)].
-        """
-        x, y = point[0], point[1]
-        n = len(polygon)
-        inside = False
-        p1x, p1y = polygon[0]
-        for i in range(1, n + 1):
-            p2x, p2y = polygon[i % n]
-            if y > min(p1y, p2y):
-                if y <= max(p1y, p2y):
-                    if x <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-        return inside
-
-    def pointOnBorder(point, polygon):
-        """
-        Return True if a coordinate (x, y) is on the edge of a polygon defined by
-        a list of verticies [(x1, y1), (x2, x2), ... , (xN, yN)].
-        """
-        x, y = point[0], point[1]
-        n = len(polygon)
-        for i in range(n):
-            p1x, p1y = polygon[i]
-            p2x, p2y = polygon[(i + 1) % n]
-            v1x = p2x - p1x
-            v1y = p2y - p1y  # vector for the edge between p1 and p2
-            v2x = x - p1x
-            v2y = y - p1y  # vector from p1 to the point in question
-            if (v1x * v2y - v1y * v2x == 0):  # if vectors are parallel
-                if (v2x / v1x > 0):  # if vectors are pointing in the same direction
-                    if (v1x * v1x + v1y * v1y >= v2x * v2x + v2y * v2y):  # if v2 is shorter than v1
-                        return True
-        return False
-
-    candidates = edge[:]
-    for i in suspecious:
-        if inside_polygon(i, candidates) and not pointOnBorder(i, candidates):
-           pass
-        else:
-            candidates.append(i)
-    print(candidates)
-
-    # after edge is finished report the index
-    left, right, up, down, result = [], [], [], [], []
-    for i in sorted(candidates):
-        if i[0] == min(xlst):  # left
-            left.append(i)
-        if i[1] == max(ylst):  # up
-            up.append(i)
-        if i[0] == max(xlst):  # right
-            right.append(i)
-        if i[1] == min(ylst):  # down
-            down.append(i)
-    restcandidates = []
-    for i in candidates:
-        if i not in up and i not in down and i not in left and i not in right:
-            restcandidates.append(i)
-
-    # organize the result with the sequence of coordinates
-    for i in left:
-        result.append(i)
-    for i in restcandidates:
-        if i[1] >= left[-1][1] and i[0] <= up[0][0]:  # left to up
-            result.append(i)
-    for i in up:
-        result.append(i)
-    for i in restcandidates:
-        if i[0] >= up[-1][0] and i[1] >= right[-1][1]:
-            result.append(i)
-    for i in right[::-1]:
-        result.append(i)
-    for i in restcandidates:
-        if i[0] >= down[-1][0] and i[1] <= right[0][1]:
-            result.append(i)
-    for i in down[::-1]:
-        result.append(i)
-    for i in restcandidates:
-        if i[0] <= down[0][0] and i[1] <= left[0][1]:
-            result.append(i)
-    print(result)
-    indexlst = []
-    for i in result:
-        if data.index(i) not in indexlst:
-            indexlst.append(data.index(i))
-    return indexlst
 
 import math
 def checkio(data):
@@ -149,6 +45,7 @@ def checkio(data):
         else:
             return 1 + (p[0] - a[0]) / distance(a, p)
 
+    # Sort the data list according to the sin_p angle
     sorted_coors = sorted(data, key=lambda x:sin_p(x))
 
     pass
