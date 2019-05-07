e423fa2d0785cd090b72f8c03747b9674a1fd971
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-07 11:05:23 -0400

    Leetcode P096 Initiation

---
+++ LeetCode/p096_unique_binary_search_trees.py
@@ -0,0 +1,71 @@
+# P096 Unique Binary Search Trees
+# Medium
+
+# Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+# TODO after learning BST
+
+from typing import *
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+
+        def layer(T, L=1):
+            if not T.val:
+                return 'N'
+
+            s = str(T.val)
+            if T.left and T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
+            elif T.left and not T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
+            elif not T.left and T.right:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
+            else:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
+
+        return layer(self)
+
+
+def genTree(lst):
+    """
+    generate a binary tree according to a non-empty list of values
+    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
+    """
+    layers = []
+    i, L = 0, 1
+    while i != len(lst):
+        layers.append(lst[i:i + L])
+        i += L
+        L *= 2
+    pre_root = [TreeNode(i) for i in layers[0]]
+    root_to_return = pre_root[0]
+
+    for k in range(1, len(layers)):
+        cur = [TreeNode(i) for i in layers[k]]
+        for j in range(len(cur)):
+            rt_idx, brc_side = divmod(j, 2)
+            if brc_side == 0:
+                pre_root[rt_idx].left = cur[j]
+            else:
+                pre_root[rt_idx].right = cur[j]
+        pre_root = cur
+
+    return root_to_return
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        pass
+
+
+
+if __name__ == '__main__':
+    assert Solution().numTrees(3) == 5, 'Example 1'
+    print('all passed')
+
