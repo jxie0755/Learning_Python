90ecfcd36e7bf8653070effbd8618084f1bcf473
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-07 11:03:24 -0400

    Leetcode P095 Initiation

---
+++ LeetCode/p095_unique_binary_search_trees_ii.py
@@ -0,0 +1,75 @@
+# P095 Unique Binary Search Trees II
+# Medium
+
+# Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+# TODO after learning BST
+
+from typing import *
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+
+        def layer(T, L=1):
+            if not T.val:
+                return 'N'
+
+            s = str(T.val)
+            if T.left and T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
+            elif T.left and not T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
+            elif not T.left and T.right:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
+            else:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
+
+        return layer(self)
+
+
+def genTree(lst):
+    """
+    generate a binary tree according to a non-empty list of values
+    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
+    """
+    layers = []
+    i, L = 0, 1
+    while i != len(lst):
+        layers.append(lst[i:i + L])
+        i += L
+        L *= 2
+    pre_root = [TreeNode(i) for i in layers[0]]
+    root_to_return = pre_root[0]
+
+    for k in range(1, len(layers)):
+        cur = [TreeNode(i) for i in layers[k]]
+        for j in range(len(cur)):
+            rt_idx, brc_side = divmod(j, 2)
+            if brc_side == 0:
+                pre_root[rt_idx].left = cur[j]
+            else:
+                pre_root[rt_idx].right = cur[j]
+        pre_root = cur
+
+    return root_to_return
+
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        pass
+
+
+if __name__ == '__main__':
+    assert Solution().generateTrees(3) == [
+        genTree([1, None, 3, 2, None]),
+        genTree([3, 2, None, 1, None]),
+        genTree([3, 1, None, None, 2]),
+        genTree([2, 1, 3, None, None]),
+        genTree([1, None, 2, None, 3]),
+    ]
