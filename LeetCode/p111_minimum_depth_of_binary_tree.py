0d31bdc24f52a3da92f61dca2dde238922bef590
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-10 14:36:50 -0400

    Leetcode p111 finished

---
+++ LeetCode/p111_minimum_depth_of_binary_tree.py
@@ -0,0 +1,149 @@
+# P111 Minimum Depth of Binary Tree
+# Easy
+
+
+
+# Given a binary tree, find its minimum depth.
+# The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+# Note: A leaf is a node with no children.
+
+from typing import *
+
+# Definition for a binary tree node.
+from math import log
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+
+        def layer(T, L=1):
+            if T.val is None:
+                return 'N'
+
+            s = str(T.val)
+            if T.left and T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
+            elif T.left and not T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
+            elif not T.left and T.right:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
+            else:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
+
+        return layer(self)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def isLeaf(self):
+        try:
+            leftval = self.left.val
+        except AttributeError:
+            leftval = None
+        try:
+            rightval = self.right.val
+        except AttributeError:
+            rightval = None
+
+        return leftval and rightval
+
+def genTree(lst):
+    """
+    generate a binary tree according to a non-empty list of values
+    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
+    """
+    LLL = log(len(lst)+1,2)
+    if int(LLL) != LLL:
+        print("List length is not complete, it must be 2^n - 1")
+        raise ZeroDivisionError
+
+    layers = []
+    i, L = 0, 1
+
+    while i != len(lst):
+        layers.append(lst[i:i + L])
+        i += L
+        L *= 2
+    pre_root = [TreeNode(i) for i in layers[0]]
+    root_to_return = pre_root[0]
+
+    for k in range(1, len(layers)):
+        cur = [TreeNode(i) for i in layers[k]]
+        for j in range(len(cur)):
+            rt_idx, brc_side = divmod(j, 2)
+            if brc_side == 0:
+                pre_root[rt_idx].left = cur[j]
+            else:
+                pre_root[rt_idx].right = cur[j]
+        pre_root = cur
+
+    return root_to_return
+
+
+
+class Solution:
+    ### The key is to find the first leaf
+    def isLeaf(self, root):
+        if (root and root.val is not None) and (not root.left or root.left.val is None) and (not root.right or root.right.val is None):
+            return True
+        return False
+
+    def minDepth(self, root):
+        if not root:
+            return 0
+
+        layer = [root]
+        depth = 1
+        while layer:
+            new_layer = []
+            for i in layer:
+                if self.isLeaf(i):
+                    return depth
+                if i.left:
+                    new_layer.append(i.left)
+                if i.right:
+                    new_layer.append(i.right)
+            layer = new_layer
+            depth += 1
+
+        return depth
+
+
+class Solution(object):
+    # STD ans
+    # @param root, a tree node
+    # @return an integer
+    def minDepth(self, root):
+        if root is None or root.val is None:
+            return 0
+        if root.left and root.left.val is not None and root.right and root.right.val is not None:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+
+
+if __name__ == '__main__':
+    A = None
+    assert Solution().minDepth(A) == 0, 'Edge 0'
+
+    A = TreeNode(1)
+    assert Solution().minDepth(A) == 1, 'Edge 1'
+
+    A = genTree([3,9,20,None, None, 15, 7])
+    assert Solution().minDepth(A) == 2, 'Example 1'
+
+    A = genTree([
+        1,
+        2, None
+    ])
+    assert Solution().minDepth(A) == 2, 'Additional 1'
+
+    print('All passed')
+
+
