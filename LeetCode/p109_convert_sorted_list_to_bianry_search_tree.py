833b2dedcf57206ff0dcd5ead01188ec43baf447
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-10 10:21:03 -0400

    Leetcode p109 finished

---
+++ LeetCode/p109_convert_sorted_list_to_bianry_search_tree.py
@@ -0,0 +1,145 @@
+# P109 Convert Sorted List to Binary Search Tree
+# Medium
+
+
+# Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self.next:
+            return "{}->{}".format(self.val, repr(self.next))
+        else:
+            return "{}".format(self.val)
+
+def genNode(*nodes, end=None):
+    if len(nodes) == 1 and type(nodes[0]) == list:
+        nodes = nodes[0]
+    for i in nodes[::-1]:
+        n = ListNode(i)
+        n.next, end = end, n
+    return n if nodes else None
+
+# Definition for a binary tree node.
+from math import log
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+
+        def layer(T, L=1):
+            if T.val is None:
+                return 'N'
+
+            s = str(T.val)
+            if T.left and T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
+            elif T.left and not T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
+            elif not T.left and T.right:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
+            else:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
+
+        return layer(self)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+
+def genTree(lst):
+    """
+    generate a binary tree according to a non-empty list of values
+    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
+    """
+    LLL = log(len(lst)+1,2)
+    if int(LLL) != LLL:
+        print("List length is not complete, it must be 2^n - 1")
+        raise ZeroDivisionError
+
+    layers = []
+    i, L = 0, 1
+
+    while i != len(lst):
+        layers.append(lst[i:i + L])
+        i += L
+        L *= 2
+    pre_root = [TreeNode(i) for i in layers[0]]
+    root_to_return = pre_root[0]
+
+    for k in range(1, len(layers)):
+        cur = [TreeNode(i) for i in layers[k]]
+        for j in range(len(cur)):
+            rt_idx, brc_side = divmod(j, 2)
+            if brc_side == 0:
+                pre_root[rt_idx].left = cur[j]
+            else:
+                pre_root[rt_idx].right = cur[j]
+        pre_root = cur
+
+    return root_to_return
+
+
+from typing import *
+
+class Solution:
+    ### Convert linked list to an arrayList then solve the problem like Leetcode P108
+
+
+
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+
+        if not head:
+            return None
+
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+
+
+        mid_idx = len(nums) // 2
+        mid = nums[mid_idx]
+        ans = TreeNode(mid)
+
+        def helper(root, left_list, right_list):
+            if left_list:
+                left_mid_idx = len(left_list) // 2
+                left_val = left_list[left_mid_idx]
+                root.left = TreeNode(left_val)
+                LL, LR = left_list[:left_mid_idx], left_list[left_mid_idx+1:]
+                helper(root.left, LL, LR)
+
+            if right_list:
+                right_mid_idx = len(right_list) // 2
+                right_val = right_list[right_mid_idx]
+                root.right = TreeNode(right_val)
+                RL, RR = right_list[:right_mid_idx], right_list[right_mid_idx + 1:]
+                helper(root.right, RL, RR)
+
+        helper(ans, nums[:mid_idx], nums[mid_idx+1:])
+        return ans
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        A = None
+        assert Solution().sortedListToBST(A) is None, 'Edge 0'
+
+        A = genNode(1)
+        assert Solution().sortedListToBST(A) == genTree([1]), 'Edge 1'
+
+        A = genNode(-10, -3, 0, 5, 9)
+        assert Solution().sortedListToBST(A) == genTree([0, -3, 9, -10, None, 5, None]), 'Example 1'
+
+        print('all passed')
