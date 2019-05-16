faf52efc474e6fb4c91c144937672671e98462b4
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-15 21:43:03 -0400

    Leetcode P105 STD answer attached

--- LeetCode/p105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ LeetCode/p105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -163,6 +163,27 @@ class Solution:
         return helper(preorder)
 
 
+class Solution(object):
+    # STD ans
+    # @param preorder, a list of integers
+    # @param inorder, a list of integers
+    # @return a tree node
+    def buildTree(self, preorder, inorder):
+        lookup = {}
+        for i, num in enumerate(inorder):
+            lookup[num] = i
+        return self.buildTreeRecu(lookup, preorder, inorder, 0, 0, len(inorder))
+
+    def buildTreeRecu(self, lookup, preorder, inorder, pre_start, in_start, in_end):
+        if in_start == in_end:
+            return None
+        node = TreeNode(preorder[pre_start])
+        i = lookup[preorder[pre_start]]
+        node.left = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1, in_start, i)
+        node.right = self.buildTreeRecu(lookup, preorder, inorder, pre_start + 1 + i - in_start, i + 1, in_end)
+        return node
+
+
 
 
 if __name__ == '__main__':
