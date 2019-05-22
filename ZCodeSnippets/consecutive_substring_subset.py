1718b14345988c2e32befa48679cdc5e0c8b0d31
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-22 10:43:13 -0400

    update naming

--- ZCodeSnippets/consecutive_substring_subset.py
+++ ZCodeSnippets/consecutive_substring_subset.py
@@ -5,7 +5,7 @@
 
 # from short to long
 # 先确定长度(从短到长),再确定起始index
-def substring_SL(iterable):
+def subSequence_SL(iterable):
     result = []
     for lenth in range(1, len(iterable) + 1):
         for i in range(len(iterable) - lenth + 1):
@@ -13,15 +13,15 @@ def substring_SL(iterable):
     return result
 
 if __name__ == '__main__':
-    print(substring_SL('abcd'))
+    print(subSequence_SL('abcd'))
     # >>> ['a', 'b', 'c', 'd', 'ab', 'bc', 'cd', 'abc', 'bcd', 'abcd']
-    print(substring_SL([1,2,3,4]))
+    print(subSequence_SL([1, 2, 3, 4]))
     # >>> [[1], [2], [3], [4], [1, 2], [2, 3], [3, 4], [1, 2, 3], [2, 3, 4], [1, 2, 3, 4]]
 
 
 # from long to short
 # 仍然是先确定长度(通过-1 step,从长到到短),再确定起始index
-def substring_LS(iterable):
+def subSequence_LS(iterable):
     result = []
     for lenth in range(len(iterable), 0, -1):
         for i in range(len(iterable) - lenth + 1):
@@ -29,15 +29,15 @@ def substring_LS(iterable):
     return result
 
 if __name__ == '__main__':
-    print(substring_LS('abcd'))
+    print(subSequence_LS('abcd'))
     # >>> ['abcd', 'abc', 'bcd', 'ab', 'bc', 'cd', 'a', 'b', 'c', 'd']
-    print(substring_LS([1,2,3,4]))
+    print(subSequence_LS([1, 2, 3, 4]))
     # >>> [[1, 2, 3, 4], [1, 2, 3], [2, 3, 4], [1, 2], [2, 3], [3, 4], [1], [2], [3], [4]]
 
 
 # 第三种方式找substring, 新的寻找顺序.
 # 交换loop的顺序,先确定起始index,再变化长度
-def substring_seq(iterable):
+def subSequence_SEQ(iterable):
     result = []
     for i in range(len(iterable)):
         for lenth in range(1, len(iterable) - i + 1):
@@ -45,14 +45,14 @@ def substring_seq(iterable):
     return result
 
 if __name__ == '__main__':
-    print(substring_seq('abcd'))
+    print(subSequence_SEQ('abcd'))
     # >>> ['a', 'ab', 'abc', 'abcd', 'b', 'bc', 'bcd', 'c', 'cd', 'd']
-    print(substring_seq([1,2,3,4]))
+    print(subSequence_SEQ([1, 2, 3, 4]))
     # >>> [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4], [2], [2, 3], [2, 3, 4], [3], [3, 4], [4]]
 
 
 # 第四种方式,寻找中心,然后向两边发散,(从palindrome algorithm中获得)
-def substring_C(iterable):
+def subSequence_C(iterable):
     result = []
     # this part assigns the moving of the center, and define the two sides from it
     for i in range(len(iterable)):
@@ -67,31 +67,31 @@ def substring_C(iterable):
     return result
 
 if __name__ == '__main__':
-    print(substring_C('abcd'))
+    print(subSequence_C('abcd'))
     # >>> ['a', 'ab', 'b', 'abc', 'bc', 'abcd', 'c', 'bcd', 'cd', 'd']
-    print(substring_C([1,2,3,4]))
+    print(subSequence_C([1, 2, 3, 4]))
     # >>> [[1], [1, 2], [2], [1, 2, 3], [2, 3], [1, 2, 3, 4], [3], [2, 3, 4], [3, 4], [4]]
 
 # generator versions of above
-def gen_substring_SL(iterable):
+def gen_subSequence_SL(iterable):
     for lenth in range(1, len(iterable) + 1):
         for i in range(len(iterable) - lenth + 1):
             yield iterable[i:i+lenth]
 
 
-def gen_substring_LS(iterable):
+def gen_subSequence_LS(iterable):
     for lenth in range(len(iterable), 0, -1):
         for i in range(len(iterable) - lenth + 1):
             yield iterable[i:i+lenth]
 
 
-def gen_substrings(iterable):
+def gen_subSequence(iterable):
     for i in range(len(iterable)):
         for lenth in range(1, len(iterable) - i + 1):
             yield iterable[i:i+lenth]
 
 
-def gen_substring_C(iterable):
+def gen_subSequence_C(iterable):
     for i in range(len(iterable)):
         for j in range(2):
             left = i
