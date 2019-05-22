e5b70942cad7dd2fe9c620d7c5266569dc6e6b9a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-22 15:19:41 -0400

    Leetcode P144 finished

---
+++ LeetCode/p144_binary_tree_preorder_traversal.py
@@ -0,0 +1,93 @@
+# P144 Binary Tree Preorder Taversal
+# Meidum
+
+# Given a binary tree, return the preorder traversal of its nodes' values.
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# Definition for a binary tree node.
+from math import log
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+
+        def layer(T, L=1):
+            if T.val is None:
+                return 'N'
+
+            s = str(T.val)
+            if T.left and T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
+            elif T.left and not T.right:
+                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
+            elif not T.left and T.right:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
+            else:
+                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
+
+        return layer(self)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def isLeaf(self):
+        try:
+            leftval = self.left.val
+        except AttributeError:
+            leftval = None
+        try:
+            rightval = self.right.val
+        except AttributeError:
+            rightval = None
+
+        return leftval and rightval
+
+def genTree(lst, i=1):
+    """
+    To generate a perfect binary tree according to a non-empty list of values
+    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
+    """
+    if lst and i <= len(lst) and lst[i-1] is not None:
+        node = TreeNode(lst[i-1])
+        node.left = genTree(lst, i*2)
+        node.right = genTree(lst, i*2+1)
+        return node
+
+
+class Solution(object):
+
+    def preorderTraversal(self, root):
+        ### Recursive method
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+
+if __name__ == '__main__':
+    A = []
+    assert not Solution().preorderTraversal(A), 'Edge 0'
+
+    A = genTree([
+        1,
+        None, 2,
+        None, None, 3, None
+    ])
+    assert Solution().preorderTraversal(A) == [1,2,3], 'Example 1'
+
+    A = genTree([
+        1,
+        4,3,
+        2
+    ])
+    assert Solution().preorderTraversal(A) == [1, 4, 2, 3], 'Example 2'
+
+    print('all passed')
+
