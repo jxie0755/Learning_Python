66c3732b267f36e7831cca05da5a2c3ccdfe3534
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-22 15:53:42 -0400

    Update TreeNode and ListNode Template

--- LeetCode/p114_flatten_bianry_tree_to_linked_list.py
+++ LeetCode/p114_flatten_bianry_tree_to_linked_list.py
@@ -3,59 +3,8 @@
 
 # Given a binary tree, flatten it to a linked list in-place.
 from typing import *
-
-# Definition for a binary tree node.
-from math import log
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-    def __str__(self):
-
-        def layer(T, L=1):
-            if T.val is None:
-                return 'N'
-
-            s = str(T.val)
-            if T.left and T.right:
-                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + layer(T.right, L + 1)
-            elif T.left and not T.right:
-                return s + '\n' + '  ' * L + layer(T.left, L + 1) + '\n' + '  ' * L + 'N'
-            elif not T.left and T.right:
-                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + layer(T.right, L + 1)
-            else:
-                return s + '\n' + '  ' * L + 'N' + '\n' + '  ' * L + 'N'
-
-        return layer(self)
-
-    def __eq__(self, other):
-        return str(self) == str(other)
-
-    def isLeaf(self):
-        try:
-            leftval = self.left.val
-        except AttributeError:
-            leftval = None
-        try:
-            rightval = self.right.val
-        except AttributeError:
-            rightval = None
-
-        return leftval and rightval
-
-def genTree(lst, i=1):
-    """
-    To generate a perfect binary tree according to a non-empty list of values
-    The lst must be all filled, even the branch is empty, then use None to suggest the empty treeNode
-    """
-    if lst and i <= len(lst) and lst[i-1] is not None:
-        node = TreeNode(lst[i-1])
-        node.left = genTree(lst, i*2)
-        node.right = genTree(lst, i*2+1)
-        return node
+from a0_TreeNode import *
+from a0_ListNode import *
 
 
 class Solution:
