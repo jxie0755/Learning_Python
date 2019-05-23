f383c4da3756f2422dabbc3f20a4a37395755904
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-23 17:47:53 -0400

    Leetcode P148 Finished With self version of mergesort

---
+++ LeetCode/p148_sort_list.py
@@ -0,0 +1,74 @@
+# P148 Sort List
+# Medium
+
+
+# Sort a linked list in O(n log n) time using constant space complexity.
+
+from typing import *
+from a0_TreeNode import *
+from a0_ListNode import *
+
+
+class Solution(object):
+
+    # Use mergesort method
+    # Create divide and merge function
+    # Divide: break the linked list at mid point and return two parts
+    # Merge: merge two sorted linked list
+
+    def mergesort(self, A, B):
+        """
+        quick sort a linked list from head to tail
+        tail must be reachable from the head
+        modify in place
+        """
+        dummy = ListNode('X')
+        cur = dummy
+        while A and B:
+            if A.val < B.val:
+                cur.next, A = A, A.next
+            else:
+                cur.next, B = B, B.next
+            cur = cur.next
+
+        cur.next = A if A else B
+        return dummy.next
+
+    def divide(self, head):
+        """divide a linked list into half and break the linkage in between"""
+        if head.next:
+            fast, slow, prev = head, head, None
+            while fast is not None and fast.next is not None:
+                fast, slow, prev = fast.next.next, slow.next, slow
+
+            prev.next = None
+            return head, slow
+
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        elif not head.next:
+            return head
+        else:
+            first, second = self.divide(head)
+            return self.mergesort(self.sortList(first), self.sortList(second))
+
+
+if __name__ == '__main__':
+    A = None
+    assert Solution().sortList(A) is None, 'Edge 0'
+
+    A = genNode(1)
+    assert Solution().sortList(A) == A, 'Edge 1'
+
+    A = genNode(4,2,1,3)
+    assert Solution().sortList(A) == genNode(1,2,3,4), 'Example 1'
+
+    A = genNode(-1, 5, 3, 4, 0)
+    assert Solution().sortList(A) == genNode(-1, 0, 3, 4, 5), 'Example 2'
+
+    print('all passed')
