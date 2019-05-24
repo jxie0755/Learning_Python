4f693305c51f6bb9073614aefcb64269bec078f4
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-24 16:30:15 -0400

    Try a new method

--- LeetCode/p160_intersection_of_two_linked_lists.py
+++ LeetCode/p160_intersection_of_two_linked_lists.py
@@ -10,17 +10,8 @@
 # Your code should preferably run in O(n) time and use only O(1) memory.
 
 from typing import *
-
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-    def __repr__(self):
-        if self.next:
-            return "{}->{}".format(self.val, repr(self.next))
-        else:
-            return "{}".format(self.val)
+from a0_TreeNode import *
+from a0_ListNode import *
 
 
 def genNode(*nodes, end=None):
@@ -35,8 +26,8 @@ def genNode(*nodes, end=None):
 
 class Solution(object):
 
+    ### Version A
     ### O(N) time, but O(N) memory
-    ### To ensure hashability, one must remove __eq__ method
     def getIntersectionNode(self, headA, headB):
         """
         :type head1, head1: ListNode
@@ -55,26 +46,45 @@ class Solution(object):
         return None
 
 
+class Solution(object):
 
-if __name__ == '__main__':
-    A = genNode(4,1)
-    B = genNode(5,0,1)
-    C = genNode(8,4,5)
-    A.next.next = C
-    B.next.next.next = C
+    ### Version B, optimized
+    ### O(N) time, and O(1) memory
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        pass
 
-    assert Solution().getIntersectionNode(A, B) == C, 'Example 1'
 
-    A = genNode(0,9,1)
-    B = genNode(3)
-    C = genNode(2,4)
-    A.next.next.next = C
-    B.next = C
-    assert Solution().getIntersectionNode(A, B) == C, 'Example 2'
+A = genNode(4,1)
+B = genNode(5,0,1)
+C = genNode(8,4,5)
+A.next.next = C
+B.next.next.next = C
+print(Solution().getIntersectionNode(A, B))
 
-    A = genNode(2,6,4)
-    B = genNode(1,5)
-    assert not Solution().getIntersectionNode(A, B) == C, 'Example 3, Edge'
 
-    print('all passed')
+# if __name__ == '__main__':
+#     A = genNode(4,1)
+#     B = genNode(5,0,1)
+#     C = genNode(8,4,5)
+#     A.next.next = C
+#     B.next.next.next = C
+#
+#     assert Solution().getIntersectionNode(A, B) == C, 'Example 1'
+#
+#     A = genNode(0,9,1)
+#     B = genNode(3)
+#     C = genNode(2,4)
+#     A.next.next.next = C
+#     B.next = C
+#     assert Solution().getIntersectionNode(A, B) == C, 'Example 2'
+#
+#     A = genNode(2,6,4)
+#     B = genNode(1,5)
+#     assert not Solution().getIntersectionNode(A, B) == C, 'Example 3, Edge'
+#
+#     print('all passed')
 
