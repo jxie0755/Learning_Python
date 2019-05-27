eead2e073c63db69f6828956edf251d77227e1e1
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-27 11:46:06 -0400

    Leetcode P162 Initiation

---
+++ LeetCode/p162_find_peak_element.py
@@ -0,0 +1,30 @@
+# P162 Find Peak Element
+# Medium
+
+
+# A peak element is an element that is greater than its neighbors.
+# Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+# The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+# You may imagine that nums[-1] = nums[n] = -∞.
+
+# Note:
+# Your solution should be in logarithmic complexity.
+
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pass
+
+
+if __name__ == '__main__':
+
+    assert Solution().findPeakElement([1,2,3,1]) == 2, 'Example 1'
+
+    B = Solution().findPeakElement([[1,2,1,3,5,6,4]])
+    assert B == 1 or B == 5, 'Example 2'
+
+    print('all passed')
