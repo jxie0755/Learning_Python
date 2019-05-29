34c189036375499fa56c4f7af7e5646d67d21505
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-29 09:06:50 -0400

    Leetcode P172 STD ans learned

--- LeetCode/p172_factorial_trailing_zeroes.py
+++ LeetCode/p172_factorial_trailing_zeroes.py
@@ -33,12 +33,33 @@ class Solution(object):
 
 
 class Solution(object):
+    # STD ans
     # @return an integer
+    # 由于只有2*5能得到尾数10, 而因数2远多于因数5, 所以只要知道从1到n总共能拆解出多少个因数5就行
+    # 注意25 = 5 * 5 是两个5, 所以要用循环找遍所有的//5
     def trailingZeroes(self, n):
         result = 0
         while n > 0:
             result += n // 5
             n //= 5
+        return result
+
+
+### 简单理解版本
+class Solution(object):
+    # Version B
+    # 逐个检查1-n中每个数,能拆出多少个5因数
+    # 但是这样还是慢这样为N(logN)
+    def trailingZeroes(self, n):
+        result = 0
+        for x in range(1, n+1):
+            while True:
+                a, b = divmod(x, 5)
+                if b == 0:
+                    result += 1
+                    x = a
+                else:
+                    break
         return result
 
 
@@ -49,6 +70,6 @@ if __name__ == '__main__':
     assert Solution().trailingZeroes(5) == 1, 'Example 2'
     assert Solution().trailingZeroes(20) == 4, 'Additional 1'
     assert Solution().trailingZeroes(3743) == 932, 'Additional 2'
-    print(Solution().trailingZeroes(1808548329))
+    # print(Solution().trailingZeroes(1808548329))
     print('all passed')
 
