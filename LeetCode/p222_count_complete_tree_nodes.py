8e40e3b674035d0194633e3861fd07877dfda7bb
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-05-31 15:45:33 -0400

    Leetcode P222 finished

--- LeetCode/p222_count_complete_tree_nodes.py
+++ LeetCode/p222_count_complete_tree_nodes.py
@@ -101,7 +101,8 @@ class Solution(object):
 class Solution(object):
 
     ### Version B
-    ### Combine isComplete and count in one function
+    ### Combine isComplete and count in one function, through a recursive call on helper
+    #
     ### by Examine the isComplete in helper(root), it forced to check isComplete every node in the root
     ### use memorization, direct tell a node is complete or not and exam every node
     ### As a side effect, when checking the isComplete in helper, add to count if it is the first time found as isComplete
