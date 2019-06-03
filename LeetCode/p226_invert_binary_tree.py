179ae6188355a267191b5dcb19274e09cdb35932
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-02 22:34:18 -0400

    Leetcode P226 initiation

---
+++ LeetCode/p226_invert_binary_tree.py
@@ -0,0 +1,57 @@
+# P226 Invert Binary Tree
+# Easy
+
+# Invert a binary tree.
+# Trivia:
+# This problem was inspired by this original tweet by Max Howell:
+# Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+
+from typing import *
+from a0_TreeNode import *
+from a0_ListNode import *
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        pass
+
+
+
+if __name__ == '__main__':
+    assert Solution().invertTree(None) is None, 'Edge 0'
+
+    A = genTree([1])
+    assert Solution().invertTree(A) == A, 'Edge 1'
+
+    A = genTree([
+        4,
+        2,7,
+        1,3,6,9
+    ])
+
+    B = genTree([
+        4,
+        7,2,
+        9,6,3,1
+    ])
+    assert Solution().invertTree(A) == B, 'Example 1'
+
+    A = genTree([
+        4,
+        2, 7,
+        None, 3, None, 9
+    ])
+
+    B = genTree([
+        4,
+        7, 2,
+        9, None, 3, None
+    ])
+    assert Solution().invertTree(A) == B, 'Example 1b'
+
+    print('all passed')
