9927d875a074a0330ea08f7e69f1e1d05e06fa1e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-03 11:08:02 -0400

    Leetcode P227 self version

--- LeetCode/p227_basic_calculator_ii.py
+++ LeetCode/p227_basic_calculator_ii.py
@@ -11,13 +11,55 @@
 
 
 class Solution(object):
+
+    ### Version A
+    ### Use the op stack and number staock
+    ### This is quite slow
     def calculate(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        nums = '0123456789'
 
+        number, op = [], []
+        temp = ''
+        for i in s:
+            if i in nums:
+                temp += i
+            elif i in '*/+-':
+                number.append(int(temp))
+                temp = ''
+                op.append(i)
+        number.append(int(temp))
+
+        k = 0
+        while k != len(op):
+            o = op[k]
+            if o in '*/':
+                if o == '*':
+                    number[k] = number[k] * number[k + 1]
+                if o == '/':
+                    number[k] = number[k] // number[k + 1]
+                number.pop(k + 1)
+                op.pop(k)
+            else:
+                k += 1
+
+        k = 0
+        while k != len(op):
+            o = op[k]
+            if o == '+':
+                number[k] = number[k] + number[k + 1]
+            if o == '-':
+                number[k] = number[k] - number[k + 1]
+            number.pop(k + 1)
+            op.pop(k)
+
+        return number[0]
+
+
+print(Solution().calculate("3+2*2"))
 
 if __name__ == '__main__':
     assert Solution().calculate("3+2*2") == 7, 'Example 1'
