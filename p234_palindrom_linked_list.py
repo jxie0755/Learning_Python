51e217103c96397a57be21b526f3fa6542b09dea
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-04 17:51:59 -0400

    Leetcode P234 STD ans attached

--- p234_palindrom_linked_list.py
+++ p234_palindrom_linked_list.py
@@ -114,6 +114,35 @@ class Solution(object):
             head = head.next
         return True
 
+class Solution(object):
+    # STD ans
+    # @param {ListNode} head
+    # @return {boolean}
+    # Same idea of Version C, but reverse first half, and do it at the same time to get second half
+
+    # Disadvantage: need to handle the odd length
+    def isPalindrome(self, head):
+        reverse, fast = None, head
+        # Reverse the first half part of the list.
+        while fast and fast.next:
+            fast = fast.next.next
+            head.next, reverse, head = reverse, head, head.next
+
+        # If the number of the nodes is odd,
+        # set the head of the tail list to the next of the median node.
+        tail = head.next if fast else head
+
+        # Compare the reversed first half list with the second half list.
+        # And restore the reversed first half list.
+        is_palindrome = True
+        while reverse:
+            is_palindrome = is_palindrome and reverse.val == tail.val
+            reverse.next, head, reverse = head, reverse, reverse.next
+            tail = tail.next
+
+        return is_palindrome
+
+
 
 if __name__ == '__main__':
     assert Solution().isPalindrome(None), 'Edge'
