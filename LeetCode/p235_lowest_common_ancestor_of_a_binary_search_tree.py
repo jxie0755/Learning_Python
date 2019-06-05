22bd07e50b7e861a91fff581d76135b3f318f270
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-04 21:00:02 -0400

    Leetcode P235 self version finished fast

--- LeetCode/p235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ LeetCode/p235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -16,6 +16,11 @@ from a0_ListNode import *
 
 
 class Solution(object):
+
+    # Version A
+    # use a helper function to recursive check
+    # first get the range of the root (min and max)
+    # if p's val and q's val within the range, check the left and right until neither's range covers
     def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
@@ -23,8 +28,25 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        pass
+        pv, qv = p.val, q.val
+        minn = maxx = root
+        while minn.left:
+            minn = minn.left
+        while maxx.right:
+            maxx = maxx.right
 
+        result = []
+        def helper(root, A, B):
+            rv = root.val
+            if pv >= A and qv >= A and pv <= B and qv <= B:
+                result.append(root)
+                if root.left:
+                    helper(root.left, A, rv-1)
+                if root.right:
+                    helper(root.right, rv+1, B)
+
+        helper(root, minn.val, maxx.val)
+        return result[-1]
 
 
 if __name__ == '__main__':
@@ -35,7 +57,7 @@ if __name__ == '__main__':
         0,4,7,9,
         None,None,3,5
     ])
-    assert Solution().lowestCommonAncestor(A, root.left, root.right) == A, 'Example 1'
+    assert Solution().lowestCommonAncestor(A, A.left, A.right) == A, 'Example 1'
 
     A = genTree([
         6,
@@ -43,8 +65,7 @@ if __name__ == '__main__':
         0, 4, 7, 9,
         None, None, 3, 5
     ])
-
-    assert Solution().lowestCommonAncestor(A, root.left, root.left.right) == root.left, 'Example 2'
+    assert Solution().lowestCommonAncestor(A, A.left, A.left.right) == A.left, 'Example 2'
 
     A = genTree([
         6,
@@ -52,6 +73,12 @@ if __name__ == '__main__':
         0, 4, 7, 9,
         None, None, 3, 5
     ])
-    assert Solution().lowestCommonAncestor(A, root.left.left, root.left.right.left) == root.left, 'Additional'
+    assert Solution().lowestCommonAncestor(A, A.left.left, A.left.right.left) == A.left, 'Additional 1'
+
+    A = genTree([
+        2,
+        None, 3
+    ])
+    assert Solution().lowestCommonAncestor(A, A, A.right) == A, 'Additional 2'
 
     print('all passed')
