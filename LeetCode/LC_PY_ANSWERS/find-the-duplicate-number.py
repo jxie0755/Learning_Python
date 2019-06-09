ffe790c530e90c6ff4be3a1b66689f23ece8c2c6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-09 11:23:16 -0400

    Leetcode P287 STD ans binary search

--- LeetCode/LC_PY_ANSWERS/find-the-duplicate-number.py
+++ LeetCode/LC_PY_ANSWERS/find-the-duplicate-number.py
@@ -24,6 +24,27 @@ class Solution(object):
         return slow
 
 
+class Solution2(object):
+    # STD ans Binary search method.
+    # Time:  O(n*logn)
+    # Space: O(1)
+    def findDuplicate(self, nums: List[int]) -> int:
+        left, right = 1, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # Get count of num <= mid.
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            if count > mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
 
 
 # Time:  O(n)
--- LeetCode/p287_find_the_duplicate_number.py
+++ LeetCode/p287_find_the_duplicate_number.py
@@ -54,25 +54,7 @@ class Solution:
                 return mid
 
 
-class Solution(object):
-    # STD ans Binary search method.
-    # Time:  O(nlogn)
-    # Space: O(1)
-    def findDuplicate(self, nums: List[int]) -> int:
-        left, right = 1, len(nums) - 1
 
-        while left <= right:
-            mid = left + (right - left) / 2
-            # Get count of num <= mid.
-            count = 0
-            for num in nums:
-                if num <= mid:
-                    count += 1
-            if count > mid:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
 
 
 
@@ -121,6 +103,29 @@ class Solution(object):
 # print(Solution().findDuplicate([1, 2, 2, 3, 4, 5, 6, 7, 8, 9]))
 # print(Solution().findDuplicate([18, 13, 14, 17, 9, 19, 7, 17, 4, 6, 17, 5, 11, 10, 2, 15, 8, 12, 16, 17]))
 #                               0   1   2   3   4  5   6  7   8  9  10  11 12  13  14 15  16 17  18  19
+
+
+class Solution(object):
+    # STD ans Binary search method.
+    # Time:  O(n*logn)
+    # Space: O(1)
+    def findDuplicate(self, nums: List[int]) -> int:
+        left, right = 1, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            print('L', left, 'R', right, 'M', mid)
+            # Get count of num <= mid.
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            if count > mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+            print('count', count)
+        return left
 
 print(Solution().findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 6]))
 #                               0  1  2  3  4  5  6  7  8
