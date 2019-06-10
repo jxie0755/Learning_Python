9320465c4a190f64929a51c1269785b91d303cb3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-10 13:37:31 -0400

    Leetcode P313 self version A failed

--- LeetCode/p313_syper_ugly_number.py
+++ LeetCode/p313_syper_ugly_number.py
@@ -35,8 +35,11 @@ class Solution:
 
 Solution().nthSuperUglyNumber(12, [2,7,13,19])
 
-if __name__ == '__main__':
-    assert Solution().nthSuperUglyNumber(12, [2,7,13,19]) == 32, 'Example 1'
-    assert Solution().nthSuperUglyNumber(800, [37,43,59,61,67,71,79,83,89,97,101,103,113,127,131,157,163,167,173,179,191,193,197,199,211,229,233,239,251,257]) == 411811, 'Long'
 
-    print('all passed')
+
+
+# if __name__ == '__main__':
+#     assert Solution().nthSuperUglyNumber(12, [2,7,13,19]) == 32, 'Example 1'
+#     assert Solution().nthSuperUglyNumber(800, [37,43,59,61,67,71,79,83,89,97,101,103,113,127,131,157,163,167,173,179,191,193,197,199,211,229,233,239,251,257]) == 411811, 'Long'
+#
+#     print('all passed')
