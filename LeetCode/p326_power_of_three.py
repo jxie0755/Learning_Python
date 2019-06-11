062310f7238080354b4ff0f50fcf7db368e53551
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-11 12:59:44 -0400

    Leetcode P326 finished with math mehtod

--- LeetCode/p326_power_of_three.py
+++ LeetCode/p326_power_of_three.py
@@ -7,23 +7,29 @@
 # Follow up:
 # Could you do it without using any loop / recursion?
 
+import math
 class Solution:
 
     # Version A, O(LogN), used loop
-    #
     def isPowerOfThree(self, n: int) -> bool:
-        while n > 3:
-            n //= 3
-        if n == 3:
-            return True
-        else:
-            return False
+        exp = 0
+        while 3 ** exp < n:
+            exp += 1
+
+        return 3 ** exp == n
+
+    def isPowerOfThree(self, n):
+        return n > 0 and (math.log10(n) / math.log10(3)).is_integer()
+
 
 
 if __name__ == '__main__':
+    assert Solution().isPowerOfThree(1), 'Edge 1'
+    assert not Solution().isPowerOfThree(2), 'Edge 2'
     assert Solution().isPowerOfThree(27), 'Example 1'
     assert not Solution().isPowerOfThree(0), 'Example 2'
     assert Solution().isPowerOfThree(9), 'Example 3'
     assert not Solution().isPowerOfThree(45), 'Example 3'
+    assert not Solution().isPowerOfThree(19684), 'Example 4'
     print('all passed')
 
