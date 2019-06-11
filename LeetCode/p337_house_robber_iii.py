5fc9f2dda3a1e5f216ab1ae3aa1b4e84eda73a1a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-11 16:16:02 -0400

    Leetcode P337 self version A exceeded max time

--- LeetCode/p337_house_robber_iii.py
+++ LeetCode/p337_house_robber_iii.py
@@ -13,16 +13,35 @@ from a0_ListNode import *
 
 
 class Solution:
+
+    # Version A
+    # Recursive check next layer and next next layer
+    # Exceeded max time limit
     def rob(self, root: TreeNode) -> int:
-        pass
+        if not root:
+            return 0
+        else:
+            L, LL, LR, R, RL, RR = 0,0,0,0,0,0
+
+            if root.left:
+                L = self.rob(root.left)
+                if root.left.left:
+                    LL = self.rob(root.left.left)
+                if root.left.right:
+                    LR = self.rob(root.left.right)
+
+            if root.right:
+                R = self.rob(root.right)
+                if root.right.left:
+                    RL = self.rob(root.right.left)
+                if root.right.right:
+                    RR = self.rob(root.right.right)
+
+            return max(root.val + LL + LR + RL + RR, L + R)
 
 
 
-
-
-
-
-if not __name__ == '__main__':
+if __name__ == '__main__':
 
     assert Solution().rob(None) == 0, 'Edge'
 
@@ -41,4 +60,22 @@ if not __name__ == '__main__':
         1,3,None,1
     ])
     assert Solution().rob(A) == 9, 'Example 2'
+
+
+    A = genTree([
+        10,
+        1,2,
+        20,2,3,4
+
+    ])
+    assert Solution().rob(A) == 39, 'Example 3'
+
+    A = genTree([
+        100,
+        1, 1,
+        10, 10, 10, 10,
+        1000,1000,1000,1000,1000,1000,1000,1000,
+    ])
+    assert Solution().rob(A) == 8100, 'Example 4'
+
     print('all passed')
