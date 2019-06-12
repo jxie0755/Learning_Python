e0157c0ec499c24ee634448ca963143513d2b115
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-12 10:49:06 -0400

    Leetcode P337 STD ans good recursion learned

--- LeetCode/p337_house_robber_iii.py
+++ LeetCode/p337_house_robber_iii.py
@@ -21,7 +21,7 @@ class Solution:
         if not root:
             return 0
         else:
-            L, LL, LR, R, RL, RR = 0,0,0,0,0,0
+            L, LL, LR, R, RL, RR = 0, 0, 0, 0, 0, 0
 
             if root.left:
                 L = self.rob(root.left)
@@ -74,125 +74,57 @@ class Solution:
                     if root.right.right:
                         RR = helper(root.right.right)
 
-
                 ans = max(root.val + LL + LR + RL + RR, L + R)
                 hmp[root] = ans
                 return ans
 
         return helper(root)
 
-class Solution:
-
-    # Version B
-    # Do it by checking the layers, but same algorithm
-    # Still exceed max time limit
-    def showPerfectFlattenLayers(self, root):  # Include None
-        """
-        Show the tree layer values by layer from top to bottom, in flatten list
-        (Replace None with 0)
-        """
-        if root is None:
-            return []
-
-        result, current = [], [root]
-        while any([i for i in current]):
-            next_level, vals = [], []
-            for node in current:
-                if node:
-                    vals.append(node.val)
-                    if node.left:
-                        next_level.append(node.left)
-                    else:
-                        next_level.append(0)
-                    if node.right:
-                        next_level.append(node.right)
-                    else:
-                        next_level.append(0)
-                else:
-                    vals.append(0)
-                    next_level.append(0)
-                    next_level.append(0)
-
-            current = next_level
-            result += vals
-
-        return [0] + result
 
 
+
+class Solution(object):
+
+    # STD ans
     def rob(self, root: TreeNode) -> int:
-        if not root:
-            return 0
 
-        layersums = self.showPerfectFlattenLayers(root)
-        N = len(layersums)
+        def robHelper(root):
+            if not root:
+                return 0, 0
 
-        hmp = {}
+            left, right = robHelper(root.left), robHelper(root.right)
 
-        def helper(i):
-            """helper list to get the node's potential best sum"""
-            if i >= N:
-                return 0
-            elif i in hmp:
-                return hmp[i]
-            else:
-                L = helper(2*i)
-                R = helper(2*i+1)
+            return root.val + left[1] + right[1], max(left) + max(right)
+                     # if skip left and right     # if skip root
 
-                LL = helper((2*i)*2)
-                LR = helper((2 * i) * 2 + 1)
-
-                RL = helper((2*i+1)*2)
-                RR = helper((2*i+1)*2+1)
-
-                result = max(layersums[i] + LL + LR + RL + RR, L + R)
-                hmp[i] = result
-                return result
-
-        return helper(1)
-
-
-
-
-A = genTree([
-    2,
-    1, 3,
-    None, 4,
-])
-print(Solution().rob(A))
-
-
-
-
+        return max(robHelper(root))
 
 
 
 
 
 if not __name__ == '__main__':
-
     assert Solution().rob(None) == 0, 'Edge'
 
     A = genTree([
         3,
-        2,3,
-        None,3,None,1
+        2, 3,
+        None, 3, None, 1
     ])
 
     assert Solution().rob(A) == 7, 'Example 1'
 
-
     A = genTree([
         3,
-        4,5,
-        1,3,None,1
+        4, 5,
+        1, 3, None, 1
     ])
     assert Solution().rob(A) == 9, 'Example 2'
 
-
     A = genTree([
         10,
-        1,2,
-        20,2,3,4
+        1, 2,
+        20, 2, 3, 4
 
     ])
     assert Solution().rob(A) == 39, 'Example 3'
@@ -201,7 +133,7 @@ if not __name__ == '__main__':
         100,
         1, 1,
         10, 10, 10, 10,
-        1000,1000,1000,1000,1000,1000,1000,1000,
+        1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
     ])
     assert Solution().rob(A) == 8100, 'Example 4'
 
