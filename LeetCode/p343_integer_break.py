39946e8d7391c8662c3fdd3d222c1fe164ffc82c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-12 11:42:41 -0400

    Leetcode P343 STD ans learned

--- LeetCode/p343_integer_break.py
+++ LeetCode/p343_integer_break.py
@@ -72,7 +72,8 @@ class Solution2(object):
 
 
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
+
     assert Solution().integerBreak(2) == 1, 'Example 1, 1+1, 1*1=1'
     assert Solution().integerBreak(10) == 36, 'Example 2, 3+3+4, 3*3*4=36'
     assert Solution().integerBreak(17) == 486, 'Example 2, 3+3+4, 3*3*4=36'
