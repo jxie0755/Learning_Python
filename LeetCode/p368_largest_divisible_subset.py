c1f90853204b89188deb7e76025e0130a740f0c9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-12 16:12:09 -0400

    Leetcode p368 self version exceed time limit

--- LeetCode/p368_largest_divisible_subset.py
+++ LeetCode/p368_largest_divisible_subset.py
@@ -4,16 +4,63 @@
 # Given a set of distinct positive integers, find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
 # Si % Sj = 0 or Sj % Si = 0.
 # If there are multiple solutions, return any subset is fine.
+from typing import *
 
 class Solution:
+
+    # 本质上这题是找出最长的subset使得他们的最大公约数在其中
+
+    # Version A brutal force
+    # Exceeded max time limit
+    def combinationSolo(self, nums, k):
+        if k == len(nums):
+            return [nums]
+        elif k == 1:
+            return [[i] for i in nums]
+        else:
+            result = []
+            next_list = nums[:]
+            head = next_list.pop(0)
+            result += [[head] + com for com in self.combinationSolo(next_list, k - 1)] + self.combinationSolo(nums[1:], k)
+            return result
+
+    def valid(self, combination):
+        """check if this sorted combination meets the requirement"""
+        if len(combination) == 1:
+            return True
+        for i in range(len(combination)-1):
+            small = combination[i]
+            large = combination[i+1]
+            if large % small != 0:
+                return False
+        return True
+
+
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        pass
+        if not nums:
+            return []
+
+        N = len(nums)
+        nums = sorted(nums)
+
+        for i in range(N, 0, -1):
+            all_combinations = self.combinationSolo(nums, i)
+            for comb in all_combinations:
+                if self.valid(comb):
+                    return comb
 
 
 
-if not __name__ == '__main__':
 
-    assert Solution().largestDivisibleSubset([1,2,3]) == [1,2] or [1,3], 'Example 1'
-    assert Solution().largestDivisibleSubset([1,2,4,8]) == [1,2,4,8], 'Example 2'
+# print(Solution().largestDivisibleSubset([2, 4, 6, 7, 10, 14, 28]))
+
+
+
+if  __name__ == '__main__':
+
+    assert Solution().largestDivisibleSubset([]) == [], 'Edge 0'
+    assert Solution().largestDivisibleSubset([1, 2, 3]) == [1, 2] or [1, 3], 'Example 1'
+    assert Solution().largestDivisibleSubset([1, 2, 4, 8]) == [1, 2, 4, 8], 'Example 2'
+    assert Solution().largestDivisibleSubset([2, 4, 6, 7, 10, 14, 28]) == [2, 4, 28], 'Example 3'
 
     print('all passed')
