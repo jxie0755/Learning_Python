46b297cc35300e39ab3a51bcc2f2f4888b9edf72
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-12 19:07:28 -0400

    Leetcode p368 self version B still exceed time limit

--- LeetCode/p368_largest_divisible_subset.py
+++ LeetCode/p368_largest_divisible_subset.py
@@ -57,35 +57,44 @@ class Solution:
 
     # Version B
     # Exclude numbers that is already checked
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+    # Still exceeded max time limit
+    def largestDivisibleSubset(self, nums: List[int]):
         if not nums:
             return []
 
         nums = sorted(nums)
         result = []
-        while nums:
-            temp = [nums.pop(0)]
-            i = 0
-            while i != len(nums):
-                if nums[i] % temp[-1] == 0:
-                    temp.append(nums.pop(i))
-                else:
-                    i += 1
-            result.append(temp)
 
+        def helper(lst, start=0):
+            """extend the lst into all possible subset"""
+            # print('cur', lst)
+
+            if start <= len(nums)-1:
+                for i in range(start+1, len(nums)):
+                    temp = nums[i]
+                    if temp % lst[-1] == 0:
+                        new_lst = lst + [temp]
+                        helper(new_lst, i)
+                    else:
+                        helper([temp], i)
+                result.append(lst)
+
+        helper([nums[0]])
         return max(result, key=len)
 
 
-print(Solution2().largestDivisibleSubset([1, 3, 9, 18, 54, 90, 108, 180, 360, 540, 720]))
+print(Solution().largestDivisibleSubset([2, 4, 6, 7, 10, 14, 28]))
 
-# if __name__ == '__main__':
-#     assert Solution().largestDivisibleSubset([]) == [], 'Edge 0'
-#     assert Solution().largestDivisibleSubset([1, 2, 3]) == [1, 2] or [1, 3], 'Example 1'
-#     assert Solution().largestDivisibleSubset([1, 2, 4, 8]) == [1, 2, 4, 8], 'Example 2'
-#     assert Solution().largestDivisibleSubset([2, 4, 6, 7, 10, 14, 28]) == [2, 4, 28], 'Example 3'
-#
-#     assert Solution().largestDivisibleSubset([1, 3, 9, 18, 54, 90, 108, 180, 360, 540, 720]) == \
-#            [1,3,9,18,90,180,360,720], \
-#         'Additional 1'
-#
-#     print('all passed')
+if __name__ == '__main__':
+    assert Solution().largestDivisibleSubset([]) == [], 'Edge 0'
+    assert Solution().largestDivisibleSubset([1, 2, 3]) == [1, 2] or [1, 3], 'Example 1'
+    assert Solution().largestDivisibleSubset([1, 2, 4, 8]) == [1, 2, 4, 8], 'Example 2'
+
+    A = Solution().largestDivisibleSubset([2, 4, 6, 7, 10, 14, 28])
+    assert A == [2, 4, 28] or A == [7, 14, 28], 'Example 3'
+
+    assert Solution().largestDivisibleSubset([1, 3, 9, 18, 54, 90, 108, 180, 360, 540, 720]) == \
+           [1,3,9,18,90,180,360,720], \
+        'Additional 1'
+
+    print('all passed')
---
+++ whiteboard.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    if i < 5:
+        print('A')
+    else:
+        print('B')
+else:
+    print('?')
