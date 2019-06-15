cc2bad56e2960488dcfd27a0f4a8ad2a35bb2b8a
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-15 16:33:53 -0400

    Leetcode P240 initiation

---
+++ LeetCode/p240_search_a_2d_matrix_ii.py
@@ -0,0 +1,39 @@
+# P240 Search a 2D Matrix II
+# Medium
+
+
+# Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+
+from typing import *
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        pass
+
+A = [
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+
+print(Solution().searchMatrix(A, 5))
+
+if not __name__ == '__main__':
+    A = [
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+
+    assert Solution().searchMatrix(A, 5), 'Example 1'
+    assert Solution().searchMatrix(A, 20), 'Example 2'
+
+    print('all passed')
