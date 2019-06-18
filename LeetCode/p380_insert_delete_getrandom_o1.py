8af599cc8f6b5340906f3364502f569f5848f7c4
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-17 21:15:10 -0400

    Leetcode P380 initiation

---
+++ LeetCode/p380_insert_delete_getrandom_o1.py
@@ -0,0 +1,47 @@
+# P380 Insert Delete GetRandom O(1)
+# Medium
+
+# Design a data structure that supports all following operations in average O(1) time.
+
+# insert(val): Inserts an item val to the set if not already present.
+# remove(val): Removes an item val from the set if present.
+# getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+
+
+
+if not __name__ == '__main__':
+    obj = RandomizedSet()
+    print(obj)
+
+    obj.insert(val)
+    print(obj)
+
+    param_2 = obj.remove(val)
+    print(obj)
+
+    print(obj.getRandom())
+
+    print('all passed')
