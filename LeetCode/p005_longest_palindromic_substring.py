795f2e7e9f4afc1dee8aa5401a4fd73d36b4ea91
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-06-30 23:04:34 -0400

    Leetcode 005 updated

--- LeetCode/p005_longest_palindromic_substring.py
+++ LeetCode/p005_longest_palindromic_substring.py
@@ -34,6 +34,7 @@ class Solution:
     # Version B
     # Time: O(N^2)
     # Space: O(1)
+    # 从中心向两侧扩展法, 遍历中心点
     def longestPalindrome(self, s: str) -> str:
 
         length = len(s)
@@ -41,15 +42,16 @@ class Solution:
         for a in range(0, len(s)):
 
             theoretical_length = (min(a-0, length -a) + 1) * 2
-            if theoretical_length < len(result):
-                break
 
-            for b in range(a+1,a+3):
+            if theoretical_length < len(result): # 此处得到一个该中心点的理论最长回文长度, 如果仍然不够当前result
+                break  # 就可以break, 因为后面也不可能出现更长的回文了
+
+            for b in range(a+1,a+3): # 这里控制起始字符要么就是a, 要么就是aa, 用于奇数和偶数回文
                 head, tail = a, b
 
                 while head >= 0 and tail <= length and s[head] == s[tail-1]:
-                    sample =s[head:tail]
-                    if len(sample) > len(result):
+                    sample =s[head:tail] # 根据起始点,向两侧扩展, 直到发现不是回文为止
+                    if len(sample) > len(result): # 如果发现长于result就取代
                         result = sample
                     head -= 1
                     tail += 1
@@ -64,22 +66,22 @@ class Solution:
 #         # O(N)
 #         pass
 
+Solution().longestPalindrome('abccba')
 
 
-
-if __name__ == '__main__':
-    assert Solution().longestPalindrome('') == '', 'Edge 1'
-    assert Solution().longestPalindrome('a') == 'a', 'Edge 2'
-    assert Solution().longestPalindrome('aaa') == 'aaa', 'Edge 3'
-
-    assert Solution().longestPalindrome('babad') == 'bab' or 'aba', 'Example 1'
-    assert Solution().longestPalindrome('cbbd') == 'bb', 'Example 2'
-
-    assert Solution().longestPalindrome('bababadddddddddddd') == 'dddddddddddd', 'Extra 1'
-    assert Solution().longestPalindrome('babababa') == 'abababa' or 'bababab', 'Extra 2'
-
-    a = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    b = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    assert Solution().longestPalindrome(a) == b
-
-    print('all passed')
+# if __name__ == '__main__':
+#     assert Solution().longestPalindrome('') == '', 'Edge 1'
+#     assert Solution().longestPalindrome('a') == 'a', 'Edge 2'
+#     assert Solution().longestPalindrome('aaa') == 'aaa', 'Edge 3'
+#
+#     assert Solution().longestPalindrome('babad') == 'bab' or 'aba', 'Example 1'
+#     assert Solution().longestPalindrome('cbbd') == 'bb', 'Example 2'
+#
+#     assert Solution().longestPalindrome('bababadddddddddddd') == 'dddddddddddd', 'Extra 1'
+#     assert Solution().longestPalindrome('babababa') == 'abababa' or 'bababab', 'Extra 2'
+#
+#     a = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+#     b = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+#     assert Solution().longestPalindrome(a) == b
+#
+#     print('all passed')
