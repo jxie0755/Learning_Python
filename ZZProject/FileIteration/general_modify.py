64b826846d106ff6dd5e000c5c3576b448cccec6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-11 23:42:33 -0400

    Update

--- ZZProject/FileIteration/general_modify.py
+++ ZZProject/FileIteration/general_modify.py
@@ -5,20 +5,25 @@ import re
 import os
 
 def general_modify(directory, fn):
+    """Single directory, non-recursive"""
     for sub_dir in os.listdir(directory):
         true_sub_dir = os.path.join(directory, sub_dir)  # 不要用拼接字符串来组子路径
-        if os.path.isdir(true_sub_dir):
-            general_modify(true_sub_dir, fn)
-        else:
+        if os.path.isfile(true_sub_dir):
             fn(true_sub_dir)
+
+def general_modify_recur(directory, fn):
+    for sub_dir in os.listdir(directory):
+        true_sub_dir = os.path.join(directory, sub_dir)  # 不要用拼接字符串来组子路径
+        if os.path.isfile(true_sub_dir):
+            fn(true_sub_dir)
+        elif os.path.isdir(true_sub_dir):
+            general_modify(true_sub_dir, fn)
 
 # test fn (show file name)
 def fn_test(file_dir):
     print(file_dir)
 # dir = r"D:\Documents\GitHub\Learning_Python\ZZProject\FileIteration\test"
-# general_modify(dir, fn_test)
-
-
+# general_modify_recur(dir, fn_test)
 
 # String modification
 def string_change(file_dir):
@@ -43,5 +48,5 @@ def string_change(file_dir):
             print(file_dir, "is excluded")
 
 # dir = r"D:\Documents\GitHub\Learning_Python"
-# general_modify(dir, string_change)
+# general_modify_recur(dir, string_change)
 
---
+++ ZZProject/FileIteration/test/test_file.py
@@ -0,0 +1,63 @@
+# P017 Letter Combination of a Phone Number
+# Medium
+
+
+# Given a string containing digits from 2-9 inclusive,
+# return all possible letter combinations that the number could represent.
+
+# A mapping of digit to letters (just like on the telephone buttons) is given below.
+# Note that 1 does not map to any letters.
+
+class Solution:
+
+    # Version A, hashtable
+    # Time:  O(n * 3^n)
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        hashtable = {"0": [" "],
+                     "1": [""],
+                     "2": ["a", "b", "c"],
+                     "3": ["d", "e", "f"],
+                     "4": ["g", "h", "i"],
+                     "5": ["j", "k", "l"],
+                     "6": ["m", "n", "o"],
+                     "7": ["p", "q", "r", "s"],
+                     "8": ["t", "u", "v"],
+                     "9": ["w", "x", "y", "z"]
+                     }
+
+        result = []
+        i = 0
+        while i != len(digits):
+            current = digits[i]
+
+            if i == 0:
+                result = hashtable[current]
+            else:
+                new = hashtable[current]
+                result_len, new_len = len(result), len(new)
+                result = sum([[i] * new_len for i in result], [])
+                j, cycle = 0, 0
+                while j != len(result):
+                    result[j] = result[j] + new[cycle % new_len]
+                    j += 1
+                    cycle += 1
+            i += 1
+            a = r'"abc"'
+
+        return result
+
+
+
+if __name__ == "__main__":
+
+    assert Solution().letterCombinations("") == [], "Edge 1"
+    assert Solution().letterCombinations("1") == [""], "Edge 2"
+    assert Solution().letterCombinations("0") == [" "], "Edge 3"
+
+    assert Solution().letterCombinations("2") == ["a", "b", "c"], "Example 1"
+    assert Solution().letterCombinations("20") == ["a ", "b ", "c "], "Example 2"
+    assert Solution().letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], "Example 3"
+    assert Solution().letterCombinations("213") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], "Example 4"
+
+    print("all passed")
