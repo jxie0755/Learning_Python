2803c3c7d23675a20617093cc668c75f76d3eb14
jxie0755 <jxie0755@users.noreply.github.com>
2019-07-14 21:49:54 -0400

    Leetcode P020 update

--- LeetCode/p020_valid_parentheses.py
+++ LeetCode/p020_valid_parentheses.py
@@ -4,17 +4,17 @@
 # Given a string containing just the characters "(", ")", "{", "}", "[" and "]", determine if the input string is valid.
 # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
-# """
-# :type s: str
-# :rtype: bool
-# """
 
 class Solution:
 
     OPEN_BRACKETS = {"(": 1, "{": 2, "[": 3}
     CLOSE_BRACKETS = {")": 1, "}": 2, "]": 3}  # setup data as class attributes
 
-    def isValid(self, s):  # beats 60.51%
+    # Version A
+    # idea from checkio/electionic station/p1_brackets and improved
+    # 不再使用index,因为太不直观,直接使用mapping把左括号和右括号对应值(其实不需要是num,任何三个键值都能成立)
+    # 仍然是用pop()来对应括号位置关系
+    def isValid(self, s: str) -> bool:
         result = []
         for i in s:
             if i in Solution.OPEN_BRACKETS:
@@ -26,9 +26,7 @@ class Solution:
                     return False
         return len(result) == 0
 
-# 改进了Checkio/electronic_station/p1_brackets的写法
-# 不再使用index,因为太不直观,直接使用mapping把左括号和右括号对应值(其实不需要是num,任何三个键值都能成立)
-# 仍然是用pop()来对应括号位置关系
+
 
 if __name__ == "__main__":
     assert Solution().isValid("((5+3)*2+1)") == True, "Simple"
