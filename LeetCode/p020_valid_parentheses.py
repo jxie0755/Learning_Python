6b826540f077db099baafae198f877f24e6ff6dd
jxie0755 <jxie0755@users.noreply.github.com>
2019-07-14 21:52:39 -0400

    Leetcode P020 update

--- LeetCode/p020_valid_parentheses.py
+++ LeetCode/p020_valid_parentheses.py
@@ -29,10 +29,11 @@ class Solution:
 
 
 if __name__ == "__main__":
-    assert Solution().isValid("((5+3)*2+1)") == True, "Simple"
-    assert Solution().isValid("{[(3+1)+2]+}") == True, "Different types"
-    assert Solution().isValid("(3+{1-1)}") == False, ") is alone inside {}"
-    assert Solution().isValid("[1+1]+(2*2)-{3/3}") == True, "Different operators"
-    assert Solution().isValid("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
-    assert Solution().isValid("2+3") == True, "No brackets, no problem"
+
+    assert Solution().isValid("((5+3)*2+1)"), "Simple"
+    assert Solution().isValid("{[(3+1)+2]+}"), "Different types"
+    assert not Solution().isValid("(3+{1-1)}"), ") is alone inside {}"
+    assert Solution().isValid("[1+1]+(2*2)-{3/3}"), "Different operators"
+    assert not Solution().isValid("(({[(((1)-2)+3)-3]/3}-3)"), "One is redundant"
+    assert Solution().isValid("2+3"), "No brackets, no problem"
     print("all passed")
