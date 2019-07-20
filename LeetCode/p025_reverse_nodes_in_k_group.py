b96db3127f5aa5afdb1e5d708f1d95a4f394a9d1
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-19 22:27:12 -0400

    Leetcode P025 finished

--- LeetCode/p025_reverse_nodes_in_k_group.py
+++ LeetCode/p025_reverse_nodes_in_k_group.py
@@ -29,32 +29,69 @@ class Solution:
 
 
     # Version A, use reverse whole linked list
+    # Recursive
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         if k == 1 or not head:
             return head
 
         cur = head            # 备份开头, 也就是将会变成结尾的节点
 
-        # 先找到k-group的终点
+        # 先找到k-group的终点, 就走一次k节
         for i in range(k-1):
-            if head.next:
-                head = head.next
+            if cur.next:
+                cur = cur.next
             else:  # 若是不满k个,就直接不要动
-                return cur
+                return head
 
         # 断开,但是不能忘记下一个节点,以备用
-        head.next, next_head = None, head.next
+        cur.next, next_cur = None, cur.next
 
         # reverse前k个节点, 此时k-group的最后一个节点变成了反转后的头
-        new_head = self.reverseNodes(cur)
+        new_cur= self.reverseNodes(head)
 
         # 反转后,cur也就是tail了
-        cur.next = self.reverseKGroup(next_head, k)
-        return new_head
+        head.next = self.reverseKGroup(next_cur, k)
+        return new_cur
 
+
+
+    # Version B, Non-recursive, using counter cycling
+    # Slower than recursive
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+
+        if k == 1 or not head:
+            return head
+
+        flag = False  # 设立一个标记, 如果结束时未走完一个循环,要处理一下这个不完整尾部
+        count = 1     # 设立一个counter, 以count % k == 0为发现完整k循环
+
+        cur = head    # 备份一个head
+        dummy = H = ListNode(0)
+
+        while cur:
+            next_node = cur.next # 备份kgroup之后的下一个开头
+
+            if count % k == 0:  # 若发现一个完整k循环
+                flag = False    # 撤销flag
+                cur.next = None # 与后面断开
+                H.next = self.reverseNodes(head) # H街上反转的head (此时head就是反转后的最后一个节点)
+                H = head                         # 移动H到新链表的最后一个节点
+                cur = head = next_node           # 把cur和head定位到next下一个开头
+
+            else:
+                flag = True      # 标记未完整k group
+                cur = cur.next   # 普通后移
+            count += 1
+
+        if flag:                 # 如果有不完整尾部, 要接上最后一个头部
+            H.next = head
+
+        return dummy.next
 
 
 if __name__ == "__main__":
+
+    assert Solution().reverseKGroup(genNode([1]), 2) == genNode([1]), "Single"
 
     a = genNode([1,2,3,4,5])
     f = Solution().reverseKGroup(a, 2)
@@ -65,3 +102,4 @@ if __name__ == "__main__":
     assert repr(g) == "3->2->1->4->5", "Example 2"
 
     print("all passed")
+
