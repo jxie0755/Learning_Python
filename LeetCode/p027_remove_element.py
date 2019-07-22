e7852ca7a14ae07e1b3bb709dfd4e70009eef576
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-22 15:22:14 -0400

    Update Leetcode P027

--- LeetCode/p027_remove_element.py
+++ LeetCode/p027_remove_element.py
@@ -5,62 +5,41 @@
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 # The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
-# """
-# :type nums: List[int]
-# :type val: int
-# :rtype: int
-# """
-
+from typing import *
 import timeit
 
+
 class Solution(object):
-    def removeElement(self, nums, val):
+
+    # Version A
+    def removeElement(self, nums: List[int], val: int) -> int:
         n = 0
-        while n < len(nums):  # this force to recalculate len(), so that index will not out of range
+        while n < len(nums):
+            # this force to recalculate len() in O(1)
+            # so that index will not out of range
+
             if nums[n] == val:
                 nums.pop(n)  # if pop(), next element will move to the current index, no need to move n
             else:
                 n += 1  # if no pop(), then move n to next index
         return len(nums)
 
-    def removeElement2(self, nums, val):  # beats 26.37%
+    # Version B
+    # This slower by checking val in nums everytime O(N)
+    def removeElement(self, nums: List[int], val: int) -> int:
         while val in nums:
             nums.remove(val)
         return len(nums)
 
+
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4, 2, 3, 4, 4, 5, 6, 1, 2, 3, 4]
-    val = 4
-    print(Solution().removeElement(nums, val))  # >>> 10
 
-    print(timeit.Timer("Solution().removeElement(nums, val)",
-                       setup="from __main__ import Solution; "
-                             "nums = [1, 2, 3, 4, 2, 3, 4, 4, 5, 6, 1, 2, 3, 4];"
-                             "val = 4")
-          .repeat(3, 1000000))
+    nums = [3, 2, 2, 3]
+    assert Solution().removeElement(nums, 3) == 2, "Example 1"
+    assert nums == [2, 2], "Example 1 final list"
 
-    # >>> [1.8547368030012876, 1.7035995290007122, 1.718884424000862]
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    assert Solution().removeElement(nums, 2) == 5, "Example 2"
+    assert nums == [0, 1, 3, 0, 4], "Example 2 final list"
 
-    print(timeit.Timer("Solution().removeElement2(nums, val)",
-                       setup="from __main__ import Solution; "
-                             "nums = [1, 2, 3, 4, 2, 3, 4, 4, 5, 6, 1, 2, 3, 4];"
-                             "val = 4")
-          .repeat(3, 1000000))
-    # >>> [0.4291627630009316, 0.4323928640005761, 0.43043123800089234]
-
-
-    # 在极端情况下,复杂度非线性递增,第一个方法就变得更快了,第二个方法反而慢了很多
-    print(timeit.Timer("Solution().removeElement(nums, val)",
-                       setup="from __main__ import Solution; "
-                             "nums = list(range(10)) * 10000;"
-                             "val = 4")
-          .repeat(3, 100))
-
-    # >>> [1.5640779950008437, 1.5756833779996668, 1.6097137039996596]
-
-    print(timeit.Timer("Solution().removeElement2(nums, val)",
-                       setup="from __main__ import Solution; "
-                             "nums = list(range(10)) * 10000;"
-                             "val = 4")
-          .repeat(3, 100))
-    # >>> [11.877236265998363, 11.790503606998755, 12.056773186999635]
+    print('all passed')
