3d671a7d4826a0b32744fc6b90112b41ed8d6798
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-26 16:26:37 -0400

    Learn SQL Chapter 2 Select sorted

--- SamTeachSQLin10Min/Chapter_3_Sorting_Retrieved.sql
+++ SamTeachSQLin10Min/Chapter_3_Sorting_Retrieved.sql
@@ -1,4 +1,28 @@
 # Sams Teach yourself SQL in 10 Mins
 # Chapter 3 Sorting Retried Data
 
+USE sams_teach_sql;
+
+# 子句概念(clause)
+# SQL 语句由子句构成
+# 有些子句是必需的
+# 有些则是可选的
+
+
+# Order By 子句
+SELECT prod_name
+FROM products
+ORDER BY prod_name;
+# 必须是最后一句
+# 按照prod_name排序
+
+
+# 按做个列排序
+SELECT prod_id, prod_price, prod_name
+FROM products
+ORDER BY prod_price, prod_name;
+# 也就是先按价格排序, 然后相同价格的内容再按名称排子顺序
+
+
+
 
