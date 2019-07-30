c76da0e5eccdac01db0e6ed871c1146fb3e65b8f
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-07-30 13:18:23 -0400

    Leetcode p037 reviewed

--- LeetCode/p037_sudoku_solver.py
+++ LeetCode/p037_sudoku_solver.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/sudoku-solver/
 # P037 Sudoku Solver
 # Hard
 
@@ -7,10 +8,10 @@
 # Each of the digits 1-9 must occur exactly once in each row.
 # Each of the digits 1-9 must occur exactly once in each column.
 # Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
-# Empty cells are indicated by the character "."
+# Empty cells are indicated by the character '.'
 
 # Note:
-# The given board contain only digits 1-9 and the character ".".
+# The given board contain only digits 1-9 and the character '.'.
 # You may assume that the given Sudoku puzzle will have a single unique solution.
 # The given board size is always 9x9.
 
