45d8aeba527a5d80bedd912f143e1acf83cf791c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-08-02 21:56:46 -0400

    Leetcode P040 reviewed

--- LeetCode/p040_combination_sum_II.py
+++ LeetCode/p040_combination_sum_II.py
@@ -1,20 +1,22 @@
+# https://leetcode.com/problems/combination-sum-ii/
 # P040 Combination Sum II
 # Medium
-
 
 # Given a collection of candidate numbers (candidates) and a target number (target),
 # find all unique combinations in candidates where the candidate numbers sums to target.
 # Each number in candidates may only be used once in the combination.
 
-
 # Note:
 # All numbers (including target) will be positive integers.
+# The difference between the P039 and P040 is that the candidates can be duplicated in P040 but not in P039
 # The solution set must not contain duplicate combinations.
+
 
 from itertools import combinations
 
-
 class Solution:
+
+    # Version A
     # brutal force, max limit time exceeded
     def combinationSum2(self, candidates, target):
 
@@ -44,16 +46,46 @@ class Solution:
         return result
 
 
-# TODO 研究答案
+class Solution(object):
+
+    # Version B
+    # Same idea with Leetcode P039 Combination Sum, but cannot repeat same elements
+    # Add a recursive process method to update result
+    def combinationSum2(self, candidates, target):
+
+        def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+            """
+            Args:
+                candidates:    ingredient numbers (sorted)
+                start:         from 0, full candidates, to a shorter candidate list
+                intermediate:  temp list to record current status List[int]]
+                target:        tartget to compose
+
+            Returns: None, Imperfect function, just update the result
+            """
+            if target == 0 and intermediate not in result:  # 相对于P039 加一个去重条件
+                result.append(list(intermediate))  # 终止case, target降到0就完成
+                # 这里使用list其实就是复制一个itermediate, 可以用interme[:]取代
+
+            while start < len(candidates) and candidates[start] <= target:  # while loop 走完全部candidates
+
+                intermediate.append(candidates[start])
+                process(candidates, start + 1, intermediate, target - candidates[start])
+                intermediate.pop()  # 这里退回相当于,即使满足条件, 也可以跳过
+                start += 1
+
+        result = []
+        candidates = sorted(candidates)
+        process(candidates, 0, [], target)
+
+        return result
 
 
 if __name__ == "__main__":
     candidates = [10, 1, 2, 7, 6, 1, 5]
-    target = 8
-    assert Solution().combinationSum2(candidates, target) == [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]
+    assert Solution().combinationSum2(candidates, 8) == [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]], "Example 1"
 
     candidates = [2, 5, 2, 1, 2]
-    target = 5
-    assert Solution().combinationSum2(candidates, target) == [[1, 2, 2], [5]]
+    assert Solution().combinationSum2(candidates, 5) == [[1, 2, 2], [5]], "Example 2"
 
     print("all passed")
