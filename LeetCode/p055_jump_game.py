a6a253c30ea48d960230738852d3f4f2572e45cd
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-09-01 22:10:56 -0400

    Leetcode P055 reviewed

--- LeetCode/p055_jump_game.py
+++ LeetCode/p055_jump_game.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     assert Solution().canJump([0]), "Edge 1"
     assert not Solution().canJump([0, 0]), "Edge 2"
     assert Solution().canJump([1, 0]), "Edge 3"
-    assert Solution().canJump([2, 0]), "Edge 4"
+    assert not Solution().canJump([1, 0, 0, 0]), "Edge 4"
 
     assert Solution().canJump([2, 3, 1, 1, 4]), "Example 1"
     assert not Solution().canJump([3, 2, 1, 0, 4]), "Example 2"
