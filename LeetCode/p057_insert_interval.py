6225d4424254a2c28ee35e9e3bf23f585073eda0
jxie0755 <jxie0755@users.noreply.github.com>
2019-09-03 22:56:45 -0400

    Update

--- LeetCode/p057_insert_interval.py
+++ LeetCode/p057_insert_interval.py
@@ -1,7 +1,6 @@
 # P056 Insert Interval
 # Hard
 
-
 # Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 # You may assume that the intervals were initially sorted according to their start times.
 
