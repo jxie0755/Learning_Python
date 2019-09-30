79fa616b02e5cfe44b517138cd122fca2738b003
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-09-30 14:16:12 -0400

    Leetcode P60 reviewed

--- LeetCode/p060_permutation_sequence.py
+++ LeetCode/p060_permutation_sequence.py
@@ -177,7 +177,7 @@ class Solution(object):
         这里利用的是每一位数字, 都会因为后面位数的总排列数为循环发生变化, 从小到大发展
         """
         fact_dict = {i: math.factorial(i) for i in range(n)} # 注意这里i从0开始到n-1, 因为不算第一位, 只计算从第二位开始有多少种排列方式
-        seq, k, fact = "", k - 1, fact_dict[n-1]  # k = k-1 因为一开始算一个,所以要去掉
+        seq, k= "", k - 1  # k = k-1 因为一开始算一个,所以要去掉
         perm = list(range(1, n + 1))
 
         for i in reversed(range(n)):
@@ -189,31 +189,31 @@ class Solution(object):
         return seq
 
 
-class Solution(object):
-
-    def getPermutation(self, n: int, k: int) -> str:
-        """
-        Version C2
-        Direct generation, digit by digit
-        recursive method
-        """
-
-        def helper(nums: List[int], k: int) -> str:
-            """
-            A helper function for recursion
-            """
-            length = len(nums)
-            if length == 1:
-                return str(nums[0])
-            else:
-                fact = fact_dict[length - 1]
-                digit = nums[k // fact]
-                nums.remove(digit)
-                return str(digit) + helper(nums, k % fact)
-
-        fact_dict = {i: math.factorial(i) for i in range(n)}
-        lst = list(range(1, n + 1))
-        return helper(lst, k - 1)  # k - 1原理同上
+# class Solution(object):
+#
+#     def getPermutation(self, n: int, k: int) -> str:
+#         """
+#         Version C2
+#         Direct generation, digit by digit
+#         recursive method
+#         """
+#
+#         def helper(nums: List[int], k: int) -> str:
+#             """
+#             A helper function for recursion
+#             """
+#             length = len(nums)
+#             if length == 1:
+#                 return str(nums[0])
+#             else:
+#                 fact = fact_dict[length - 1]
+#                 digit = nums[k // fact]
+#                 nums.remove(digit)
+#                 return str(digit) + helper(nums, k % fact)
+#
+#         fact_dict = {i: math.factorial(i) for i in range(n)}
+#         lst = list(range(1, n + 1))
+#         return helper(lst, k - 1)  # k - 1原理同上
 
 
 if __name__ == "__main__":
