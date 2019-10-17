c38d55659bb415d3abcc7ea7b3642ac6d7b06734
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-17 08:49:01 -0400

    Leetcode P063 reviewed

--- LeetCode/p063_unique_paths_ii.py
+++ LeetCode/p063_unique_paths_ii.py
@@ -1,39 +1,45 @@
-# P063 Unique Paths II
-# Medium
+"""
+https://leetcode.com/problems/unique-paths-ii/
+P063 Unique Paths II
+Medium
 
-# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked "Finish" in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked "Finish" in the diagram below).
 
-# Now consider if some obstacles are added to the grids. How many unique paths would there be?
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
 
-# An obstacle and empty space is marked as 1 and 0 respectively in the grid.
-# Note: m and n will be at most 100.
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+Note: m and n will be at most 100.
+"""
 
-from math import factorial
 
+from typing import *
 
 class Solution:
 
-    # According to ProjectEuler p015 2nd version
-    # plot the ways to each coor for the whole grid
-    def uniquePathsWithObstacles(self, obstacleGrid):
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """
+        Similar idea from Leetcode 062 iteration method
+        Additional: set the number of path at the obstacle to be 0
+        """
+
         m, n = len(obstacleGrid[0]), len(obstacleGrid)
-        paths_to = [[1 for _ in range(m)] for _ in range(n)]
+        pathgrid = [[1 for _ in range(m)] for _ in range(n)]
 
         for nn in range(n):
             for mm in range(m):
                 if obstacleGrid[nn][mm] == 1:
-                    paths_to[nn][mm] = 0
+                    pathgrid[nn][mm] = 0
                 elif nn == 0:
-                    paths_to[nn][mm] = paths_to[nn][mm - 1]
+                    pathgrid[nn][mm] = pathgrid[nn][mm - 1]
                 elif mm == 0:
-                    paths_to[nn][mm] = paths_to[nn - 1][mm]
+                    pathgrid[nn][mm] = pathgrid[nn - 1][mm]
                 else:
-                    paths_to[nn][mm] = paths_to[nn][mm - 1] + paths_to[nn - 1][mm]
+                    pathgrid[nn][mm] = pathgrid[nn][mm - 1] + pathgrid[nn - 1][mm]
 
         # for i in paths_to:
         #     print(i)
 
-        return paths_to[n - 1][m - 1]
+        return pathgrid[n - 1][m - 1]
 
 
 if __name__ == "__main__":
