69691a46de437d3a47f4371c1fc3077557dad5f9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-21 12:21:39 -0400

    Leetcode P066 reviewed

--- LeetCode/p066_plus_one.py
+++ LeetCode/p066_plus_one.py
@@ -1,16 +1,20 @@
-# p066 Plus One
-# Easy
+"""
+https://leetcode.com/problems/plus-one/
+p066 Plus One
+Easy
 
-# Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
-# The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
-# You may assume the integer does not contain any leading zero, except the number 0 itself.
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+"""
 
+
+from typing import *
 
 class Solution:
-    def plusOne(self, digits):
+    def plusOne(self, digits: List[int]) -> List[int]:
         """
-        :type digits: List[int]
-        :rtype: List[int]
+        Version A, quick recursion
         """
         if digits[-1] == 9:
             if len(digits) == 1:
@@ -23,9 +27,9 @@ class Solution:
 
 
 if __name__ == "__main__":
-    assert Solution().plusOne([0]) == [1]
-    assert Solution().plusOne([1, 2, 3]) == [1, 2, 4]
-    assert Solution().plusOne([4, 3, 2, 1]) == [4, 3, 2, 2]
-    assert Solution().plusOne([1, 9, 9]) == [2, 0, 0]
-    assert Solution().plusOne([9, 9, 9]) == [1, 0, 0, 0]
+    assert Solution().plusOne([0]) == [1], "Edge 0"
+    assert Solution().plusOne([1, 2, 3]) == [1, 2, 4], "Example 1"
+    assert Solution().plusOne([4, 3, 2, 1]) == [4, 3, 2, 2], "Example 2"
+    assert Solution().plusOne([1, 9, 9]) == [2, 0, 0], "Example 3"
+    assert Solution().plusOne([9, 9, 9]) == [1, 0, 0, 0], "Example 4"
     print("all passed")
