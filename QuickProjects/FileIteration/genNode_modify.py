65cb52e105435d16dbc90f6f26964d661cbef9ca
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-23 13:27:53 -0400

    Update

--- QuickProjects/FileIteration/genNode_modify.py
+++ QuickProjects/FileIteration/genNode_modify.py
@@ -48,40 +48,35 @@ with_brack_pattern = re.compile(raw_with)
 without_brack_pattern = re.compile(raw_without)
 
 
-def add_brack(content, filedir):
+def add_brack(filedir):
+
+    with open(filedir, "r", encoding="utf-8") as f:
+        content = f.read()
+
     new_content = without_brack_pattern.sub(r"\g<1>[\g<2>]\g<3>", content)
+
     with open(filedir, "w", encoding="utf-8") as f:
         f.write(new_content)
 
 
-def del_brack(content, filedir):
+def del_brack(filedir):
+
+    with open(filedir, "r", encoding="utf-8") as f:
+        content = f.read()
+
     new_content = with_brack_pattern.sub(r"\g<1>\g<2>", content)
+
     with open(filedir, "w", encoding="utf-8") as f:
         f.write(new_content)
 
-def operate_brack(command, content, filedir):
-    if command == "add":
-        return add_brack(content, filedir)
-    elif command == "del":
-        return del_brack(content, filedir)
+
+def general_modify(directory, fn):
+    """Single directory, non-recursive"""
+    for sub_dir in os.listdir(directory):
+        true_sub_dir = os.path.join(directory, sub_dir)  # 不要用拼接字符串来组子路径
+        if true_sub_dir.endswith(".py"):
+            fn(true_sub_dir)
 
 
 if __name__ == "__main__":
-
-    # Single directory, no need to recursion
-    while True:
-        command = input("Please choose a command to [add] or [del] :")
-        if command != "add" and command != "del":
-            continue
-        else:
-            break
-
-    for sub_dir in os.listdir(sourcedir):
-        full_sub_dir = sourcedir + "/" + sub_dir
-        if full_sub_dir.endswith(".py"):
-            with open(full_sub_dir, "r", encoding="utf-8") as f:
-                content = f.read()
-
-            ## choose here to add or del []
-            operate_brack(command, content, full_sub_dir)
-
+    general_modify(sourcedir, add_brack)
