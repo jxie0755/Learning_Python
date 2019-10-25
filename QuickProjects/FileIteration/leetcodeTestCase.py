f83850021507e80240d34653a5ec6a131342086d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-24 22:43:03 -0400

    Finished java fix

--- QuickProjects/FileIteration/leetcodeTestCase.py
+++ QuickProjects/FileIteration/leetcodeTestCase.py
@@ -7,7 +7,7 @@ import re
 import os
 
 directory_py = "D:/Documents/GitHub/Learning_Python/LeetCode"
-directory_java = "D:/Documents/GitHub/Learning_Python/LeetCode"
+directory_java = "D:/Documents/GitHub/Data_Structure_with_Java/Leetcode"
 
 
 def LC_testCase_fix_py(directory):
@@ -39,7 +39,6 @@ def LC_testCase_fix_py(directory):
     functionLine = raw_functionline_founder.search(content)
 
     if functionLine:
-        print("Changing")
         fucntionName = functionLine.group(3)
 
         # Replace the functionLine by removing the funcName
@@ -51,8 +50,9 @@ def LC_testCase_fix_py(directory):
         # Write content back
         with open(directory, "w", encoding="utf-8") as f:
             f.write(new_content_case_built)
+        print("file changed")
     else:
-        print("Not Chaning")
+        print("file NOT changed")
 
 
 def LC_testCase_fix_java(directory):
@@ -71,20 +71,47 @@ def LC_testCase_fix_java(directory):
     1. Build a testCase as the class instance
     2. Replace each new instance in actual testing with the same testCase
     """
-    pass
+    # read file
+    print("Checking:", directory)
+    with open(directory, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    raw_class_identifier = re.compile(r"(class )(\S+)( {)")
+    classFinder = raw_class_identifier.search(content)
+    if classFinder and not re.search(r"testCase", content):
+        className = classFinder.group(2)
+        raw_main_identifier = re.compile(r"public static void main\(String\[] args\) {")
+        new_content = raw_main_identifier.sub(r"\g<0>\n\t\t" + className + " testCase = new " + className + "();", content)
+        caseFinder = re.compile(r"new " + className + r"\(\)\.")
+        for case in caseFinder.finditer(content):
+            new_content = case.re.sub("testCase.", new_content)
+
+        with open(directory, "w", encoding="utf-8") as f:
+            f.write(new_content)
+        print("file changed")
+    else:
+        print("file NOT changed")
 
 
 
 
-def general_modify(directory, fn, languange):
+
+
+def general_modify_py(directory, fn):
     """Single directory, non-recursive"""
     for sub_dir in os.listdir(directory):
         true_sub_dir = os.path.join(directory, sub_dir)  # 不要用拼接字符串来组子路径
-        if true_sub_dir.endswith("." + languange):
+        if true_sub_dir.endswith(".py"):
+            fn(true_sub_dir)
+
+def general_modify_java(directory, fn):
+    """Single directory, non-recursive"""
+    for sub_dir in os.listdir(directory):
+        true_sub_dir = os.path.join(directory, sub_dir)  # 不要用拼接字符串来组子路径
+        if true_sub_dir.endswith(".java") and sub_dir.startswith("LC"):
             fn(true_sub_dir)
 
 
-
-if __name__ == '__main__':
-    general_modify(directory_py, LC_testCase_fix_py, "py")
-    general_modify(directory_java, LC_testCase_fix_java, "java")
+# if __name__ == '__main__':
+    # general_modify_py(directory_py, LC_testCase_fix_py)
+    # general_modify_java(directory_java, LC_testCase_fix_java)
