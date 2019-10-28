dc3283b7ce0a3c8a97daad30ca1a49b9d3fef0c3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-28 15:55:45 -0400

    Leetcode P074 reviewed

--- LeetCode/p074_search_a_2d_matrix.py
+++ LeetCode/p074_search_a_2d_matrix.py
@@ -30,8 +30,54 @@ class Solution_A:
         return False
 
 
+class Solution_B:
+
+    def searchMatrix(self, matrix, target: int) -> bool:
+        """
+        Direct binary search on the grid, without flatten
+        Need to do two binary search
+        """
+        global row_mid
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+
+        row_lo = 0
+        row_hi = len(matrix) - 1
+        col_lo = 0
+        col_hi = len(matrix[0]) - 1
+
+        while (row_lo <= row_hi):
+            row_mid = (row_lo + row_hi) // 2
+            row_head = matrix[row_mid][0]
+            row_tail = matrix[row_mid][len(matrix[0])-1]
+            if row_head <= target <= row_tail:
+                break
+            elif target < row_head:
+                row_hi = row_mid - 1
+            else:
+                row_lo = row_mid + 1
+
+        row = matrix[row_mid]
+
+        while col_lo <= col_hi:
+            col_mid = (col_lo + col_hi) // 2
+            check = row[col_mid]
+            if check == target:
+                return True
+            elif target < check:
+                col_hi = col_mid - 1
+            else:
+                col_lo = col_mid + 1
+        return False
+
+
+
+
 if __name__ == "__main__":
-    testCase = Solution_A()
+    testCase = Solution_B()
+
+    matrix = []
+    assert testCase.searchMatrix(matrix, 3) == False, "Empty"
 
     matrix = [
         [],
@@ -53,7 +99,7 @@ if __name__ == "__main__":
         [10, 11, 16, 20],
         [23, 30, 34, 50]
     ]
-    assert testCase.searchMatrix(matrix, 3) == True, "Example 1"
+    assert testCase.searchMatrix(matrix, 5) == True, "Example 1"
 
     matrix = [
         [1, 3, 5, 7],
