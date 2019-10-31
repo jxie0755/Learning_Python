9a9bbee37da816d8db0513e6244bdbe7cd8014a8
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2019-10-31 15:28:50 -0400

    Leetcode P077 reviewed

--- LeetCode/p077_combinations.py
+++ LeetCode/p077_combinations.py
@@ -10,34 +10,34 @@ import itertools
 from typing import *
 
 class Solution_A1:
-
     def combine(self, n: int, k: int) -> List[List[int]]:
         """
         Convert the n into an actual list, then use a helper to recursive run on the list
         """
         nums = list(range(1, n + 1))
+        return self.combinationSolo(nums, k)
 
-        # this can individually work as a combination of list of elements
-        def helper(nums: List[int], k: int) -> List[List[int]]:
-            if k == 0:
-                return [[]]
-            elif k == len(nums):
-                return [nums]
-            elif k == 1:
-                return [[i] for i in nums]
-            else:
-                result = []
-                next_list = nums[:]
-                head = next_list.pop(0)
-                result += [[head] + com for com in helper(next_list, k - 1)] + helper(nums[1:], k)
-                return result
-
-        return helper(nums, k)
+    def combinationSolo(self, nums: List[int], k: int) -> List[List[int]]:
+        """
+        Helper for A1
+        Change the paramter type from n to list(range(1, n+1))
+        """
+        if k == len(nums):
+            return [nums]
+        elif k == 1:
+            return [[i] for i in nums]
+        else:
+            result = []
+            next_list = nums[:]
+            head = next_list.pop(0)
+            result += [[head] + com for com in self.combinationSolo(next_list, k - 1)] + self.combinationSolo(nums[1:], k)
+            return result
 
 class Solution_A2:
     def combine(self, n: int, k: int) -> List[List[int]]:
         """
         Direct version of A1, without helper, but the sequence is reversed
+        It will still pass Leetcode, and it contains the full combinations
         """
         # this can individually work as a combination of list of elements
         if k == 0:
@@ -50,24 +50,8 @@ class Solution_A2:
             return [[n] + com for com in self.combine(n - 1, k - 1)] + self.combine(n-1, k)
 
 
-class Solution_A_extra:
-    def combinationSolo(self, nums: List[int], k: int) -> List[List[int]]:
-        """Extracted out from the helper of A1"""
-        if k == len(nums):
-            return [nums]
-        elif k == 1:
-            return [[i] for i in nums]
-        else:
-            result = []
-            next_list = nums[:]
-            head = next_list.pop(0)
-            result += [[head] + com for com in self.combinationSolo(next_list, k - 1)] + self.combinationSolo(nums[1:], k)
-            return result
-
-
 
 if __name__ == "__main__":
-
     testCase = Solution_A1()
 
     assert testCase.combine(1, 1) == [
