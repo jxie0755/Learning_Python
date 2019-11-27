12d3a3753d3cfdd2e953e60a4a8055cb6b4f4b9c
jxie0755 <jxie0755@users.noreply.github.com>
2019-11-26 22:02:03 -0500

    Leetcode P079 review Update with STD ans And solved version A's problem

--- LeetCode/p079_word_search.py
+++ LeetCode/p079_word_search.py
@@ -9,7 +9,6 @@ The word can be constructed from letters of sequentially adjacent cell, where "a
 
 from typing import *
 
-
 class Solution_A:
 
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -57,11 +56,10 @@ class Solution_A:
             # result = any([self.finder(board, word, idx + 1, new_coor, new_prev) for new_coor in [up, down, left, right]])
 
             # THIS is correct, using or instead of any
-            result = \
-                self.finder(board, word, idx + 1, up, new_prev) or \
-                self.finder(board, word, idx + 1, down, new_prev) or \
-                self.finder(board, word, idx + 1, left, new_prev) or \
-                self.finder(board, word, idx + 1, right, new_prev)
+            result = self.finder(board, word, idx + 1, up, new_prev) or \
+                   self.finder(board, word, idx + 1, down, new_prev) or \
+                   self.finder(board, word, idx + 1, left, new_prev) or \
+                   self.finder(board, word, idx + 1, right, new_prev)
 
             return result
 
@@ -191,6 +189,6 @@ if __name__ == "__main__":
          "a", "a", "a", "a", "a", "a", "a", "b"]
     ]
     target = "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    print(testCase.exist(long, target))  # Maximum Recursion Depth reached
+    assert testCase.exist(long, target), "Long"
 
     print("all passed")
