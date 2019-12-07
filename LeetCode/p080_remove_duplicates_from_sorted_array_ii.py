efd805ef21932ac3fea93503e1ec8be4f7504926
jxie0755 <jxie0755@users.noreply.github.com>
2019-12-06 22:27:35 -0500

    Leetcode P080 reviewed

--- LeetCode/p080_remove_duplicates_from_sorted_array_ii.py
+++ LeetCode/p080_remove_duplicates_from_sorted_array_ii.py
@@ -1,35 +1,37 @@
-# P080 Remove Duplicates from Sorted Array II
-# Medium
+"""
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+P080 Remove Duplicates from Sorted Array II
+Medium
 
 
-# Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
 
-# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-# Clarification:
-#
-# Confused why the returned value is an integer but your answer is an array?
-#
-# Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
 
-# Internally you can think of this:
-#
-# // nums is passed in by reference. (i.e., without making a copy)
-# int len = removeDuplicates(nums);
-#
-# // any modification to nums in your function would be known by the caller.
-# // using the length returned by your function, it prints the first len elements.
-# for (int i = 0; i < len; i++) {
-#     print(nums[i]);
-# }
+Internally you can think of this:
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
 
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
 
 from typing import *
 
 
-class Solution:
-    # But this is not O(1) memory, dict was created to solve the problem
+class Solution_A:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        Using a hash table to record number of occurrence.
+        But this is not O(1) memory
+        """
         hmp = dict()
         i = 0
         while i != len(nums):
@@ -45,10 +47,15 @@ class Solution:
 
         return len(nums)
 
-    # A method without the dict
-    # use a label duplicate to record whether a duplicate if found
-    # (can set to numbers to accomodate to any level of duplication
+
+class Solution_B:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        A method without the hash table
+        use a label duplicate to record whether a duplicate if found
+        (can set to numbers to accomodate to any level of duplication
+        This makes space O(1)
+        """
         if len(nums) <= 2:
             return len(nums)
 
@@ -56,7 +63,9 @@ class Solution:
         i = 1
         count = 1
         while i != len(nums):
+            # sorted array repeating elements will link together
             prev, cur = nums[i - 1], nums[i]
+
             if cur == prev:
                 if not duplicate:
                     duplicate = True
@@ -73,18 +82,19 @@ class Solution:
 
 
 if __name__ == "__main__":
+    testCase = Solution_B()
     a = []
-    assert Solution().removeDuplicates(a) == 0, "Edge 0"
+    assert testCase.removeDuplicates(a) == 0, "Edge 0"
 
     a = [1]
-    assert Solution().removeDuplicates(a) == 1, "Edge 1"
+    assert testCase.removeDuplicates(a) == 1, "Edge 1"
 
     a = [1, 1, 1, 2, 2, 3]
-    assert Solution().removeDuplicates(a) == 5, "Example 1"
+    assert testCase.removeDuplicates(a) == 5, "Example 1"
     assert a == [1, 1, 2, 2, 3]
 
     a = [0, 0, 1, 1, 1, 1, 2, 3, 3]
-    assert Solution().removeDuplicates(a) == 7, "Example 2"
+    assert testCase.removeDuplicates(a) == 7, "Example 2"
     assert a == [0, 0, 1, 1, 2, 3, 3]
 
     print("all passed")
