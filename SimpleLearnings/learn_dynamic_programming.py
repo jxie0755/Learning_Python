81c3c00d5fae1d000654a99c4198a1f47f4cf60a
jxie0755 <jxie0755@users.noreply.github.com>
2019-12-15 13:37:59 -0500

    Dynamic programming review

--- SimpleLearnings/learn_dynamic_programming.py
+++ SimpleLearnings/learn_dynamic_programming.py
@@ -3,21 +3,21 @@ Dynamic Programming Summary
 动态规划
 Multiple sources
 """
-
+import math
 from typing import *
 
-# https://www.bilibili.com/video/av47420469/
-print("From https://www.bilibili.com/video/av47420469/")
-# Q1:
-print("\nQ1: 间隔最大和")
-# 有n个正整数, 排成一列, 可重复, 选取一些数,两两不相邻, 但是总和最大
-# 例如:
+"""
+https://www.bilibili.com/video/av47420469/
+Q1: 间隔最大和
+有n个正整数, 排成一列, 可重复, 选取一些数,两两不相邻, 但是总和最大
+例如:
 A = [1,2,3,2,1,5,7,6]
-# 分解成多个小问题:
-# F(A) = max(F[A:-2] + A[7], F[A:-1])
+分解成多个小问题:
+F(A) = max(F[A:-2] + A[7], F[A:-1])
+"""
 
 # Recursive method
-def foo_recur(lst):
+def foo_recur(lst: List[int]) -> int:
     if len(lst) == 0:
         return 0
     elif len(lst) == 1:
@@ -25,36 +25,37 @@ def foo_recur(lst):
     else:
         return max(foo_recur(lst[:-2]) + lst[-1], foo_recur(lst[:-1]))
 
-print(foo_recur(A))
-# >>> 15
+
 
 # Non-recursive method
-def foo_iter(lst):
+def foo_iter(lst: List[int]) -> int:
     sum_so_far = [0, lst[0]]  # 逐个记录到达list每个长度的最大可能性, 然后拓展下一个
 
     for i in range(1, len(lst)):
         sum_so_far.append(max(sum_so_far[-1], lst[i]+sum_so_far[-2]))
     return sum_so_far[-1]
 
-print(foo_iter(A))
-# >>> 15
+
+if __name__ == '__main__':
+    assert foo_recur([1,2,3,2,1,5,7,6]) == 15, "Q1 间隔最大和, recursive"
+    assert foo_iter([1,2,3,2,1,5,7,6]) == 15, "Q1 间隔最大和, iterative"
 
 
-
-
-# Q2:
-print("\nQ2: 找零钱")
-# 找零钱的问题, 总金额n, 有m种硬币面值, 各有无限多个数, 问最少需要几个硬币?
+"""
+https://www.bilibili.com/video/av47420469/
+Q2: 找零钱"
+找零钱的问题, 总金额n, 有m种硬币面值, 各有无限多个数, 问最少需要几个硬币?
+"""
 
 # Recursive
 # 递归法是倒着处理, 把大的case变成小的case, 这样分叉太多影响速度
 # 可以用memorization记录, 避免重复计算
-def change_recur(target: int, coins: List[int]):
+def change_recur(target: int, coins: List[int]) -> int:
     """coins are sorted from small to large"""
 
     memory = {}
 
-    def helper(target):
+    def helper(target) -> int:
 
         if target == 0:
             return 0
@@ -73,13 +74,13 @@ def change_recur(target: int, coins: List[int]):
 
 # Non recursive
 # 非递归法, 是从小的case积累变成大的case, 这样就有效减少了重复运算
-def change_iter(price, coins):
+def change_iter(target: int, coins: List[int]) -> int:
 
     num = [0]
     # num represents number of coins needed for a value ( value = numbers of coins, idex of num is the value)
     # for example [0,3,5]  means, need 0 coins for 0, need 3 coins for value 1, and need 5 coins for value 2
     #              0 1 2
-    while len(num) <= price:
+    while len(num) <= target:
         temp = []
         for i in coins:
             if i <= len(num):
@@ -92,22 +93,21 @@ def change_iter(price, coins):
     return ans if ans != float("inf") else None
 
 
-print(change_recur(12, [1, 4, 5]))
-# >>> 3 (4 * 3)
+if __name__ == '__main__':
+    assert change_recur(12, [1, 4, 5]) == 3, "Q2 找零钱, (4 * 3)"
+    assert change_recur(55, [1, 3, 7]) == 9, "Q2 找零钱, (7*7 + 3*2)"
+    assert change_iter(15, [6, 7]) is None, "Q2 找零钱, Not possible"
+    # big case does not work in recursion: maximum depth issue
+    assert change_iter(123456, [1, 6, 7, 456, 678]) == 187,"Q2 找零钱, Long"
 
-print(change_recur(55, [1, 3, 7]))
-# >>> 9 (7*7 + 3*2)
 
-print(change_recur(15, [6, 7]))
-# >>> None
-
-# big case does not work in recursion: maximum depth issue
-print(change_iter(123456, [1, 6, 7, 456, 678]))
-# >>> 187
 
 # 以上两题可以总结出, 递归是把大case化成n个小case, 一直化解到最basic case, 然后再反推回来
 # 而非递归方法就是, 避免拆解大case, 而是直接从最小的case开始, 一步一步推导出更大的case的最优值, 这样就避免了分叉递归的重复计算
 # 所以非递归方法最常见的就是建立一个list, 记录下来1到n的所有最优解, 从而推导出n+1的最优解, 实现用一个线性推进, 结合具体判断最优解的方法复杂度,来实现低复杂度
+
+
+
 
 
 """
@@ -125,20 +125,17 @@ def stair_recur(n):
     else:
         return stair_recur(n-1) + stair_recur(n-2)
 
-print(stair_recur(10))
-# >>> 89
-
-
 # 非递归, 动态规划
 def stair_iter(n):
     steps = [0,1,2]  # 同理, 代表了走index步的步数
     for i in range(3, n+1):
         steps.append(steps[-1]+steps[-2])
-    print(steps)
     return steps[-1]
-print(stair_iter(10))
-# >>> 89
 
+
+if __name__ == '__main__':
+    assert stair_recur(10) == 89, "Q1 stairs, Recursive"
+    assert stair_iter(10) == 89, "Q1 stairs, Iterative"
 
 
 """
@@ -194,12 +191,6 @@ def golddig_by_man(lst, man_total):
     return gold[-1][1:] # 返回第man_total个最佳, 不需要包括[0,0]
 
 
-if __name__ == '__main__':
-    assert golddig_by_man(
-        [[5, 400], [5, 500], [3, 200], [4, 300], [3, 350]],
-        10
-    ) == [[5, 500], [5, 400]], "金矿第一解"
-
 
 # 第二解, 根据金矿数目来遍历而到达当前金矿获得最优解
 def golddig_by_mine(lst, man_total):
@@ -237,6 +228,11 @@ def golddig_by_mine(lst, man_total):
 # 若n比较大, 导致m*n大于2^m, 则不如用递归来的容易!
 
 if __name__ == '__main__':
+    assert golddig_by_man(
+        [[5, 400], [5, 500], [3, 200], [4, 300], [3, 350]],
+        10
+    ) == [[5, 500], [5, 400]], "金矿第一解"
+
     assert golddig_by_mine(
         [[5, 400], [5, 500], [3, 200], [4, 300], [3, 350]],
         10
