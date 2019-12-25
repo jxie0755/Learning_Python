fd1350d86ec168c9d17315ca80e8d0e1fa1d9158
jxie0755 <jxie0755@users.noreply.github.com>
2019-12-24 21:18:07 -0500

    Leetcode P81 REVIEWED

--- LeetCode/p081_search_in_rotated_sorted_array_ii.py
+++ LeetCode/p081_search_in_rotated_sorted_array_ii.py
@@ -1,19 +1,20 @@
-# P081 Search in Rotated Sorted Array II
-# Medium
+"""
+P081 Search in Rotated Sorted Array II
+Medium
 
 
-# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-# (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
-# You are given a target value to search. If found in the array return true, otherwise return false.
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+You are given a target value to search. If found in the array return true, otherwise return false.
 
 
-# Follow up:
-# This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
-# Would this affect the run-time complexity? How and why?
+Follow up:
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
 
-# before: [0,1,2,3,4,5,6] might become [3,4,5,6,0,1,2]).
-# now:    [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]
-
+before: [0,1,2,3,4,5,6] might become [3,4,5,6,0,1,2]).
+now:    [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]
+"""
 
 from typing import *
 
