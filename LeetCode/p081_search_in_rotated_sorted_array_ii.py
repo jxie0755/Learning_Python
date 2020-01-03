61e8615f83fdd3c2322c253b1ce245c4c0cac48c
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-01-03 16:34:47 -0500

    Leetcode P081 reviewed

--- LeetCode/p081_search_in_rotated_sorted_array_ii.py
+++ LeetCode/p081_search_in_rotated_sorted_array_ii.py
@@ -1,12 +1,11 @@
 """
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 P081 Search in Rotated Sorted Array II
 Medium
-
 
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
 You are given a target value to search. If found in the array return true, otherwise return false.
-
 
 Follow up:
 This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
@@ -19,9 +18,9 @@ now:    [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]
 from typing import *
 
 
-class Solution:
-    def binarySearch(self, nums, target):
-        """regular binary search in sorted list"""
+class Solution_A:
+    def binarySearch(self, nums: List[int], target: int) -> bool:
+        """regular binary search if a value is in sorted list"""
         lo, high = 0, len(nums) - 1
         while lo <= high:
             mid = (lo + high) // 2
@@ -33,8 +32,7 @@ class Solution:
                 high = mid - 1
         return False
 
-    def isSortedQuick(self, nums):
-
+    def isSortedQuick(self, nums: List[int]) -> bool:
         if len(set(nums)) <= 1:
             return True
         else:
@@ -44,7 +42,12 @@ class Solution:
                 return False
 
     def search(self, nums: List[int], target: int) -> bool:
-        lo, high = 0, len(nums) - 1
+        """
+        Recursive method to break the array into two halves:
+            - binary search the sorted part
+            - Recursive run on the other half
+        """
+        lo, high = 0, len(nums) - 1  # index
         mid = (lo + high) // 2 + 1
         first, second = nums[:mid], nums[mid:]
 
@@ -64,14 +67,11 @@ class Solution:
                 return self.search(first, target)
 
 
-class Solution:
-    # Method modified from Leetcode P033
-    def search(self, nums, target):
-        # Regular while loop, binary search O(logN)
+class Solution_B:
+    def search(self, nums: List[int], target: int) -> bool:
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
+        Regular while loop, binary search O(logN)
+        Method modified from Leetcode P033
         """
 
         if not nums:
@@ -82,50 +82,35 @@ class Solution:
         while L <= H:
             M = (L + H) // 2
             low, mid, high = nums[L], nums[M], nums[H]
-            print("L", L, "M", M, "H", H)
-            if L == H:
-                return low == target
-            if mid == target:
+
+            if L == H:  # means the length of the array is 1
+                return low == high == target
+            elif mid == target:
                 return True
-            if low <= mid and low <= target <= mid:
-                H = M - 1
-            elif mid <= high and mid <= target <= high:
-                L = M + 1
+            else:
+                if low <= target <= mid:  # if the first half is sorted and target in range
+                    H = M - 1
+                elif mid <= target <= high:  # if the second half is sorted and target in range
+                    L = M + 1
+                else:
+                    if low >= mid:  # if first half is unsorted, then must be in first half
+                        H = M - 1
+                    elif mid >= high: # if scond half is unsorted, then must be in second half
+                        L = M + 1
 
             # if target not in sorted sub-array, then it must be in the unsorted sub-array
-            # an array can be sorted in two way:
-            # 1 - Truely sorted
-            # 2 - Fake sorted because all elements is the same
-            # modified the way to tell which half is sorted, considering repeating elements, by excluding situation 2
-            elif len(set(nums[L:M + 1])) == 1 or low < mid:
-                L = M + 1
-            elif len(set(nums[M:H + 1])) == 1 or mid < high:
-                H = M - 1
+            # an array can be unsorted in two way:
+                # 1 - Truely unsorted sorted
+                # 2 - Fake sorted because all elements is the same
+                    # modified the way to tell which half is sorted, considering repeating elements, by excluding situation 2
 
 
 if __name__ == "__main__":
-    nums = [1]
-    target = 1
-    assert Solution().search(nums, target), "Edge 1"
-
-    nums = [1, 1]
-    target = 1
-    assert Solution().search(nums, target), "Edge 2"
-
-    nums = [3, 1]
-    target = 0
-    assert not Solution().search(nums, target), "Edge 3"
-
-    nums = [2, 5, 6, 0, 0, 1, 2]
-    target = 0
-    assert Solution().search(nums, target), "Example 1"
-
-    nums = [2, 5, 6, 0, 0, 1, 2]
-    target = 3
-    assert not Solution().search(nums, target), "Example 2"
-
-    nums = [1, 3, 1, 1, 1]
-    target = 3
-    assert Solution().search(nums, target), "Additional 1"
-
+    testCase = Solution_B()
+    assert testCase.search([1], 1), "Edge 1"
+    assert testCase.search([1, 1], 1), "Edge 2"
+    assert not testCase.search([3, 1], 0), "Edge 3"
+    assert testCase.search([2, 5, 6, 0, 0, 1, 2], 0), "Example 1"
+    assert not testCase.search([2, 5, 6, 0, 0, 1, 2], 3), "Example 2"
+    assert testCase.search([1, 3, 1, 1, 1], 3), "Additional 1"
     print("all passed")
