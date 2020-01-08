0a988a9d8d046db6895fa264b21f162778cb6d41
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-01-08 14:01:00 -0500

    Leetcode P084 reviewed

--- LeetCode/p084_largest_rectangle_in)histogram.py
+++ LeetCode/p084_largest_rectangle_in)histogram.py
@@ -1,13 +1,12 @@
-# https://leetcode.com/problems/largest-rectangle-in-histogram/
-# P084 Largest Rectangle in Histogram
-# Hard
+"""
+https://leetcode.com/problems/largest-rectangle-in-histogram/
+P084 Largest Rectangle in Histogram
+Hard
 
-
-# Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
-
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+"""
 
 from typing import *
-
 
 class Solution_A:
     # Worked, but exceeded maximum recursion depth
