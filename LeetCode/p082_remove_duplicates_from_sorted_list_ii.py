f69d99269f7d24a0b23ac4487d09f7e0350e4262
jxie0755 <jxie0755@users.noreply.github.com>
2020-01-11 21:57:14 -0500

    Leetcode P082 reviewed

--- LeetCode/p082_remove_duplicates_from_sorted_list_ii.py
+++ LeetCode/p082_remove_duplicates_from_sorted_list_ii.py
@@ -1,26 +1,27 @@
-# P82 Remove Duplicates from Sorted List II
-# Medium
+"""
+https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+P82 Remove Duplicates from Sorted List II
+Medium
 
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+"""
 
-# Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
-
-from typing import *
-from a0_TreeNode import *
 from a0_ListNode import *
 
 
-class Solution:
-    # O(N)
-    # Go over and skip the repeating elements
+class Solution_A:
+
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if not head:
-            return None
-        if not head.next:  # this can be removed but this will go faster
+        """
+        Time O(N)
+        Go over and skip the repeating elements
+        This is not remove in-place, but create another new linked list
+        """
+        if not head or not head.next:
             return head
 
         check = "X"
-        new_head = ListNode("X")
-        cur = new_head
+        new_head = cur = ListNode(check)
 
         while head:
             if head.next:
@@ -38,20 +39,51 @@ class Solution:
         return new_head.next
 
 
+class Solution_B:
+
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        """
+        Time O(N)
+        Move in place
+        Put a dummy before head and check move three nodes at the same time to find if a repeat happened at the 2nd and 3rd.
+        """
+        if not head or not head.next:
+            return head
+
+        dummy = cur = ListNode("X")
+        dummy.next = head
+        tail = head.next
+        repeat = False
+
+        while tail:
+            if head.val != tail.val and not repeat:
+                cur.next = head
+                cur = cur.next
+
+            elif head.val != tail.val and repeat:
+                if tail.next:
+                    repeat = False
+                else:
+                    cur.next = tail
+            else:
+                cur.next = None
+                repeat = True
+
+            head = head.next
+            tail = tail.next
+
+        return dummy.next
+
+
 if __name__ == "__main__":
-    L = None
-    assert not Solution().deleteDuplicates(L), "Edge 1"
+    testCase = Solution_B()
 
-    L = genNode([1, 1])
-    assert not Solution().deleteDuplicates(L), "Edge 2"
+    assert testCase.deleteDuplicates(None) is None, "Edge 1"
+    assert testCase.deleteDuplicates(genNode([1, 1])) is None, "Edge 2"
+    assert repr(testCase.deleteDuplicates(genNode([1]))) == "1", "Edge 3"
 
-    L = genNode([1])
-    assert repr(Solution().deleteDuplicates(L)) == "1", "Edge 3"
-
-    L = genNode([1, 2, 3, 3, 4, 4, 5])
-    assert repr(Solution().deleteDuplicates(L)) == "1->2->5", "Example 1"
-
-    L = genNode([1, 1, 1, 2, 3])
-    assert repr(Solution().deleteDuplicates(L)) == "2->3", "Example 1"
+    assert repr(testCase.deleteDuplicates(genNode([1, 2, 3, 3, 4, 4, 5]))) == "1->2->5", "Example 1"
+    assert repr(testCase.deleteDuplicates(genNode([1, 1, 1, 2, 3]))) == "2->3", "Example 2"
 
     print("all passed")
+
