960173d1a9d1a8bf0f6345477b509fc68a238a55
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-01-31 09:56:32 -0500

    Initiation zhon translation project

---
+++ QuickProjects/zhon_symbol_translation/zhonTranslate.py
@@ -0,0 +1,7 @@
+"""
+This is to create a script to rewrite a file with a lot of 中文标点 and convert to English symbols
+The regular Chinese characters should remain unchanged
+
+The file should be in utf-8 in the first place
+"""
+
