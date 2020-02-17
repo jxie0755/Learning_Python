d4e6c6eb3c4bdd72a7e7d3b923ed57d7899e97b1
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-02-16 22:39:36 -0500

    LC416 Attached Luo Ye's method and STD method
    Both are recursive

--- LeetCode/LC416_partition_equal_subset_sum.py
+++ LeetCode/LC416_partition_equal_subset_sum.py
@@ -14,18 +14,65 @@ from typing import *
 
 class Solution_A:
     def canPartition(self, nums: List[int]) -> bool:
-        pass
+        """
+        Luo Ye's recursive method, will exceed time limit
+        """
+        def helper(local_sum: int, global_sum: int, i: int):
+            if local_sum < global_sum//2 and i<len(nums) -1:
+                return helper(local_sum + nums[i], global_sum, i + 1) or helper(local_sum, global_sum, i + 1)
+            elif local_sum == global_sum//2:
+                return True
+            else:
+                return False
+
+        global_sum = sum(nums)
+        if global_sum % 2:
+            return False
+        return helper(0, global_sum, 0)
+
+
+class Solution_STD_A:
+    def build_combinations(self, nums: List[int], index: int, sum_: int, target: int) -> bool:
+        """
+        A helper to build up combinations check
+        """
+        sum_ += nums[index]
+        if sum_ < target:
+            for a in range(index+1, len(nums)):
+                ret = self.build_combinations(nums, a, sum_, target)
+                if ret:
+                    return True
+            return False
+        elif sum_ == target:
+            return True
+        else:
+            return False
+
+
+    def canPartition(self, nums: List[int]) -> bool:
+        """
+        A better recursive method
+        """
+        # nums.sort(reverse=True)
+        if sum(nums) % 2:
+            return False
+        else:
+            return self.build_combinations(nums, 0, 0, sum(nums))
+
+
 
 
 if __name__ == '__main__':
-    testCase = Solution_A()
-    # assert not testCase.canPartition([1]), "Edge 1"
-    # assert not testCase.canPartition([1,2]), "Edge 2"
-    # assert testCase.canPartition([2,2]), "Edge 3"
-    #
-    # assert testCase.canPartition([1,5,11,5]), "Example 1"
-    # assert not testCase.canPartition([1,2,3,5]), "Example 2"
-    #
-    # assert testCase.canPartition([1,2,3]), "Additional 1"
+    testCase = Solution_STD_A()
+    assert not testCase.canPartition([1]), "Edge 1"
+    assert not testCase.canPartition([1,2]), "Edge 2"
+    assert testCase.canPartition([2,2]), "Edge 3"
 
-    print(testCase.canPartition([1,5,11,5]))
+    assert testCase.canPartition([1,5,11,5]), "Example 1"
+    assert not testCase.canPartition([1,2,3,5]), "Example 2"
+
+    assert testCase.canPartition([1,2,3]), "Additional 1"
+
+    assert testCase.canPartition([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100]), "Long"
+
+    print('all passed')
