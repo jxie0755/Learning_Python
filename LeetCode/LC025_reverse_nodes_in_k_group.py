9c227c51fc1d2fbf342ca3ea748d30eaa4f7f096
jxie0755 <jxie0755@users.noreply.github.com>
2020-08-24 21:51:30 -0400

    Code review LC025_reverse_nodes_in_k_group.py

--- LeetCode/LC025_reverse_nodes_in_k_group.py
+++ LeetCode/LC025_reverse_nodes_in_k_group.py
@@ -15,22 +15,6 @@ Note:
 
 from a0_ListNode import *
 
-def reverseNodes(head: ListNode) -> ListNode:
-    """
-    Helper
-    参见Leetcode P206, reverse the whole linked-list
-    """
-
-    dummy = ListNode(float("-inf"))
-    while head:
-        rest = head.next
-        tail = dummy.next
-        head.next = tail
-        dummy.next = head
-        head = rest
-    return dummy.next
-
-
 class Solution_A:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         """
@@ -49,14 +33,29 @@ class Solution_A:
                 return head
 
         # 断开,但是不能忘记下一个节点,以备用
-        cur.next, next_cur = None, cur.next
+        cur.next, next_head = None, cur.next
 
         # reverse前k个节点, 此时k-group的最后一个节点变成了反转后的头
-        new_cur = reverseNodes(head)
+        new_head = self.reverseNodes(head)
 
-        # 反转后,cur也就是tail了
-        head.next = self.reverseKGroup(next_cur, k)
-        return new_cur
+        # 反转后,原来的的head也就是新的tail了,这里接上下一组k group
+        head.next = self.reverseKGroup(next_head, k)
+        return new_head
+
+    def reverseNodes(self, head: ListNode) -> ListNode:
+        """
+        Helper for both Solution A and Solution B
+        参见Leetcode P206, reverse the whole linked-list
+        """
+
+        dummy = ListNode(float("-inf"))
+        while head:
+            rest = head.next
+            head.next = dummy.next
+            dummy.next = head
+            head = rest
+        return dummy.next
+
 
 class Solution_B:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
@@ -80,7 +79,7 @@ class Solution_B:
             if count % k == 0:  # 若发现一个完整k循环
                 flag = False  # 撤销flag
                 cur.next = None  # 与后面断开
-                H.next = reverseNodes(head)  # H接上反转的head (此时head就是反转后的最后一个节点)
+                H.next = self.reverseNodes(head)  # H接上反转的head (此时head就是反转后的最后一个节点)
                 H = head  # 移动H到新链表的最后一个节点
                 cur = head = next_node  # 把cur和head定位到next下一个开头
 
@@ -94,11 +93,34 @@ class Solution_B:
 
         return dummy.next
 
+    def reverseNodes(self, head: ListNode) -> ListNode:
+        """
+        Helper for both Solution A and Solution B
+        参见Leetcode P206, reverse the whole linked-list
+        """
+
+        dummy = ListNode(float("-inf"))
+        while head:
+            rest = head.next
+            head.next = dummy.next
+            dummy.next = head
+            head = rest
+        return dummy.next
+
+
 
 if __name__ == "__main__":
-    testCase = Solution_B()
+    testCase = Solution_A()
 
     assert repr(testCase.reverseKGroup(genNode([1]), 2)) == "1", "Single"
+
+    a = genNode([1, 2, 3, 4, 5])
+    f = testCase.reverseKGroup(a, 1)
+    assert repr(f) == "1->2->3->4->5", "k=1"
+
+    a = genNode([1, 2])
+    f = testCase.reverseKGroup(a, 2)
+    assert repr(f) == "2->1", "k = length"
 
     a = genNode([1, 2, 3, 4, 5])
     f = testCase.reverseKGroup(a, 2)
