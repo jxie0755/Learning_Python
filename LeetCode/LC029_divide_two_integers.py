1a041f55d4e93a17245e8ad7690f00b9c7f31e4a
jxie0755 <jxie0755@users.noreply.github.com>
2020-08-27 21:37:13 -0400

    Code review LC029_divide_two_integers.py

--- LeetCode/LC029_divide_two_integers.py
+++ LeetCode/LC029_divide_two_integers.py
@@ -116,6 +116,6 @@ if __name__ == "__main__":
 
     assert testCase.divide(-2147483648, 1) == -2147483648, "Edge 2"
     assert testCase.divide(-2147483648, -1) == 2147483647, "Edge 3"
-    assert testCase.divide(-2147483648, -3) ==715827882, "Long 3"
+    assert testCase.divide(-2147483648, -3) == 715827882, "Long 3"
 
     print("all passed")
