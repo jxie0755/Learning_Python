a7efeba543b8b390b051345a6e89eb195b96813b
jxie0755 <jxie0755@users.noreply.github.com>
2020-08-27 21:32:47 -0400

    Code review LC029_divide_two_integers.py

--- LeetCode/LC029_divide_two_integers.py
+++ LeetCode/LC029_divide_two_integers.py
@@ -16,32 +16,26 @@ Note:
 """
 from typing import *
 
-class Solution_A:
+class Solution_A1:
     def divide(self, dividend: int, divisor: int) -> int:
         """
         Integer method
         Time:  O(logn) = O(1), Space: O(1)
         """
 
-        def manual_divide(son: int, mom: int) -> Tuple[int, int]:
-            """
-            Helper
-            Achive log(n) division by doubling mom, before son > mom
-            Mom and Son are both non-negative integers
-            """
-
-            result = 1
-            while son - mom > mom:
-                mom += mom
-                result += result
-            new_son = son - mom
-            return result, new_son
-
         result = 0
         son, mom = abs(dividend), abs(divisor)
+
         while son != 0 and son >= mom:
-            temp_result, son = manual_divide(son, mom)
-            result += temp_result
+
+            quotient = 1
+            while son - mom > mom: # 不断内循环迭代求商值
+                mom += mom
+                quotient += quotient
+
+            son = son - mom  # 更新余下的分子
+            mom = abs(divisor)  # 分母重回原值
+            result += quotient  # 结果累加商值
 
         if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
             result *= -1
@@ -53,12 +47,46 @@ class Solution_A:
         else:
             return result
 
+class Solution_A2:
+    def divide(self, dividend: int, divisor: int) -> int:
+        """
+        Recursive version of Solution A1
+        不断翻倍商,直到马上溢出.
+        然后对余值递归,直到abs(分母)<=abs(分子)
+        """
 
-class Solution_B:
+        son = abs(dividend)
+        mom = abs(divisor)
+        prefix = 1 if dividend * divisor >= 0 else -1  # 这里确定答案正负性
+
+        if son < mom:
+            return 0
+        elif son == mom:
+            return prefix
+        else:
+            quotient = 1
+            while son - mom > mom:
+                mom *= 2
+                quotient *= 2
+
+            new_son = son - mom  # 准备余下来的分子,继续递归求解
+
+            # 注意,为了避免多次求商时反复正负性叠加,递归时把各商绝对值累加后只测一次正负性
+            result = prefix * (quotient + abs(self.divide(new_son, divisor)))
+
+            # 规避Integer溢出
+            if -2147483648 <= result <= 2147483647:
+                return result
+            elif result < -2147483648:
+                return -2147483648
+            else:
+                return 2147483647
+
+class Solution_C:
 
     def divide(self, dividend: int, divisor: int) -> int:
         """
-        Same idea but use bit calculation << and >>
+        Same idea as Solution A but use bit calculation << and >>
         """
         positive = (dividend < 0) is (divisor < 0)  # great way to align two integer on the same sideK
         dividend, divisor = abs(dividend), abs(divisor)
@@ -75,8 +103,11 @@ class Solution_B:
         return min(max(-2147483648, res), 2147483647)
 
 
+
+
+
 if __name__ == "__main__":
-    testCase = Solution_B()
+    testCase = Solution_A1()
     assert testCase.divide(1, 3) == 0, "Edge 1"
 
     assert testCase.divide(10, 3) == 3, "Example 1"
