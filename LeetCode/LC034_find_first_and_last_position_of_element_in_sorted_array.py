a0c5149361a626fc56b3fd2136034bda8aae2492
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-09-01 15:00:06 -0400

    Code review LC034_find_first_and_last_position_of_element_in_sorted_array.py

--- LeetCode/LC034_find_first_and_last_position_of_element_in_sorted_array.py
+++ LeetCode/LC034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -15,123 +15,67 @@ from typing import *
 class Solution_A:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         """
-        3 binary search, find catch mid value, and Low/High of the section,
-        Then according to mid value and Low/High to find head and tail by 2 additional binary search
-        O(LogN), Space O(1)
-        """
-        if not nums:  # 处理空list
-            return [-1, -1]
-        if len(nums) == 1:  # 处理单元素list
-            return [0, 0] if nums[0] == target else [-1, -1]
-
-        L, H = 0, len(nums) - 1
-        M = (L + H) // 2 + 1
-
-        while nums[M] != target:
-            if H - L <= 1 and nums[H] == target:
-                return [H, H]
-            elif H - L <= 1 and nums[L] != target and nums[H] != target:  # if cannot find M, target not in list
-                return [-1, -1]
-            if target < nums[M]:
-                H = M
-            elif target > nums[M]:
-                L = M
-
-            M = (L + H) // 2
-
-        # Locate head
-        head = L
-        while True:
-            head_value = nums[head]
-            if head_value == target and head == L:
-                break
-            elif head_value < target:
-                L = head
-                head = (L + M + 1) // 2
-            elif head_value == target and nums[head - 1] == target:
-                head = (head + L + 1) // 2
-            else:
-                break
-
-        # locate tail
-        tail = H
-        while True:
-            tail_value = nums[tail]
-            if tail_value == target and tail == H:
-                break
-            elif tail_value > target:
-                H = tail
-                tail = (H + M) // 2
-            elif nums[tail] == target and nums[tail + 1] == target:
-                tail = (tail + H) // 2
-            else:
-                break
-
-        return [head, tail]
-
-
-class Solution_B:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        """
         improved binary search
         3 binary search, find catch mid value, and Low/High of the section,
         Then according to mid value and Low/High to find head and tail by 2 additional binary search
         O(LogN), Space O(1)
         """
 
-        if not nums:  # 处理空list
-            return [-1, -1]
-        if len(nums) == 1:  # 处理单元素list
-            return [0, 0] if nums[0] == target else [-1, -1]
-
         L, H = 0, len(nums) - 1
-        M = (L + H) // 2
+        target_found = False
+        # first determine if target is in nums
         while L <= H:
-            check = nums[M]
-            if check < target:
+            M = (L + H) // 2
+            low, mid, high = nums[L], nums[M], nums[H]
+
+            if mid < target:
                 L = M + 1
-            elif check > target:
+            elif target < mid:
                 H = M - 1
             else:
+                target_found = True
                 break
-            M = (L + H) // 2
 
-        if L > M:  # can't find M match target
+        if not target_found:
             return [-1, -1]
 
-        # Locate head
-        Lo, Hi = L, M
-        while True:
-            head = (Lo + Hi) // 2
-            head_value = nums[head]
-            if head_value == target and head == 0:
-                break
-            elif head_value < target:
-                Lo = head + 1
-            elif head_value == target and nums[head - 1] == target:
-                Hi = head - 1
-            else:
-                break
 
-        # locate tail
-        Lo, Hi = M, H
+        head = tail = M
+
+        # first find head
         while True:
-            tail = (Lo + Hi) // 2
-            tail_value = nums[tail]
-            if tail_value == target and tail == Hi:
+            M = (L + head) // 2
+
+            if nums[L] == target:
+                head = L
                 break
-            elif tail_value > target:
-                Hi = tail - 1
-            elif nums[tail] == target and nums[tail + 1] == target:
-                Lo = tail + 1
+            elif nums[M] < target:
+                L = M + 1
             else:
+                head = M
+                if nums[M - 1] != target:
+                    break
+
+
+        # second find tail
+        while True:
+            M = (tail + H) // 2
+
+            if nums[H] == target:
+                tail = H
                 break
+            elif nums[M] > target:
+                H = M - 1
+            else:
+                tail = M
+                if nums[M + 1] != target:
+                    break
 
         return [head, tail]
 
 
 if __name__ == "__main__":
-    testCase = Solution_B()
+    testCase = Solution_A()
     assert testCase.searchRange([], 0) == [-1, -1], "Edge 1"
     assert testCase.searchRange([8], 8) == [0, 0], "Edge 2"
     assert testCase.searchRange([0], 8) == [-1, -1], "Edge 3"
