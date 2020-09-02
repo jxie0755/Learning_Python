94e89e9c497ecfe794f2b2813481931b27540c8d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-09-02 15:23:02 -0400

    Code review LC035_search_insert_position.py

--- LeetCode/LC035_search_insert_position.py
+++ LeetCode/LC035_search_insert_position.py
@@ -29,8 +29,9 @@ class Solution_A:
 if __name__ == "__main__":
     testCase = Solution_A()
     assert testCase.searchInsert([], 5) == 0, "Empty"
-    assert testCase.searchInsert([1, 3, 5, 6], 5) == 2, "In the list"
     assert testCase.searchInsert([1, 3, 5, 6], 2) == 1, "Not in the list"
-    assert testCase.searchInsert([1, 3, 5, 6], 7) == 4, "At the end"
     assert testCase.searchInsert([1, 3, 5, 6], 0) == 0, "At the beginning"
+    assert testCase.searchInsert([1, 3, 5, 6], 5) == 2, "In the list"
+    assert testCase.searchInsert([1, 3, 5, 6], 7) == 4, "At the end"
+
     print("all passed")
