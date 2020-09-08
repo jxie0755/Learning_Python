fdbc67a8ef0a902e5967f13d9999fdfc8dd38fdc
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-09-08 16:04:10 -0400

    Code Review LC039_combination_sum.py

--- LeetCode/LC039_combination_sum.py
+++ LeetCode/LC039_combination_sum.py
@@ -15,7 +15,42 @@ Note:
 
 from typing import *
 
+
 class Solution_A:
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        This will pass but very slowly
+        """
+        result = []
+        available = [[[], 0] for _ in
+                     range(len(candidates))]  # must remember which idx this combination is at to save repeatition
+
+        while available:
+            new_available = []
+
+            for pair in available:
+                comb = pair[0]
+                idx = pair[1]
+
+                for i in range(idx, len(candidates)):
+                    cur = comb[:] + [candidates[i]]  # i.e.: [2,3] will start adding at 3 instead of 2
+
+                    if sum(cur) < target:
+                        new_available.append([cur, i])
+                    elif sum(cur) == target:
+                        if sorted(cur) not in result:
+                            result.append(sorted(cur))
+                    else:
+                        pass
+            available = new_available
+
+        return result
+        # Leetcode does not require sequence, but locally it is required to pass case
+
+
+
+class Solution_B:
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         """
@@ -51,7 +86,7 @@ class Solution_A:
         return result
 
 
-class Solution_B:
+class Solution_C:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         """
         Add a recursive process method to update result
@@ -81,7 +116,7 @@ class Solution_B:
 
 from itertools import combinations_with_replacement
 
-class Solution_C:
+class Solution_D:
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         """
@@ -90,6 +125,7 @@ class Solution_C:
         上解就是通过自己构建组合,并融合target条件所以直接得出答案.
         这里利用python自带组合函数同样可以实现
         """
+        candidates = sorted(candidates)  # must sort first
         if not candidates:
             return []
         max_n = target // min(candidates)
@@ -99,20 +135,32 @@ class Solution_C:
             for j in all_comb:
                 if sum(j) == target:
                     result.append(list(j))
-        return result
+        return result  # must sort at the end to pass case
 
 
 if __name__ == "__main__":
-    testCase = Solution_B()
-    assert testCase.combinationSum([], 1) == [], "Edge 1"
-    assert testCase.combinationSum([1], 1) == [[1]], "Edge 2"
-    assert testCase.combinationSum([1], 2) == [[1, 1]], "Edge 3"
-    assert testCase.combinationSum([2], 1) == [], "Edge 4"
-    assert testCase.combinationSum([2], 5) == [], "Edge 5"
+    testCase = Solution_A()
 
-    assert testCase.combinationSum([2, 3, 6, 7], 7) == [[2, 2, 3], [7]], "Example 1"
-    assert testCase.combinationSum([2, 3, 5], 8) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]], "Example 2"
+    # Test cases are check after sorting, to avoid sequence error
+    assert sorted(testCase.combinationSum([], 1)) == [], "Edge 1"
+    assert sorted(testCase.combinationSum([1], 1)) == [[1]], "Edge 2"
+    assert sorted(testCase.combinationSum([1], 2)) == [[1, 1]], "Edge 3"
+    assert sorted(testCase.combinationSum([2], 1)) == [], "Edge 4"
+    assert sorted(testCase.combinationSum([2], 5)) == [], "Edge 5"
 
-    assert testCase.combinationSum([2, 4], 10) == [[2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4]], "Extra 1"
+    assert sorted(testCase.combinationSum([2, 3, 6, 7], 7)) == [[2, 2, 3], [7]], "Example 1"
+    assert sorted(testCase.combinationSum([2, 3, 5], 8)) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]], "Example 2"
+
+    assert sorted(testCase.combinationSum([2, 4], 10)) == [[2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4]], "Extra 1"
+
+    assert sorted(testCase.combinationSum([7, 3, 2], 18)) == [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 3, 3],
+        [2, 2, 2, 2, 3, 7],
+        [2, 2, 2, 3, 3, 3, 3],
+        [2, 2, 7, 7],
+        [2, 3, 3, 3, 7],
+        [3, 3, 3, 3, 3, 3]
+    ], "Extra 2"
 
     print("all passed")
--- whiteboard.py
+++ whiteboard.py
@@ -19,23 +19,61 @@ from typing import *
 class Solution:
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+        """
+        This will pass but very slowly
+        """
+        result = []
+        available = [[[],0] for _ in range(len(candidates))]  # must remember which idx this combination is at to save repeatition
 
 
+        while available:
+            new_available = []
+
+            for pair in available:
+                comb = pair[0]
+                idx = pair[1]
+
+                for i in range(idx, len(candidates)):
+                    cur = comb[:] + [candidates[i]]    # i.e.: [2,3] will start adding at 3 instead of 2
+
+                    if sum(cur) < target:
+                        new_available.append([cur, i])
+                    elif sum(cur) == target:
+                        if sorted(cur) not in result:
+                            result.append(sorted(cur))
+                    else:
+                        pass
+            available = new_available
+
+        return result
+        # Leetcode does not require sequence, but locally it is required to pass case
 
 
 
 if __name__ == "__main__":
     testCase = Solution()
-    assert testCase.combinationSum([], 1) == [], "Edge 1"
-    assert testCase.combinationSum([1], 1) == [[1]], "Edge 2"
-    assert testCase.combinationSum([1], 2) == [[1, 1]], "Edge 3"
-    assert testCase.combinationSum([2], 1) == [], "Edge 4"
-    assert testCase.combinationSum([2], 5) == [], "Edge 5"
 
-    assert testCase.combinationSum([2, 3, 6, 7], 7) == [[2, 2, 3], [7]], "Example 1"
-    assert testCase.combinationSum([2, 3, 5], 8) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]], "Example 2"
+    # Test cases are check after sorting, to avoid sequence error
+    assert sorted(testCase.combinationSum([], 1)) == [], "Edge 1"
+    assert sorted(testCase.combinationSum([1], 1)) == [[1]], "Edge 2"
+    assert sorted(testCase.combinationSum([1], 2)) == [[1, 1]], "Edge 3"
+    assert sorted(testCase.combinationSum([2], 1)) == [], "Edge 4"
+    assert sorted(testCase.combinationSum([2], 5)) == [], "Edge 5"
 
-    assert testCase.combinationSum([2, 4], 10) == [[2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4]], "Extra 1"
+    assert sorted(testCase.combinationSum([2, 3, 6, 7], 7)) == [[2, 2, 3], [7]], "Example 1"
+    assert sorted(testCase.combinationSum([2, 3, 5], 8)) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]], "Example 2"
+
+    assert sorted(testCase.combinationSum([2, 4], 10)) == [[2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4]], "Extra 1"
+
+    assert sorted(testCase.combinationSum([7, 3, 2], 18)) == [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 3, 3],
+        [2, 2, 2, 2, 3, 7],
+        [2, 2, 2, 3, 3, 3, 3],
+        [2, 2, 7, 7],
+        [2, 3, 3, 3, 7],
+        [3, 3, 3, 3, 3, 3]
+    ], "Extra 2"
+
 
     print("all passed")
