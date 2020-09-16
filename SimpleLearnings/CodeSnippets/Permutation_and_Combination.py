061ce9f820444f649140c8729d3b3a71e073ae3d
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-09-15 21:04:14 -0400

    Setup code snippet for Permutation and Combination

--- SimpleLearnings/CodeSnippets/Permutation_and_Combination.py
+++ SimpleLearnings/CodeSnippets/Permutation_and_Combination.py
@@ -14,17 +14,58 @@ Combiantion:
 from typing import *
 import itertools
 
-print(list(itertools.combinations([1,2,3], 2)))
+
+# Combinations with no replacement
+def combinations(candidates: List[int], pick: int) -> List[List[int]]:
+    """
+    Self verison of combination algorithm, with no repeating
+    Use a proxy helper to generate indices of based on length of candidates, then convert to real elments
+    """
+    # this can individually work as a combination of list of elements
+    def combinesSolo(n: int, pick: int) -> List[List[int]]:
+        """
+        Helper function
+        Combination of indices
+        """
+        if pick == 0:
+            return [[]]
+        elif pick == n:
+            return [list(range(n))]
+        elif pick == 1:
+            return [[i] for i in range(n)]
+        else:
+            return [com + [n - 1] for com in combinesSolo(n - 1, pick - 1)] + combinesSolo(n - 1, pick)
+                          # n-1 because end index of n length is n-1
+
+    proxy_ans = combinesSolo(len(candidates), pick)
+
+    # convert proxy answer into real elements in candidates
+    ans = []
+    for proxy_comb in proxy_ans:
+        ans_comb = [candidates[i] for i in proxy_comb]
+        ans.append(ans_comb)
+
+    return ans
+
+
+if __name__ == '__main__':
+    assert sorted(combinations([1,2,3,4], 2)) == [
+        [1, 2],
+        [1, 3],
+        [1, 4],
+        [2, 3],
+        [2, 4],
+        [3, 4]
+    ]
 
 
 
-
-
-# Combination with replacement
+# Combinations with replacement
 def combinations_with_replacements(candidates: List[int], pick: int) -> List[List[int]]:
     """
-    self verison of combination algorithm, with repeating
-    almost the same as itertools.combinations_with_replacement
+    Self verison of combination algorithm, with repeating
+    Almost the same as itertools.combinations_with_replacement
+    Also use a proxy to generate indices of based on length of candidates, then convert to real elments
     """
     if pick == 0:
         return []
@@ -44,7 +85,7 @@ def combinations_with_replacements(candidates: List[int], pick: int) -> List[Lis
         proxy_ans = new_proxy_ans
         p += 1
 
-    # put proxy answer idx into real elements in candidates
+    # convert proxy answer into real elements in candidates
     ans = []
     for proxy_comb in proxy_ans:
         ans_comb = [candidates[i] for i in proxy_comb]
@@ -59,5 +100,3 @@ if __name__ == '__main__':
         [2, 2, 2], [2, 2, 3], [2, 3, 3],
         [3, 3, 3]
     ]
-
-
