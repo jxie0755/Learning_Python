c240d31761399cc4af14faf3892ae361fed67aed
jxie0755 <jxie0755@users.noreply.github.com>
2020-09-17 08:27:05 -0400

    Code Snippet Permutation and Combination adding non-proxy method

--- SimpleLearnings/CodeSnippets/Permutation_and_Combination.py
+++ SimpleLearnings/CodeSnippets/Permutation_and_Combination.py
@@ -58,6 +58,27 @@ def combinations(candidates: List, r: int) -> List[List]:
     return ans
 
 
+def combinations(candidates: List, r: int) -> List[List]:
+    """
+    Self verison of combination algorithm, with no repeating
+    None-proxy, directly forming, Recursive
+    Similar to itertools.combinations
+    """
+
+    n = len(candidates)
+
+    if r == 0:
+        return [[]]
+    elif r == n:
+        return [candidates]
+    elif r == 1:
+        return [[i] for i in candidates]
+    else:
+        sub_candidates = candidates[:]
+        sub_can_last = sub_candidates.pop()
+        return [com + [sub_can_last] for com in combinations(sub_candidates, r - 1)] + combinations(sub_candidates, r)
+
+
 if __name__ == '__main__':
     assert sorted(combinations(["A","B","C","D"], 2)) == [
         ['A', 'B'],
@@ -107,6 +128,35 @@ def combinations_with_replacements(candidates: List, r: int) -> List[List]:
     return ans
 
 
+def combinations_with_replacements(candidates: List, r: int) -> List[List]:
+    """
+    Self verison of combination algorithm, with repeating
+    None-proxy, directly forming
+    Almost the same as itertools.combinations_with_replacement
+    """
+    if r > len(candidates):
+        raise ValueError("r > len(candidates)")
+
+    if r == 0:
+        return []
+
+    # setup a proxy (idx of candidates)
+    ans = [[i] for i in candidates]
+
+    p = 1
+    while p < r:
+        new_ans = []
+        for comb in ans:
+            for idx in candidates:
+                if idx >= max(comb):  # a very critical step to remove repeating
+                    new = comb + [idx]
+                    new_ans.append(new)
+        ans = new_ans
+        p += 1
+
+    return ans
+
+
 if __name__ == '__main__':
     assert sorted(sorted(i) for i in combinations_with_replacements(["A", "B", "C"], 3)) == [
         ['A', 'A', 'A'],
@@ -130,8 +180,8 @@ def permutations(candidates: List, r: int) -> List[List]:
     """
     def permute(indices: List[int]) -> List[List[int]]:
         """
-        Recursive Helper proxy function, take indices as input and output indices
-        This will permute all indices
+        Recursive Helper doesn't matter if proxy or not
+        But in this implementation, indices are sent in, thus output permutations of indices
         """
         if len(indices) == 1:
             return [indices]
@@ -165,6 +215,43 @@ def permutations(candidates: List, r: int) -> List[List]:
         ans.append(ans_perm)
 
     return ans
+
+
+def permutations(candidates: List, r: int) -> List[List]:
+    """
+    Self verison of permutation algorithm
+    Non-proxy, directly forming
+    Need to use the Combinations first to pick r number of elements, and then do permutations on each combination
+    """
+
+    def permute(indices: List[int]) -> List[List[int]]:
+        """
+        Recursive Helper, doesn't matter if proxy or not
+        This will permute all indices
+        """
+        if len(indices) == 1:
+            return [indices]
+        else:
+            result = []
+            for i in indices:
+                sub_indices = indices[:]
+                sub_indices.remove(i)
+                result += [[i] + per for per in permute(sub_indices)]
+            return result
+
+    if r > len(candidates):
+        raise ValueError("r > len(candidates)")
+
+    # Get all proxy combinations picking r elements, in indices
+    all_combinations = combinations(candidates, r)
+
+    # get all proxy permutations for each combination
+    all_permutations = []
+    for comb in all_combinations:
+        perm = permute(comb)
+        all_permutations += perm
+
+    return all_permutations
 
 if __name__ == '__main__':
     assert sorted(permutations(["A", "B", "C"], 3)) == [
