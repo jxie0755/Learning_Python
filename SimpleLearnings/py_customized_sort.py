4362787c3dbfb752ca66ba089a0e080a5674a0cf
jxie0755 <jxie0755@users.noreply.github.com>
2020-09-21 13:48:02 -0400

    Learn customized sort

--- SimpleLearnings/py_customized_sort.py
+++ SimpleLearnings/py_customized_sort.py
@@ -9,21 +9,21 @@ from typing import *
 def absSort(a: int) -> int:
     return a * a
 
+if __name__ == '__main__':
+    # 三者等同
+    a = [-5, 4, -3, 2]
+    print(max(a, key=abs))
+    print(max(a, key=absSort))
+    print(max(a, key=lambda x: abs(x)))
+    # >>> -5
 
-# 三者等同
-a = [-5, 4, -3, 2]
-print(max(a, key=abs))
-print(max(a, key=absSort))
-print(max(a, key=lambda x: abs(x)))
-# >>> -5
+    print(sorted(a, key=abs))
+    print(sorted(a, key=absSort))
+    print(sorted(a, key=lambda x: abs(x)))
 
-print(sorted(a, key=abs))
-print(sorted(a, key=absSort))
-print(sorted(a, key=lambda x: abs(x)))
-
-# key也可以写成两者比较的函数,需要调用functools.cmp_to_key
-print(sorted(a, key=functools.cmp_to_key(lambda x, y: abs(x) - abs(y))))
-# >>> [2, -3, 4, -5]
+    # key也可以写成两者比较的函数,需要调用functools.cmp_to_key
+    print(sorted(a, key=functools.cmp_to_key(lambda x, y: abs(x) - abs(y))))
+    # >>> [2, -3, 4, -5]
 
 
 # 更复杂点的intGrid排序, 需要对比两个值, 返回-1,0,1来比较x和y的关系
@@ -46,17 +46,17 @@ def intGridCompare(x: List[int], y: List[int]) -> int:
     else:
         return 1
 
+if __name__ == '__main__':
+    grid = [
+        [3, 2, 1],
+        [1, 3, 2],
+        [2, 3, 1],
+        [2, 1, 3],
+        [1, 2, 3],
+        [3, 1, 2]
+    ]
 
-grid = [
-    [3, 2, 1],
-    [1, 3, 2],
-    [2, 3, 1],
-    [2, 1, 3],
-    [1, 2, 3],
-    [3, 1, 2]
-]
-
-# 自定义compare同样需要调用unctools.cmp_to_key(cmp)来做key
-print(sorted(grid))
-print(sorted(grid, key=functools.cmp_to_key(intGridCompare)))
-# >>> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    # 自定义compare同样需要调用unctools.cmp_to_key(cmp)来做key
+    print(sorted(grid))
+    print(sorted(grid, key=functools.cmp_to_key(intGridCompare)))
+    # >>> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
