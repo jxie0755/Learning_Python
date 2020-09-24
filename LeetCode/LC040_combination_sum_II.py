6d10021c90fe8a78dc8f3173a0fc51e3f70a4f7b
jxie0755 <jxie0755@users.noreply.github.com>
2020-09-24 19:22:29 -0400

    Code review LC040_combination_sum_II.py

--- LeetCode/LC040_combination_sum_II.py
+++ LeetCode/LC040_combination_sum_II.py
@@ -78,13 +78,62 @@ class Solution_B:
         return result
 
 
+class Solution_C:
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        本质上这是一道考排列组合的题,如果能构建排列组合的话, 直接对每个组合考虑是否之和等于target就可以了
+        (可以通过python自带组合函数同样可以实现,也可以通过自建一个Combinations)
+
+        类似LC039,但是这次会超时
+        """
+        if not candidates:
+            return []
+
+        result = []
+        for i in range(1, len(candidates) + 1):
+            for comb in self.combinations(candidates, i):
+                sorted_comb = sorted(comb)  # 确保排序,以防重复组出现(原combinations不需要)
+                if sum(sorted_comb) == target and sorted_comb not in result:
+                    result.append(sorted_comb)
+        return result  # must sort at the end to pass case
+
+    def combinations(self, candidates: List, r: int) -> List[List]:
+        """
+        Self verison of combination algorithm, with no repeating
+        None-proxy, directly forming, Recursive
+        Similar to itertools.combinations_with_replacement(iterable, r)
+        """
+
+        n = len(candidates)
+
+        if r == 0:
+            return [[]]
+        elif r == n:
+            return [candidates]
+        elif r == 1:
+            return [[i] for i in candidates]
+        else:
+            sub_candidates = candidates[:]
+            popped = sub_candidates.pop()
+            return [com + [popped] for com in self.combinations(sub_candidates, r - 1)] + self.combinations(
+                sub_candidates, r)
+
 if __name__ == "__main__":
-    testCase = Solution_B()
+    testCase = Solution_C()
 
-    candidates = [10, 1, 2, 7, 6, 1, 5]
-    assert testCase.combinationSum2(candidates, 8) == [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]], "Example 1"
+    q1 = [10, 1, 2, 7, 6, 1, 5]
+    assert sorted([sorted(comb) for comb in testCase.combinationSum2(q1, 8)]) == [
+        [1, 1, 6],
+        [1, 2, 5],
+        [1, 7],
+        [2, 6]
+    ], "Example 1"
 
-    candidates = [2, 5, 2, 1, 2]
-    assert testCase.combinationSum2(candidates, 5) == [[1, 2, 2], [5]], "Example 2"
+    q2 = [2, 5, 2, 1, 2]
+    assert sorted([sorted(comb) for comb in testCase.combinationSum2(q2, 5)]) == [
+        [1, 2, 2],
+        [5]
+    ], "Example 2"
 
     print("all passed")
