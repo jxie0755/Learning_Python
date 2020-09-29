f659f34891c48517b0dc89b7e3d60e104a93b741
jxie0755 <jxie0755@users.noreply.github.com>
2020-09-28 22:58:24 -0400

    Code review LC041_first_missing_positive.py

--- LeetCode/LC041_first_missing_positive.py
+++ LeetCode/LC041_first_missing_positive.py
@@ -16,40 +16,38 @@ class Solution_A:
     def firstMissingPositive(self, nums: List[int]) -> int:
         """
         O(N), space O(N) (not constant space)
+        Use set to remove element, increase spped
         """
-        if not nums:
-            return 1
-        all_int = list(range(1, len(nums) + 1))
+        all_possible = set(range(1, len(nums) + 2)) # range from [1 to len + 1]
         for i in nums:
-            try:
-                all_int.remove(i)
-            except ValueError:
-                pass
-        if not all_int:
-            return len(nums) + 1
-        else:
-            return all_int[0]
+            all_possible -= {i}
+        return min(all_possible)
+
 
 class Solution_STD:
     def firstMissingPositive(self, nums: List[int]) -> int:
         """
-        Avoid use try except block
+        桶排序法
+        Time: O(N)
         """
 
-        nums.append(0)
         n = len(nums)
+        i = 0
+        while i < n:  # while里面那句话执行的总次数最大就是nums的长度。一旦一个数字被放对位置,则不会再被移动,所以最多只能交换nums长度次数
+            if 0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]: # 交换两者
+                A = nums[i]
+                B = nums[nums[i] - 1]
+                nums[nums[i] - 1] = A
+                nums[i] = B
+            else:  # 连续性调换,直到每个排序正确为止
+                i += 1
 
-        for i in range(n):  # delete those useless elements
-            if nums[i] < 0 or nums[i] >= n:
-                nums[i] = 0
+        # 第二轮排序后, 遍历找到第一个不符合者, 它的index+1即为结果,不然则是n+1在最末端
+        for j in range(n):
+            if nums[j] != j + 1:
+                return i + 1
+        return n + 1
 
-        for i in range(len(nums)):  # use the index as the hash to record the frequency of each number
-            nums[nums[i] % n] += n  # 在每个数上加n, 则不会改变这个数%n的余数
-
-        for i in range(0, len(nums)):
-            if nums[i] / n == 0:
-                return i
-        return n
 
 
 if __name__ == "__main__":
@@ -62,5 +60,7 @@ if __name__ == "__main__":
     assert testCase.firstMissingPositive([1, 2, 0]) == 3, "Example 1"
     assert testCase.firstMissingPositive([3, 4, -1, 1]) == 2, "Example 2"
     assert testCase.firstMissingPositive([7, 8, 9, 11, 12]) == 1, "Example 3"
+
+    assert testCase.firstMissingPositive([2,2]) == 1, "Extra 1"
 
     print("all passed")
