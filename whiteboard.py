2dbba8e65b06f4ad024c9f75d7aa628cb0875da4
jxie0755 <jxie0755@users.noreply.github.com>
2020-10-08 21:22:18 -0400

    Code Review LC042

--- whiteboard.py
+++ whiteboard.py
@@ -9,7 +9,6 @@ compute how much water it is able to trap after raining.
 
 from typing import *
 
-
 class Solution:
     def trap(self, height: List[int]) -> int:
 
@@ -50,27 +49,6 @@ class Solution:
 
         if height[i] > height[i-1]:
             tops.append((height[i], i))
-        print(tops)
-
-        check_range = [-1, -1]  # initiate the check range with false idx, that nothing will be in
-        volume = 0
-
-        tops = sorted(tops)
-        while len(tops) > 1:
-            top_one = tops.pop()
-            top_two = tops[-1]
-            lower_top = min(top_one[0], top_two[0])
-            idx_hi = max(top_one[1], top_two[1])
-            idx_lo = min(top_one[1], top_two[1])
-            if check_range[0] <= idx_hi <= check_range[1] and check_range[0] <= idx_lo <= check_range[1]:
-                    pass
-            else:
-                check_range[0] = idx_lo
-                check_range[1] = idx_hi
-                for i in range(idx_lo+1, idx_hi):
-                    volume += (lower_top - height[i])
-        return volume
-
 
 
 
@@ -86,17 +64,17 @@ print(Solution().trap([5, 5, 1, 7, 1, 1, 5, 2, 7, 6]))
 
 
 
-if __name__ == "__main__":
-    testCase = Solution()
-    assert testCase.trap([]) == 0, "Edge 1"
-    assert testCase.trap([0]) == 0, "Edge 2"
-    assert testCase.trap([1]) == 0, "Edge 3"
-    assert testCase.trap([1, 1]) == 0, "Edge 4"
-    assert testCase.trap([2, 2, 2]) == 0, "Edge 5"
-    assert testCase.trap([2, 0, 2]) == 2, "Edge 6"
-
-    assert testCase.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6, "Example 1"
-    assert testCase.trap([10, 1, 2, 1, 2, 1, 10]) == 43, "Example 2"
-    assert testCase.trap([5, 5, 1, 7, 1, 1, 5, 2, 7, 6]) == 23, "Example 3"
-    assert testCase.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6, "Extra 1"
-    print("all passed")
+# if __name__ == "__main__":
+#     testCase = Solution()
+#     assert testCase.trap([]) == 0, "Edge 1"
+#     assert testCase.trap([0]) == 0, "Edge 2"
+#     assert testCase.trap([1]) == 0, "Edge 3"
+#     assert testCase.trap([1, 1]) == 0, "Edge 4"
+#     assert testCase.trap([2, 2, 2]) == 0, "Edge 5"
+#     assert testCase.trap([2, 0, 2]) == 2, "Edge 6"
+#
+#     assert testCase.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6, "Example 1"
+#     assert testCase.trap([10, 1, 2, 1, 2, 1, 10]) == 43, "Example 2"
+#     assert testCase.trap([5, 5, 1, 7, 1, 1, 5, 2, 7, 6]) == 23, "Example 3"
+#     assert testCase.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6, "Extra 1"
+#     print("all passed")
