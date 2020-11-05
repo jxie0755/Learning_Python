d9c2dd9ae0362a1aab187de39652433622b7f3cf
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2020-11-05 12:59:15 -0500

    Code Review LC054_spiral_matrix.py

--- LeetCode/LC054_spiral_matrix.py
+++ LeetCode/LC054_spiral_matrix.py
@@ -8,12 +8,14 @@ Given a matrix of m x n elements (m rows, n columns), return all elements of the
 
 from typing import *
 
+
 class Solution_A:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         """
         Use multiple helper functions to solve
         Needs buffer area around the matrix
         """
+
         def next_coor(coor, direction):
             """Helper A1"""
             x, y = coor[0], coor[1]
@@ -61,16 +63,18 @@ class Solution_A:
 
         D = gen_direction()
         go = next(D)
-        coor = [1, 1]
-        while total_steps:
-            result.append(get_coor(coor))
-            set_coor(coor, "X")
+        coor = [1, 1]  # after adding buffer area, original starting point become [1,1] instead of [0,0]
 
+        for each_step in range(total_steps):
+            result.append(get_coor(coor))
+            set_coor(coor, "X")  # when iterated, change value to "X" so that buffer area updateds
             go_coor = next_coor(coor, go)
+
+            # use buffer area detection to tell if a turn is needed
             if get_coor(go_coor) == "X":
                 go = next(D)
+
             coor = next_coor(coor, go)
-            total_steps -= 1
 
         return result
 
@@ -81,26 +85,30 @@ class Solution_B:
         Direct output, no buffer needed
         """
         result = []
-        if len(matrix)==0 or len(matrix[0])==0:
+
+        if len(matrix) == 0 or len(matrix[0]) == 0:
             return result
 
         m, n = len(matrix), len(matrix[0])
         top, bot, left, right = 0, m - 1, 0, n - 1
 
         while top <= bot and left <= right:
-            for a in range(left, right + 1):
+
+            for a in range(left, right + 1):  # left -> right
                 result.append(matrix[top][a])
             top += 1
-            for b in range(top, bot + 1):
+
+            for b in range(top, bot + 1):  # top -> bot
                 result.append(matrix[b][right])
             right -= 1
-            for c in range(right, left - 1, -1):
-                # 注意在这里要补一个条件,因为矩形不一定是sqaure 所以会出现只剩下横或者只剩下列的情况要规避
-                if top <= bot: # 注意不能是<
+
+            if top <= bot: # 反向移动时, 注意在这里要补一个条件, 所以会出现只剩下横或者只剩下列的情况要规避
+                for c in range(right, left - 1, -1):  # right -> left
                     result.append(matrix[bot][c])
             bot -= 1
-            for d in range(bot, top - 1, -1):
-                if left <= right:  # 同上
+
+            if left <= right:  # 同上
+                for d in range(bot, top - 1, -1):  #
                     result.append(matrix[d][left])
             left += 1
 
@@ -140,9 +148,17 @@ if __name__ == "__main__":
     assert testCase.spiralOrder(sample_2) == [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7], "Example 2"
 
     sample_3 = [
-        [1,2],
-        [3,4]
+        [1, 2],
+        [3, 4]
     ]
-    assert testCase.spiralOrder(sample_3) == [1,2,4,3], "Example 3"
+    assert testCase.spiralOrder(sample_3) == [1, 2, 4, 3], "Example 3"
+
+    sample_4 = [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+        [7, 8]
+    ]
+    assert testCase.spiralOrder(sample_4) == [1, 2, 4, 6, 8, 7, 5, 3], "Extra"
 
     print("all passed")
