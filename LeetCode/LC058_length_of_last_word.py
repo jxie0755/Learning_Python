1f1682b8facbcf37916d788612b998912c3eb948
jxie0755 <jxie0755@users.noreply.github.com>
2020-11-14 22:09:49 -0500

    Code Review LC058_length_of_last_word.py

--- LeetCode/LC058_length_of_last_word.py
+++ LeetCode/LC058_length_of_last_word.py
@@ -10,6 +10,8 @@ Note:
     A word is defined as a character sequence consists of non-space characters only.
 """
 
+import re
+
 class Solution_A:
     def lengthOfLastWord(self, s: str) -> int:
         """
@@ -30,14 +32,47 @@ class Solution_B:
             if v.isspace():  # to avoid end with a " "
                 if cnt != 0:  # force to move down from the end " ",
                     break  # but break at next " "
+                else:
+                    pass
             else:
                 cnt += 1
         return cnt
+
+
+class Solution_C:
+    def lengthOfLastWord(self, s: str) -> int:
+        """
+        Use regex match
+        """
+        if not s:
+            return 0
+
+        raw_pattern = r"(.*\s|\.*)([A-Za-z]*\b)"
+        # 2 situations
+        # whatever + a space + word (some other thing, but then must be separated by at least a space)
+        # whatever + word (just spaces)
+
+        # use \b to have clear cut on words at the end to avoid suffix spaces
+
+        match = re.search(raw_pattern, s)
+        if match:
+            return len(match.group(2))
+        else:
+            return 0
+
 
 
 if __name__ == "__main__":
     testCase = Solution_B()
     assert testCase.lengthOfLastWord("") == 0, "Edge 1"
     assert testCase.lengthOfLastWord(" ") == 0, "Edge 2"
+
     assert testCase.lengthOfLastWord("Hello World") == 5, "Regular"
+
+    assert testCase.lengthOfLastWord("Today is a nice day") == 3, "Extra 1"
+    assert testCase.lengthOfLastWord("a") == 1, "Extra 2"
+    assert testCase.lengthOfLastWord(" a") == 1, "Extra 3"
+    assert testCase.lengthOfLastWord("  a") == 1, "Extra 4"
+    assert testCase.lengthOfLastWord("aaaa ") == 4, "Extra 5"
+
     print("all passed")
