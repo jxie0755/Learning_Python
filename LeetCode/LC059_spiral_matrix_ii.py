457beabcf60a979ff8af7b500837a6f93996b651
jxie0755 <jxie0755@users.noreply.github.com>
2020-11-16 23:07:36 -0500

    Learn spiral matrix

--- LeetCode/LC059_spiral_matrix_ii.py
+++ LeetCode/LC059_spiral_matrix_ii.py
@@ -110,10 +110,10 @@ if __name__ == "__main__":
     ], "Extra 1"
 
     assert testCase.generateMatrix(4) == [
-        [1, 2, 3, 4],
+        [1,  2,  3,  4],
         [12, 13, 14, 5],
         [11, 16, 15, 6],
-        [10, 9, 8, 7]
+        [10, 9,  8,  7]
     ], "Extra 2"
 
     print("all passed")
