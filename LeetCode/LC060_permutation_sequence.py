c1d2b526cc6a1c3f92ad38be9a9ab3771d064e2c
jxie0755 <jxie0755@users.noreply.github.com>
2020-11-21 15:32:36 -0500

    Code Review

--- LeetCode/LC060_permutation_sequence.py
+++ LeetCode/LC060_permutation_sequence.py
@@ -5,12 +5,12 @@ Medium
 
 The set [1,2,3,...,n] contains a total of n! unique permutations.
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
-1. "123"
-2. "132"
-3. "213"
-4. "231"
-5. "312"
-6. "321"
+1 -> "123"
+2 -> "132"
+3 -> "213"
+4 -> "231"
+5 -> "312"
+6 -> "321"
 
 Given n and k, return the kth permutation sequence.
 
