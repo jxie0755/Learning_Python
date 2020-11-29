c922ef7531e19746a85ac8b2855217c4b43a7bba
jxie0755 <jxie0755@users.noreply.github.com>
2020-11-29 16:47:45 -0500

    Code Review LC062_unique_paths.py

--- LeetCode/LC062_unique_paths.py
+++ LeetCode/LC062_unique_paths.py
@@ -18,10 +18,13 @@ class Solution_A:
         """
         The best direct math calculation
         This is the same as ProjectEuler p015 lattice paths
-        Use combination method: Combination pick r out of n : n! // r! // (n-r)!
+        Use combination method: Combination pick r out of x : x! // (x! * (x-r)!)
         """
-        total = m + n - 2
-        return factorial(total) // (factorial(total - (n - 1)) * factorial(n - 1))
+        total = m + n - 2 # actually m-1 + n-1, as path is node-1
+
+        # picke m-1 steps out of total steps, to go right
+        # this mathmetically equals to pick n-1 steps out of total steps
+        return factorial(total) // (factorial(m-1) * factorial(n - 1))
 
 
 class Solution_B:
@@ -33,13 +36,16 @@ class Solution_B:
 
         def uniquePaths_recur_mem(m: int, n: int) -> int:
             """recursion helper"""
-            if m <= 1 or n <= 1:
+            if m == 1 or n == 1: # end case, when there is only one direction to go
                 return 1
             elif (m, n) in hmp:
                 return hmp[(m, n)]
             else:
+
+                # branch recursion, the sum of paths of two directions
                 result = uniquePaths_recur_mem(m-1, n) + uniquePaths_recur_mem(m, n-1)
-                hmp[(m, n)] = result
+
+                hmp[(m, n)] = result # record the result to avoid repeating recursion
                 return result
 
         return uniquePaths_recur_mem(m, n)
@@ -51,18 +57,16 @@ class Solution_C:
         Get last grid value by adding the value of the neighbors from up and left
         This can avoid large number factorial calculation
         """
-        grid = [[1] * m for _ in range(n)]
+        grid = [[1] * m for _ in range(n)] # m*n grid with every element value = 1
         for i in range(n):
             for j in range(m):
-                if i != 0 and j != 0:
+                if i != 0 and j != 0: # avoid edge element as there can be only one way to reach, already set at 1
                     grid[i][j] = grid[i-1][j] + grid[i][j-1]
         return grid[n-1][m-1]
 
 
-
-
 if __name__ == "__main__":
-    testCase = Solution_C()
+    testCase = Solution_A()
     assert testCase.uniquePaths(1, 1) == 1, "Edge 1"
     assert testCase.uniquePaths(4, 1) == 1, "Edge 2"
     assert testCase.uniquePaths(1, 4) == 1, "Edge 3"
