73b43ba0e06f737e97019c7018fc5c8aa60da2d9
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-01 20:03:46 -0500

    Code Review LC078_subsets.py

--- LeetCode/LC078_subsets.py
+++ LeetCode/LC078_subsets.py
@@ -34,8 +34,9 @@ class Solution_A:
         else:
             result = []
             next_list = nums[:]
-            head = next_list.pop(0)
-            result += [[head] + com for com in self.combinationSolo(next_list, k - 1)] + self.combinationSolo(nums[1:], k)
+            tail = next_list.pop()
+            result += self.combinationSolo(nums[:len(nums)-1], k)
+            result += [com + [tail] for com in self.combinationSolo(next_list, k - 1)]
             return result
 
 if __name__ == "__main__":
