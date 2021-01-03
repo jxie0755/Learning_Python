bcdf25266877d4e67a7eee933709a051012df1b0
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-03 13:16:26 -0500

    Code Review LC079_word_search.py

--- LeetCode/LC079_word_search.py
+++ LeetCode/LC079_word_search.py
@@ -8,6 +8,7 @@ The word can be constructed from letters of sequentially adjacent cell, where "a
 """
 
 from typing import *
+from copy import deepcopy
 
 class Solution_A:
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -16,89 +17,84 @@ class Solution_A:
         This is still an brutal force method to check every route until find the correct one.
         Exceeded max time limit when case is long.
         """
-
-        row, col = len(board), len(board[0])
-
-        for coor in [(x, y) for x in range(row) for y in range(col)]:
-            if self.finder(board, word, 0, coor, set()):
-                return True
+        for row in range(0, len(board)):
+            for col in range(0, len(board[0])):
+                board_copy = deepcopy(board)
+                if self.find(board_copy, word, row, col):
+                    return True
         return False
 
-    def finder(self, board: List[List[str]], word: str, idx: int, coor: Tuple[int, int], prev: Set[Tuple[int]]):
+    def find(self, board, word, row, col) -> bool:
         """
         A helper function to find whether a word can be found in the matrix
-        需要使用哈希set来记忆之前走过哪些坐标
+        Use dupplicate board to track previous change and avoid conflict in split recursion
         """
-
-        N = len(word)
-        x, y = coor[0], coor[1]
-        row, col = len(board), len(board[0])  # 长宽范围
-
-        if idx == N:  # 如果走完整个word, 就是可以找到
+        if len(word) == 0:
             return True
-        elif not 0 <= x < row or not 0 <= y < col:  # 确保坐标位于board内部
+        elif row < 0 or row == len(board):  # Edge breaking
             return False
-        elif coor in prev:  # 如果坐标之前被算入过则不行
+        elif col < 0 or col == len(board[0]):  # Edge breaking
             return False
-        elif board[x][y] != word[idx]:  # 不符合单词
+        elif board[row][col] is None:
+            return False
+        elif board[row][col] != word[0]:  # miss-matching
             return False
         else:
-            new_prev = prev.copy()  # 复制一份为递归作准备
-            new_prev.add(coor)  # 加入字典以防将来被再次算入
-
-            up = (x - 1, y)
-            down = (x + 1, y)
-            left = (x, y - 1)
-            right = (x, y + 1)
-
-            # THIS IS WRONG! This will cost maximum depth
-            # result = any([self.finder(board, word, idx + 1, new_coor, new_prev) for new_coor in [up, down, left, right]])
-
-            # THIS is correct, using or instead of any
-            result = self.finder(board, word, idx + 1, up, new_prev) or \
-                   self.finder(board, word, idx + 1, down, new_prev) or \
-                   self.finder(board, word, idx + 1, left, new_prev) or \
-                   self.finder(board, word, idx + 1, right, new_prev)
-
-            return result
+            board[row][col] = None
+            return self.find(board, word[1:], row - 1, col) or \
+                   self.find(board, word[1:], row + 1, col) or \
+                   self.find(board, word[1:], row, col - 1) or \
+                   self.find(board, word[1:], row, col + 1)
 
 
 class Solution_STD:
 
     def exist(self, board: List[List[str]], word: str) -> bool:
-        visited = [[False for j in range(len(board[0]))] for i in range(len(board))]
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self.existRecu(board, word, 0, i, j, visited):
+        # This is the key to solve, use a visitied table to record path
+        visited = [[False for col in range(len(board[0]))] for row in range(len(board))]
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if self.existRecu(board, visited, word, row, col):
                     return True
 
         return False
 
-    def existRecu(self, board: List[List[str]], word: str, cur: int, i: int, j: int, visited: List[List[bool]]):
+    def existRecu(self, board: List[List[str]], visited: List[List[bool]], word: str, row: int, col: int):
         """
         The general idea is almost the same, the key is using visitied table to record path
         This saves space and time
         """
-        if cur == len(word):
+        if len(word) == 0: # finished going through all letters in the word
             return True
 
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or visited[i][j] or board[i][j] != word[cur]:
+        elif row < 0 or row >= len(board) or col < 0 or col >= len(board[0]): # edge breaking
             return False
 
-        visited[i][j] = True
+        elif visited[row][col]: # already visisted
+            return False
 
-        result = self.existRecu(board, word, cur + 1, i + 1, j, visited) or \
-                 self.existRecu(board, word, cur + 1, i - 1, j, visited) or \
-                 self.existRecu(board, word, cur + 1, i, j + 1, visited) or \
-                 self.existRecu(board, word, cur + 1, i, j - 1, visited)
-        visited[i][j] = False
+        elif board[row][col] != word[0]: # miss match letter
+            return False
 
-        return result
+        else:
+            # Label visisted if letter match
+            visited[row][col] = True
+
+            result = self.existRecu(board, visited, word[1:], row + 1, col) or \
+                     self.existRecu(board, visited, word[1:], row - 1, col) or \
+                     self.existRecu(board, visited, word[1:], row, col + 1) or \
+                     self.existRecu(board, visited, word[1:], row, col - 1)
+
+            # VERY IMPORT STEP to set the memory back avoid conflit in checking next [row, col]
+            visited[row][col] = False
+
+            return result
 
 
 if __name__ == "__main__":
-    testCase = Solution_A()
+    testCase = Solution_STD()
     board = [
         ["A", "B", "C", "E"],
         ["S", "F", "C", "S"],
