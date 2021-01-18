5a42c42ea5814b6625c824409c08a1dfdfff248e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-01-18 14:27:43 -0500

    Code Review

--- LeetCode/LC088_merge_sorted_array.py
+++ LeetCode/LC088_merge_sorted_array.py
@@ -10,11 +10,6 @@ Note:
 The number of elements initialized in nums1 and nums2 are m and n respectively.
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
 
-Example:
-nums1 = [1,2,3,0,0,0], m = 3
-nums2 = [2,5,6],       n = 3
-Output: [1,2,2,3,5,6]
-
 Key is to modified nums1, not returning a new list
 """
 
@@ -23,11 +18,7 @@ from typing import *
 class Solution_A:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
+
         """
         cur = 0
         tmp_n = 0
