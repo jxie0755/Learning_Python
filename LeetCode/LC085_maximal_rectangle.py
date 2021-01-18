b06c71f5209636cf23df90f3299492055d1c3761
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-01-18 14:23:57 -0500

    Update code style

--- LeetCode/LC085_maximal_rectangle.py
+++ LeetCode/LC085_maximal_rectangle.py
@@ -3,7 +3,6 @@ https://leetcode.com/problems/maximal-rectangle/
 P085 Maximal Rectangle
 Hard
 
-
 Given a 2D binary matrix filled with 0"s and 1"s, find the largest rectangle containing only 1's and return its area.
 """
 
--- LeetCode/LC088_merge_sorted_array.py
+++ LeetCode/LC088_merge_sorted_array.py
@@ -1,22 +1,27 @@
-# p088 Merge Sorted Array
-# Easy
+"""
+https://leetcode.com/problems/merge-sorted-array/
+p088 Merge Sorted Array
+Easy
 
 
-# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-# Note:
-# The number of elements initialized in nums1 and nums2 are m and n respectively.
-# You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Note:
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
 
-# Example:
-# nums1 = [1,2,3,0,0,0], m = 3
-# nums2 = [2,5,6],       n = 3
-# Output: [1,2,2,3,5,6]
+Example:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+Output: [1,2,2,3,5,6]
 
-# Key is to modified nums1, not returning a new list
+Key is to modified nums1, not returning a new list
+"""
 
-class Solution:
-    def merge(self, nums1, m, nums2, n):
+from typing import *
+
+class Solution_A:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         :type nums1: List[int]
         :type m: int
@@ -35,54 +40,56 @@ class Solution:
 
 
 if __name__ == "__main__":
+    testCase = Solution_A()
+
     nums1 = [1]
     nums2 = []
-    Solution().merge(nums1, 1, nums2, 0)
+    testCase.merge(nums1, 1, nums2, 0)
     assert nums1 == [1], "T1"
 
     nums1 = [0]
     nums2 = [1]
-    Solution().merge(nums1, 0, nums2, 1)
+    testCase.merge(nums1, 0, nums2, 1)
     assert nums1 == [1], "T2"
 
     nums1 = [1, 2, 3, 0, 0, 0]
     nums2 = [2, 5, 6]
-    Solution().merge(nums1, 3, nums2, 3)
+    testCase.merge(nums1, 3, nums2, 3)
     assert nums1 == [1, 2, 2, 3, 5, 6], "T3"
 
     nums1 = [1, 5, 7, 0, 0, 0]
     nums2 = [2, 4, 10]
-    Solution().merge(nums1, 3, nums2, 3)
+    testCase.merge(nums1, 3, nums2, 3)
     assert nums1 == [1, 2, 4, 5, 7, 10], "T4"
 
     nums1 = [8, 8, 8, 0, 0, 0]
     nums2 = [1, 2, 3]
-    Solution().merge(nums1, 3, nums2, 3)
+    testCase.merge(nums1, 3, nums2, 3)
     assert nums1 == [1, 2, 3, 8, 8, 8], "T5"
 
     nums1 = [1, 0, 0, 0]
     nums2 = [5, 5, 5]
-    Solution().merge(nums1, 1, nums2, 3)
+    testCase.merge(nums1, 1, nums2, 3)
     assert nums1 == [1, 5, 5, 5], "T6"
 
     nums1 = [1, 2, 4, 5, 6, 0]
     nums2 = [3]
-    Solution().merge(nums1, 5, nums2, 1)
+    testCase.merge(nums1, 5, nums2, 1)
     assert nums1 == [1, 2, 3, 4, 5, 6], "T7"
 
     nums1 = [4, 0, 0, 0, 0, 0]
     nums2 = [1, 2, 3, 5, 6]
-    Solution().merge(nums1, 1, nums2, 5)
+    testCase.merge(nums1, 1, nums2, 5)
     assert nums1 == [1, 2, 3, 4, 5, 6], "T8"
 
     nums1 = [4, 0, 0, 0, 0, 0, 0]
     nums2 = [1, 2, 3, 5, 6]
-    Solution().merge(nums1, 1, nums2, 5)
+    testCase.merge(nums1, 1, nums2, 5)
     assert nums1 == [1, 2, 3, 4, 5, 6, 0], "T9"
 
     nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0]
     nums2 = [1, 2, 2]
-    Solution().merge(nums1, 6, nums2, 3)
+    testCase.merge(nums1, 6, nums2, 3)
     assert nums1 == [-1, 0, 0, 1, 2, 2, 3, 3, 3], "T10"
 
     print("All passed")
