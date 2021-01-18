60e9f248beaacfccc1843a5a054fc5cac2445912
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-01-18 14:23:26 -0500

    Code style update

--- LeetCode/LC085_maximal_rectangle.py
+++ LeetCode/LC085_maximal_rectangle.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/maximal-rectangle/
 P085 Maximal Rectangle
 Hard
 
+
 Given a 2D binary matrix filled with 0"s and 1"s, find the largest rectangle containing only 1's and return its area.
 """
 
