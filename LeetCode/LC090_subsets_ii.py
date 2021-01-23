c80c0aeb3c5ce46e313145f955ea3871e0b10581
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-22 21:28:32 -0500

    Code Review LC090_subsets_ii.py

--- LeetCode/LC090_subsets_ii.py
+++ LeetCode/LC090_subsets_ii.py
@@ -15,6 +15,7 @@ class Solution_A:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         """
         With the help from the combinationSolo from Leetcode P077
+        use tuple and set to remove repeat and convert back to list
         """
         result = []
         for i in range(0, len(nums) + 1):
@@ -41,6 +42,45 @@ class Solution_A:
             tail = next_list.pop()
             result += self.combinationSolo(nums[:len(nums) - 1], k)
             result += [com + (tail,) for com in self.combinationSolo(next_list, k - 1)]
+            return result
+
+class Solution_B:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+        With the help from the combinationSolo from Leetcode P077
+        no need to use tuple, direct use list and remove duplicated on the run
+        """
+        result = []
+        for i in range(0, len(nums) + 1):
+            result += self.combinationSolo(nums, i)
+        return result
+
+    def combinationSolo(self, nums: List[int], k: int) -> List[List[int]]:
+        """
+        Helper for A1, refer to LC077, modify to output in none repeated elements
+        """
+        nums = sorted(nums)  # make sure the nums is sorted to begin with
+
+        if k == 0:
+            return [[]]
+        elif k == len(nums):
+            return [nums]
+        elif k == 1:
+            result = []
+            for i in nums:  # remove repeat
+                if [i] not in result:
+                    result.append([i])
+            return result
+        else:
+            result = []
+            next_list = nums[:]
+            tail = next_list.pop()
+            result += self.combinationSolo(nums[:len(nums) - 1], k)
+
+            # remove repeat
+            for _ in [com + [tail] for com in self.combinationSolo(next_list, k - 1)]:
+                if _ not in result:
+                    result.append(_)
             return result
 
 
