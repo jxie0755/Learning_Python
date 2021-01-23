2428c22237a05c342e12f544f81be60038077bee
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-23 16:46:27 -0500

    Code Review

--- whiteboard.py
+++ whiteboard.py
@@ -11,25 +11,27 @@
 # Given a non-empty string containing only digits, determine the total number of ways to decode it.
 
 
-class Solution:
-    # This will now work, but exceeded max timei limit
-    # Recursion depth is not the problem
+class Solution_A:
     def numDecodings(self, s: str) -> int:
         pass
 
 
+
+
 if __name__ == "__main__":
-    assert Solution().numDecodings("0") == 0, "Edge 1"
-    assert Solution().numDecodings("00") == 0, "Edge 2"
-    assert Solution().numDecodings("230") == 0, "Edge 3"
-    assert Solution().numDecodings("1") == 1, "Edge 4"
+    testCase = Solution_A()
 
-    assert Solution().numDecodings("12") == 2, "Example 1"
-    assert Solution().numDecodings("226") == 3, "Example 2"
+    assert testCase.numDecodings("0") == 0, "Edge 1"
+    assert testCase.numDecodings("00") == 0, "Edge 2"
+    assert testCase.numDecodings("230") == 0, "Edge 3"
+    assert testCase.numDecodings("1") == 1, "Edge 4"
 
-    assert Solution().numDecodings("227") == 2, "Additional 1"
-    assert Solution().numDecodings("611") == 2, "Additional 2"
+    assert testCase.numDecodings("12") == 2, "Example 1"
+    assert testCase.numDecodings("226") == 3, "Example 2"
+
+    assert testCase.numDecodings("227") == 2, "Additional 1"
+    assert testCase.numDecodings("611") == 2, "Additional 2"
     long = "9371597631128776948387197132267188677349946742344217846154932859125134924241649584251978418763151253"
-    print(Solution().numDecodings(long))
+    print(testCase.numDecodings(long))
 
     print("All passed")
