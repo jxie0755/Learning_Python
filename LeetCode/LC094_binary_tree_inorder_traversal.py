6ad9c599c20ab9fb67d986d4cd5936d98be2f151
jxie0755 <jxie0755@users.noreply.github.com>
2021-01-30 22:08:17 -0500

    Code Review LC094_binary_tree_inorder_traversal.py

--- LeetCode/LC094_binary_tree_inorder_traversal.py
+++ LeetCode/LC094_binary_tree_inorder_traversal.py
@@ -1,10 +1,12 @@
-# P094 Binary Tree Inorder Traversal
-# Medium
+"""
+P094 Binary Tree Inorder Traversal
+Medium
 
 
-# Given a binary tree, return the inorder traversal of its nodes' values.
-# 中序遍历就是二叉树的平面投影,从左到右
-# Follow up: Recursive solution is trivial, could you do it iteratively?
+Given a binary tree, return the inorder traversal of its nodes' values.
+中序遍历就是二叉树的平面投影,从左到右
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
 
 
 from typing import *
@@ -74,13 +76,16 @@ class Solution_B(object):
 
 
 if __name__ == "__main__":
+    testCase = Solution()
+
+
     t0 = None
-    # assert Solution().inorderTraversal(t0) == []
+    assert testCase.inorderTraversal(t0) == []
 
     t1 = genTree([
         1,
         None, 2,
         None, None, 3, None])
-    assert Solution().inorderTraversal(t1) == [1, 3, 2]
+    assert testCase.inorderTraversal(t1) == [1, 3, 2]
 
     print("All passed")
