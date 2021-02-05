0dd4b0555eeebfd935f0f67bbf6d09102aa3d7e9
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-05 14:10:43 -0500

    Code Review

--- whiteboard.py
+++ whiteboard.py
@@ -113,15 +113,4 @@ for i in range(1, 3):
 #
 #     print("All passed")
 
-a = [[1, 1],
-[1, 2],
-[1, 3],
-[2, 1],
-[2, 2],
-[2, 3],
-[3, 1],
-[3, 2],
-[3, 3]]
-
-for r in a:
-    print(r)
+print([[x,y] for x in range(1,4) for y in range(1,4)])
