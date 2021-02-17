2f8205350bc199f8e6e0d190b85a33ae35323960
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-17 11:45:50 -0500

    Code Review LC105_construct_binary_tree_from_preorder_and_inorder_traversal.py

--- LeetCode/LC105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ LeetCode/LC105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,17 +1,20 @@
-# P105 Construct Binary Tree from Preorder and Inorder Traversal
-# Medium
+"""
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+P105 Construct Binary Tree from Preorder and Inorder Traversal
+Medium
 
 
-# Given preorder and inorder traversal of a tree, construct the binary tree.
+Given preorder and inorder traversal of a tree, construct the binary tree.
 
-# Note:
-# You may assume that duplicates do not exist in the tree.
+Note:
+You may assume that duplicates do not exist in the tree.
+"""
 
 
 from a0_TreeNode import *
 
 
-class Solution:
+class Solution_A1:
     # This will pass but exceed max time limit
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         if not preorder:
@@ -45,7 +48,7 @@ class Solution:
         return root
 
 
-class Solution:
+class Solution_A2:
     # Same method idea as above but build a hash table to store the index of each value
     # Passed, but still very slow
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
@@ -78,7 +81,7 @@ class Solution:
         return helper(preorder)
 
 
-class Solution(object):
+class Solution_STD:
     # STD ans
     # @param preorder, a list of integers
     # @param inorder, a list of integers
@@ -100,9 +103,13 @@ class Solution(object):
 
 
 if __name__ == "__main__":
-    assert not Solution().buildTree([], []), "Edge 0"
-    assert Solution().buildTree([1], [1]) == genTree([1]), "Edge 1"
-    assert Solution().buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]) == genTree([
+    testCase = Solution_A2()
+
+    assert not testCase.buildTree([], []), "Edge 0"
+
+    assert testCase.buildTree([1], [1]) == genTree([1]), "Edge 1"
+
+    assert testCase.buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]) == genTree([
         3,
         9, 20,
         None, None, 15, 7
