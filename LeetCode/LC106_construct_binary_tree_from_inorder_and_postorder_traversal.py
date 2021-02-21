b10e210ecfb5f86a5da4a4477e16851b69ae1d57
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-21 13:01:04 -0500

    Code Review LC106_construct_binary_tree_from_inorder_and_postorder_traversal.py

--- LeetCode/LC106_construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ LeetCode/LC106_construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -1,26 +1,23 @@
-# P106 Construct Binary Tree from Inorder and Postorder Traversal
-# Medium
+"""
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+P106 Construct Binary Tree from Inorder and Postorder Traversal
+Medium
 
+Given inorder and postorder traversal of a tree, construct the binary tree.
 
-# Given inorder and postorder traversal of a tree, construct the binary tree.
+Note:
+You may assume that duplicates do not exist in the tree.
+"""
 
-# Note:
-# You may assume that duplicates do not exist in the tree.
-
-
-from typing import *
 
 from typing import *
 from a0_TreeNode import *
 
 
-class Solution:
-    # Basically the same idea of my own slow version in leetcode P105
-    def buildTree(self, inorder, postorder):
+class Solution_A:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         """
-        :type inorder: List[int]
-        :type postorder: List[int]
-        :rtype: TreeNode
+        Basically the same idea of my own slow version in leetcode P105
         """
         hmp = dict()
         for idx, val in enumerate(inorder):
@@ -48,12 +45,11 @@ class Solution:
         return helper(postorder)
 
 
-class Solution(object):
-    # STD ans
-    # @param inorder, a list of integers
-    # @param postorder, a list of integers
-    # @return a tree node
-    def buildTree(self, inorder, postorder):
+class Solution_STD:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        """
+
+        """
         lookup = {}
         for i, num in enumerate(inorder):
             lookup[num] = i
@@ -70,9 +66,13 @@ class Solution(object):
 
 
 if __name__ == "__main__":
-    assert not Solution().buildTree([], []), "Edge 0"
-    assert Solution().buildTree([1], [1]) == genTree([1]), "Edge 1"
-    assert Solution().buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]) == genTree([
+    testCase = Solution_STD()
+
+    assert not testCase.buildTree([], []), "Edge 0"
+
+    assert testCase.buildTree([1], [1]) == genTree([1]), "Edge 1"
+
+    assert testCase.buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]) == genTree([
         3,
         9, 20,
         None, None, 15, 7
