34ac9e2398faf1fb96d3e0dda037ce0f04cc36ec
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-23 13:29:56 -0500

    Code Review LC107_binary_tree_level_order_traversal_ii.py

--- LeetCode/LC107_binary_tree_level_order_traversal_ii.py
+++ LeetCode/LC107_binary_tree_level_order_traversal_ii.py
@@ -13,15 +13,9 @@ class Solution_A:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         """
         Show the tree layer by layer from top to bottom
-        This will omit None nodes
         Refer to LC102
         Use the levelOrderTraversal operation but the maximum time limit is exceeded
         """
-        if not root:
-            return []
-        elif not root.left and not root.right:
-            return [[root.val]]
-
         return self.levelOrderTraversal(root)[::-1]
 
     def levelOrderTraversal(self, root: TreeNode) -> List[List[int]]:
@@ -48,22 +42,12 @@ class Solution_A:
 
         return result
 
-testCase = Solution_A()
-A = genTree([
-    0,
-    2, 4,
-    1, None, 3, -1,
-    5, 1, None, None, 6, None, 8, None
-])
-testCase.levelOrderBottom(A)
-
 
 class Solution_STD:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         """
         STD ans
-        Almost the same way as I tried, but it does not add None to next layer
-        Use i.left and i.right to screen each node in current layer and only add the branch to next layer if the branch exist
+        Basically combine the helper function into version A
         """
         if root is None:
             return []
@@ -72,8 +56,8 @@ class Solution_STD:
         while current:
             next_level, vals = [], []
             for node in current:
-                if node:
-                    vals.append(node.val)
+                vals.append(node.val)
+
                 if node.left:
                     next_level.append(node.left)
                 if node.right:
@@ -84,41 +68,9 @@ class Solution_STD:
         return result[::-1]
 
 
-class Solution_STD_2:
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
-        """
-        A more structed stack method accoding to STD method
-        First build up the stack all the way to the bottom to get nodes at each layer
-        Then extract the values of each node in every layer
-        """
-        if root is None:
-            return []
-
-        stack = [[root]]
-        current = [root]
-        while current:
-            next_level = []
-            for node in current:
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            stack.append(next_level)
-            current = next_level
-
-        result = []
-        for i in stack[-2::-1]:
-            vals = []
-            for node in i:
-                if node:
-                    vals.append(node.val)
-            result.append(vals)
-
-        return result
-
 
 if __name__ == "__main__":
-    testCase = Solution_A()
+    testCase = Solution_STD()
 
     T0 = None
     assert testCase.levelOrderBottom(T0) == [], "Edge 0"
