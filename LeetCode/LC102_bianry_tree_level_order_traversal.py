f61909bd98421ed8ac7fb3faf97e89a8101158eb
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-23 13:24:27 -0500

    Code Review LC107_binary_tree_level_order_traversal_ii.py

--- LeetCode/LC102_bianry_tree_level_order_traversal.py
+++ LeetCode/LC102_bianry_tree_level_order_traversal.py
@@ -26,7 +26,7 @@ class Solution_A:
             for node in layer:
                 node_vals.append(node.val)
 
-                # omit by checking left tand right None
+                # omit None by checking left tand right None
                 if node.left:
                     next_layer.append(node.left)
                 if node.right:
--- LeetCode/LC107_binary_tree_level_order_traversal_ii.py
+++ LeetCode/LC107_binary_tree_level_order_traversal_ii.py
@@ -1,19 +1,33 @@
-# P107 Binary Tree Level Order Traversal II
-# Easy
+"""
+https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+P107 Binary Tree Level Order Traversal II
+Easy
 
-
-# Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+"""
 
 from a0_TreeNode import *
 
 
-class Solution:
-    # Use the levelOrderTraversal operation but the maximum time limit is exceeded
-    def levelOrderTraversal(self, root):
+class Solution_A:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         """
         Show the tree layer by layer from top to bottom
         This will omit None nodes
         Refer to LC102
+        Use the levelOrderTraversal operation but the maximum time limit is exceeded
+        """
+        if not root:
+            return []
+        elif not root.left and not root.right:
+            return [[root.val]]
+
+        return self.levelOrderTraversal(root)[::-1]
+
+    def levelOrderTraversal(self, root: TreeNode) -> List[List[int]]:
+        """
+        Helper function from LC102
+        Omit None nodes
         """
         if root is None:
             return []
@@ -23,6 +37,8 @@ class Solution:
             next_level, vals = [], []
             for node in current:
                 vals.append(node.val)
+
+                # omit None by checking left tand right None
                 if node.left:
                     next_level.append(node.left)
                 if node.right:
@@ -32,27 +48,22 @@ class Solution:
 
         return result
 
-    def levelOrderBottom(self, root: TreeNode):
-        if not root:
-            return []
-        elif not root.left and not root.right:
-            return [[root.val]]
-
-        result = []
-        for i in self.levelOrderTraversal(root)[::-1]:
-            filtered = list(filter(lambda x: x is not None, i))  # Be careful with the t.val == 0
-            result.append(filtered)
-        return result[1:]
+testCase = Solution_A()
+A = genTree([
+    0,
+    2, 4,
+    1, None, 3, -1,
+    5, 1, None, None, 6, None, 8, None
+])
+testCase.levelOrderBottom(A)
 
 
-class Solution:
-    def levelOrderBottom(self, root):
-        # STD ans
-        # Almost the same way as I tried, but it does not add None to next layer
-        # Use i.left and i.right to screen each node in current layer and only add the branch to next layer if the branch exist
+class Solution_STD:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         """
-        :type root: TreeNode
-        :rtype: List[List[int]]
+        STD ans
+        Almost the same way as I tried, but it does not add None to next layer
+        Use i.left and i.right to screen each node in current layer and only add the branch to next layer if the branch exist
         """
         if root is None:
             return []
@@ -73,14 +84,12 @@ class Solution:
         return result[::-1]
 
 
-class Solution:
-    def levelOrderBottom(self, root):
-        # A more structed stack method accoding to last method
-        # First build up the stack all the way to the bottom to get nodes at each layer
-        # Then extract the values of each node in every layer
+class Solution_STD_2:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         """
-        :type root: TreeNode
-        :rtype: List[List[int]]
+        A more structed stack method accoding to STD method
+        First build up the stack all the way to the bottom to get nodes at each layer
+        Then extract the values of each node in every layer
         """
         if root is None:
             return []
@@ -109,29 +118,37 @@ class Solution:
 
 
 if __name__ == "__main__":
-    A = None
-    assert Solution().levelOrderBottom(A) == [], "Edge 0"
+    testCase = Solution_A()
 
-    A = genTree([1])
-    assert Solution().levelOrderBottom(A) == [[1]], "Edge 1"
+    T0 = None
+    assert testCase.levelOrderBottom(T0) == [], "Edge 0"
 
-    A = genTree([3, 9, 20, None, None, 15, 7])
-    assert Solution().levelOrderBottom(A) == [
+    T1 = genTree([1])
+    assert testCase.levelOrderBottom(T1) == [[1]], "Edge 1"
+
+    T2 = genTree([
+        3,
+        9, 20,
+        None, None, 15, 7
+    ])
+
+    assert testCase.levelOrderBottom(T2) == [
         [15, 7],
         [9, 20],
         [3]
     ], "Example 1"
 
-    A = genTree([
+    T3 = genTree([
         0,
         2, 4,
         1, None, 3, -1,
         5, 1, None, None, 6, None, 8, None])
-    assert Solution().levelOrderBottom(A) == [
+
+    assert testCase.levelOrderBottom(T3) == [
         [5, 1, 6, 8],
         [1, 3, -1],
         [2, 4],
         [0]
-    ], "Additional"
+    ], "Additional 1"
 
     print("All passed")
