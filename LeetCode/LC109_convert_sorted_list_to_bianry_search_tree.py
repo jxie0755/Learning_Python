72336cc8e8bd5c9f4913bda8af37a4e873e892a6
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-02-26 10:57:43 -0500

    Code Review LC109_convert_sorted_list_to_bianry_search_tree.py

--- LeetCode/LC109_convert_sorted_list_to_bianry_search_tree.py
+++ LeetCode/LC109_convert_sorted_list_to_bianry_search_tree.py
@@ -1,61 +1,64 @@
-# P109 Convert Sorted List to Binary Search Tree
-# Medium
+"""
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+P109 Convert Sorted List to Binary Search Tree
+Medium
 
 
-# Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 
-# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+"""
 
 from typing import *
 from a0_TreeNode import *
 from a0_ListNode import *
 
 
-class Solution:
-    # Convert linked list to an arrayList then solve the problem like Leetcode P108
-
+class Solution_A:
     def sortedListToBST(self, head: ListNode) -> TreeNode:
+        """
+        Convert linked list to an arrayList then solve the problem like Leetcode P108
+        """
 
         if not head:
             return None
 
-        nums = []
+        numsList = []
         while head:
-            nums.append(head.val)
+            numsList.append(head.val)
             head = head.next
 
+        return self.sortedArrayToBST(numsList)
+
+    def sortedArrayToBST(self, nums: List[int]):
+        """
+        Helpfer functionfrom LC108
+        """
+        if not nums:
+            return None
+
         mid_idx = len(nums) // 2
-        mid = nums[mid_idx]
-        ans = TreeNode(mid)
+        mid_val = nums[mid_idx]
+        T = TreeNode(mid_val)
 
-        def helper(root, left_list, right_list):
-            if left_list:
-                left_mid_idx = len(left_list) // 2
-                left_val = left_list[left_mid_idx]
-                root.left = TreeNode(left_val)
-                LL, LR = left_list[:left_mid_idx], left_list[left_mid_idx + 1:]
-                helper(root.left, LL, LR)
+        T.left = self.sortedArrayToBST(nums[:mid_idx])
+        T.right = self.sortedArrayToBST(nums[mid_idx + 1:])
 
-            if right_list:
-                right_mid_idx = len(right_list) // 2
-                right_val = right_list[right_mid_idx]
-                root.right = TreeNode(right_val)
-                RL, RR = right_list[:right_mid_idx], right_list[right_mid_idx + 1:]
-                helper(root.right, RL, RR)
-
-        helper(ans, nums[:mid_idx], nums[mid_idx + 1:])
-        return ans
-
+        return T
 
 if __name__ == "__main__":
-    if __name__ == "__main__":
-        A = None
-        assert Solution().sortedListToBST(A) is None, "Edge 0"
+    testCase = Solution_A()
 
-        A = genNode([1])
-        assert Solution().sortedListToBST(A) == genTree([1]), "Edge 1"
+    A = None
+    assert testCase.sortedListToBST(A) is None, "Edge 0"
 
-        A = genNode([-10, -3, 0, 5, 9])
-        assert Solution().sortedListToBST(A) == genTree([0, -3, 9, -10, None, 5, None]), "Example 1"
+    A = genNode([1])
+    assert testCase.sortedListToBST(A) == genTree([1]), "Edge 1"
 
-        print("All passed")
+    A = genNode([-10, -3, 0, 5, 9])
+    assert testCase.sortedListToBST(A) == genTree([
+        0, -3,
+        9, -10, None, 5, None
+    ]), "Example 1"
+
+    print("All passed")
