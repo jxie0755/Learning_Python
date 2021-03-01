1e5e134be7dee10263d660f3e5b4fdc5dd9a216e
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-01 11:26:29 -0500

    Code Review LC112_path_sum.py

--- LeetCode/LC112_path_sum.py
+++ LeetCode/LC112_path_sum.py
@@ -1,15 +1,38 @@
-# P112 Path Sum
-# Easy
+"""
+https://leetcode.com/problems/path-sum/
+P112 Path Sum
+Easy
 
-# Given a binary tree and a sum,
-# determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+Given a binary tree and a sum,
+determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
-# Note: A leaf is a node with no children.
+Note: A leaf is a node with no children.
+"""
 
 from a0_TreeNode import *
 
 
-class Solution:
+class Solution_A:
+    def hasPathSum(self, root: TreeNode, target: int) -> bool:
+        """
+        Borrow the idea of allPath
+        """
+        def helper(root, cur=[]):
+            if not root:
+                return False
+
+            elif not root.left and not root.right:
+                cur.append(root.val)
+                return sum(cur) == target
+
+            else:
+                left_cur, right_cur = cur[:], cur[:]
+                left_cur.append(root.val)
+                right_cur.append(root.val)
+                return helper(root.left, left_cur) or helper(root.right, right_cur)
+
+        return helper(root)
+
     def allPath(self, root):
         """show all the paths in a non-empty root"""
         result = []
@@ -33,39 +56,34 @@ class Solution:
         helper(root)
         return result
 
-    def hasPathSum(self, root: TreeNode, target: int) -> bool:
-
-        # Borrow the idea of allPath
-        def helper(root, cur=[]):
-            if not root:
-                return False
-
-            elif not root.left and not root.right:
-                cur.append(root.val)
-                return sum(cur) == target
-
-            else:
-                left_cur, right_cur = cur[:], cur[:]
-                left_cur.append(root.val)
-                right_cur.append(root.val)
-                return helper(root.left, left_cur) or helper(root.right, right_cur)
-
-        return helper(root)
-
 
 if __name__ == "__main__":
-    A = None
-    assert not Solution().hasPathSum(A, 0), "Edge 0"
+    testCase = Solution_A()
 
-    A = genTree([1])
-    assert Solution().hasPathSum(A, 1), "Edge 1"
+    T0 = None
+    assert not testCase.hasPathSum(T0, 0), "Edge 0"
 
-    A = genTree([
+    T1 = genTree([1])
+    assert testCase.hasPathSum(T1, 1), "Edge 1"
+
+    T2 = genTree([
         5,
         4, 8,
         11, None, 13, 4,
         7, 2, None, None, None, None, None, 1
     ])
-    assert Solution().hasPathSum(A, 22), "Example 1"
+    assert testCase.hasPathSum(T2, 22), "Example 1, 5+4+11+2"
+
+    T3 = genTree([
+        1,
+        2, 3
+    ])
+    assert not testCase.hasPathSum(T3, 5), "Example 2"
+
+    T4 = genTree([
+        1,
+        2, None
+    ])
+    assert not testCase.hasPathSum(T4, 0), "Example 3"
 
     print("All passed")
