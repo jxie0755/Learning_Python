cf67bcd1ba980a7a49dc5c408c0c0d00a2a0cf50
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-04 19:34:16 -0500

    Code Review LC114_flatten_bianry_tree_to_linked_list.py

--- LeetCode/LC114_flatten_bianry_tree_to_linked_list.py
+++ LeetCode/LC114_flatten_bianry_tree_to_linked_list.py
@@ -1,18 +1,14 @@
-# P114 Flatten Binary Tree to Linked List
-# Medium
+"""
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+P114 Flatten Binary Tree to Linked List
+Medium
 
-# Given a binary tree, flatten it to a linked list in-place.
-
+Given a binary tree, flatten it to a linked list in-place.
+"""
 from a0_TreeNode import *
 
 
-class Solution:
-    def preorderTraversal(self, t):
-        """return a flat list of the binary tree including None"""
-        if not t:
-            return []  # skip none
-        return [t.val] + self.preorderTraversal(t.left) + self.preorderTraversal(t.right)
-
+class Solution_A:
     def flatten(self, root: TreeNode) -> None:
         """
         Do not return anything, modify root in-place instead.
@@ -27,36 +23,41 @@ class Solution:
                 root = new
                 i += 1
 
+    def preorderTraversal(self, t):
+        """return a flat list of the binary tree including None"""
+        if not t:
+            return []  # skip none
+        return [t.val] + self.preorderTraversal(t.left) + self.preorderTraversal(t.right)
+
 
 if __name__ == "__main__":
-    A = None
-    Solution().flatten(A)
-    assert not A, "Edge 0"
+    testCase = Solution_A()
 
-    A = TreeNode(0)
-    Solution().flatten(A)
-    assert A == A, "Edge 1"
+    T0 = None
+    testCase.flatten(T0)
+    assert not T0, "Edge 0"
 
-    A = TreeNode(1)
-    Solution().flatten(A)
-    assert A == A, "Edge 2"
+    T1 = TreeNode(0)
+    testCase.flatten(T1)
+    assert T1 == T1, "Edge 1"
 
-    A = genTree([
+    T2 = TreeNode(1)
+    testCase.flatten(T2)
+    assert T2 == T2, "Edge 11"
+
+    T3 = genTree([
         1,
         2, 5,
         3, 4, None, 6
     ])
-    Solution().flatten(A)
-    assert A == genTree([
+    testCase.flatten(T3)
+    assert T3 == genTree([
         1,
         None, 2,
         None, None, None, 3,
-        None, None, None, None,
-        None, None, None, 4,
-        None, None, None, None, None, None, None, None,
-        None, None, None, None, None, None, None, 5,
-        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 6
+        None, None, None, None,None, None, None, 4,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 5,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 6
     ]), "Example 1"
 
     print("All passed")
