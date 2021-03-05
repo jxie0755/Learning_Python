5d81f94fb6bee9d23c3fda73b84700900e6dc2d3
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-04 19:36:25 -0500

    Code Review LC114_flatten_bianry_tree_to_linked_list.py

--- LeetCode/LC114_flatten_bianry_tree_to_linked_list.py
+++ LeetCode/LC114_flatten_bianry_tree_to_linked_list.py
@@ -57,7 +57,8 @@ if __name__ == "__main__":
         None, None, None, 3,
         None, None, None, None,None, None, None, 4,
         None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 5,
-        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 6
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 6
     ]), "Example 1"
 
     print("All passed")
---
+++ whiteboard.py
@@ -0,0 +1,47 @@
+"""
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+P114 Flatten Binary Tree to Linked List
+Medium
+
+Given a binary tree, flatten it to a linked list in-place.
+"""
+from a0_TreeNode import *
+
+
+class Solution_A:
+    def flatten(self, root: TreeNode) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    testCase = Solution_A()
+
+    T0 = None
+    testCase.flatten(T0)
+    assert not T0, "Edge 0"
+
+    T1 = TreeNode(0)
+    testCase.flatten(T1)
+    assert T1 == T1, "Edge 1"
+
+    T2 = TreeNode(1)
+    testCase.flatten(T2)
+    assert T2 == T2, "Edge 11"
+
+    T3 = genTree([
+        1,
+        2, 5,
+        3, 4, None, 6
+    ])
+    testCase.flatten(T3)
+    assert T3 == genTree([
+        1,
+        None, 2,
+        None, None, None, 3,
+        None, None, None, None, None, None, None, 4,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 5,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+        None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 6
+    ]), "Example 1"
+
+    print("All passed")
