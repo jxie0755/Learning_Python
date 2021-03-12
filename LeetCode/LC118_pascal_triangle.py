e3ba4c6fbb549016a27f428f38674eb6cda8a51b
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-11 20:55:14 -0500

    Code Review LC118_pascal_triangle.py

--- LeetCode/LC118_pascal_triangle.py
+++ LeetCode/LC118_pascal_triangle.py
@@ -1,14 +1,37 @@
-# LC118 Pascal's triangle
-# Easy
+"""
+https://leetcode.com/problems/pascals-triangle/
+LC118 Pascal's triangle
+Easy
 
-# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+"""
 
+from typing import *
 
-class Solution:
-    def generate(self, numRows):
+class Solution_A1:
+    def generate(self, numRows: int) -> List[List[int]]:
         """
-        :type numRows: int
-        :rtype: List[List[int]]
+        a recursion version
+        Time limit exceeded
+        """
+        if numRows == 0:
+            return []
+        elif numRows == 1:
+            return self.generate(0) + [[1]]
+        elif numRows == 2:
+            return self.generate(1) + [[1, 1]]
+        else:
+            previous = self.generate(numRows - 1)[-1]
+            new = [1]
+            for i in range(len(previous) - 1):
+                new.append(previous[i] + previous[i + 1])
+            return self.generate(numRows - 1) + [new + [1]]
+
+
+class Solution_A2:
+    def generate(self, numRows: int) -> List[List[int]]:
+        """
+        Iteration method
         """
         if numRows == 0:
             return []
@@ -32,42 +55,26 @@ class Solution:
 
         return result
 
-    def generate2(self, numRows):
-        # a recursion version
-        # Time limit exceed
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        if numRows == 0:
-            return []
-        elif numRows == 1:
-            return self.generate2(0) + [[1]]
-        elif numRows == 2:
-            return self.generate2(1) + [[1, 1]]
-        else:
-            previous = self.generate2(numRows - 1)[-1]
-            new = [1]
-            for i in range(len(previous) - 1):
-                new.append(previous[i] + previous[i + 1])
-            return self.generate2(numRows - 1) + [new + [1]]
+
+
 
 
 if __name__ == "__main__":
-    assert Solution().generate(5) == [
-        [1],
-        [1, 1],
-        [1, 2, 1],
-        [1, 3, 3, 1],
-        [1, 4, 6, 4, 1]
-    ]
+    testCase = Solution_A1()
 
-    assert Solution().generate2(5) == [
+    assert testCase.generate(0) == [
+    ], "Edge 0"
+
+    assert testCase.generate(5) == [
         [1],
         [1, 1],
         [1, 2, 1],
         [1, 3, 3, 1],
         [1, 4, 6, 4, 1]
-    ]
+    ], "Example 1"
+
+    assert testCase.generate(1) == [
+        [1]
+    ], "Example 2"
 
     print("All passed")
