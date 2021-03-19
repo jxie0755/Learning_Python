1ff4f43dd505f2af388dd9e2fae872cac19a5f13
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-19 19:30:05 -0400

    Code Review LC121_best_time_to_buy_and_sell_stock.py

--- LeetCode/LC121_best_time_to_buy_and_sell_stock.py
+++ LeetCode/LC121_best_time_to_buy_and_sell_stock.py
@@ -1,15 +1,18 @@
-# LC121 Best Time to Buy and Sell Stock
-# Easy
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+LC121 Best Time to Buy and Sell Stock
+Easy
 
-# Say you have an array for which the ith element is the price of a given stock on day i.
+Say you have an array for which the ith element is the price of a given stock on day i.
 
-# If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
-# Note that you cannot sell a stock before you buy one.
+Note that you cannot sell a stock before you buy one.
+"""
+from typing import *
 
-
-class Solution:
-    def maxProfit(self, prices):
+class Solution_A:
+    def maxProfit(self, prices: List[int]) -> int:
         # This method exceeded max time limit :(
         if not prices:
             return 0
@@ -26,7 +29,8 @@ class Solution:
         answer = max(profits)
         return answer if answer > 0 else 0
 
-    def maxProfit(self, prices):
+class Solution_B:
+    def maxProfit(self, prices: List[int]) -> int:
         # This method still exceeded max time limit :(
         # 折断法, 以list中最小值做切割
         profits = [0]
@@ -39,7 +43,8 @@ class Solution:
 
         return max(profits)
 
-    def maxProfit_r(self, prices):
+class Solution_C:
+    def maxProfit(self, prices: List[int]) -> int:
         # 折断法, 递归来做
         if not prices:
             return 0
@@ -47,9 +52,10 @@ class Solution:
             low = min(prices)
             low_n = prices.index(low)
             profit = max(prices[low_n:]) - low
-            return max(profit, self.maxProfit_r(prices[:low_n]))
+            return max(profit, self.maxProfit(prices[:low_n]))
 
-    def maxProfit(self, prices):
+class Solution_D:
+    def maxProfit(self, prices: List[int]) -> int:
         # filter the list first, to only obtain the turn points, combine with 折断法
         # first part is O(N), Second part is also O(N), so overall O(N).
         # Accepted
@@ -77,7 +83,8 @@ class Solution:
 
         return max(profits)
 
-    def maxProfit_best(self, prices):
+class Solution_STD:
+    def maxProfit(self, prices: List[int]) -> int:
         # STD ans
         # Best method evaluate on the run
         max_profit, min_price = 0, float("inf")
@@ -87,7 +94,8 @@ class Solution:
             max_profit = max(max_profit, price - min_price)
         return max_profit
 
-    def maxProfit(self, prices):
+class Solution_E:
+    def maxProfit(self, prices: List[int]) -> int:
         # according to the best method, Denis modified for clearer logic
         min_price = float("inf")
         profit = 0
@@ -105,12 +113,14 @@ class Solution:
 
 
 if __name__ == "__main__":
-    assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 5, "Smart trader"
+    testCase = Solution_A()
+
+    assert testCase.maxProfit([7, 1, 5, 3, 6, 4]) == 5, "Smart trader"
     # Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
 
-    assert Solution().maxProfit([7, 6, 4, 3, 1]) == 0, "No Transaction"
+    assert testCase.maxProfit([7, 6, 4, 3, 1]) == 0, "No Transaction"
     # In this case, no transaction is done, i.e. max profit = 0
 
-    assert Solution().maxProfit([100, 5, 25, 1, 20]) == 20, "Tricky"
+    assert testCase.maxProfit([100, 5, 25, 1, 20]) == 20, "Tricky"
 
     print("All passed")
