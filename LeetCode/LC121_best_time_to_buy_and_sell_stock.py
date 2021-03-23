80212865232d2b0aa68cdf54ccd4e5c6267e0fb5
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-22 20:56:50 -0400

    Code Review LC121_best_time_to_buy_and_sell_stock.py

--- LeetCode/LC121_best_time_to_buy_and_sell_stock.py
+++ LeetCode/LC121_best_time_to_buy_and_sell_stock.py
@@ -11,109 +11,86 @@ Note that you cannot sell a stock before you buy one.
 """
 from typing import *
 
+
 class Solution_A:
     def maxProfit(self, prices: List[int]) -> int:
-        # This method exceeded max time limit :(
-        if not prices:
-            return 0
+        """
+        starting from each element, find the max value after the element
+        the max of each (max value - element) if >0, else return 0
+        Time O(N^2), exceeded max time limit
+        """
+        max_so_far = 0
+        for i in range(len(prices)):
+            max_so_far = max(max(prices[i:]) - prices[i], max_so_far)
+        return max_so_far
 
-        profits = []
-        i = 0
 
-        while i != len(prices):
-            current = prices[i:]
-            high = max(current)
-            profits.append(high - current[0])
-            i += 1
-
-        answer = max(profits)
-        return answer if answer > 0 else 0
-
-class Solution_B:
+class Solution_B1:
     def maxProfit(self, prices: List[int]) -> int:
-        # This method still exceeded max time limit :(
-        # 折断法, 以list中最小值做切割
+        """
+        折断法, 以list中最小值做切割
+        This will pass but very slow
+        """
         profits = [0]
         while prices:
             low = min(prices)
-            low_n = prices.index(low)
-            profit = max(prices[low_n:]) - low
+            low_idx = prices.index(low)
+            profit = max(prices[low_idx:]) - low
             profits.append(profit)
-            prices = prices[:low_n]
+            prices = prices[:low_idx]
 
         return max(profits)
 
-class Solution_C:
+class Solution_B2:
     def maxProfit(self, prices: List[int]) -> int:
-        # 折断法, 递归来做
+        """
+        折断法 with recursion
+        This will pass but very slow
+        """
         if not prices:
             return 0
         else:
             low = min(prices)
-            low_n = prices.index(low)
-            profit = max(prices[low_n:]) - low
-            return max(profit, self.maxProfit(prices[:low_n]))
-
-class Solution_D:
-    def maxProfit(self, prices: List[int]) -> int:
-        # filter the list first, to only obtain the turn points, combine with 折断法
-        # first part is O(N), Second part is also O(N), so overall O(N).
-        # Accepted
-        if len(prices) == 0 or len(prices) == 1:
-            return 0
-
-        turn_points = [prices[0]]  # keep the head
-        i = 0
-        while i != len(prices) - 2:
-            current, next, further = prices[i], prices[i + 1], prices[i + 2]
-            if next == max(current, next, further) or next == min(current, next, further):
-                turn_points.append(next)
-            i += 1
-
-        turn_points += [prices[-1]]  # keep the tail
-
-        # 结合折断法, 以list中最小值做切割
-        profits = [0]
-        while turn_points:
-            low = min(turn_points)
-            low_n = turn_points.index(low)
-            profit = max(turn_points[low_n:]) - low
-            profits.append(profit)
-            turn_points = turn_points[:low_n]
-
-        return max(profits)
+            low_idx = prices.index(low)
+            profit = max(prices[low_idx:]) - low
+            return max(profit, self.maxProfit(prices[:low_idx]))
 
 class Solution_STD:
     def maxProfit(self, prices: List[int]) -> int:
-        # STD ans
-        # Best method evaluate on the run
+        """
+        Keep tracking minimum price and max profit
+        And compare current price - minimum price with current max_profit
+        """
         max_profit, min_price = 0, float("inf")
         # 无穷大
+
         for price in prices:
             min_price = min(min_price, price)
             max_profit = max(max_profit, price - min_price)
         return max_profit
 
-class Solution_E:
+class Solution_STD2:
     def maxProfit(self, prices: List[int]) -> int:
-        # according to the best method, Denis modified for clearer logic
+        """
+        Modified from STD, with better logic expression
+        此题本质就是迭代刷新最低价,
+        然后得到一个新的最低价之后的(当前价-最低价)得到一个利润, 然后基于此最低价寻找更高的利润
+        若出现新的最低价, 那么就从新的最低价开始重新寻找新的利润并与之前的最高利润做对比
+        由于价格有变化按照时间线,得到了新的最低价,此前的出现过的高价位不可能在这个最低价之后被买入,所以不必重复计算
+        """
         min_price = float("inf")
         profit = 0
-        i = 0
-        while i != len(prices):
+        for i in range(len(prices)):
             current = prices[i]
             if current < min_price:
-                min_price = current
+                min_price = current # update minimum price
             profit = max(current - min_price, profit)
-            i += 1
         return profit
-        # 此题本质就是迭代刷新最低价,
-        # 然后得到最低价之后的(当前价-最低价)得到一个利润, 然后基于此最低价寻找更高的利润
-        # 若出现新的最低价, 那么就从新的最低价开始重新寻找新的利润并与之前的最高利润做对比
+
 
 
 if __name__ == "__main__":
-    testCase = Solution_A()
+    testCase = Solution_STD2()
 
     assert testCase.maxProfit([7, 1, 5, 3, 6, 4]) == 5, "Example 1"
     # Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
