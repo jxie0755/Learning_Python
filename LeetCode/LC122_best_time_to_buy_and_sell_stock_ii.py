5474bc7a4e38f18de22ff172212ce1c0af391fbf
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-03-23 12:35:33 -0400

    Code Review LC122_best_time_to_buy_and_sell_stock_ii.py

--- LeetCode/LC122_best_time_to_buy_and_sell_stock_ii.py
+++ LeetCode/LC122_best_time_to_buy_and_sell_stock_ii.py
@@ -1,19 +1,22 @@
-# LC122 Best time to buy and sell stock II
-# Easy
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+LC122 Best time to buy and sell stock II
+Easy
 
 
-# Say you have an array for which the ith element is the price of a given stock on day i.
+Say you have an array for which the ith element is the price of a given stock on day i.
 
-# Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
-# Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+"""
 
+from typing import *
 
-class Solution:
-    def maxProfit(self, prices):
+class Solution_A:
+    def maxProfit(self, prices: List[int]) -> int:
         """
-        :type prices: List[int]
-        :rtype: int
+
         """
         if len(prices) == 0:
             return 0
@@ -42,25 +45,27 @@ class Solution:
 
 
 if __name__ == "__main__":
-    assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7, "Case 1"
+    testCase = Solution_A()
+
+    assert testCase.maxProfit([0]) == 0, "Edge 0"
+    assert testCase.maxProfit([1]) == 0, "Edge 1"
+    assert testCase.maxProfit([1, 1, 1]) == 0, "Edge 2"
+
+    assert testCase.maxProfit([7, 1, 5, 3, 6, 4]) == 7, "Example 1"
     # Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
     # Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 
-    assert Solution().maxProfit([1, 2, 3, 4, 5]) == 4, "Case 2"
+    assert testCase.maxProfit([1, 2, 3, 4, 5]) == 4, "Example 2"
     # Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
     # Note that you cannot buy on day 1, buy on day 2 and sell them later
     # as you are engaging multiple transactions at the same time. You must sell before buying again.
 
-    assert Solution().maxProfit([7, 6, 4, 3, 1]) == 0, "Case 3"
+    assert testCase.maxProfit([7, 6, 4, 3, 1]) == 0, "Example 3"
     # In this case, no transaction is done, i.e.max profit = 0.
 
-    assert Solution().maxProfit([5, 2, 3, 2, 6, 6, 2, 9, 1, 0, 7, 4, 5, 0]) == 20, "Case 4"
+    assert testCase.maxProfit([5, 2, 3, 2, 6, 6, 2, 9, 1, 0, 7, 4, 5, 0]) == 20, "Additional 1"
     # 平峰需要注意
 
-    assert Solution().maxProfit([2, 1, 4]) == 3, "Case 5"
-
-    assert Solution().maxProfit([0]) == 0
-    assert Solution().maxProfit([1]) == 0
-    assert Solution().maxProfit([1, 1, 1]) == 0
+    assert testCase.maxProfit([2, 1, 4]) == 3, "Additional 2"
 
     print("All passed")
---
+++ whiteboard.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+LC122 Best time to buy and sell stock II
+Easy
+
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+"""
+
+from typing import *
+
+
+class Solution_A:
+    def maxProfit(self, prices: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    testCase = Solution_A()
+
+    assert testCase.maxProfit([0]) == 0, "Edge 0"
+    assert testCase.maxProfit([1]) == 0, "Edge 1"
+    assert testCase.maxProfit([1, 1, 1]) == 0, "Edge 2"
+
+    assert testCase.maxProfit([7, 1, 5, 3, 6, 4]) == 7, "Example 1"
+    # Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+    # Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+    assert testCase.maxProfit([1, 2, 3, 4, 5]) == 4, "Example 2"
+    # Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+    # Note that you cannot buy on day 1, buy on day 2 and sell them later
+    # as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+    assert testCase.maxProfit([7, 6, 4, 3, 1]) == 0, "Example 3"
+    # In this case, no transaction is done, i.e.max profit = 0.
+
+    assert testCase.maxProfit([5, 2, 3, 2, 6, 6, 2, 9, 1, 0, 7, 4, 5, 0]) == 20, "Additional 1"
+    # 平峰需要注意
+
+    assert testCase.maxProfit([2, 1, 4]) == 3, "Additional 2"
+
+    print("All passed")
