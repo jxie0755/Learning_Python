63fe06691c9c396ea64ab8f72431bcd036fccb92
jxie0755 <30805062+jxie0755@users.noreply.github.com>
2021-04-11 14:22:48 -0400

    Code Review LC125_valid_palindrome.py

--- LeetCode/LC125_valid_palindrome.py
+++ LeetCode/LC125_valid_palindrome.py
@@ -1,19 +1,20 @@
-# LC125 Valid Palindrome
-# Easy
+"""
+https: // leetcode.com / problems / valid - palindrome /
+LC125 Valid Palindrome
+Easy
 
-# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-# Note:
-# Have you consider that the string might be empty? This is a good question to ask during an interview.
-# For the purpose of this problem, we define empty string as valid palindrome.
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
-# """
-# :type s: str
-# :rtype: bool
-# """
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+For the purpose of this problem, we define empty string as valid palindrome.
+"""
 
-class Solution:
-    def isPalindrome(self, s):
-        # take the space to process the string
+class Solution_A:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        take extra space to process the string
+        """
         if not s:
             return True
         # process the string
@@ -21,12 +22,14 @@ class Solution:
         for i in s:
             if i.isalnum():
                 s_processed += i.lower()
-        print(s_processed)
         # Check if palindrome
         return s_processed == s_processed[::-1]  # String[::-1], O(n)
 
-    def isPalindrome(self, s):
-        # Two pointer method (no extra space needed)
+class Solution_B:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        Two pointer method (no extra space needed)
+        """
         left, right = 0, len(s) - 1
         while left < right:
             while left < right and not s[left].isalnum():  # 注意while loop的嵌套
@@ -41,6 +44,7 @@ class Solution:
 
 
 if __name__ == "__main__":
-    assert Solution().isPalindrome("A man, a plan, a canal: Panama") == True
-    assert Solution().isPalindrome("race a car") == False
+    testCase = Solution_B()
+    assert testCase.isPalindrome("A man, a plan, a canal: Panama") == True
+    assert testCase.isPalindrome("race a car") == False
     print("All passed")
