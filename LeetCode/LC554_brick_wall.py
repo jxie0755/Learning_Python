ab9ebc072ae9cf502d1a1ae62956f5bedf0875fc
jxie0755 <jxie0755@users.noreply.github.com>
2021-07-01 21:08:15 -0700

    LC553 finished

--- LeetCode/LC554_brick_wall.py
+++ LeetCode/LC554_brick_wall.py
@@ -14,7 +14,24 @@ from typing import *
 
 class Solution_A:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        pass
+        total_length = sum(wall[0])
+
+        hmp = {}
+        for layer in wall:
+            NL = [0]
+            for i in layer:
+                NL.append(NL[-1]+i) # this will avoid key value 0
+                if NL[-1] not in hmp:
+                    # avoid create hmp with every value, just add on the run when new value shows up
+                    # this saves time when one brick is very long
+                    hmp[NL[-1]] = 1
+                else:
+                    hmp[NL[-1]] += 1
+
+        hmp[total_length] = 0 # adjust back the max_length value to be 0
+        max_L = max(hmp, key=lambda i: hmp[i])
+        return len(wall) - hmp[max_L]
+
 
 
 if __name__ == '__main__':
@@ -34,6 +51,19 @@ if __name__ == '__main__':
         [1],
         [1]
     ]) == 3, "Example 2"
+
+    assert testCase.leastBricks([
+        [2],
+        [2],
+        [2]
+    ]) == 3, "Additional edge 1"
+
+    assert testCase.leastBricks([
+        [100000000],
+        [100000000],
+        [100000000]
+    ]) == 3, "Additional edge 1"
+
 
     print("All passed")
 
