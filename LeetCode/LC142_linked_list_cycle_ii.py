f2908f72fd7e19dee7dc71a717c5e81a77ccf98f
jxie0755 <jxie0755@users.noreply.github.com>
2021-07-05 10:30:08 -0700

    LC142 code review

--- LeetCode/LC142_linked_list_cycle_ii.py
+++ LeetCode/LC142_linked_list_cycle_ii.py
@@ -1,22 +1,26 @@
-# LC142 Linked List Cycle II
-# Medium
+"""
+https://leetcode.com/problems/linked-list-cycle-ii/
+LC142 Linked List Cycle II
+Medium
 
-# Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
-# To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
 
-# Note: Do not modify the linked list.
+Note: Do not modify the linked list.
+"""
 
 from typing import *
 from A01_ListNode import *
 
 
-class Solution(object):
+class Solution_A:
 
-    # Time O(N), Space O(N), use hashtable to search faster
-    # ListNode instance is hashable, this method search at O(1), and will not break down the original linked list
-    def detectCycle(self, head):
-
+    def detectCycle(self, head: ListNode) -> ListNode:
+        """
+        Time O(N), Space O(N), use hashtable to search faster
+        ListNode instance is hashable, this method search at O(1), and will not break down the original linked list
+        """
         if not head:
             return None
 
@@ -36,15 +40,23 @@ class Solution(object):
 
 if __name__ == "__main__":
     # Use is instead of == to avoid max recursion when comparing cycling linked list
+    testCase = Solution_A()
 
     A = genNode([3, 2, 0, 4])
     A.next.next.next.next = A.next
-    assert Solution().detectCycle(A) is A.next, "Example 1"
+    # 3 2 0 4
+    # a b c d
+    # d->b
+    assert testCase.detectCycle(A) is A.next, "Example 1"
 
     A = genNode([1, 2])
     A.next.next = A
-    assert Solution().detectCycle(A) is A, "Example 2"
+    # 1 2
+    # a b
+    # b->a
+    assert testCase.detectCycle(A) is A, "Example 2"
 
     A = genNode([1])
-    assert not Solution().detectCycle(A), "Edge 1, no cycle"
+    assert not testCase.detectCycle(A), "Edge 1, no cycle"
+
     print("All passed")
