5dc170a59810f5e9336f7c75aa1b3b930e5d58d3
jxie0755 <jxie0755@users.noreply.github.com>
2021-07-05 21:04:41 -0700

    LC406 initiation

--- LeetCode/LC406_queue_reconstruction_by_height.py
+++ LeetCode/LC406_queue_reconstruction_by_height.py
@@ -14,28 +14,37 @@ from typing import *
 
 class Solution_A:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        pass
+        translate = [[] for _ in range(len(people))]
+        for p in people:
+            translate[p[1]].append(p)
+        for x in translate:
+            x.sort()
+        for x in translate:
+            print(x)
 
 
-if __name__ == '__main__':
-    testCase = Solution_A()
+testCase = Solution_A()
+testCase.reconstructQueue([[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]])
 
-    assert testCase.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]) == [
-        [5, 0], [7, 0],
-        [5, 2],
-        [6, 1],
-        [4, 4],
-        [7, 1]
-    ], "Example 1"
-
-    assert testCase.reconstructQueue([[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]]) == [
-        [4, 0], [5, 0],
-        [2, 2], [3, 2],
-        [1, 4],
-        [6, 0]
-    ], "Example 2"
-
-    print("All passed")
+# if __name__ == '__main__':
+#     testCase = Solution_A()
+#
+#     assert testCase.reconstructQueue([[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]) == [
+#         [5, 0], [7, 0],
+#         [5, 2],
+#         [6, 1],
+#         [4, 4],
+#         [7, 1]
+#     ], "Example 1"
+#
+#     assert testCase.reconstructQueue([[6, 0], [5, 0], [4, 0], [3, 2], [2, 2], [1, 4]]) == [
+#         [4, 0], [5, 0],
+#         [2, 2], [3, 2],
+#         [1, 4],
+#         [6, 0]
+#     ], "Example 2"
+#
+#     print("All passed")
 
 
 
