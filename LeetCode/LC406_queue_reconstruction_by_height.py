288925caac9fe08634c9e988c94a3779fdd3e286
jxie0755 <jxie0755@users.noreply.github.com>
2021-07-05 17:09:36 -0700

    LC406 initiation

---
+++ LeetCode/LC406_queue_reconstruction_by_height.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/queue-reconstruction-by-height/
+LC406 Queue Reconstruction by Height
+Medium
+
+You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+
+Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+"""
+
+from typing import *
+
+class Solution_A:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
